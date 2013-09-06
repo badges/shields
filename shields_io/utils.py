fe0a78102234b2fe446cd98b8b2846250306a288
Chad Whitacre <chad@zetaweb.com>
2013-09-06 15:39:22 -0400

    Here's an implementation of a path collapsing util
    This includes more tests. Next step is to wire this up in an Aspen hook
    to run at request time.

--- shields_io/utils.py
+++ shields_io/utils.py
@@ -1,2 +1,10 @@
+from __future__ import unicode_literals
+
+
 def collapse_path_parts(path):
+    parts = path.split(b'/', 2)
+    if len(parts) == 3:
+        left = b'/'.join(parts[:2] + [b''])
+        right = parts[2].replace(b'/', b'%2F')
+        path = left + right
     return path
