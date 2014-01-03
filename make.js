cb3da5cb17db07e85425ab545cc21316bfb7e7df
Thaddee Tyl <thaddee.tyl@gmail.com>
2014-01-03 12:29:54 +0100

    Automatic measurement of text.

--- make.js
+++ make.js
@@ -1,8 +1,16 @@
 var fs = require('fs');
 var path = require('path');
 var Promise = require('es6-promise').Promise;
-var dot = require('dot');
 var SVGO = require('svgo');
+
+// Initialize what will be used for automatic text measurement.
+var Canvas = require('canvas');
+var canvasElement = new Canvas(0, 0);   // Width and height are irrelevant.
+var canvasContext = canvasElement.getContext('2d');
+canvasContext.font = '10px Verdana';
+
+// Template crafting action below.
+var dot = require('dot');
 var badgeData = require('./badges.json');
 var badges = badgeData.badges;
 var colorscheme = badgeData.colorschemes;
@@ -26,6 +34,10 @@ function makeImage(name, data, cb) {
     data.colorA = colorscheme[data.colorscheme].colorA;
     data.colorB = colorscheme[data.colorscheme].colorB;
   }
+  data.widths = [
+    (canvasContext.measureText(data.text[0]).width|0) + 10,
+    (canvasContext.measureText(data.text[1]).width|0) + 10,
+  ];
   var result = imageTemplate(data);
   // Run the SVG through SVGO.
   optimize(result, function(object) {
