c04d52da734deb6015aae2604408f14eaf327d68
Thaddee Tyl <thaddee.tyl@gmail.com>
2014-01-03 23:49:05 +0100

    Proper escaping for the web service.

--- server.js
+++ server.js
@@ -6,7 +6,13 @@ var badge = require('./badge.js');
 // Escapes `t` using the format specified in
 // <https://github.com/espadrine/gh-badges/issues/12#issuecomment-31518129>
 function escapeFormat(t) {
-  return t.replace('_', ' ').replace('__', '_').replace('--', '-');
+  return t
+    // Inline single underscore.
+    .replace(/([^_])_([^_])/g, '$1 $2')
+    // Leading or trailing underscore.
+    .replace(/([^_])_$/, '$1 ').replace(/^_([^_])/, ' $1')
+    // Double underscore and double dash.
+    .replace(/__/g, '_').replace(/--/g, '-');
 }
 
 camp.route(/^\/(([^-]|--)+)-(([^-]|--)+)-(([^-]|--)+).svg$/,
