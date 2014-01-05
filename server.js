5aab5a870576de953a93a762bef2d369d5f315b1
Thaddee Tyl <thaddee.tyl@gmail.com>
2014-01-05 14:42:54 +0100

    Travis-CI integration.
    Part of issue #16.

--- server.js
+++ server.js
@@ -1,9 +1,83 @@
 var camp = require('camp').start({
   port: process.env.PORT||+process.argv[2]||80
 });
+var https = require('https');
 var badge = require('./badge.js');
 var svg2img = require('./svg-to-img.js');
 var serverStartTime = new Date((new Date()).toGMTString());
+
+// Travis integration.
+camp.route(/^\/travis\/(.*)\.(svg|png|gif|jpg)$/,
+function(data, match, end, ask) {
+  var userRepo = match[1];  // eg, `joyent/node`.
+  var format = match[2];
+  var apiUrl = 'https://api.travis-ci.org/repositories/' + userRepo + '.json';
+  var badgeData = {text:['build', 'n/a'], colorscheme:'lightgrey'};
+  https.get(apiUrl, function(res) {
+    var buffer = '';
+    res.on('data', function(chunk) { buffer += ''+chunk; });
+    res.on('end', function(chunk) {
+      if (chunk) { buffer += ''+chunk; }
+      try {
+        var data = JSON.parse(buffer);
+      } catch(e) {
+        badgeData.text[1] = 'invalid';
+        badge(badgeData, makeSend(format, ask.res, end));
+        return;
+      }
+      if (data.last_build_status === 0) {
+        badgeData.text[1] = 'passing';
+        badgeData.colorscheme = 'green';
+      } else if (data.last_build_status === 1) {
+        badgeData.text[1] = 'failing';
+        badgeData.colorscheme = 'red';
+      } else if (data.last_build_status === null) {
+        badgeData.text[1] = 'pending';
+        badgeData.colorscheme = 'yellow';
+      } else {
+        badgeData.text[1] = 'n/a';
+        badgeData.colorscheme = 'lightgrey';
+      }
+      badge(badgeData, makeSend(format, ask.res, end));
+    });
+  }).on('error', function(e) {
+    badgeData.text[1] = 'inaccessible';
+    badge(badgeData, makeSend(format, ask.res, end));
+  });
+});
+
+// Any badge.
+camp.route(/^\/:(([^-]|--)+)-(([^-]|--)+)-(([^-]|--)+)\.(svg|png|gif|jpg)$/,
+function(data, match, end, ask) {
+  var subject = escapeFormat(match[1]);
+  var status = escapeFormat(match[3]);
+  var color = escapeFormat(match[5]);
+  var format = match[7];
+
+  // Cache management.
+  var cacheDuration = (3600*24*1)|0;  // 1 day.
+  ask.res.setHeader('Cache-Control', 'public, max-age=' + cacheDuration);
+  if (+(new Date(ask.req.headers['if-modified-since'])) >= +serverStartTime) {
+    ask.res.statusCode = 304;
+    ask.res.end();  // not modified.
+    return;
+  }
+  ask.res.setHeader('Last-Modified', serverStartTime.toGMTString());
+
+  // Badge creation.
+  try {
+    var badgeData = {text: [subject, status]};
+    if (sixHex(color)) {
+      badgeData.colorB = '#' + color;
+    } else {
+      badgeData.colorscheme = color;
+    }
+    badge(badgeData, makeSend(format, ask.res, end));
+  } catch(e) {
+    badge({text: ['error', 'bad badge'], colorscheme: 'red'},
+      makeSend(format, ask.res, end));
+  }
+});
 
 // Escapes `t` using the format specified in
 // <https://github.com/espadrine/gh-badges/issues/12#issuecomment-31518129>
@@ -18,38 +92,6 @@ function escapeFormat(t) {
 }
 
 function sixHex(s) { return /^[0-9a-fA-F]{6}$/.test(s); }
-
-camp.route(/^\/:(([^-]|--)+)-(([^-]|--)+)-(([^-]|--)+).(svg|png|gif|jpg|pdf)$/,
-  function(data, match, end, ask) {
-    var subject = escapeFormat(match[1]);
-    var status = escapeFormat(match[3]);
-    var color = escapeFormat(match[5]);
-    var format = match[7];
-
-    // Cache management.
-    var cacheDuration = (3600*24*1)|0;  // 1 day.
-    ask.res.setHeader('Cache-Control', 'public, max-age=' + cacheDuration);
-    if (+(new Date(ask.req.headers['if-modified-since'])) >= +serverStartTime) {
-      ask.res.statusCode = 304;
-      ask.res.end();  // not modified.
-      return;
-    }
-    ask.res.setHeader('Last-Modified', serverStartTime.toGMTString());
-
-    // Badge creation.
-    try {
-      var badgeData = {text: [subject, status]};
-      if (sixHex(color)) {
-        badgeData.colorB = '#' + color;
-      } else {
-        badgeData.colorscheme = color;
-      }
-      badge(badgeData, makeSend(format, ask.res, end));
-    } catch(e) {
-      badge({text: ["error", "bad badge"], colorscheme: "red"},
-        makeSend(format, ask.res, end));
-    }
-});
 
 function makeSend(format, askres, end) {
   if (format === 'svg') {
