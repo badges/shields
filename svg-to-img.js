2877a788d77eb25e9199b521a0af70fa558d9e3a
Thaddee Tyl <thaddee.tyl@gmail.com>
2014-01-10 16:09:37 +0100

    Bitmap cache.
    Bitmap images (PNG & friends) are cached in memory.
    
    Note: the cache is not discriminatory and grows constantly.
    
    Part of issue #23.

--- svg-to-img.js
+++ svg-to-img.js
@@ -5,6 +5,8 @@ var phantom = require('phantomjs');
 var childProcess = require('child_process');
 var phantomScript = path.join(__dirname, 'phantomjs-svg2png.js');
 
+var imgCache = Object.create(null);
+
 // If available, use the font here.
 var fontPath = './Verbana.ttf';
 try {
@@ -13,17 +15,43 @@ try {
 } catch(e) {}
 
 module.exports = function (svg, format, out, cb) {
+  var cacheIndex = format + svg;
+  if (imgCache[cacheIndex] !== undefined) {
+    // We own a cache for this svg conversion.
+    imgCache[cacheIndex].pipe(out);
+  }
   var tmpFile = path.join(os.tmpdir(),
       "svg2img-" + (Math.random()*2147483648|0) + "." + format);
   // Conversion to PNG happens in the phantom script.
   childProcess.execFile(phantom.path, [phantomScript, svg, tmpFile],
   function(err, stdout, stderr) {
     if (stdout) { console.log(stdout); }
-    if (stderr) { console.log(stderr); }
+    if (stderr) { console.error(stderr); }
     if (err != null) { console.error(err.stack); if (cb) { cb(err); } return; }
     var inStream = fs.createReadStream(tmpFile);
-    inStream.pipe(out);
+    var cached = [];
+    inStream.on('data', function(chunk) {
+      cached.push(chunk);
+      out.write(chunk);
+    });
     // Remove the temporary file after use.
-    inStream.on('end', function() { fs.unlink(tmpFile, cb); });
+    inStream.on('end', function() {
+      out.end();
+      imgCache[cacheIndex] = streamFromData(cached);
+      fs.unlink(tmpFile, cb);
+    });
   });
 };
+
+// Fake stream from the cache.
+var stream = require('stream');
+function streamFromData(data) {
+  var newStream = new stream.Readable();
+  newStream._read = function() {
+    for (var i = 0; i < data.length; i++) {
+      newStream.push(data[i]);
+    }
+    newStream.push(null)
+  };
+  return newStream;
+}
