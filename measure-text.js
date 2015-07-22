36b664fc45ae2bdffaa6d1becc6d97d078e0b4b8
ForbesLindesay <forbes@lindesay.co.uk>
2015-07-22 17:31:24 +0100

    Support fallback font when Verdana isn't available

--- measure-text.js
+++ measure-text.js
@@ -1,11 +1,17 @@
 'use strict';
 
 var path = require('path');
+var fs = require('fs');
 var PDFDocument = require('pdfkit');
 
-var doc = (new PDFDocument({size:'A4', layout:'landscape'}))
-  .font(path.join(__dirname, 'Verdana.ttf'))
-  .fontSize(11);
+var doc = (new PDFDocument({size:'A4', layout:'landscape'}));
+try {
+  doc = doc.font(path.join(__dirname, 'Verdana.ttf'));
+} catch (ex) {
+  doc = doc.font('Helvetica-Bold')
+  console.warn('Could not load font file "Verdana.ttf", text widths will therefore be approximate', ex);
+}
+doc = doc.fontSize(11);
 
 module.exports = measure;
 function measure(str) {
