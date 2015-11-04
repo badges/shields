3f7414f8ae1d8a4e7c6de55d5750edd9cd62332d
Thaddee Tyl <thaddee.tyl@gmail.com>
2015-11-05 00:01:56 +0100

    Omit needless removal of multiple slashes
    Related to #535

--- server.js
+++ server.js
@@ -2642,9 +2642,7 @@ cache(function(data, match, sendBadge, request) {
   var asset_name = match[4].toLowerCase(); // eg. total, atom-amd64.deb, atom.x86_64.rpm
   var format = match[5];
 
-  if (tag && (tag.indexOf('/') !== -1)) {
-    tag = tag.split('/').join('');
-  }
+  if (tag) { tag = tag.slice(1); }
 
   var total = true;
   if (tag) {
