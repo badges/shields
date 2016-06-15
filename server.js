0b3afa4da148a387d1ffc2a8967b2d3a6ad8a35b
Nick Timkovich <prometheus235@gmail.com>
2016-06-14 23:40:54 -0500

    Add PyPI prerelease filtering

--- server.js
+++ server.js
@@ -1800,6 +1800,12 @@ cache(function(data, match, sendBadge, request) {
         badgeData.text[1] = vdata.version;
         badgeData.colorscheme = vdata.color;
         sendBadge(format, badgeData);
+    } else if (info === 'vnopre') {
+        var version = pypiLatestStableVersion(Object.keys(data.releases));
+        var vdata = versionColor(version);
+        badgeData.text[1] = vdata.version;
+        badgeData.colorscheme = vdata.color;
+        sendBadge(format, badgeData);
       } else if (info === 'l') {
         var license = data.info.license;
         badgeData.text[0] = 'license';
@@ -5873,4 +5879,47 @@ function fetchTwitterToken() {
       }
     });
   }
+}
+
+// from pip: https://github.com/pypa/pip/blob/231178f/pip/_vendor/packaging/version.py#L159
+var pypiVersionRegEx = /v?(?:(?:([0-9]+)!)?([0-9]+(?:\.[0-9]+)*)([-_\.]?(?:a|b|c|rc|alpha|beta|pre|preview)[-_\.]?(?:[0-9]+)?)?((?:-(?:[0-9]+))|(?:[-_\.]?(?:post|rev|r)[-_\.]?(?:[0-9]+)?))?([-_\.]?(?:dev)[-_\.]?(?:[0-9]+)?)?)(?:\+([a-z0-9]+(?:[-_\.][a-z0-9]+)*))?/;
+
+function pypiVersionParts(version) {
+    var parts = version.match(pypiVersionRegEx);
+    // epoch, release, pre, post, dev, local
+    return {'epoch': parts[1], 'release': parts[2], 'pre': parts[3],
+            'post': parts[4], 'dev': parts[5], 'local': parts[6]};
+}
+
+function pypiVersionCmp(v1, v2) {
+    if (v1.epoch > v2.epoch) {return 1;}
+    else if (v1.epoch < v2.epoch) {return -1;}
+
+    r1 = v1.release.split('.').map(Number);
+    r2 = v2.release.split('.').map(Number);
+    for (i = 0; i < Math.max(r1.length, r2.length); i++) {
+        if ((r1[i] || 0) > (r2[i] || 0)) {return 1;}
+        else if ((r1[i] || 0) < (r2[i] || 0)) {return -1;}
+    }
+
+    if (v1.post > v2.post) {return 1;}
+    else if (v1.post < v2.post) {return -1;}
+
+    return 0;
+}
+
+function pypiLatestStableVersion(versions) {
+    versions = (versions
+        .map(pypiVersionParts)
+        .filter(function(v) {return !(v.pre || v.dev)})
+        .sort(pypiVersionCmp));
+
+    var latest = versions[versions.length - 1];
+    var version = '';
+
+    if (latest.epoch) {version += latest.epoch + '!';}
+    if (latest.release) {version += latest.release;}
+    if (latest.post) {version += latest.post;}
+
+    return version;
 }
