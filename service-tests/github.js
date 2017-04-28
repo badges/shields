f8432b375fc1b8b18524e53dc06d3392c5c627c2
NHellFire <nathan.renniewaldock@gmail.com>
2017-04-28 22:55:13 +0100

    [GitHub] Add service test for download counts

---
+++ service-tests/github.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const Joi = require('joi');
+const ServiceTester = require('./runner/service-tester');
+
+const t = new ServiceTester({ id: 'github', title: 'GitHub' });
+module.exports = t;
+
+t.create('downloads for release without slash')
+  .get('/downloads/atom/atom/v0.190.0/total.json')
+  .expectJSONTypes(Joi.object().keys({
+    name: Joi.equal('downloads'),
+    value: Joi.not('none')
+  }));
+
+t.create('downloads for specific asset without slash')
+  .get('/downloads/atom/atom/v0.190.0/atom-amd64.deb.json')
+  .expectJSONTypes(Joi.object().keys({
+    name: Joi.equal('downloads'),
+    value: Joi.not('none')
+  }));
+
+t.create('downloads for release with slash')
+  .get('/downloads/NHellFire/dban/stable/v2.2.8/total.json')
+  .expectJSONTypes(Joi.object().keys({
+    name: Joi.equal('downloads'),
+    value: Joi.not('none')
+  }));
+
+t.create('downloads for specific asset with slash')
+  .get('/downloads/NHellFire/dban/stable/v2.2.8/dban-2.2.8_i586.iso.json')
+  .expectJSONTypes(Joi.object().keys({
+    name: Joi.equal('downloads'),
+    value: Joi.not('none')
+  }));
+
+t.create('downloads for unknown release')
+  .get('/downloads/atom/atom/does-not-exist/total.json')
+  .expectJSON({ name: 'downloads', value: 'none' });
