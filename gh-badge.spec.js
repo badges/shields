742e5768cb28ef9740f53b13ea1fec480ad30492
Paul Melnikow <email@paulmelnikow.com>
2017-04-28 01:23:44 -0400

    Tests: Convert to ES6, use node-fetch, and clean up style

--- gh-badge.spec.js
+++ gh-badge.spec.js
@@ -1,61 +1,53 @@
-var assert = require('assert');
-var cproc = require('child_process');
-var isPng = require('is-png');
-var isSvg = require('is-svg');
+const assert = require('assert');
+const isPng = require('is-png');
+const isSvg = require('is-svg');
+const {spawn} = require('child_process');
 
 describe('The CLI', function () {
-
-  it('should provide a help message', function(done) {
-    var child = cproc.spawn('node', ['gh-badge.js']);
-    var buffer = '';
-    child.stdout.on('data', function(chunk) {
-      buffer += ''+chunk;
-    });
-    child.stdout.on('end', function() {
+  it('should provide a help message', function (done) {
+    const child = spawn('node', ['gh-badge.js']);
+    let buffer = '';
+    child.stdout.on('data', chunk => { buffer += ''+chunk; });
+    child.stdout.on('end', () => {
       assert(buffer.startsWith('Usage'));
       done();
     });
   });
 
-  it('should produce default badges', function(done) {
-    var child = cproc.spawn('node',
-      ['gh-badge.js', 'cactus', 'grown']);
-    child.stdout.once('data', function(chunk) {
-      var buffer = ''+chunk;
+  it('should produce default badges', function (done) {
+    const child = spawn('node', ['gh-badge.js', 'cactus', 'grown']);
+    child.stdout.once('data', chunk => {
+      const buffer = ''+chunk;
       assert.ok(isSvg(buffer));
-      assert(buffer.includes('cactus'), 'cactus');
-      assert(buffer.includes('grown'), 'grown');
+      assert.ok(buffer.includes('cactus'), 'cactus');
+      assert.ok(buffer.includes('grown'), 'grown');
       done();
     });
   });
 
-  it('should produce colorschemed badges', function(done) {
-    var child = cproc.spawn('node',
-      ['gh-badge.js', 'cactus', 'grown', ':green']);
-    child.stdout.once('data', function(chunk) {
-      var buffer = ''+chunk;
+  it('should produce colorschemed badges', function (done) {
+    const child = spawn('node', ['gh-badge.js', 'cactus', 'grown', ':green']);
+    child.stdout.once('data', chunk => {
+      const buffer = ''+chunk;
       assert.ok(isSvg(buffer));
       done();
     });
   });
 
-  it('should produce right-color badges', function(done) {
-    var child = cproc.spawn('node',
-      ['gh-badge.js', 'cactus', 'grown', '#abcdef']);
-    child.stdout.once('data', function(chunk) {
-      var buffer = ''+chunk;
-      assert(buffer.includes('#abcdef'), '#abcdef');
+  it('should produce right-color badges', function (done) {
+    const child = spawn('node', ['gh-badge.js', 'cactus', 'grown', '#abcdef']);
+    child.stdout.once('data', chunk => {
+      const buffer = ''+chunk;
+      assert.ok(buffer.includes('#abcdef'), '#abcdef');
       done();
     });
   });
 
   it('should produce PNG badges', function(done) {
-    var child = cproc.spawn('node',
-      ['gh-badge.js', 'cactus', 'grown', '.png']);
-    child.stdout.once('data', function(chunk) {
+    const child = spawn('node', ['gh-badge.js', 'cactus', 'grown', '.png']);
+    child.stdout.once('data', chunk => {
       assert.ok(isPng(chunk));
       done();
     });
   });
-
 });
--- server.spec.js
+++ server.spec.js
@@ -1,18 +1,17 @@
-var assert = require('assert');
-var sinon = require('sinon');
-var http = require('http');
-var fs = require('fs');
-var path = require('path');
-var isPng = require('is-png');
-var isSvg = require('is-svg');
+const assert = require('assert');
+const config = require('./lib/test-config');
 const fetch = require('node-fetch');
-var svg2img = require('./lib/svg-to-img');
+const fs = require('fs');
+const isPng = require('is-png');
+const isSvg = require('is-svg');
+const path = require('path');
 const serverHelpers = require('./lib/in-process-server-test-helpers');
-
-var port = '1111';
-var url = 'http://127.0.0.1:' + port + '/';
+const sinon = require('sinon');
+const svg2img = require('./lib/svg-to-img');
 
 describe('The server', function () {
+  const baseUri = `http://127.0.0.1:${config.port}`;
+
   let server;
   before('Start running the server', function () {
     this.timeout(5000);
@@ -20,61 +19,55 @@ describe('The server', function () {
   });
   after('Shut down the server', function () { serverHelpers.stop(server); });
 
-  it('should produce colorscheme badges', function(done) {
-    http.get(url + ':fruit-apple-green.svg',
-      function(res) {
-        var buffer = '';
-        res.on('data', function(chunk) { buffer += ''+chunk; });
-        res.on('end', function() {
-          assert.ok(isSvg(buffer));
-          assert(buffer.includes('fruit'), 'fruit');
-          assert(buffer.includes('apple'), 'apple');
-          done();
-        });
-    });
+  it('should produce colorscheme badges', function () {
+    return fetch(`${baseUri}/:fruit-apple-green.svg`)
+      .then(res => {
+        assert.ok(res.ok);
+        return res.text();
+      }).then(text => {
+        assert.ok(isSvg(text));
+        assert(text.includes('fruit'), 'fruit');
+        assert(text.includes('apple'), 'apple');
+      });
   });
 
-  it('should produce colorscheme PNG badges', function(done) {
-    http.get(url + ':fruit-apple-green.png',
-      function(res) {
-        res.once('data', function(chunk) {
-          assert.ok(isPng(chunk));
-          done();
-        });
-    });
+  it('should produce colorscheme PNG badges', function () {
+    return fetch(`${baseUri}/:fruit-apple-green.png`)
+      .then(res => {
+        assert.ok(res.ok);
+        return res.buffer();
+      }).then(data => {
+        assert.ok(isPng(data));
+      });
   });
 
   context('with svg2img error', function () {
-    var expectedError = fs.readFileSync(path.resolve(__dirname, 'public', '500.html'));
+    const expectedError = fs.readFileSync(path.resolve(__dirname, 'public', '500.html'));
 
-    var toBufferStub;
+    let toBufferStub;
     beforeEach(function () {
       toBufferStub = sinon.stub(svg2img._imageMagick.prototype, 'toBuffer')
         .callsArgWith(1, Error('whoops'));
     });
     afterEach(function () { toBufferStub.restore(); });
 
-    it('should emit the 500 message', function (done) {
-      http.get(url + ':some_new-badge-green.png',
-        function(res) {
+    it('should emit the 500 message', function () {
+      return fetch(`${baseUri}/:some_new-badge-green.png`)
+        .then(res => {
           // This emits status code 200, though 500 would be preferable.
-          assert.equal(res.statusCode, 200);
-
-          var buffer = '';
-          res.on('data', function(chunk) { buffer += ''+chunk; });
-          res.on('end', function() {
-            assert.equal(buffer, expectedError);
-            done();
-          });
-      });
+          assert.equal(res.status, 200);
+          return res.text();
+        }).then(text => {
+          assert.equal(text, expectedError);
+        });
     });
   });
 
   describe('analytics endpoint', function () {
     it('should return analytics in the expected format', function () {
-      return fetch(`${url}$analytics/v1`)
+      return fetch(`${baseUri}/$analytics/v1`)
         .then(res => {
-          assert(res.ok);
+          assert.ok(res.ok);
           return res.json();
         }).then(json => {
           const keys = Object.keys(json);
