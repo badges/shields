acedfef40c45572fc89a8fa7e5b163ead08cbe1f
Dag Wieers <dag@wieers.com>
2017-06-28 23:27:50 +0200

    Also add -raw option to remove 'release' from badge
    So it is more concise.

--- server.js
+++ server.js
@@ -3241,15 +3241,16 @@ cache(function(data, match, sendBadge, request) {
 }));
 
 // GitHub release integration
-camp.route(/^\/github\/release(-name)?\/([^\/]+)\/([^\/]+)(?:\/(all))?\.(svg|png|gif|jpg|json)$/,
+camp.route(/^\/github\/release(-name)?(-raw)?\/([^\/]+)\/([^\/]+)(?:\/(all))?\.(svg|png|gif|jpg|json)$/,
 cache(function(data, match, sendBadge, request) {
   var showName = !!match[1];
-  var user = match[2];  // eg, qubyte
-  var repo = match[3];  // eg, rubidium
-  var allReleases = match[4];
-  var format = match[5];
+  var isRaw = !!match[2];
+  var user = match[3];  // eg, qubyte
+  var repo = match[4];  // eg, rubidium
+  var allReleases = match[5];
+  var format = match[6];
   var apiUrl = githubApiUrl + '/repos/' + user + '/' + repo + '/releases';
-  var labelText = showName? repo + ' release': 'release'
+  var labelText = showName? repo + (isRaw? '': ' release'): 'release'
   var badgeData = getBadgeData(labelText, data);
   if (allReleases === undefined) {
     apiUrl = apiUrl + '/latest';
