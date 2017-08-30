46072e641d197a99d4717232964e13af3489ba7f
Andre Caetano <webandrecaetano@gmail.com>
2017-08-30 18:08:15 -0300

    file-size path fix

--- service-tests/github.js
+++ service-tests/github.js
@@ -7,7 +7,7 @@ const t = new ServiceTester({ id: 'github', title: 'Github' });
 module.exports = t;
 
 t.create('File size')
-  .get('/size/webcaetano/craft/build/craft.min.js.json')
+  .get('/size/webcaetano/craft/build/phaser-craft.min.js.json')
   .expectJSONTypes(Joi.object().keys({
     name: Joi.equal('size'),
     value: Joi.string().regex(/^[0-9]*[.]?[0-9]+\s(B|kB|MB|GB|TB|PB|EB|ZB|YB)$/),
