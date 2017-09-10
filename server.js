ccd66807c014e55a31c35a936448f8068d0a0caf
Ritwick Dey <ritwickdey@outlook.com>
2017-09-10 23:48:59 +0530

    Vscode download badge added

--- server.js
+++ server.js
@@ -4865,6 +4865,56 @@ cache(function(data, match, sendBadge, request) {
   });
 }));
 
+//vscode downloads
+camp.route(/^\/vscode\/downloads\/(.*)\.(svg|png|gif|jpg|json)$/,
+  cache(function (data, match, sendBadge, request) {
+    var repo = match[1];  // eg, `ritwickdey.LiveServer`.
+    var format = match[2];
+    console.log(repo);
+    var apiUrl = 'https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery/';
+    var badgeData = getBadgeData('downloads', data);
+
+    var options = {
+      method: 'POST',
+      url: 'https://marketplace.visualstudio.com/_apis/public/gallery/extensionquery/',
+      headers:
+      {
+        accept: 'application/json;api-version=3.0-preview.1',
+        'content-type': 'application/json'
+      },
+      body:
+      {
+        filters: [{
+          criteria:[
+            { filterType: 7, value: repo },
+            { filterType: 12, value: '4096' }]
+        }],
+        flags: 914
+      },
+      json: true
+    };
+
+    request(options, function (err, res, buffer) {
+      if (err != null) {
+        badgeData.text[1] = 'inaccessible';
+        sendBadge(format, badgeData);
+        return;
+      }
+      console.log(JSON.stringify(buffer, null, 4));
+      try {
+        var dls = buffer.results[0].extensions[0].statistics[0].value;
+      } catch (e) {
+        badgeData.text[1] = 'invalid';
+        sendBadge(format, badgeData);
+        return;
+      }
+      badgeData.text[1] = metric(dls);
+      badgeData.colorscheme = 'green';
+      sendBadge(format, badgeData);
+    });
+  }));
+
+
 camp.route(/^\/dockbit\/([A-Za-z0-9-_]+)\/([A-Za-z0-9-_]+)\.(svg|png|gif|jpg|json)$/,
 cache(function(data, match, sendBadge, request) {
   const org      = match[1];
