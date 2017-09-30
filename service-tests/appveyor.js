aed89560a8bfa7ea12ffac4599f24c5b7ec201f6
NZSmartie <nzsmartie@gmail.com>
2017-10-01 04:34:00 +1300

    Add comments to appveyor tests

--- service-tests/appveyor.js
+++ service-tests/appveyor.js
@@ -6,6 +6,7 @@ const ServiceTester = require('./runner/service-tester');
 const t = new ServiceTester({ id: 'appveyor', title: 'AppVeyor' });
 module.exports = t;
 
+// Test AppVeyor build status badge
 t.create('CI build status')
   .get('/ci/gruntjs/grunt.json')
   .expectJSONTypes(Joi.object().keys({
@@ -13,6 +14,7 @@ t.create('CI build status')
     value: Joi.equal('failing', 'passing', 'running', 'queued')
   }));
 
+// Test AppVeyor branch build status badge 
 t.create('CI build status on master branch')
   .get('/ci/gruntjs/grunt/master.json')
   .expectJSONTypes(Joi.object().keys({
@@ -20,6 +22,7 @@ t.create('CI build status on master branch')
     value: Joi.equal('failing', 'passing', 'running', 'queued')
   }));
 
+// Test AppVeyor build status badge on a non-existing project
 t.create('CI 404')
 .get('/ci/somerandomproject/thatdoesntexits.json')
   .expectJSONTypes(Joi.object().keys({
@@ -27,6 +30,7 @@ t.create('CI 404')
     value: Joi.string().regex(/^project not found or access denied$/),
   }));
 
+// Test AppVeyor tests status badge
 t.create('tests status')
   .get('/tests/NZSmartie/coap-net-iu0to.json')
   .expectJSONTypes(Joi.object().keys({
@@ -34,14 +38,16 @@ t.create('tests status')
     value: Joi.string().regex(/^(?:[0-9]+ (?:passed|skipped|failed)(?:, )?)+$/),
   }));
 
-  t.create('tests status on master branch')
+// Test AppVeyor branch tests status badge
+t.create('tests status on master branch')
   .get('/tests/NZSmartie/coap-net-iu0to/master.json')
   .expectJSONTypes(Joi.object().keys({
     name: Joi.equal('tests'),
     value: Joi.string().regex(/^(?:[0-9]+ (?:passed|skipped|failed)(?:, )?)+$/),
   }));
 
-t.create('CI 404')
+// Test AppVeyor tests status badge for a non-existing project
+t.create('tests 404')
 .get('/tests/somerandomproject/thatdoesntexits.json')
   .expectJSONTypes(Joi.object().keys({
     name: Joi.equal('tests'),
