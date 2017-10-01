283507b1e34879be2198f719c639ca211e337fe1
Ritwick Dey <ritwickdey@outlook.com>
2017-10-01 17:01:09 +0530

    /github/commits-since/:ower/:repo/latest.svg added

--- server.js
+++ server.js
@@ -3414,27 +3414,54 @@ cache(function(data, match, sendBadge, request) {
   var repo = match[2];  // eg, subtitleedit
   var version = match[3];  // eg, 3.4.7
   var format = match[4];
-  var apiUrl = githubApiUrl + '/repos/' + user + '/' + repo + '/compare/' + version + '...master';
-  var badgeData = getBadgeData('commits since ' + version, data);
-  if (badgeData.template === 'social') {
-    badgeData.logo = getLogo('github', data);
+
+  function setCommitsSinceBadge(user, repo, version) {
+    var apiUrl = githubApiUrl + '/repos/' + user + '/' + repo + '/compare/' + version + '...master';
+    var badgeData = getBadgeData('commits since ' + version, data);
+    if (badgeData.template === 'social') {
+      badgeData.logo = getLogo('github', data);
+    }
+    githubAuth.request(request, apiUrl, {}, function (err, res, buffer) {
+      if (err != null) {
+        badgeData.text[1] = 'inaccessible';
+        sendBadge(format, badgeData);
+        return;
+      }
+      try {
+        var data = JSON.parse(buffer);
+        badgeData.text[1] = metric(data.ahead_by);
+        badgeData.colorscheme = 'blue';
+        sendBadge(format, badgeData);
+      } catch (e) {
+        badgeData.text[1] = 'none';
+        sendBadge(format, badgeData);
+      }
+    });
   }
-  githubAuth.request(request, apiUrl, {}, function(err, res, buffer) {
-    if (err != null) {
-      badgeData.text[1] = 'inaccessible';
-      sendBadge(format, badgeData);
-      return;
-    }
-    try {
-      var data = JSON.parse(buffer);
-      badgeData.text[1] = metric(data.ahead_by);
-      badgeData.colorscheme = 'blue';
-      sendBadge(format, badgeData);
-    } catch(e) {
-      badgeData.text[1] = 'none';
-      sendBadge(format, badgeData);
-    }
-  });
+
+  if(version.toLowerCase() === 'latest') {
+    let url = `https://api.github.com/repos/${user}/${repo}/releases/latest`
+    githubAuth.request(request, url, {}, (err, res, buffer) => {
+      if (err != null) {
+        badgeData.text[1] = 'inaccessible';
+        sendBadge(format, badgeData);
+        return;
+      }
+      try {
+        const data = JSON.parse(buffer);
+        setCommitsSinceBadge(user, repo, data.tag_name);
+      } catch(e) {
+        badgeData.text[1] = 'none';
+        sendBadge(format, badgeData);
+        return;
+      }
+
+    });
+  }
+  else {
+    setCommitsSinceBadge(user, repo, version)
+  }
+ 
 }));
 
 // GitHub release-download-count integration.
