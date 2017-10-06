5a3516c687f0c743f1730e0dd1110d10a189b2bf
Paul Melnikow <github@paulmelnikow.com>
2017-10-06 13:11:29 -0400

    Clean up some helpers (#1117)
    Address comments from #1109 and make several code-quality fixes which were caught by a local run of eslint-config-standard.

--- service-tests/codecov.js
+++ service-tests/codecov.js
@@ -10,12 +10,12 @@ t.create('gets coverage status')
   .get('/c/github/codecov/example-python.json')
   .expectJSONTypes(Joi.object().keys({
     name: 'coverage',
-    value: Joi.string().regex(/^[0-9]+\%$/),
+    value: Joi.string().regex(/^[0-9]+%$/),
   }));
 
 t.create('gets coverate status for branch')
   .get('/c/github/codecov/example-python/master.json')
   .expectJSONTypes({
     name: 'coverage',
-    value: Joi.string().regex(/^[0-9]+\%$/),
+    value: Joi.string().regex(/^[0-9]+%$/),
   });
