a5903d8301250494e48f0b78da87f6013cd73655
Paul Melnikow <email@paulmelnikow.com>
2017-10-09 14:34:53 -0400

    Fix for latest master

--- service-tests/github.js
+++ service-tests/github.js
@@ -158,14 +158,14 @@ t.create('Release Date. e.g release date|today')
 .get('/release-date/microsoft/vscode.json')
 .expectJSONTypes(Joi.object().keys({
   name: 'release date',
-  value: validDateString
+  value: isFormattedDate
 }));
 
 t.create('Release Date - Custom Label. e.g myRelease|today')
 .get('/release-date/microsoft/vscode.json?label=myRelease')
 .expectJSONTypes(Joi.object().keys({
   name: 'myRelease',
-  value: validDateString
+  value: isFormattedDate
 }));
 
 t.create('Release Date - Should return `no releases or repo not found` for invalid repo')
@@ -176,14 +176,14 @@ t.create('(Pre-)Release Date. e.g release date|today')
 .get('/release-date-pre/microsoft/vscode.json')
 .expectJSONTypes(Joi.object().keys({
   name: 'release date',
-  value: validDateString
+  value: isFormattedDate
 }));
 
 t.create('(Pre-)Release Date - Custom Label. e.g myRelease|today')
 .get('/release-date-pre/microsoft/vscode.json?label=myRelease')
 .expectJSONTypes(Joi.object().keys({
   name: 'myRelease',
-  value: validDateString
+  value: isFormattedDate
 }));
 
 t.create('(Pre-)Release Date - Should return `no releases or repo not found` for invalid repo')
