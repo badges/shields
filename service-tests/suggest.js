e4a87c07287a90b767a28feee81ce0fcd786463f
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2017-10-11 22:33:01 +0200

    Tests for license suggest

--- service-tests/suggest.js
+++ service-tests/suggest.js
@@ -51,3 +51,44 @@ t.create('license')
     link: 'https://github.com/atom/atom/blob/master/LICENSE.md',
     badge: 'https://img.shields.io/github/license/atom/atom.svg'
   }));
+
+t.create('no license for non-existing project')
+  .get('/v1?url=' + encodeURIComponent('https://github.com/atom/atom'))
+  .addHeader('origin', 'https://shields.io')
+  .intercept(nock => nock('https://api.github.com')
+    .get(/\/repos\/atom\/atom\/license/)
+    .reply(404))
+  .expectJSONTypes('badges.*', Joi.object().keys({
+    name: Joi.not('GitHub license'),
+    badge: Joi.allow(),
+    link: Joi.allow()
+  }));
+
+t.create('no license when json response is invalid')
+  .get('/v1?url=' + encodeURIComponent('https://github.com/atom/atom'))
+  .addHeader('origin', 'https://shields.io')
+  .intercept(nock => nock('https://api.github.com')
+    .get(/\/repos\/atom\/atom\/license/)
+    .reply(200, 'invalid json'), {
+    'Content-Type': 'application/json;charset=UTF-8'
+  })
+  .expectJSONTypes('badges.*', Joi.object().keys({
+    name: Joi.not('GitHub license'),
+    badge: Joi.allow(),
+    link: Joi.allow()
+  }));
+
+t.create('no license when html_url not found in GitHub api response')
+  .get('/v1?url=' + encodeURIComponent('https://github.com/atom/atom'))
+  .addHeader('origin', 'https://shields.io')
+  .intercept(nock => nock('https://api.github.com')
+    .get(/\/repos\/atom\/atom\/license/)
+    .reply(200, {
+      license: 'MIT'
+    }))
+  .expectJSONTypes('badges.*', Joi.object().keys({
+    name: Joi.not('GitHub license'),
+    badge: Joi.allow(),
+    link: Joi.allow()
+  }));
+
