7b761134b369a0f67673a6ac5bbbd7039e627e58
jaredcnance <jaredcnance@gmail.com>
2017-10-12 21:37:47 -0500

    cleanup test

--- service-tests/dotnetstatus.js
+++ service-tests/dotnetstatus.js
@@ -22,19 +22,9 @@ t
 t
   .create("get nuget package status")
   .get("/gh/jaredcnance/dotnet-status/invalid-project.json")
-  .expectJSONTypes(
-    Joi.object().keys({
-      name: Joi.equal("dependencies"),
-      value: Joi.equal("project not found")
-    })
-  );
+  .expectJSON({ name: "dependencies", value: "project not found" });
 
 t
   .create("get nuget package status error")
   .get("/not-a-valid-uri.json")
-  .expectJSONTypes(
-    Joi.object().keys({
-      name: Joi.equal("dependencies"),
-      value: Joi.equal("inconclusive")
-    })
-  );
+  .expectJSON({ name: "dependencies", value: "inconclusive" });
