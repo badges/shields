badges/shields

commit 848422c645d2c8de1539cf0ad0a561ed6c4cc4b7

[Github] download count for latest pre-release (#1140)
Implements #1113.

Removed extra space in 2 cases (failing tests without this change):

  1) Github downloads for latest release 
	[ GET http://localhost:1111/github/downloads/photonstorm/phaser/latest/total.json ]:
     ValidationError: child "value" fails because ["value" with value "63k " fails to match the required pattern: /^[0-9]+[kMGTPEZY]?$/]
--
  2) Github downloads-pre for latest release 
	[ GET http://localhost:1111/github/downloads-pre/photonstorm/phaser/latest/total.json ]:
     ValidationError: child "value" fails because ["value" with value "34 " fails to match the required pattern: /^[0-9]+[kMGTPEZY]?$/]
--
  3) Github downloads for specific asset from latest release 
	[ GET http://localhost:1111/github/downloads/atom/atom/latest/atom-amd64.deb.json ]:
     ValidationError: child "value" fails because ["value" with value "3k  &#x5b;atom-amd64.deb&#x5d;" fails to match the required pattern: /^[0-9]+[kMGTPEZY]? \[atom-amd64\.deb\]$/]
--
  4) Github downloads-pre for specific asset from latest release 
	[ GET http://localhost:1111/github/downloads-pre/atom/atom/latest/atom-amd64.deb.json ]:
     ValidationError: child "value" fails because ["value" with value "372  &#x5b;atom-amd64.deb&#x5d;" fails to match the required pattern: /^[0-9]+[kMGTPEZY]? \[atom-amd64\.deb\]$/]

server.js
@@ -3330,15 +3330,16 @@ mapGithubReleaseDate(camp, githubApiUrl, githubAuth);
 // GitHub commits since integration.
 mapGithubCommitsSince(camp, githubApiUrl ,githubAuth);
 
-// GitHub release-download-count integration.
-camp.route(/^\/github\/downloads\/([^\/]+)\/([^\/]+)(\/.+)?\/([^\/]+)\.(svg|png|gif|jpg|json)$/,
+// GitHub release-download-count and pre-release-download-count integration.
+camp.route(/^\/github\/(downloads|downloads-pre)\/([^\/]+)\/([^\/]+)(\/.+)?\/([^\/]+)\.(svg|png|gif|jpg|json)$/,
 cache(function(data, match, sendBadge, request) {
-  var user = match[1];  // eg, qubyte/rubidium
-  var repo = match[2];
+  const type = match[1]; // downloads or downloads-pre
+  var user = match[2];  // eg, qubyte/rubidium
+  var repo = match[3];
 
-  var tag = match[3];  // eg, v0.190.0, latest, null if querying all releases
-  var asset_name = match[4].toLowerCase(); // eg. total, atom-amd64.deb, atom.x86_64.rpm
-  var format = match[5];
+  var tag = match[4];  // eg, v0.190.0, latest, null if querying all releases
+  var asset_name = match[5].toLowerCase(); // eg. total, atom-amd64.deb, atom.x86_64.rpm
+  var format = match[6];
 
   if (tag) { tag = tag.slice(1); }
 
@@ -3349,8 +3350,10 @@ cache(function(data, match, sendBadge, request) {
 
   var apiUrl = githubApiUrl + '/repos/' + user + '/' + repo + '/releases';
   if (!total) {
-    var release_path = tag !== 'latest' ? 'tags/' + tag : 'latest';
-    apiUrl = apiUrl + '/' + release_path;
+    var release_path = tag === 'latest' ? (type === 'downloads' ? 'latest' : '') : 'tags/' + tag;
+    if (release_path) {
+      apiUrl = apiUrl + '/' + release_path;
+    }
   }
   var badgeData = getBadgeData('downloads', data);
   if (badgeData.template === 'social') {
@@ -3363,9 +3366,12 @@ cache(function(data, match, sendBadge, request) {
     }
     try {
       var data = JSON.parse(buffer);
+      if (type === 'downloads-pre' && tag === 'latest') {
+        data = data[0];
+      }
       var downloads = 0;
 
-      var label;
+      const labelWords = [metric(downloads)];
       if (total) {
         data.forEach(function (tagData) {
           tagData.assets.forEach(function (asset) {
@@ -3375,9 +3381,9 @@ cache(function(data, match, sendBadge, request) {
           });
         });
 
-        label = 'total';
+        labelWords.push('total');
         if (asset_name !== 'total') {
-          label += ' ' + '[' + asset_name + ']';
+          labelWords.push(`[${asset_name}]`);
         }
       } else {
         data.assets.forEach(function (asset) {
@@ -3386,12 +3392,14 @@ cache(function(data, match, sendBadge, request) {
           }
         });
 
-        label = tag !== 'latest' ?  tag : '';
+        if (tag !== 'latest') {
+          labelWords.push(tag);
+        }
         if (asset_name !== 'total') {
-          label += ' ' + '[' + asset_name + ']';
+          labelWords.push(`[${asset_name}]`);
         }
       }
-      badgeData.text[1] = metric(downloads) + ' ' + label;
+      badgeData.text[1] = labelWords.join(' ');
       badgeData.colorscheme = 'brightgreen';
       sendBadge(format, badgeData);
     } catch(e) {

service-tests/github.js
@@ -279,6 +279,14 @@ t.create('Downloads all releases')
     value: Joi.string().regex(/^\w+\s+total$/)
   }));
 
+t.create('downloads for latest release')
+  .get('/downloads/photonstorm/phaser/latest/total.json')
+  .expectJSONTypes(Joi.object().keys({ name: 'downloads', value: isMetric }));
+
+t.create('downloads-pre for latest release')
+  .get('/downloads-pre/photonstorm/phaser/latest/total.json')
+  .expectJSONTypes(Joi.object().keys({ name: 'downloads', value: isMetric }));
+
 t.create('downloads for release without slash')
   .get('/downloads/atom/atom/v0.190.0/total.json')
   .expectJSONTypes(Joi.object().keys({
@@ -291,6 +299,20 @@ t.create('downloads for specific asset without slash')
   .expectJSONTypes(Joi.object().keys({
     name: 'downloads',
     value: Joi.string().regex(/^[0-9]+[kMGTPEZY]? v0\.190\.0 \[atom-amd64\.deb\]$/)
+  }));
+
+t.create('downloads for specific asset from latest release')
+  .get('/downloads/atom/atom/latest/atom-amd64.deb.json')
+  .expectJSONTypes(Joi.object().keys({
+    name: 'downloads',
+    value: Joi.string().regex(/^[0-9]+[kMGTPEZY]? \[atom-amd64\.deb\]$/)
+  }));
+
+t.create('downloads-pre for specific asset from latest release')
+  .get('/downloads-pre/atom/atom/latest/atom-amd64.deb.json')
+  .expectJSONTypes(Joi.object().keys({
+    name: 'downloads',
+    value: Joi.string().regex(/^[0-9]+[kMGTPEZY]? \[atom-amd64\.deb\]$/)
   }));
 
 t.create('downloads for release with slash')

try.html
@@ -259,6 +259,11 @@ Pixel-perfect &nbsp; Retina-ready &nbsp; Fast &nbsp; Consistent &nbsp; Hackable 
   <td><img src='/github/downloads/atom/atom/latest/total.svg' alt=''/></td>
   <td><code>https://img.shields.io/github/downloads/atom/atom/latest/total.svg</code></td>
   </tr>
+  <tr>
+    <th data-keywords='github' data-doc='githubDoc'> Github Pre-Releases: </th>
+    <td><img src='/github/downloads-pre/atom/atom/latest/total.svg' alt=''/></td>
+    <td><code>https://img.shields.io/github/downloads-pre/atom/atom/latest/total.svg</code></td>
+  </tr>
   <tr><th data-keywords='github' data-doc='githubDoc'> Github Releases (by Release): </th>
   <td><img src='/github/downloads/atom/atom/v0.190.0/total.svg' alt=''/></td>
   <td><code>https://img.shields.io/github/downloads/atom/atom/v0.190.0/total.svg</code></td>
@@ -266,6 +271,11 @@ Pixel-perfect &nbsp; Retina-ready &nbsp; Fast &nbsp; Consistent &nbsp; Hackable 
   <tr><th data-keywords='github' data-doc='githubDoc'> Github Releases (by Asset): </th>
   <td><img src='/github/downloads/atom/atom/latest/atom-amd64.deb.svg' alt=''/></td>
   <td><code>https://img.shields.io/github/downloads/atom/atom/latest/atom-amd64.deb.svg</code></td>
+  </tr>
+  <tr>
+    <th data-keywords='github' data-doc='githubDoc'> Github Pre-Releases (by Asset): </th>
+    <td><img src='/github/downloads-pre/atom/atom/latest/atom-amd64.deb.svg' alt=''/></td>
+    <td><code>https://img.shields.io/github/downloads-pre/atom/atom/latest/atom-amd64.deb.svg</code></td>
   </tr>
   <tr><th data-keywords='node'> npm: </th>
   <td><img src='/npm/dw/localeval.svg' alt=''/></td>

commit 366c939cdcd06a3df8a4fc55319936905059cbfb

Fix downloads and commit activity badges showing zero (#1201)
- `isMetric` tests must be > 0 to pass.
- Fix downloads badge bug introduced in #1140. (So much for [being clever](https://github.com/badges/shields/pull/1140#discussion_r143544396)!)
- Fix 1 week activity badge by returning the previous week instead of the current week.

server.js
@@ -3476,7 +3476,7 @@ cache(function(data, match, sendBadge, request) {
       }
       var downloads = 0;
 
-      const labelWords = [metric(downloads)];
+      const labelWords = [];
       if (total) {
         data.forEach(function (tagData) {
           tagData.assets.forEach(function (asset) {
@@ -3504,6 +3504,7 @@ cache(function(data, match, sendBadge, request) {
           labelWords.push(`[${asset_name}]`);
         }
       }
+      labelWords.unshift(metric(downloads));
       badgeData.text[1] = labelWords.join(' ');
       badgeData.colorscheme = 'brightgreen';
       sendBadge(format, badgeData);
@@ -3969,7 +3970,7 @@ cache(function(data, match, sendBadge, request) {
           intervalLabel = '/4 weeks';
           break;
         case 'w':
-          value = parsedData.slice(-1)[0].total;
+          value = parsedData.slice(-2)[0].total;
           intervalLabel = '/week';
           break;
         default:

service-tests/github.js
@@ -99,16 +99,16 @@ t.create('GitHub open issues by label (raw)')
   }));
 
 t.create('GitHub open pull requests by label')
-  .get('/issues-pr/badges/shields/vendor-badge.json')
+  .get('/issues-pr/badges/shields/service-badge.json')
   .expectJSONTypes(Joi.object().keys({
-    name: 'vendor-badge pull requests',
+    name: 'service-badge pull requests',
     value: Joi.string().regex(/^[0-9]+[kMGTPEZY]? open$/)
   }));
 
 t.create('GitHub open pull requests by label (raw)')
-  .get('/issues-pr-raw/badges/shields/vendor-badge.json')
+  .get('/issues-pr-raw/badges/shields/service-badge.json')
   .expectJSONTypes(Joi.object().keys({
-    name: 'open vendor-badge pull requests',
+    name: 'open service-badge pull requests',
     value: isMetric
   }));
 

service-tests/helpers/validators.js
@@ -13,9 +13,10 @@ const isVPlusDottedVersionAtLeastOne = withRegex(/^v\d+(\.\d+)?(\.\d+)?$/);
 
 const isStarRating = withRegex(/^(?=.{5}$)(\u2605{0,5}[\u00BC\u00BD\u00BE]?\u2606{0,5})$/);
 
-const isMetric = withRegex(/^[0-9]+[kMGTPEZY]?$/);
+// Required to be > 0, beacuse accepting zero masks many problems.
+const isMetric = withRegex(/^[1-9][0-9]*[kMGTPEZY]?$/);
 
-const isMetricOverTimePeriod = withRegex(/^[0-9]+[kMGTPEZY]?\/(year|month|4 weeks|week|day)$/);
+const isMetricOverTimePeriod = withRegex(/^[1-9][0-9]*[kMGTPEZY]?\/(year|month|4 weeks|week|day)$/);
 
 const isPercentage = withRegex(/^[0-9]+%$/);
 