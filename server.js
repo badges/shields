3c37dbd3dc7ab6b33bf8f87c49496bde791f0ddd
Paul Melnikow <github@paulmelnikow.com>
2017-10-28 15:12:44 -0400

    [maven-metadata] Add support for maven-metadata.xml (#1214)
    Based on ArsenArsen's work in #953.

--- server.js
+++ server.js
@@ -7075,6 +7075,40 @@ cache((data, match, sendBadge, request) => {
   });
 }));
 
+// Maven metadata versioning integration.
+camp.route(/^\/maven-metadata\/v\/(https?)\/(.+\.xml)\.(svg|png|gif|jpg|json)$/,
+  cache(function (data, match, sendBadge, request) {
+    const [, scheme, hostAndPath, format] = match;
+    const metadataUri = `${scheme}://${hostAndPath}`;
+    request(metadataUri, (error, response, body) => {
+      const badge = getBadgeData('maven', data);
+      if (!error && response.statusCode >= 200 && response.statusCode < 300) {
+        try {
+          xml2js.parseString(body, (err, result) => {
+            if (err) {
+              badge.text[1] = 'error';
+              badge.colorscheme = 'red';
+              sendBadge(format, badge);
+            } else {
+              const version = result.metadata.versioning[0].versions[0].version.slice(-1)[0];
+              badge.text[1] = `v${version}`;
+              badge.colorscheme = /^.*-SNAPSHOT$/.test(version) ? 'green' : 'brightgreen';
+              sendBadge(format, badge);
+            }
+          });
+        } catch (e) {
+          badge.text[1] = 'error';
+          badge.colorscheme = 'red';
+          sendBadge(format, badge);
+        }
+      } else {
+        badge.text[1] = 'error';
+        badge.colorscheme = 'red';
+        sendBadge(format, badge);
+      }
+    });
+}));
+
 // nsp for npm packages
 camp.route(/^\/nsp\/npm\/(?:@([^/]+)?\/)?([^/]+)?(?:\/([^/]+)?)?\.(svg|png|gif|jpg|json)?$/, cache((data, match, sendBadge, request) => {
   // A: /nsp/npm/:package.:format
