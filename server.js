618f0593d2a780c31fbe45238104bebb1105a84d
Paul Melnikow <email@paulmelnikow.com>
2017-10-28 15:02:52 -0400

    Tweaks

--- server.js
+++ server.js
@@ -7076,22 +7076,23 @@ cache((data, match, sendBadge, request) => {
 }));
 
 // Maven metadata versioning integration.
-camp.route(/^\/maven-metadata\/v\/((?:https?:\/\/.+\/)maven-metadata\.xml)\.(svg|png|gif|jpg|json)$/,
+camp.route(/^\/maven-metadata\/v\/(https?)\/(.+\.xml)\.(svg|png|gif|jpg|json)$/,
   cache(function (data, match, sendBadge, request) {
-    const [, metadataUri, format] = match;
+    const [, scheme, hostAndPath, format] = match;
+    const metadataUri = `${scheme}://${hostAndPath}`;
     request(metadataUri, (error, response, body) => {
       const badge = getBadgeData('maven', data);
       if (!error && response.statusCode >= 200 && response.statusCode < 300) {
         try {
-          xml2js.parseString(body, function (err, result) {
-            if (!err) {
-              const version = result.metadata.versioning[0].versions[0].version || '';
-              badge.text[1] = 'v' + version[version.length - 1];
-              badge.colorscheme = /^.*-SNAPSHOT$/.test(badge.text[1]) ? 'green' : 'brightgreen';
-              sendBadge(format, badge);
-            } else {
+          xml2js.parseString(body, (err, result) => {
+            if (err) {
               badge.text[1] = 'error';
               badge.colorscheme = 'red';
+              sendBadge(format, badge);
+            } else {
+              const version = result.metadata.versioning[0].versions[0].version.slice(-1)[0];
+              badge.text[1] = `v${version}`;
+              badge.colorscheme = /^.*-SNAPSHOT$/.test(version) ? 'green' : 'brightgreen';
               sendBadge(format, badge);
             }
           });
--- service-tests/maven-metadata.js
+++ service-tests/maven-metadata.js
@@ -8,8 +8,8 @@ const t = new ServiceTester({ id: 'maven-metadata', title: 'maven-metadata badge
 module.exports = t;
 
 
-t.create('valid maven-metadata.xml')
-  .get('/v/http://central.maven.org/maven2/com/google/code/gson/gson/maven-metadata.xml.json')
+t.create('valid maven-metadata.xml uri')
+  .get('/v/http/central.maven.org/maven2/com/google/code/gson/gson/maven-metadata.xml.json')
   .expectJSONTypes(Joi.object().keys({
     name: 'maven',
     value: isVPlusDottedVersionAtLeastOne
