c0b4a580c8d07cb0082fae96e68c25b098d894cd
Paul Melnikow <email@paulmelnikow.com>
2017-10-28 15:09:59 -0400

    tweaks

--- service-tests/maven-metadata.js
+++ service-tests/maven-metadata.js
@@ -7,7 +7,6 @@ const { isVPlusDottedVersionAtLeastOne } = require('./helpers/validators');
 const t = new ServiceTester({ id: 'maven-metadata', title: 'maven-metadata badge' });
 module.exports = t;
 
-
 t.create('valid maven-metadata.xml uri')
   .get('/v/http/central.maven.org/maven2/com/google/code/gson/gson/maven-metadata.xml.json')
   .expectJSONTypes(Joi.object().keys({
