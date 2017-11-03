4be63dc072628e5dd36e2cadb2879805cdd21ec0
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2017-11-03 12:29:44 +0100

    Tests for npm license badge

--- service-tests/npm.js
+++ service-tests/npm.js
@@ -3,9 +3,11 @@
 const Joi = require('joi');
 const ServiceTester = require('./runner/service-tester');
 const { isSemver } = require('./helpers/validators');
+const colorscheme = require('../lib/colorscheme.json');
 
 const t = new ServiceTester({ id: 'npm', title: 'NPM' });
 module.exports = t;
+const colorsB = Object.assign({}, ...Object.keys(colorscheme).map(color => ({ [color]: colorscheme[color].colorB })));
 
 t.create('gets the package version of left-pad')
   .get('/v/left-pad.json')
@@ -26,3 +28,15 @@ t.create('gets the tagged package version of @cycle/core')
 t.create('invalid package name')
   .get('/v/frodo-is-not-a-package.json')
   .expectJSON({ name: 'npm', value: 'invalid' });
+
+t.create('public domain license')
+  .get('/l/redux-auth.json?style=_shields_test')
+  .expectJSON({ name: 'license', value: 'WTFPL', colorB: colorsB.blue });
+
+t.create('copyleft license')
+  .get('/l/trianglify.json?style=_shields_test')
+  .expectJSON({ name: 'license', value: 'GPL-3.0', colorB: colorsB.blue });
+
+t.create('permissive license')
+  .get('/l/express.json?style=_shields_test')
+  .expectJSON({ name: 'license', value: 'MIT', colorB: colorsB.blue });
