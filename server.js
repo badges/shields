6788a31ed571a38b4b3a2c3ea2b31410f28265e4
Tatsuya Saito <twopackas@gmail.com>
2017-11-07 00:59:26 +0900

    Adds new badge for [Redmine] plugin rating (#1247)

--- server.js
+++ server.js
@@ -7283,6 +7283,48 @@ camp.route(/^\/nsp\/npm\/(?:@([^/]+)?\/)?([^/]+)?(?:\/([^/]+)?)?\.(svg|png|gif|j
   }
 }));
 
+// Redmine plugin rating.
+camp.route(/^\/redmine\/plugin\/(rating|stars)\/(.*)\.(svg|png|gif|jpg|json)$/,
+cache(function(data, match, sendBadge, request) {
+  var type = match[1];
+  var plugin = match[2];
+  var format = match[3];
+  var options = {
+    method: 'GET',
+    uri: 'https://www.redmine.org/plugins/' + plugin + '.xml',
+  };
+
+  var badgeData = getBadgeData(type, data);
+  request(options, function(err, res, buffer) {
+    if (err != null) {
+      badgeData.text[1] = 'inaccessible';
+      sendBadge(format, badgeData);
+      return;
+    }
+
+    xml2js.parseString(buffer.toString(), function (err, data) {
+      try {
+        var rating = data['redmine-plugin']['ratings-average'][0]._;
+        badgeData.colorscheme = floorCountColor(rating, 2, 3, 4);
+
+        switch (type) {
+          case 'rating':
+            badgeData.text[1] = rating + '/5.0';
+            break;
+          case 'stars':
+            badgeData.text[1] = starRating(Math.round(rating));
+            break;
+        }
+
+        sendBadge(format, badgeData);
+      } catch(e) {
+        badgeData.text[1] = 'invalid';
+        sendBadge(format, badgeData);
+      }
+    });
+  });
+}));
+
 // Any badge.
 camp.route(/^\/(:|badge\/)(([^-]|--)*?)-(([^-]|--)*)-(([^-]|--)+)\.(svg|png|gif|jpg)$/,
 function(data, match, end, ask) {
