48b8daebc3a98d15501864f87ec84cef12f9c640
Matt <bedrocks@gmail.com>
2017-11-08 17:30:03 -0600

    Add more tests.

--- service-tests/liberapay.js
+++ service-tests/liberapay.js
@@ -13,6 +13,35 @@ t.create('Receiving')
     value: Joi.string().regex(/^[0-9]+(\.[0-9]{2})?[ A-Za-z]{4}\/week/)
   }));
 
+t.create('Giving')
+  .get('/Liberapay/gives.json')
+  .expectJSONTypes(Joi.object().keys({
+    name: 'gives',
+    value: Joi.string().regex(/^[0-9]+(\.[0-9]{2})?[ A-Za-z]{4}\/week/)
+  }));
+
+t.create('Patrons')
+  .get('/Liberapay/patrons.json')
+  .expectJSONTypes(Joi.object().keys({
+    name: 'patrons',
+    value: Joi.string().regex(/^[0-9]+/)
+  }));
+
+t.create('Goal Progress')
+  .get('/Liberapay/goal.json')
+  .expectJSONTypes(Joi.object().keys({
+    name: 'goal progress',
+    value: Joi.string().regex(/^[0-9]+\%/)
+  }));
+
+t.create('No Goal')
+  .get('/Liberapay/goal.json')
+  .intercept(nock => nock('https://liberapay.com')
+    .get('/Liberapay/public.json')
+    .reply(200, { goal: null })
+  )
+  .expectJSON({ name: 'goal progress', value: 'invalid'});
+
 t.create('Empty')
   .get('/Liberapay/receives.json')
   .intercept(nock => nock('https://liberapay.com')
