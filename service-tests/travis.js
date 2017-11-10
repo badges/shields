dae2a093dacc4f035c6c1af17f37335e550c38bf
Peter Gribanov <wcode404@gmail.com>
2017-11-10 19:37:37 +0300

    simplify tests

--- service-tests/travis.js
+++ service-tests/travis.js
@@ -8,15 +8,15 @@ module.exports = t;
 
 t.create('gets the package version of symfony')
     .get('/php-v/symfony/symfony.json')
-    .expectJSONTypes(Joi.object().keys({ name: 'PHP', value: '>= 7.1' }));
+    .expectJSON({ name: 'PHP', value: '>= 7.1' });
 
 t.create('gets the package version of symfony 2.8')
     .get('/php-v/symfony/symfony/2.8.json')
-    .expectJSONTypes(Joi.object().keys({ name: 'PHP', value: '>= 5.4, HHVM' }));
+    .expectJSON({ name: 'PHP', value: '>= 5.4, HHVM' });
 
 t.create('gets the package version of yii')
     .get('/php-v/yiisoft/yii.json')
-    .expectJSONTypes(Joi.object().keys({ name: 'PHP', value: '5.3 - 7.1, HHVM' }));
+    .expectJSON({ name: 'PHP', value: '5.3 - 7.1, HHVM' });
 
 t.create('invalid package name')
     .get('/php-v/frodo/is-not-a-package.json')
