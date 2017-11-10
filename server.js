0d3d33bf1bfb7570eb6070c9d2cd2b1bc07451d2
Peter Gribanov <wcode404@gmail.com>
2017-11-10 20:46:52 +0300

    Get PHP version from [Packagist] (#1256)

--- server.js
+++ server.js
@@ -7395,6 +7395,39 @@ cache(function(data, match, sendBadge, request) {
   });
 }));
 
+// PHP version from Packagist
+camp.route(/^\/packagist\/php-v\/([^/]+\/[^/]+)(?:\/([^/]+))?\.(svg|png|gif|jpg|json)$/,
+cache(function(data, match, sendBadge, request) {
+  const userRepo = match[1];  // eg, espadrine/sc
+  const version = match[2] ? match[2] : 'dev-master';
+  const format = match[3];
+  const options = {
+    method: 'GET',
+    uri: 'https://packagist.org/p/' + userRepo + '.json',
+  };
+  const badgeData = getBadgeData('PHP', data);
+  request(options, function(err, res, buffer) {
+    if (err !== null) {
+      log.error('Packagist error: ' + err.stack);
+      if (res) {
+        log.error('' + res);
+      }
+      badgeData.text[1] = 'invalid';
+      sendBadge(format, badgeData);
+      return;
+    }
+
+    try {
+      const data = JSON.parse(buffer);
+      badgeData.text[1] = data.packages[userRepo][version].require.php;
+      badgeData.colorscheme = 'blue';
+    } catch(e) {
+      badgeData.text[1] = 'invalid';
+    }
+    sendBadge(format, badgeData);
+  });
+}));
+
 // Any badge.
 camp.route(/^\/(:|badge\/)(([^-]|--)*?)-(([^-]|--)*)-(([^-]|--)+)\.(svg|png|gif|jpg)$/,
 function(data, match, end, ask) {
