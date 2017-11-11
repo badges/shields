badges/shields

commit 076cb14b3bed2610b9dc10d6181fee35b2daf416

Perform constant equal comparison for shared Shields secret
This should prevent timing attacks.

lib/github-auth.js
@@ -59,7 +59,6 @@ function setRoutes(server) {
       if (!token) {
         return end('The GitHub OAuth process did not return a user token.');
       }
-      console.log('GitHub OAuth: ' + token);
 
       ask.res.setHeader('Content-Type', 'text/html');
       end('<p>Shields.io has received your app-specific GitHub user token. ' +
@@ -80,8 +79,7 @@ function setRoutes(server) {
   });
 
   server.route(/^\/github-auth\/add-token$/, function(data, match, end, ask) {
-    console.log('GitHub add token called with', JSON.stringify(data));
-    if (data.shieldsSecret !== serverSecrets.shieldsSecret) {
+    if (constEq(data.shieldsSecret, serverSecrets.shieldsSecret)) {
       // An unknown entity tries to connect. Let the connection linger for a minute.
       return setTimeout(function() { end('Invalid secret.'); }, 60000);
     }
@@ -247,6 +245,15 @@ function githubRequest(request, url, query, cb) {
     }
     cb(err, res, buffer);
   });
+}
+
+function constEq(a, b) {
+  if (a.length !== b.length) { return false; }
+  var zero = 0;
+  for (var i = 0; i < a.length; i++) {
+    zero |= a.charCodeAt(i) ^ b.charCodeAt(i);
+  }
+  return (zero === 0);
 }
 
 exports.setRoutes = setRoutes;

commit 59c06628740bcc672b28d35d9ac6fb1832bfe222

Fix bug saving GitHub tokens from other servers
This fixes a bug introduced in 076cb14, wherein we discarded tokens received
from other servers, and wherein we could save tokens with invalid
identification.

The bug was raised by Paul Melnikow.

lib/github-auth.js
@@ -104,9 +104,9 @@ function setRoutes(server) {
   });
 
   server.route(/^\/github-auth\/add-token$/, function(data, match, end, ask) {
-    if (constEq(data.shieldsSecret, serverSecrets.shieldsSecret)) {
-      // An unknown entity tries to connect. Let the connection linger for a minute.
-      return setTimeout(function() { end('Invalid secret.'); }, 60000);
+    if (!constEq(data.shieldsSecret, serverSecrets.shieldsSecret)) {
+      // An unknown entity tries to connect. Let the connection linger for 10s.
+      return setTimeout(function() { end('Invalid secret.'); }, 10000);
     }
     addGithubToken(data.token);
     end('Thanks!');