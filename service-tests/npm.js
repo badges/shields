90f8899fdc1d1b675b665c56e1d1bca83727a48f
Vladimir Starkov <iamstarkov@gmail.com>
2017-11-19 23:59:06 +0100

    [npm] add tests for a license part

--- service-tests/npm.js
+++ service-tests/npm.js
@@ -31,6 +31,14 @@ t.create('gets the tagged package version of @cycle/core from a custom registry'
   .get('/v/@cycle/core/canary.json?registry_uri=https://registry.npmjs.com')
   .expectJSONTypes(Joi.object().keys({ name: 'npm@canary', value: isSemver }));
 
+t.create('gets the license of express')
+  .get('/l/express.json')
+  .expectJSONTypes(Joi.object().keys({ name: 'license', value: 'MIT' }));
+
+t.create('gets the license of express from a custom registry')
+  .get('/l/express.json?registry_uri=https://registry.npmjs.com')
+  .expectJSONTypes(Joi.object().keys({ name: 'license', value: 'MIT' }));
+
 t.create('invalid package name')
   .get('/v/frodo-is-not-a-package.json')
   .expectJSON({ name: 'npm', value: 'invalid' });
