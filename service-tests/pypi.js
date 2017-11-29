a460b30920f75ad2c1c8b05a7a8caf6056386c9a
chris48s <chris.shaw480@gmail.com>
2017-11-29 20:12:48 +0000

    use .expectJSON() if expected value is obj literal

--- service-tests/pypi.js
+++ service-tests/pypi.js
@@ -20,27 +20,27 @@ module.exports = t;
 */
 t.create('daily downloads (expected failure)')
   .get('/dd/djangorestframework.json')
-  .expectJSONTypes({ name: 'downloads', value: 'no longer available' });
+  .expectJSON({ name: 'downloads', value: 'no longer available' });
 
 t.create('weekly downloads (expected failure)')
   .get('/dw/djangorestframework.json')
-  .expectJSONTypes({ name: 'downloads', value: 'no longer available' });
+  .expectJSON({ name: 'downloads', value: 'no longer available' });
 
 t.create('monthly downloads (expected failure)')
   .get('/dm/djangorestframework.json')
-  .expectJSONTypes({ name: 'downloads', value: 'no longer available' });
+  .expectJSON({ name: 'downloads', value: 'no longer available' });
 
 t.create('daily downloads (invalid)')
   .get('/dd/not-a-package.json')
-  .expectJSONTypes({ name: 'pypi', value: 'invalid' });
+  .expectJSON({ name: 'pypi', value: 'invalid' });
 
 t.create('weekly downloads (invalid)')
   .get('/dw/not-a-package.json')
-  .expectJSONTypes({ name: 'pypi', value: 'invalid' });
+  .expectJSON({ name: 'pypi', value: 'invalid' });
 
 t.create('monthly downloads (invalid)')
   .get('/dm/not-a-package.json')
-  .expectJSONTypes({ name: 'pypi', value: 'invalid' });
+  .expectJSON({ name: 'pypi', value: 'invalid' });
 
 
 /*
@@ -74,64 +74,64 @@ t.create('version (not semver)')
 
 t.create('version (invalid)')
   .get('/v/not-a-package.json')
-  .expectJSONTypes({ name: 'pypi', value: 'invalid' });
+  .expectJSON({ name: 'pypi', value: 'invalid' });
 
 
 // tests for license endpoint
 
 t.create('license (valid, package version in request)')
   .get('/l/requests/2.18.4.json')
-  .expectJSONTypes({ name: 'license', value: 'Apache 2.0' });
+  .expectJSON({ name: 'license', value: 'Apache 2.0' });
 
 t.create('license (valid, no package version specified)')
   .get('/l/requests.json')
-  .expectJSONTypes({ name: 'license', value: 'Apache 2.0' });
+  .expectJSON({ name: 'license', value: 'Apache 2.0' });
 
 t.create('license (invalid)')
   .get('/l/not-a-package.json')
-  .expectJSONTypes({ name: 'pypi', value: 'invalid' });
+  .expectJSON({ name: 'pypi', value: 'invalid' });
 
 
 // tests for wheel endpoint
 
 t.create('wheel (has wheel, package version in request)')
   .get('/wheel/requests/2.18.4.json')
-  .expectJSONTypes({ name: 'wheel', value: 'yes' });
+  .expectJSON({ name: 'wheel', value: 'yes' });
 
 t.create('wheel (has wheel, no package version specified)')
   .get('/wheel/requests.json')
-  .expectJSONTypes({ name: 'wheel', value: 'yes' });
+  .expectJSON({ name: 'wheel', value: 'yes' });
 
 t.create('wheel (no wheel)')
   .get('/wheel/chai/1.1.2.json')
-  .expectJSONTypes({ name: 'wheel', value: 'no' });
+  .expectJSON({ name: 'wheel', value: 'no' });
 
 t.create('wheel (invalid)')
   .get('/wheel/not-a-package.json')
-  .expectJSONTypes({ name: 'pypi', value: 'invalid' });
+  .expectJSON({ name: 'pypi', value: 'invalid' });
 
 
 // tests for format endpoint
 
 t.create('format (wheel, package version in request)')
   .get('/format/requests/2.18.4.json')
-  .expectJSONTypes({ name: 'format', value: 'wheel' });
+  .expectJSON({ name: 'format', value: 'wheel' });
 
 t.create('format (wheel, no package version specified)')
   .get('/format/requests.json')
-  .expectJSONTypes({ name: 'format', value: 'wheel' });
+  .expectJSON({ name: 'format', value: 'wheel' });
 
 t.create('format (source)')
   .get('/format/chai/1.1.2.json')
-  .expectJSONTypes({ name: 'format', value: 'source' });
+  .expectJSON({ name: 'format', value: 'source' });
 
 t.create('format (egg)')
   .get('/format/virtualenv/0.8.2.json')
-  .expectJSONTypes({ name: 'format', value: 'egg' });
+  .expectJSON({ name: 'format', value: 'egg' });
 
 t.create('format (invalid)')
   .get('/format/not-a-package.json')
-  .expectJSONTypes({ name: 'pypi', value: 'invalid' });
+  .expectJSON({ name: 'pypi', value: 'invalid' });
 
 
 // tests for pyversions endpoint
@@ -152,46 +152,46 @@ t.create('python versions (valid, no package version specified)')
 
 t.create('python versions (no versions specified)')
   .get('/pyversions/pyshp/1.2.12.json')
-  .expectJSONTypes({ name: 'python', value: 'not found' });
+  .expectJSON({ name: 'python', value: 'not found' });
 
 t.create('python versions (invalid)')
   .get('/pyversions/not-a-package.json')
-  .expectJSONTypes({ name: 'pypi', value: 'invalid' });
+  .expectJSON({ name: 'pypi', value: 'invalid' });
 
 
 // tests for implementation endpoint
 
 t.create('implementation (valid, package version in request)')
   .get('/implementation/beehive/1.0.json')
-  .expectJSONTypes({ name: 'implementation', value: 'cpython, jython, pypy' });
+  .expectJSON({ name: 'implementation', value: 'cpython, jython, pypy' });
 
 t.create('implementation (valid, no package version specified)')
   .get('/implementation/numpy.json')
-  .expectJSONTypes({ name: 'implementation', value: 'cpython' });
+  .expectJSON({ name: 'implementation', value: 'cpython' });
 
 t.create('implementation (not specified)')
   .get('/implementation/chai/1.1.2.json')
-  .expectJSONTypes({ name: 'implementation', value: 'cpython' });
+  .expectJSON({ name: 'implementation', value: 'cpython' });
 
 t.create('implementation (invalid)')
   .get('/implementation/not-a-package.json')
-  .expectJSONTypes({ name: 'pypi', value: 'invalid' });
+  .expectJSON({ name: 'pypi', value: 'invalid' });
 
 
 // tests for status endpoint
 
 t.create('status (valid, stable, package version in request)')
   .get('/status/django/1.11.json')
-  .expectJSONTypes({name: 'status', value: 'stable' });
+  .expectJSON({name: 'status', value: 'stable' });
 
 t.create('status (valid, no package version specified)')
   .get('/status/typing.json')
-  .expectJSONTypes({name: 'status', value: 'stable' });
+  .expectJSON({name: 'status', value: 'stable' });
 
 t.create('status (valid, beta)')
   .get('/status/django/2.0rc1.json')
-  .expectJSONTypes({ name: 'status', value: 'beta' });
+  .expectJSON({ name: 'status', value: 'beta' });
 
 t.create('status (invalid)')
   .get('/status/not-a-package.json')
-  .expectJSONTypes({ name: 'pypi', value: 'invalid' });
+  .expectJSON({ name: 'pypi', value: 'invalid' });
