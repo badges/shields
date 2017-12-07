b1ca6391d0d0204505bb942034fd94b91eea78c9
Krutov Alexander <goozler@users.noreply.github.com>
2017-12-08 02:30:42 +0400

    Add MicroBadger (#1340)

--- server.js
+++ server.js
@@ -6528,6 +6528,76 @@ cache(function(data, match, sendBadge, request) {
   });
 }));
 
+// MicroBadger integration.
+camp.route(/^\/microbadger\/(image-size|layers)\/([^/]+)\/([^/]+)\/?([^/]*)\.(svg|png|gif|jpg|json)$/,
+cache(function(data, match, sendBadge, request) {
+  const type = match[1];
+  let user = match[2];
+  const repo = match[3];
+  const tag = match[4];
+  const format = match[5];
+  if (user === '_') {
+    user = 'library';
+  }
+  const url = `https://api.microbadger.com/v1/images/${user}/${repo}`;
+
+  let badgeData = getBadgeData(type, data);
+  if (type === 'image-size') {
+    badgeData.text[0] = getLabel('image size', data);
+  }
+
+  const options = {
+    method: 'GET',
+    uri: url,
+    headers: {
+      'Accept': 'application/json'
+    }
+  };
+  request(options, function(err, res, buffer) {
+    if (res && res.statusCode === 404) {
+      badgeData.text[1] = 'not found';
+      sendBadge(format, badgeData);
+      return;
+    }
+
+    if (err != null || !res || res.statusCode !== 200) {
+      badgeData.text[1] = 'inaccessible';
+      sendBadge(format, badgeData);
+      return;
+    }
+
+    try {
+      const parsedData = JSON.parse(buffer);
+      let image;
+
+      if (tag) {
+        image = parsedData.Versions && parsedData.Versions.find(v => v.Tags.some(t => t.tag === tag));
+        if (!image) {
+          badgeData.text[1] = 'not found';
+          sendBadge(format, badgeData);
+          return;
+        }
+      } else {
+        image = parsedData;
+      }
+
+      if (type === 'image-size') {
+        const size = prettyBytes(parseInt(image.DownloadSize));
+        badgeData.text[1] = size;
+      } else if (type === 'layers') {
+        badgeData.text[1] = image.LayerCount;
+      }
+      badgeData.colorscheme = null;
+      badgeData.colorB = '#007ec6';
+      sendBadge(format, badgeData);
+    } catch(e) {
+      badgeData.colorscheme = 'red';
+      badgeData.text[1] = 'error';
+      sendBadge(format, badgeData);
+    }
+  });
+}));
+
 // Gitter room integration.
 camp.route(/^\/gitter\/room\/([^/]+\/[^/]+)\.(svg|png|gif|jpg|json)$/,
 cache(function(data, match, sendBadge, request) {
