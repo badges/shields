73d05da7f31f09fdaebf3b1d254325e621d45286
Pravdomil <pravdomil.toman@gmail.com>
2017-12-12 04:16:53 +0100

    space

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -7,7 +7,7 @@ const Footer = ({ baseUri }) => (
     <h2 id="like-this">Like This?</h2>
 
     <p>
-      What is your favorite badge service to use?<br/>
+      What is your favorite badge service to use?<br />
       <a href="https://github.com/badges/shields/issues/new">Tell us</a> and we might bring it to you!
     </p>
     <p>
