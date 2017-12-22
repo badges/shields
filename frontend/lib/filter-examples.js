9328cfd1763f152a09dc06100ffbdcfcc0d7e280
Paul Melnikow <email@paulmelnikow.com>
2017-12-22 09:54:35 -0500

    Improve search performance
    This change applies a unique ID to each badge example, and uses that to key the DOM.
    
    Before this change, the key was the array index. While constant with the filter, it mutated as the filter changed, causing React to mutate the DOM very inefficiently. Badges were filtered out by mutating al the rows instead of simply removing the affected rows.
    
    I first tried a version that applied a class with `display: none` to the hidden elements. That worked; however the implementation has a bunch of indirection, and this works nearly as well.

--- frontend/lib/filter-examples.js
+++ frontend/lib/filter-examples.js
@@ -4,18 +4,34 @@ function exampleMatchesRegex(example, regex) {
   return regex.test(haystack);
 }
 
-export default function filterExamples(examples, query) {
-  if (! query) {
-    return examples;
+function predicateFromQuery(query) {
+  if (query) {
+    const regex = new RegExp(query, 'i'); // Case-insensitive.
+    return example => exampleMatchesRegex(example, regex);
+  } else {
+    return () => true;
   }
+}
 
-  const regex = new RegExp(query, 'i'); // Case-insensitive.
-
-  return examples
+export function mapExamples(categories, iteratee) {
+  return categories
     .map(({ category, examples }) => ({
       category,
-      examples: examples.filter(ex => exampleMatchesRegex(ex, regex)),
+      examples: iteratee(examples),
     }))
-    // Remove empty sections.
+    // Remove empty categories.
     .filter(({ category, examples }) => examples.length > 0);
+}
+
+// Assign each example a unique ID.
+export function prepareExamples(categories) {
+  let nextKey = 0;
+  return mapExamples(categories, examples => examples.map(example => Object.assign(example, {
+    key: nextKey++,
+  })));
+}
+
+export function filterExamples(categories, query) {
+  const predicate = predicateFromQuery(query);
+  return mapExamples(categories, examples => examples.filter(predicate));
 }
