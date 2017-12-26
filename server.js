92710a949644fc594c61df0baf35898b61f1903c
Paul Melnikow <email@paulmelnikow.com>
2017-12-25 22:59:32 -0500

    Check in profiling script and put it behind a flag

--- server.js
+++ server.js
@@ -7837,9 +7837,13 @@ function(data, match, end, ask) {
     if (isValidStyle(data.style)) {
       badgeData.template = data.style;
     }
-    console.time('makeBadge total');
+    if (config.profiling.makeBadge) {
+      console.time('makeBadge total');
+    }
     const svg = makeBadge(badgeData);
-    console.timeEnd('makeBadge total');
+    if (config.profiling.makeBadge) {
+      console.timeEnd('makeBadge total');
+    }
     makeSend(format, ask.res, end)(svg);
   } catch(e) {
     log.error(e.stack);
