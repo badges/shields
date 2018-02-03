005fb37601fb27c8aee4c876360fc1ff641a7234
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2018-02-03 20:54:31 +0100

    isPercentage supports decimal and integer values

--- service-tests/uptimerobot.js
+++ service-tests/uptimerobot.js
@@ -4,7 +4,7 @@ const Joi = require('joi');
 const ServiceTester = require('./runner/service-tester');
 
 const isUptimeStatus = Joi.string().regex(/^(paused|not checked yet|up|seems down|down)$/);
-const { isDecimalPercentage } = require('./helpers/validators');
+const { isPercentage } = require('./helpers/validators');
 const { invalidJSON } = require('./helpers/response-fixtures');
 
 const t = new ServiceTester({ id: 'uptimerobot', title: 'Uptime Robot' });
@@ -67,14 +67,14 @@ t.create('Uptime Robot: Percentage (valid)')
   .get('/ratio/m778918918-3e92c097147760ee39d02d36.json')
   .expectJSONTypes(Joi.object().keys({
     name: 'uptime',
-    value: isDecimalPercentage,
+    value: isPercentage,
   }));
 
 t.create('Uptime Robot: Percentage (valid, with numberOfDays param)')
   .get('/ratio/7/m778918918-3e92c097147760ee39d02d36.json')
   .expectJSONTypes(Joi.object().keys({
     name: 'uptime',
-    value: isDecimalPercentage,
+    value: isPercentage,
   }));
 
 t.create('Uptime Robot: Percentage (invalid, correct format)')
