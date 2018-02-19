57c72795406ac3dcea0effcedfbe773fa50320ca
Patrick Streule <pstreule@gmail.com>
2018-02-20 07:33:09 +1100

    [Bitbucket] #1509 - Add Bitbucket Pipelines service (#1510)
    * [Bitbucket] #1509 - Add Bitbucket Pipelines service
    
    * Use ES6 const and error helper

--- server.js
+++ server.js
@@ -4458,6 +4458,67 @@ cache(function(data, match, sendBadge, request) {
   });
 }));
 
+// Bitbucket Pipelines integration.
+camp.route(/^\/bitbucket\/pipelines\/([^/]+)\/([^/]+)(?:\/(.+))?\.(svg|png|gif|jpg|json)$/,
+cache(function(data, match, sendBadge, request) {
+  const user = match[1];  // eg, atlassian
+  const repo = match[2];  // eg, adf-builder-javascript
+  const branch = match[3] || 'master';  // eg, development
+  const format = match[4];
+  const apiUrl = 'https://api.bitbucket.org/2.0/repositories/'
+    + encodeURIComponent(user) + '/' + encodeURIComponent(repo)
+    + '/pipelines/?fields=values.state&page=1&pagelen=2&sort=-created_on'
+    + '&target.ref_type=BRANCH&target.ref_name=' + encodeURIComponent(branch);
+
+  const badgeData = getBadgeData('build', data);
+
+  request(apiUrl, function(err, res, buffer) {
+    if (checkErrorResponse(badgeData, err, res)) {
+      sendBadge(format, badgeData);
+      return;
+    }
+    try {
+      const data = JSON.parse(buffer);
+      if (!data.values) {
+        throw Error('Unexpected response');
+      }
+      const values = data.values.filter(value => value.state && value.state.name === 'COMPLETED');
+      if (values.length > 0) {
+        switch (values[0].state.result.name) {
+          case 'SUCCESSFUL':
+            badgeData.text[1] = 'passing';
+            badgeData.colorscheme = 'brightgreen';
+            break;
+          case 'FAILED':
+            badgeData.text[1] = 'failing';
+            badgeData.colorscheme = 'red';
+            break;
+          case 'ERROR':
+            badgeData.text[1] = 'error';
+            badgeData.colorscheme = 'red';
+            break;
+          case 'STOPPED':
+            badgeData.text[1] = 'stopped';
+            badgeData.colorscheme = 'yellow';
+            break;
+          case 'EXPIRED':
+            badgeData.text[1] = 'expired';
+            badgeData.colorscheme = 'yellow';
+            break;
+          default:
+            badgeData.text[1] = 'unknown';
+        }
+      } else {
+        badgeData.text[1] = 'never built';
+      }
+      sendBadge(format, badgeData);
+    } catch(e) {
+      badgeData.text[1] = 'invalid';
+      sendBadge(format, badgeData);
+    }
+  });
+}));
+
 // Chef cookbook integration.
 camp.route(/^\/cookbook\/v\/(.*)\.(svg|png|gif|jpg|json)$/,
 cache(function(data, match, sendBadge, request) {
