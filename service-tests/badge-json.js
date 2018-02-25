33560502d68424c8dbd6d25e3a99259f522761c3
Brad Kent <bkfake-github@yahoo.com>
2018-02-25 13:08:44 -0600

    additional unit tests, testing colorB

--- service-tests/badge-json.js
+++ service-tests/badge-json.js
@@ -6,6 +6,17 @@ const { invalidJSON } = require('./helpers/response-fixtures');
 
 const t = new ServiceTester({ id: 'badge-json', title: 'badge json', pathPrefix: '/json' });
 
+function genJsonResponse(obj) {
+  return function() {
+    return [
+      200,
+      JSON.stringify(obj),
+      { 'Content-Type': 'application/json' }
+    ];
+  };
+}
+
+
 module.exports = t;
 
 t.create('invalid urlencoding')
@@ -29,26 +40,54 @@ t.create('invalid badge json')
   .get('/http%3A%2F%2Ftest.com%2Fbadge.json.json')
   .intercept(nock => nock('http://test.com')
     .get('/badge.json')
-    .reply(function() {
-      return [
-        200,
-        '{}',
-        { 'Content-Type': 'application/json' }
-      ];
-    })
+    .reply(genJsonResponse({}))
   )
   .expectJSON({name: 'badge json', value: 'invalid badge-json'});
 
 t.create('label & value')
-  .get('/http%3A%2F%2Ftest.com%2Fbadge.json.json')
+  .get('/http%3A%2F%2Ftest.com%2Fbadge.json.json?style=_shields_test')
   .intercept(nock => nock('http://test.com')
     .get('/badge.json')
-    .reply(function() {
-      return [
-        200,
-        '{"label":"test","value":"success"}',
-        { 'Content-Type': 'application/json' }
-      ];
-    })
+    .reply(genJsonResponse({label:"test", value:"success"}))
   )
-  .expectJSON({name: 'test', value: 'success'});
+  .expectJSON({
+    name: 'test',
+    value: 'success',
+    colorB: '#9f9f9f'  // default
+  });
+
+t.create('label, value, & class')
+  .get('/http%3A%2F%2Ftest.com%2Fbadge.json.json?style=_shields_test')
+  .intercept(nock => nock('http://test.com')
+    .get('/badge.json')
+    .reply(genJsonResponse({label:"test", value:"success", valueClass:"success"}))
+  )
+  .expectJSON({
+    name: 'test',
+    value: 'success',
+    colorB: '#4c1'  // success
+  });
+
+ t.create('label, value, class, & colorB')
+  .get('/http%3A%2F%2Ftest.com%2Fbadge.json.json?style=_shields_test')
+  .intercept(nock => nock('http://test.com')
+    .get('/badge.json')
+    .reply(genJsonResponse({label:"test", value:"success", valueClass:"success", colorB:"#D00D00"}))
+  )
+  .expectJSON({
+    name: 'test',
+    value: 'success',
+    colorB: '#D00D00' // colorB override
+  });
+
+ t.create('label, value, class, colorB, & colorB param')
+  .get('/http%3A%2F%2Ftest.com%2Fbadge.json.json?style=_shields_test&colorB=C001E0')
+  .intercept(nock => nock('http://test.com')
+    .get('/badge.json')
+    .reply(genJsonResponse({label:"test", value:"success", valueClass:"success", colorB:"#D00D00"}))
+  )
+  .expectJSON({
+    name: 'test',
+    value: 'success',
+    colorB: '#C001E0' // colorB param
+  });
