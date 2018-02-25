e4633d32d6a45d70f2066a3a8236addef6c12070
Brad Kent <bkfake-github@yahoo.com>
2018-02-25 00:45:30 -0600

    remove unused var

--- service-tests/badge-json.js
+++ service-tests/badge-json.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const Joi = require('joi');
+// const Joi = require('joi');
 const ServiceTester = require('./runner/service-tester');
 const { invalidJSON } = require('./helpers/response-fixtures');
 
