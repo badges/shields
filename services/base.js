0a91a9dccd557191b398d401d08adae4d76861e6
Paul Melnikow <email@paulmelnikow.com>
2018-03-11 21:00:18 -0500

    Clean lint

--- services/base.js
+++ services/base.js
@@ -129,7 +129,7 @@ module.exports = class BaseService {
   }
 
   static register(camp, handleRequest) {
-    const serviceClass = this; // In a static context, "this" is the class.
+    const ServiceClass = this; // In a static context, "this" is the class.
 
     camp.route(this._regex,
     handleRequest(async (queryParams, match, sendBadge, request) => {
@@ -137,7 +137,7 @@ module.exports = class BaseService {
 
       try {
         const namedParams = this._namedParamsForMatch(match);
-        const serviceInstance = new serviceClass({
+        const serviceInstance = new ServiceClass({
           sendAndCacheRequest: request.asPromise,
         });
         serviceData = await serviceInstance.handle(namedParams);
