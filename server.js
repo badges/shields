a497ccabf7959baf214f2ff484e4a6a6f89287fe
Darren Labithiotis <labithiotis@users.noreply.github.com>
2018-03-19 23:23:13 +0000

    [dynamic-xml dynamic-json] Set request options for dynamic json badges. (#1575)
    * Set request options for dynamic json badges to include accept header of application/json and json:true
    
    * Added headers for XML dynamic badges
    
    * Added tests for dynamic badges to check correct Accept headers are set

--- server.js
+++ server.js
@@ -7468,6 +7468,7 @@ cache({
     var prefix = query.prefix || '';
     var suffix = query.suffix || '';
     var pathExpression = query.query;
+    var requestOptions = {};
 
     var badgeData = getBadgeData('custom badge', query);
 
@@ -7479,7 +7480,25 @@ cache({
     }
     var url = encodeURI(decodeURIComponent(query.url || query.uri));
 
-    request(url, (err, res, data) => {
+    switch (type) {
+      case 'json':
+        requestOptions = {
+          headers: {
+            Accept: 'application/json'
+          },
+          json: true
+        };
+        break;
+      case 'xml':
+        requestOptions = {
+          headers: {
+            Accept: 'application/xml, text/xml'
+          }
+        };
+        break;
+    }
+
+    request(url, requestOptions, (err, res, data) => {
       try {
         if (checkErrorResponse(badgeData, err, res, 'resource not found')) {
           return;
