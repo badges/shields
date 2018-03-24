6ce6259f9a5f5789f9b235da5948eb5584c96eae
Thaddee Tyl <thaddee.tyl@gmail.com>
2018-03-24 23:19:53 +0100

    [DEPLOY] Build index.html

---
+++ build/_next/676b8b9d-2f97-41fd-abc2-224f26fe5097/page/_error/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/_error', function() {
+            var comp = module.exports=webpackJsonp([3],[],[249]);
+            return { page: comp.default }
+          })
+        
