fdfa368bb5ee2f0b3f2aa117d1dca7fbe2850916
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2018-03-30 08:45:55 +0100

    Switch tests to yaml data source

--- services/yaml/yaml.tester.js
+++ services/yaml/yaml.tester.js
@@ -20,49 +20,49 @@ t.create('No URL specified')
   .expectJSON({ name: 'Package Name', value: 'no url specified', colorB: colorsB.red });
 
 t.create('No query specified')
-  .get('.json?url=https://github.com/badges/shields/raw/master/package.json&label=Package Name&style=_shields_test')
+  .get('.json?url=https://raw.githubusercontent.com/kubernetes/charts/568291d6e476c39ca8322c30c3f601d0383d4760/stable/coredns/Chart.yaml&label=Package Name&style=_shields_test')
   .expectJSON({ name: 'Package Name', value: 'no query specified', colorB: colorsB.red });
 
 t.create('YAML from url')
-  .get('.json?url=https://github.com/badges/shields/raw/master/package.json&query=$.name&style=_shields_test')
-  .expectJSON({ name: 'custom badge', value: 'gh-badges', colorB: colorsB.brightgreen });
+  .get('.json?url=https://raw.githubusercontent.com/kubernetes/charts/568291d6e476c39ca8322c30c3f601d0383d4760/stable/coredns/Chart.yaml&query=$.name&style=_shields_test')
+  .expectJSON({ name: 'custom badge', value: 'coredns', colorB: colorsB.brightgreen });
 
 t.create('YAML from uri (support uri query paramater)')
-  .get('.json?uri=https://github.com/badges/shields/raw/master/package.json&query=$.name&style=_shields_test')
-  .expectJSON({ name: 'custom badge', value: 'gh-badges', colorB: colorsB.brightgreen });
+  .get('.json?uri=https://raw.githubusercontent.com/kubernetes/charts/568291d6e476c39ca8322c30c3f601d0383d4760/stable/coredns/Chart.yaml&query=$.name&style=_shields_test')
+  .expectJSON({ name: 'custom badge', value: 'coredns', colorB: colorsB.brightgreen });
 
 t.create('YAML from url | multiple results')
-  .get('.json?url=https://github.com/badges/shields/raw/master/package.json&query=$..keywords[0:2:1]')
-  .expectJSON({ name: 'custom badge', value: 'GitHub, badge' });
+  .get('.json?url=https://raw.githubusercontent.com/kubernetes/charts/568291d6e476c39ca8322c30c3f601d0383d4760/stable/coredns/Chart.yaml&query=$..keywords[0:2:1]')
+  .expectJSON({ name: 'custom badge', value: 'coredns, dns' });
 
 t.create('YAML from url | caching with new query params')
-  .get('.json?url=https://github.com/badges/shields/raw/master/package.json&query=$.version')
+  .get('.json?url=https://raw.githubusercontent.com/kubernetes/charts/568291d6e476c39ca8322c30c3f601d0383d4760/stable/coredns/Chart.yaml&query=$.version')
   .expectJSONTypes(Joi.object().keys({
     name: 'custom badge',
     value: Joi.string().regex(/^\d+(\.\d+)?(\.\d+)?$/)
   }));
 
 t.create('YAML from url | with prefix & suffix & label')
-  .get('.json?url=https://github.com/badges/shields/raw/master/package.json&query=$.version&prefix=v&suffix= dev&label=Shields')
+  .get('.json?url=https://raw.githubusercontent.com/kubernetes/charts/568291d6e476c39ca8322c30c3f601d0383d4760/stable/coredns/Chart.yaml&query=$.version&prefix=v&suffix= dev&label=Shields')
   .expectJSONTypes(Joi.object().keys({
     name: 'Shields',
     value: Joi.string().regex(/^v\d+(\.\d+)?(\.\d+)?\sdev$/)
   }));
 
 t.create('YAML from url | object doesnt exist')
-  .get('.json?url=https://github.com/badges/shields/raw/master/package.json&query=$.does_not_exist&style=_shields_test')
+  .get('.json?url=https://raw.githubusercontent.com/kubernetes/charts/568291d6e476c39ca8322c30c3f601d0383d4760/stable/coredns/Chart.yaml&query=$.does_not_exist&style=_shields_test')
   .expectJSON({ name: 'custom badge', value: 'no result', colorB: colorsB.lightgrey });
 
 t.create('YAML from url | invalid url')
-  .get('.json?url=https://github.com/badges/shields/raw/master/notafile.json&query=$.version&style=_shields_test')
+  .get('.json?url=https://raw.githubusercontent.com/kubernetes/charts/568291d6e476c39ca8322c30c3f601d0383d4760/stable/coredns/notafile.yaml&query=$.version&style=_shields_test')
   .expectJSON({ name: 'custom badge', value: 'resource not found', colorB: colorsB.lightgrey });
 
 t.create('YAML from url | user color overrides default')
-  .get('.json?url=https://github.com/badges/shields/raw/master/package.json&query=$.name&colorB=10ADED&style=_shields_test')
-  .expectJSON({ name: 'custom badge', value: 'gh-badges', colorB: '#10ADED' });
+  .get('.json?url=https://raw.githubusercontent.com/kubernetes/charts/568291d6e476c39ca8322c30c3f601d0383d4760/stable/coredns/Chart.yaml&query=$.name&colorB=10ADED&style=_shields_test')
+  .expectJSON({ name: 'custom badge', value: 'coredns', colorB: '#10ADED' });
 
 t.create('YAML from url | error color overrides default')
-  .get('.json?url=https://github.com/badges/shields/raw/master/notafile.json&query=$.version&style=_shields_test')
+  .get('.json?url=https://raw.githubusercontent.com/kubernetes/charts/568291d6e476c39ca8322c30c3f601d0383d4760/stable/coredns/notafile.yaml&query=$.version&style=_shields_test')
   .expectJSON({ name: 'custom badge', value: 'resource not found', colorB: colorsB.lightgrey });
 
 t.create('YAML from url | error color overrides user specified')
