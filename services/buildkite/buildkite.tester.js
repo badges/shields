1f5e3a1cd4cf5ea1bf88cca0b2da0f79ed886efc
Raul Riera <rieraraul@gmail.com>
2018-04-22 21:25:59 -0400

    Improve the tests

--- services/buildkite/buildkite.tester.js
+++ services/buildkite/buildkite.tester.js
@@ -1,24 +1,13 @@
 'use strict';
 
-const Joi = require('joi');
 const ServiceTester = require('../service-tester');
-const isBuildStatus = Joi.string().regex(/^(passing|failing|unknown)$/);
-
 const t = new ServiceTester({ id: 'buildkite', title: 'Buildkite Builds' });
 module.exports = t;
 
-// automated build endpoint
+t.create('buildkite invalid pipeline')
+  .get('/unknown-identifier/unknown-branch.json')
+  .expectJSON({ name: 'buildkite', value: 'invalid' });
 
-t.create('buildkite branch build (valid, branch)')
-  .get('/_/_.json')
-  .expectJSONTypes(Joi.object().keys({
-    name: 'Buildkite',
-    value: isBuildStatus
-  }));
-
-t.create('buildkite branch build with label (valid, branch, label)')
-  .get('/_/_/.json?label=custom%20label')
-  .expectJSONTypes(Joi.object().keys({
-    name: 'custom label',
-    value: isBuildStatus
-  }));
+t.create('buildkite valid pipeline')
+  .get('/3826789cf8890b426057e6fe1c4e683bdf04fa24d498885489/master.json')
+  .expectJSON({ name: 'buildkite', value: 'Passing' });
