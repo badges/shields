223510b3f80b4a4e0b9aafa3067c2ba64632f10c
Sam Lanning <sam@samlanning.com>
2018-05-04 15:24:37 -0700

    Address comments on #1672

--- services/lgtm/lgtm.tester.js
+++ services/lgtm/lgtm.tester.js
@@ -15,10 +15,31 @@ t.create('total alerts for a project')
 
 t.create('missing project')
   .get('/alerts/g/some-org/this-project-doesnt-exist.json')
-  .expectJSONTypes(Joi.object().keys({
+  .expectJSON({
     name: 'lgtm',
     value: 'project not found'
-  }));
+  });
+
+t.create('no alerts')
+  .get('/alerts/g/apache/cloudstack.json')
+  .intercept(nock => nock('https://lgtm.com')
+    .get('/api/v0.1/project/g/apache/cloudstack/details')
+    .reply(200, {alerts: 0}))
+  .expectJSON({ name: 'lgtm', value: '0 alerts' });
+
+t.create('single alert')
+  .get('/alerts/g/apache/cloudstack.json')
+  .intercept(nock => nock('https://lgtm.com')
+    .get('/api/v0.1/project/g/apache/cloudstack/details')
+    .reply(200, {alerts: 1}))
+  .expectJSON({ name: 'lgtm', value: '1 alert' });
+
+t.create('multiple alerts')
+  .get('/alerts/g/apache/cloudstack.json')
+  .intercept(nock => nock('https://lgtm.com')
+    .get('/api/v0.1/project/g/apache/cloudstack/details')
+    .reply(200, {alerts: 123}))
+  .expectJSON({ name: 'lgtm', value: '123 alerts' });
 
 t.create('json missing alerts')
   .get('/alerts/g/apache/cloudstack.json')
