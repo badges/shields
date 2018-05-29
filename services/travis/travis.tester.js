f93b9f9d4bcb764303b05817e4fb5a6da368b841
Ivan de la Beldad Fernandez <ivandelabeldad@gmail.com>
2018-05-29 16:13:19 +0200

    add travis-ci.com tests

--- services/travis/travis.tester.js
+++ services/travis/travis.tester.js
@@ -39,6 +39,38 @@ t.create('connection error')
   .networkOff()
   .expectJSON({ name: 'build', value: 'inaccessible' });
 
+// Travis (.com) CI
+
+t.create('build status on default branch')
+  .get('/com/ivandelabeldad/rackian-gateway.json')
+  .expectJSONTypes(Joi.object().keys({
+    name: 'build',
+    value: Joi.equal('failing', 'passing', 'unknown')
+  }));
+
+t.create('build status on named branch')
+  .get('/com/ivandelabeldad/rackian-gateway.json')
+  .expectJSONTypes(Joi.object().keys({
+    name: 'build',
+    value: Joi.equal('failing', 'passing', 'unknown')
+  }));
+
+t.create('unknown repo')
+  .get('/com/this-repo/does-not-exist.json')
+  .expectJSON({ name: 'build', value: 'unknown' });
+
+t.create('missing content-disposition header')
+  .get('/com/foo/bar.json')
+  .intercept(nock => nock('https://api.travis-ci.com')
+    .head('/foo/bar.svg')
+    .reply(200))
+  .expectJSON({ name: 'build', value: 'invalid' });
+
+t.create('connection error')
+  .get('/com/foo/bar.json')
+  .networkOff()
+  .expectJSON({ name: 'build', value: 'inaccessible' });
+
 // PHP version from .travis.yml
 
 t.create('gets the package version of symfony')
