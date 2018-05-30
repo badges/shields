9d90bd1f9549b3c8b59ad8338a8acd92e1e36f3c
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2018-05-30 18:51:52 +0200

    [github] Commit merge status badge (#1709)
    * Basic version of commit-status badge added
    
    * Support for case with no common ancestor
    
    * Handle unknown branch
    
    * Service tests with error responses
    
    * Handle unexpected 404 responses from github
    
    * Branch is a base
    
    * Tests reordered
    
    * Using not the newest commit in tests
    
    * Test for checked commit identical with the newest commit in branch
    
    * Code refactoring
    
    * Example for Github commit merge status

--- server.js
+++ server.js
@@ -4272,6 +4272,47 @@ cache(function(data, match, sendBadge, request) {
   });
 }));
 
+// GitHub commit status integration.
+camp.route(/^\/github\/commit-status\/([^/]+)\/([^/]+)\/([^/]+)\/([^/]+)\.(svg|png|gif|jpg|json)$/,
+cache(function(data, match, sendBadge, request) {
+  const [, user, repo, branch, commit, format] = match;
+  const apiUrl = `${githubApiUrl}/repos/${user}/${repo}/compare/${branch}...${commit}`;
+  const badgeData = getBadgeData('commit status', data);
+  githubAuth.request(request, apiUrl, {}, function(err, res, buffer) {
+    if (checkErrorResponse(badgeData, err, res, 'commit or branch not found')) {
+      if (res && res.statusCode === 404) {
+        try {
+          if (JSON.parse(buffer).message.startsWith('No common ancestor between')) {
+            badgeData.text[1] = 'no common ancestor';
+            badgeData.colorscheme = 'lightgrey';
+          }
+        } catch(e) {
+          badgeData.text[1] = 'invalid';
+          badgeData.colorscheme = 'lightgrey';
+        }
+      }
+      sendBadge(format, badgeData);
+      return;
+    }
+    try {
+      const parsedData = JSON.parse(buffer);
+      const isInBranch = parsedData.status === 'identical' || parsedData.status === 'behind';
+      if (isInBranch) {
+        badgeData.text[1] = `in ${branch}`;
+        badgeData.colorscheme = 'brightgreen';
+      } else {
+        // status: ahead or diverged
+        badgeData.text[1] = `not in ${branch}`;
+        badgeData.colorscheme = 'yellow';
+      }
+      sendBadge(format, badgeData);
+    } catch(e) {
+      badgeData.text[1] = 'invalid';
+      sendBadge(format, badgeData);
+    }
+  });
+}));
+
 // Bitbucket issues integration.
 camp.route(/^\/bitbucket\/issues(-raw)?\/([^/]+)\/([^/]+)\.(svg|png|gif|jpg|json)$/,
 cache(function(data, match, sendBadge, request) {
