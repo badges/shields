b62aa778b0bfbe9fe75460a759743d1d43c614fb
RedSparr0w <admin@redsparr0w.com>
2018-06-03 10:13:18 +1200

    [packagist] phpLatestVersion -> latestVersion

--- server.js
+++ server.js
@@ -1564,9 +1564,9 @@ cache(function(data, match, sendBadge, request) {
       switch (info) {
       case 'v':
         var stableVersions = versions.filter(phpStableVersion);
-        var stableVersion = phpLatestVersion(stableVersions);
+        var stableVersion = latestVersion(stableVersions);
         if (!stableVersion) {
-          stableVersion = phpLatestVersion(versions);
+          stableVersion = latestVersion(versions);
         }
         //if (!!aliasesMap[stableVersion]) {
         //  stableVersion = aliasesMap[stableVersion];
@@ -1575,7 +1575,7 @@ cache(function(data, match, sendBadge, request) {
         badgeColor = versionColor(stableVersion);
         break;
       case 'vpre':
-        var unstableVersion = phpLatestVersion(versions);
+        var unstableVersion = latestVersion(versions);
         //if (!!aliasesMap[unstableVersion]) {
         //  unstableVersion = aliasesMap[unstableVersion];
         //}
