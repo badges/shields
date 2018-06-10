b418235c7faa2bb93ea07c4188df917d61d917d7
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2018-06-10 21:36:40 +0200

    Standard name in nuget v2 downloads badge (#1726)

--- services/chocolatey/chocolatey.tester.js
+++ services/chocolatey/chocolatey.tester.js
@@ -24,18 +24,18 @@ module.exports = t;
 t.create('total downloads (valid)')
   .get('/dt/scriptcs.json')
   .expectJSONTypes(Joi.object().keys({
-    name: 'chocolatey',
+    name: 'downloads',
     value: isMetric,
   }));
 
 t.create('total downloads (not found)')
   .get('/dt/not-a-real-package.json')
-  .expectJSON({name: 'chocolatey', value: 'not found'});
+  .expectJSON({name: 'downloads', value: 'not found'});
 
 t.create('total downloads (connection error)')
   .get('/dt/scriptcs.json')
   .networkOff()
-  .expectJSON({name: 'chocolatey', value: 'inaccessible'});
+  .expectJSON({name: 'downloads', value: 'inaccessible'});
 
 t.create('total downloads (unexpected response)')
   .get('/dt/scriptcs.json')
@@ -43,7 +43,7 @@ t.create('total downloads (unexpected response)')
     .get("/api/v2/Packages()?$filter=Id%20eq%20%27scriptcs%27%20and%20IsLatestVersion%20eq%20true")
     .reply(invalidJSON)
   )
-  .expectJSON({name: 'chocolatey', value: 'invalid'});
+  .expectJSON({name: 'downloads', value: 'invalid'});
 
 
 // version
--- services/resharper/resharper.tester.js
+++ services/resharper/resharper.tester.js
@@ -24,18 +24,18 @@ module.exports = t;
 t.create('total downloads (valid)')
   .get('/dt/ReSharper.Nuke.json')
   .expectJSONTypes(Joi.object().keys({
-    name: 'resharper',
+    name: 'downloads',
     value: isMetric,
   }));
 
 t.create('total downloads (not found)')
   .get('/dt/not-a-real-package.json')
-  .expectJSON({name: 'resharper', value: 'not found'});
+  .expectJSON({name: 'downloads', value: 'not found'});
 
 t.create('total downloads (connection error)')
   .get('/dt/ReSharper.Nuke.json')
   .networkOff()
-  .expectJSON({name: 'resharper', value: 'inaccessible'});
+  .expectJSON({name: 'downloads', value: 'inaccessible'});
 
 t.create('total downloads (unexpected response)')
   .get('/dt/ReSharper.Nuke.json')
@@ -43,7 +43,7 @@ t.create('total downloads (unexpected response)')
     .get("/api/v2/Packages()?$filter=Id%20eq%20%27ReSharper.Nuke%27%20and%20IsLatestVersion%20eq%20true")
     .reply(invalidJSON)
   )
-  .expectJSON({name: 'resharper', value: 'invalid'});
+  .expectJSON({name: 'downloads', value: 'invalid'});
 
 
 // version
