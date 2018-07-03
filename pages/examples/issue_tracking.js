d4ae8d6df4717c31a6bf8595f825f60986053477
chris48s <chris.shaw480@gmail.com>
2018-07-03 22:20:26 +0100

    move all the issues/pull requests badges to a 'issue tracking' category

---
+++ pages/examples/issue_tracking.js
@@ -0,0 +1,10 @@
+import ExamplesPage from '../../frontend/components/examples-page';
+
+export default class IssueTrackingExamplePage extends ExamplesPage {
+
+  constructor(props) {
+    super(props);
+    this.preparedExamples = this.prepareExamples('issue_tracking');
+  }
+
+}
