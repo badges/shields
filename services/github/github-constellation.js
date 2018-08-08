d342690f08482e969136a8afed58363e6e819605
Paul Melnikow <email@paulmelnikow.com>
2018-08-08 18:25:25 -0400

    Run prettier

--- services/github/github-constellation.js
+++ services/github/github-constellation.js
@@ -1,55 +1,55 @@
-'use strict';
+'use strict'
 
-const path = require('path');
-const githubAuth = require('../../lib/github-auth');
-const serverSecrets = require('../../lib/server-secrets');
-const log = require('../../lib/log');
-const GithubApiProvider = require('./github-api-provider');
-const { setRoutes: setAdminRoutes } = require('./auth/admin');
+const path = require('path')
+const githubAuth = require('../../lib/github-auth')
+const serverSecrets = require('../../lib/server-secrets')
+const log = require('../../lib/log')
+const GithubApiProvider = require('./github-api-provider')
+const { setRoutes: setAdminRoutes } = require('./auth/admin')
 
 // Convenience class with all the stuff related to the Github API and its
 // authorization tokens, to simplify server initialization.
 class GithubConstellation {
   constructor(config) {
-    this._debugEnabled = config.service.debug.enabled;
-    this._debugIntervalSeconds = config.service.debug.intervalSeconds;
+    this._debugEnabled = config.service.debug.enabled
+    this._debugIntervalSeconds = config.service.debug.intervalSeconds
     this._userTokensPath = path.resolve(
       config.persistence.dir,
       'github-user-tokens.json'
-    );
+    )
 
-    const baseUrl = process.env.GITHUB_URL || 'https://api.github.com';
-    this.apiProvider = new GithubApiProvider({ baseUrl });
+    const baseUrl = process.env.GITHUB_URL || 'https://api.github.com'
+    this.apiProvider = new GithubApiProvider({ baseUrl })
   }
 
   scheduleDebugLogging() {
     if (this._debugEnabled) {
       this.debugInterval = setInterval(() => {
-        log(githubAuth.serializeDebugInfo());
-      }, 1000 * this._debugIntervalSeconds);
+        log(githubAuth.serializeDebugInfo())
+      }, 1000 * this._debugIntervalSeconds)
     }
   }
 
   async initialize(server) {
-    this.scheduleDebugLogging();
+    this.scheduleDebugLogging()
 
-    githubAuth.scheduleAutosaving(this._userTokensPath);
+    githubAuth.scheduleAutosaving(this._userTokensPath)
     // TODO Catch errors and send them to Sentry.
 
-    setAdminRoutes(server);
+    setAdminRoutes(server)
 
     if (serverSecrets && serverSecrets.gh_client_id) {
-      githubAuth.setRoutes(server);
+      githubAuth.setRoutes(server)
     }
   }
 
   stop() {
-    githubAuth.cancelAutosaving();
+    githubAuth.cancelAutosaving()
     if (this.debugInterval) {
-      clearInterval(this.debugInterval);
-      this.debugInterval = undefined;
+      clearInterval(this.debugInterval)
+      this.debugInterval = undefined
     }
   }
 }
 
-module.exports = GithubConstellation;
+module.exports = GithubConstellation
