c7bdf1ff96b5e04fea09e319c4f1779e7ce66cf1
Paul Melnikow <email@paulmelnikow.com>
2018-08-10 23:15:34 -0400

    Improve this test

--- services/npm/npm-type-definitions.spec.js
+++ services/npm/npm-type-definitions.spec.js
@@ -3,32 +3,22 @@
 const { test, given, forCases } = require('sazerac')
 const NpmTypeDefinitions = require('./npm-type-definitions.service')
 
-const transformAndRender = json =>
-  NpmTypeDefinitions.render(NpmTypeDefinitions.transform(json))
-
-describe.only('NPM type definitions badge', function() {
-  test(transformAndRender, () => {
+describe('NPM type definitions badge', function() {
+  test(NpmTypeDefinitions.transform, () => {
     forCases([
       given({ devDependencies: { typescript: '^2.4.7' } }),
       given({ devDependencies: {}, types: 'types/index.d.ts' }),
       given({ devDependencies: {}, types: 'types/index.d.ts' }),
-    ]).expect({
-      message: 'TypeScript',
-      color: 'blue',
-    })
+    ]).expect({ supportedLanguages: ['TypeScript'] })
+
     given({ devDependencies: { 'flow-bin': '1.2.3' } }).expect({
-      message: 'Flow',
-      color: 'blue',
+      supportedLanguages: ['Flow'],
     })
+
     given({
       devDependencies: { 'flow-bin': '1.2.3', typescript: '^2.4.7' },
-    }).expect({
-      message: 'TypeScript | Flow',
-      color: 'blue',
-    })
-    given({ devDependencies: {} }).expect({
-      message: 'none',
-      color: 'lightgray',
-    })
+    }).expect({ supportedLanguages: ['TypeScript', 'Flow'] })
+
+    given({ devDependencies: {} }).expect({ supportedLanguages: [] })
   })
 })
