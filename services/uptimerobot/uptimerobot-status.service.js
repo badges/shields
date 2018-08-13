0c703c11b5c132a82a9ee14ae5d752e55545c887
Paul Melnikow <github@paulmelnikow.com>
2018-08-12 21:47:30 -0700

    Rewrite [uptimerobot] (#1891)

---
+++ services/uptimerobot/uptimerobot-status.service.js
@@ -0,0 +1,51 @@
+'use strict'
+
+const UptimeRobotBase = require('./uptimerobot-base')
+
+module.exports = class UptimeRobotStatus extends UptimeRobotBase {
+  static get defaultBadgeData() {
+    return {
+      label: 'status',
+    }
+  }
+
+  static get url() {
+    return {
+      base: 'uptimerobot/status',
+      format: '(.*)',
+      capture: ['monitorApiKey'],
+    }
+  }
+
+  static get examples() {
+    return [
+      {
+        title: 'Uptime Robot status',
+        previewUrl: 'm778918918-3e92c097147760ee39d02d36',
+      },
+    ]
+  }
+
+  static async render({ status }) {
+    switch (status) {
+      case 0:
+        return { message: 'paused', color: 'yellow' }
+      case 1:
+        return { message: 'not checked yet', color: 'yellowgreen' }
+      case 2:
+        return { message: 'up', color: 'brightgreen' }
+      case 8:
+        return { message: 'seems down', color: 'orange' }
+      case 9:
+        return { message: 'down', color: 'red' }
+      default:
+        throw Error('Should not get here due to validation')
+    }
+  }
+
+  async handle({ monitorApiKey }) {
+    const { monitors } = await this.fetch({ monitorApiKey })
+    const { status } = monitors[0]
+    return this.constructor.render({ status })
+  }
+}
