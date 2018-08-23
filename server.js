9d12558b6607f84b4cff858d62d989f1a1b65f20
Paul Melnikow <email@paulmelnikow.com>
2018-08-23 17:34:00 -0400

    hexpm

--- server.js
+++ server.js
@@ -39,7 +39,6 @@ const {
   metric,
   starRating,
   addv: versionText,
-  maybePluralize,
   formatDate,
 } = require('./lib/text-formatters');
 const {
@@ -348,64 +347,6 @@ cache(function(data, match, sendBadge, request) {
       } else {
         // That request is incorrect.
         badgeData.text[1] = 'request unknown';
-        sendBadge(format, badgeData);
-      }
-    } catch(e) {
-      badgeData.text[1] = 'invalid';
-      sendBadge(format, badgeData);
-    }
-  });
-}));
-
-// Hex.pm integration.
-camp.route(/^\/hexpm\/([^/]+)\/(.*)\.(svg|png|gif|jpg|json)$/,
-cache(function(queryParams, match, sendBadge, request) {
-  const info = match[1];
-  const repo = match[2];  // eg, `httpotion`.
-  const format = match[3];
-  const apiUrl = 'https://hex.pm/api/packages/' + repo;
-  const badgeData = getBadgeData('hex', queryParams);
-  request(apiUrl, function(err, res, buffer) {
-    if (err != null) {
-      badgeData.text[1] = 'inaccessible';
-      sendBadge(format, badgeData);
-      return;
-    }
-    try {
-      const data = JSON.parse(buffer);
-      if (info.charAt(0) === 'd') {
-        badgeData.text[0] = getLabel('downloads', queryParams);
-        var downloads;
-        switch (info.charAt(1)) {
-          case 'w':
-            downloads = data.downloads.week;
-            badgeData.text[1] = metric(downloads) + '/week';
-            break;
-          case 'd':
-            downloads = data.downloads.day;
-            badgeData.text[1] = metric(downloads) + '/day';
-            break;
-          case 't':
-            downloads = data.downloads.all;
-            badgeData.text[1] = metric(downloads);
-            break;
-        }
-        badgeData.colorscheme = downloadCountColor(downloads);
-        sendBadge(format, badgeData);
-      } else if (info === 'v') {
-        const version = data.releases[0].version;
-        badgeData.text[1] = versionText(version);
-        badgeData.colorscheme = versionColor(version);
-        sendBadge(format, badgeData);
-      } else if (info == 'l') {
-        const license = (data.meta.licenses || []).join(', ');
-        badgeData.text[0] = getLabel(maybePluralize('license', data.meta.licenses), queryParams);
-        if (license == '') {
-          badgeData.text[1] = 'Unknown';
-        } else {
-          badgeData.text[1] = license;
-          badgeData.colorscheme = 'blue';
-        }
         sendBadge(format, badgeData);
       }
     } catch(e) {
