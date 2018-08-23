7cfce52674c17009a7c8288997a7099579311e72
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-08-24 00:10:29 +0200

    add new style F-Droid service
    - related: https://github.com/badges/shields/issues/1963#issuecomment-415559706

---
+++ services/f-droid/f-droid.service.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const BaseHTTPService = require('../base-http')
+const { addv: versionText } = require('../../lib/text-formatters')
+const { version: versionColor } = require('../../lib/color-formatters')
+const { InvalidResponse } = require('../errors')
+
+module.exports = class FDroid extends BaseHTTPService {
+  async fetch({ appId }) {
+    const url = `https://f-droid.org/en/packages/${appId}/`;
+    return this._requestHTTP({
+      url,
+      options: { },
+      errorMessages: {
+        404: 'application not found',
+      },
+    }).then(({ res, buffer }) => {
+      const website = buffer.toString();
+      const match = website.match(/<div\s[^>]*class="package-version-header"(?:\s[^>]*)?>[^<]*<a\s+name="([^:>]*)"(?:\s[^>]*)?>/);
+      if (!match) {
+        throw new InvalidResponse({
+          prettyMessage: 'fix this badge',
+          underlyingError: new Error("could not find version on website"),
+        })
+      }
+      return { version: match[1] };
+    });
+  }
+  
+  static render({ version }) {
+    return {
+      message: versionText(version),
+      color: versionColor(version),
+    }
+  }
+
+  async handle({ appId }) {
+    const result = await this.fetch({ appId })
+    return this.constructor.render(result)
+  }
+
+  // Metadata
+  static get defaultBadgeData() {
+    return { label: 'F-Droid' }
+  }
+  
+  static get category() {
+    return 'build'
+  }
+
+  static get url() {
+    return {
+      base: 'f-droid/version',
+      format: '(.+)',
+      capture: ['appId'],
+    }
+  }
+
+  static get examples() {
+    return [
+      {
+        previewUrl: 'wercker/go-wercker-api',
+      },
+      {
+        title: `${this.name} branch`,
+        previewUrl: 'wercker/go-wercker-api/master',
+      },
+    ]
+  }
+}
+
