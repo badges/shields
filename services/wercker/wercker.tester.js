e7899d12b6606ed25f0ac6f9c8ce77f4bf37dee3
Paul Melnikow <email@paulmelnikow.com>
2018-08-23 12:54:18 -0400

    Fix wercker service

--- services/wercker/wercker.tester.js
+++ services/wercker/wercker.tester.js
@@ -9,21 +9,21 @@ const t = createServiceTester()
 module.exports = t
 
 t.create('CI build status')
-  .get('/ci/wercker/go-wercker-api.json')
+  .get('/wercker/go-wercker-api.json')
   .expectJSONTypes(Joi.object().keys({ name: 'build', value: isBuildStatus }))
 
 t.create('CI build status (branch)')
-  .get('/ci/wercker/go-wercker-api/master.json')
+  .get('/wercker/go-wercker-api/master.json')
   .expectJSONTypes(Joi.object().keys({ name: 'build', value: isBuildStatus }))
 
 t.create('CI build status (old v1 API)')
-  .get('/ci/559e33c8e982fc615500b357.json')
+  .get('/559e33c8e982fc615500b357.json')
   .expectJSONTypes(Joi.object().keys({ name: 'build', value: isBuildStatus }))
 
 t.create('CI application not found')
-  .get('/ci/somerandomproject/thatdoesntexits.json')
+  .get('/somerandomproject/thatdoesntexits.json')
   .expectJSON({ name: 'build', value: 'application not found' })
 
 t.create('CI private application')
-  .get('/ci/wercker/blueprint.json')
+  .get('/wercker/blueprint.json')
   .expectJSON({ name: 'build', value: 'private application not supported' })
