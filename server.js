6f281cdf42f814d82b795319118059cab122bb60
Paul Melnikow <email@paulmelnikow.com>
2018-08-24 11:52:12 -0400

    magnum-ci

--- server.js
+++ server.js
@@ -333,14 +333,6 @@ cache(function(data, match, sendBadge, request) {
   });
 }));
 
-// Magnum CI integration - deprecated as of July 2018
-camp.route(/^\/magnumci\/ci\/([^/]+)(?:\/(.+))?\.(svg|png|gif|jpg|json)$/,
-cache(function(data, match, sendBadge, request) {
-  const format = match[3];
-  const badgeData = getDeprecatedBadge('magnum ci', data);
-  sendBadge(format, badgeData);
-}));
-
 // Maven-Central artifact version integration
 // (based on repo1.maven.org rather than search.maven.org because of #846)
 camp.route(/^\/maven-central\/v\/([^/]*)\/([^/]*)(?:\/([^/]*))?\.(svg|png|gif|jpg|json)$/,
