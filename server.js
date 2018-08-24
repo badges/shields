55eab74afb2043475a82fb6d8716e963ea98f9c1
Paul Melnikow <email@paulmelnikow.com>
2018-08-24 11:16:56 -0400

    github-last-commit

--- server.js
+++ server.js
@@ -344,40 +344,6 @@ cache(function(data, match, sendBadge, request) {
   });
 }));
 
-// GitHub last commit integration.
-camp.route(/^\/github\/last-commit\/([^/]+)\/([^/]+)(?:\/(.+))?\.(svg|png|gif|jpg|json)$/,
-cache(function(data, match, sendBadge, request) {
-  const user = match[1];  // eg, mashape
-  const repo = match[2];  // eg, apistatus
-  const branch = match[3];
-  const format = match[4];
-  let apiUrl = `/repos/${user}/${repo}/commits`;
-  if (branch) {
-    apiUrl += `?sha=${branch}`;
-  }
-  const badgeData = getBadgeData('last commit', data);
-  if (badgeData.template === 'social') {
-    badgeData.logo = getLogo('github', data);
-    badgeData.links = [`https://github.com/${user}/${repo}`];
-  }
-  githubApiProvider.request(request, apiUrl, {}, (err, res, buffer) => {
-    if (githubCheckErrorResponse(badgeData, err, res)) {
-      sendBadge(format, badgeData);
-      return;
-    }
-    try {
-      const parsedData = JSON.parse(buffer);
-      const commitDate = parsedData[0].commit.author.date;
-      badgeData.text[1] = formatDate(commitDate);
-      badgeData.colorscheme = ageColor(Date.parse(commitDate));
-      sendBadge(format, badgeData);
-    } catch(e) {
-      badgeData.text[1] = 'invalid';
-      sendBadge(format, badgeData);
-    }
-  });
-}));
-
 // GitHub languages integration.
 camp.route(/^\/github\/languages\/(top|count|code-size)\/([^/]+)\/([^/]+)\.(svg|png|gif|jpg|json)$/,
 cache(function(data, match, sendBadge, request) {
