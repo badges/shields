ee65e73d4669460b42bcfd938cb28375fb4bba9f
Paul Melnikow <email@paulmelnikow.com>
2018-08-24 09:24:52 -0400

    github-tag

--- server.js
+++ server.js
@@ -29,7 +29,6 @@ const { makeMakeBadgeFn } = require('./lib/make-badge');
 const { QuickTextMeasurer } = require('./lib/text-measurer');
 const suggest = require('./lib/suggest');
 const { licenseToColor } = require('./lib/licenses');
-const { latest: latestVersion } = require('./lib/version');
 const {
   versionReduction: phpVersionReduction,
   getPhpReleases,
@@ -349,37 +348,6 @@ cache(function(data, match, sendBadge, request) {
       }
     } catch(e) {
       badgeData.text[1] = 'invalid';
-      sendBadge(format, badgeData);
-    }
-  });
-}));
-
-// GitHub tag integration.
-camp.route(/^\/github\/tag(-?pre)?\/([^/]+)\/([^/]+)\.(svg|png|gif|jpg|json)$/,
-cache(function(data, match, sendBadge, request) {
-  const includePre = Boolean(match[1]);
-  const user = match[2];  // eg, expressjs/express
-  const repo = match[3];
-  const format = match[4];
-  const apiUrl = `/repos/${user}/${repo}/tags`;
-  let badgeData = getBadgeData('tag', data);
-  if (badgeData.template === 'social') {
-    badgeData.logo = getLogo('github', data);
-  }
-  githubApiProvider.request(request, apiUrl, {}, (err, res, buffer) => {
-    if (githubCheckErrorResponse(badgeData, err, res)) {
-      sendBadge(format, badgeData);
-      return;
-    }
-    try {
-      var data = JSON.parse(buffer);
-      var versions = data.map(function(e) { return e.name; });
-      var tag = latestVersion(versions, { pre: includePre });
-      badgeData.text[1] = versionText(tag);
-      badgeData.colorscheme = versionColor(tag);
-      sendBadge(format, badgeData);
-    } catch(e) {
-      badgeData.text[1] = 'none';
       sendBadge(format, badgeData);
     }
   });
