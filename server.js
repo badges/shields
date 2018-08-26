09380003777d5690172152aa78f806a72c0ab553
Paul Melnikow <email@paulmelnikow.com>
2018-08-25 20:40:18 -0400

    maven-metadata

--- server.js
+++ server.js
@@ -396,40 +396,6 @@ cache(function(data, match, sendBadge, request) {
   });
 }));
 
-// Maven metadata versioning integration.
-camp.route(/^\/maven-metadata\/v\/(https?)\/(.+\.xml)\.(svg|png|gif|jpg|json)$/,
-  cache(function (data, match, sendBadge, request) {
-    const [, scheme, hostAndPath, format] = match;
-    const metadataUri = `${scheme}://${hostAndPath}`;
-    request(metadataUri, (error, response, body) => {
-      const badge = getBadgeData('maven', data);
-      if (!error && response.statusCode >= 200 && response.statusCode < 300) {
-        try {
-          xml2js.parseString(body, (err, result) => {
-            if (err) {
-              badge.text[1] = 'error';
-              badge.colorscheme = 'red';
-              sendBadge(format, badge);
-            } else {
-              const version = result.metadata.versioning[0].versions[0].version.slice(-1)[0];
-              badge.text[1] = versionText(version);
-              badge.colorscheme = versionColor(version);
-              sendBadge(format, badge);
-            }
-          });
-        } catch (e) {
-          badge.text[1] = 'error';
-          badge.colorscheme = 'red';
-          sendBadge(format, badge);
-        }
-      } else {
-        badge.text[1] = 'error';
-        badge.colorscheme = 'red';
-        sendBadge(format, badge);
-      }
-    });
-}));
-
 // User defined sources - JSON response
 camp.route(/^\/badge\/dynamic\/(json|xml|yaml)\.(svg|png|gif|jpg|json)$/,
 cache({
