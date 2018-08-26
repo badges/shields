86db649cd2578bff3e52abdb682456092d9af430
Nicco Kunzmann <niccokunzmann@rambler.ru>
2018-08-26 18:48:44 +0200

    test the real f-droid website
    Getting results from the website is taking quite some time,
    thus a timeout is added.
    - related: https://github.com/badges/shields/pull/1965#discussion_r212831642

--- services/f-droid/f-droid.tester.js
+++ services/f-droid/f-droid.tester.js
@@ -2,13 +2,14 @@
 
 const ServiceTester = require('../service-tester')
 const t = new ServiceTester({ id: 'f-droid', title: 'F-Droid' })
+const Joi = require('joi')
 module.exports = t
 
 const manyVersions =
   '<span></span><span class="doctype">&lt;!DOCTYPE html&gt;</span><span>\n\t<ul class="package-versions-list">\n\t\t\t\n\t\t\t<li class="package-version">\n\t\t\t\t<div class="package-version-header">\n\t\t\t\t\t\n\t\t\t\t\t\n                                        <a name="0.19"></a>\n                                        <a name="19"></a>\n\t\t\t\t\t<b>Version 0.19</b> (19) - Added on 2018-08-23\n\t\t\t\t</div>\n\t\t\t\t<p class="package-version-requirement">\n\t\t\t\t\t\n\t\t\t\t\tThis version requires Android 5.0 or newer.\n\t\t\t\t</p>\n\n\t\t\t\t<p class="package-version-source">\n\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\tIt is built and signed by F-Droid, and guaranteed to correspond to <a href="https://f-droid.org/repo/org.pacien.tincapp_19_src.tar.gz">this source tarball</a>.\n\t\t\t\t\t\n\t\t\t\t</p>\n\n\t\t\t\t<p class="package-version-download">\n\t\t\t\t\t<a href="https://f-droid.org/repo/org.pacien.tincapp_19.apk">\n\t\t\t\t\t\tDownload APK\n\t\t\t\t\t</a>\n\t\t\t\t\t6.9 MiB\n\t\t\t\t\t<a href="https://f-droid.org/repo/org.pacien.tincapp_19.apk.asc">PGP Signature</a>\n\t\t\t\t</p>\n\n\t\t\t\t<div class="package-version-permissions">\n                    <details open="">\n                        <summary class="package-version-permissions-summary">\n                            Permissions\n                        </summary>\n                        <ul class="package-version-permissions-list">\n                            \n                            <li>\n                                android.permission.INTERNET\n                                \n                            </li>\n                            \n                            <li>\n                                android.permission.CAMERA\n                                \n                            </li>\n                            \n                        </ul>\n                    </details>\n\t\t\t\t</div>\n\n\t\t\t</li>\n\n\t\t\t\n\t\t\t<li class="package-version">\n\t\t\t\t<div class="package-version-header">\n\t\t\t\t\t\n\t\t\t\t\t\n                                        <a name="0.18"></a>\n                                        <a name="18"></a>\n\t\t\t\t\t<b>Version 0.18</b> (18) - Added on 2018-08-12\n\t\t\t\t</div>\n\t\t\t\t<p class="package-version-requirement">\n\t\t\t\t\t\n\t\t\t\t\tThis version requires Android 5.0 or newer.\n\t\t\t\t</p>\n\n\t\t\t\t<p class="package-version-source">\n\t\t\t\t\t\n\t\t\t\t\t\t\n\t\t\t\t\t\tIt is built and signed by F-Droid, and guaranteed to correspond to <a href="https://f-droid.org/repo/org.pacien.tincapp_18_src.tar.gz">this source tarball</a>.\n\t\t\t\t\t\n\t\t\t\t</p>\n\n\t\t\t\t<p class="package-version-download">\n\t\t\t\t\t<a href="https://f-droid.org/repo/org.pacien.tincapp_18.apk">\n\t\t\t\t\t\tDownload APK\n\t\t\t\t\t</a>\n\t\t\t\t\t6.7 MiB\n\t\t\t\t\t<a href="https://f-droid.org/repo/org.pacien.tincapp_18.apk.asc">PGP Signature</a>\n\t\t\t\t</p>\n\n\t\t\t\t<div class="package-version-permissions">\n                    <details>\n                        <summary class="package-version-permissions-summary">\n                            Permissions\n                        </summary>\n                        <ul class="package-version-permissions-list">\n                            \n                            <li>\n                                android.permission.INTERNET\n                                \n                            </li>\n                            \n                        </ul>\n                    </details>\n\t\t\t\t</div>\n\n\t\t\t</li>\n\n</ul></span>'
 
 t.create('Package is found')
-  .get('/version/org.pacien.tincapp.json')
+  .get('/v/org.pacien.tincapp.json')
   .intercept(nock =>
     nock('https://f-droid.org')
       .get('/en/packages/org.pacien.tincapp/')
@@ -17,7 +18,7 @@ t.create('Package is found')
   .expectJSON({ name: 'F-Droid', value: 'v0.19' })
 
 t.create('Package is not found')
-  .get('/version/org.pacien.tincapp.json')
+  .get('/v/org.pacien.tincapp.json')
   .intercept(nock =>
     nock('https://f-droid.org')
       .get('/en/packages/org.pacien.tincapp/')
@@ -26,21 +27,26 @@ t.create('Package is not found')
   .expectJSON({ name: 'F-Droid', value: 'app not found' })
 
 t.create('The api changed')
-  .get('/version/org.pacien.tincapp.json')
+  .get('/v/org.pacien.tincapp.json')
   .intercept(nock =>
     nock('https://f-droid.org')
       .get('/en/packages/org.pacien.tincapp/')
       .reply(200, '')
   )
-  .expectJSON({ name: 'F-Droid', value: 'fix this badge' }) // INVALID_API_RESPONSE
+  .expectJSON({ name: 'F-Droid', value: 'fix this badge' })
 
-t.create('The real api does not change')
-  .get('/version/org.pacien.tincapp.json')
-  .intercept(nock =>
+t.create('The real api did not change')
+  .get('/v/org.thosp.yourlocalweather.json')
+/*  .intercept(nock => // uncomment to test the test
     nock('https://f-droid.org')
       .get('/en/packages/org.pacien.tincapp/')
       .reply(200, '')
-  )
-  .expectJSON.property("value").to.not.equal("fix this badge")
+  )*/
+  .expectJSONTypes({name: Joi.string(), value: Joi.string()})
+  .timeout(10000)
+  .afterJSON(({name, value}) => {
+    expect(value).to.not.equal('fix this badge')
+    expect(value).to.not.equal('app not found')
+  })
 
 
