6ecc970e6d5ae406d16bae1c0a193bef025ce273
PyvesB <PyvesDev@gmail.com>
2018-08-28 22:31:22 +0100

    Moved word around

--- .github/probot.js
+++ .github/probot.js
@@ -5,7 +5,7 @@ on('pull_request.closed')
       context.payload.pull_request.head.ref.slice(0, 11) !== 'dependabot/'
   )
   .filter(context => context.payload.pull_request.base.ref === 'master')
-  .comment(`This pull request was merged to [{{ pull_request.base.ref }}]({{ repository.html_url }}/tree/{{ pull_request.base.ref }}) branch. Now this change is waiting for deployment, which will usually happen within a few days. Stay tuned by joining our #ops channel on [Discord](https://discordapp.com/invite/HjJCwm5)!
+  .comment(`This pull request was merged to [{{ pull_request.base.ref }}]({{ repository.html_url }}/tree/{{ pull_request.base.ref }}) branch. This change is now waiting for deployment, which will usually happen within a few days. Stay tuned by joining our #ops channel on [Discord](https://discordapp.com/invite/HjJCwm5)!
   
 After deployment changes are copied to [gh-pages]({{ repository.html_url }}/tree/gh-pages) branch. 
 
