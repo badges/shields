df30785c4be36dfee2ccd2a05e57d62751325ad4
Pyves <PyvesDev@gmail.com>
2018-08-29 09:28:34 +0100

    Improved deployment bot message (#1996)

--- .github/probot.js
+++ .github/probot.js
@@ -5,8 +5,9 @@ on('pull_request.closed')
       context.payload.pull_request.head.ref.slice(0, 11) !== 'dependabot/'
   )
   .filter(context => context.payload.pull_request.base.ref === 'master')
-  .comment(`This pull request was merged to [{{ pull_request.base.ref }}]({{ repository.html_url }}/tree/{{ pull_request.base.ref }}) branch. Now this change is waiting for deployment. 
-Deploys usually happen every few weeks. After deployment changes are copied to [gh-pages]({{ repository.html_url }}/tree/gh-pages) branch. 
+  .comment(`This pull request was merged to [{{ pull_request.base.ref }}]({{ repository.html_url }}/tree/{{ pull_request.base.ref }}) branch. This change is now waiting for deployment, which will usually happen within a few days. Stay tuned by joining our #ops channel on [Discord](https://discordapp.com/invite/HjJCwm5)!
+  
+After deployment changes are copied to [gh-pages]({{ repository.html_url }}/tree/gh-pages) branch. 
 
 This badge displays deployment status:
 ![](https://img.shields.io/github/commit-status/{{ repository.full_name }}/gh-pages/{{ pull_request.merge_commit_sha }}.svg?label=deploy%20status)`)
