7e1dc6ecd2d09dac13adcc653be790c7e89845ac
Thomas Démoulins <thomas.demoulins@enablon.com>
2018-09-04 00:25:53 +0200

    #1570 Add support for VSTS releases and support for named branches on VSTS builds

--- services/vso/vso.service.js
+++ services/vso/vso.service.js
@@ -4,24 +4,21 @@ const LegacyService = require('../legacy-service')
 const { fetchFromSvg } = require('../../lib/svg-badge-parser')
 const { makeBadgeData: getBadgeData } = require('../../lib/badge-data')
 
-// For Visual Studio Team Services build.
 module.exports = class Vso extends LegacyService {
   static registerLegacyRouteHandler({ camp, cache }) {
+    // For Visual Studio Team Services builds.
     camp.route(
-      /^\/vso\/build\/([^/]+)\/([^/]+)\/([^/]+)\.(svg|png|gif|jpg|json)$/,
+      /^\/vso\/build\/([^/]+)\/([^/]+)\/([^/]+)(?:\/(.+))?\.(svg|png|gif|jpg|json)$/,
       cache((data, match, sendBadge, request) => {
         const name = match[1] // User name
         const project = match[2] // Project ID, e.g. 953a34b9-5966-4923-a48a-c41874cfb5f5
         const build = match[3] // Build definition ID, e.g. 1
-        const format = match[4]
-        const url =
-          'https://' +
-          name +
-          '.visualstudio.com/DefaultCollection/_apis/public/build/definitions/' +
-          project +
-          '/' +
-          build +
-          '/badge'
+        const branch = match[4]
+        const format = match[5]
+        let url = `https://${name}.visualstudio.com/_apis/public/build/definitions/${project}/${build}/badge`
+        if (branch != null) {
+          url += `?branchName=${branch}`
+        }
         const badgeData = getBadgeData('build', data)
         fetchFromSvg(request, url, (err, res) => {
           if (err != null) {
@@ -33,6 +30,47 @@ module.exports = class Vso extends LegacyService {
             badgeData.text[1] = res.toLowerCase()
             if (res === 'succeeded') {
               badgeData.colorscheme = 'brightgreen'
+              badgeData.text[1] = 'passing'
+            } else if (res === 'partially succeeded') {
+              badgeData.colorscheme = 'orange'
+              badgeData.text[1] = 'passing'
+            } else if (res === 'failed') {
+              badgeData.colorscheme = 'red'
+              badgeData.text[1] = 'failing'
+            }
+            sendBadge(format, badgeData)
+          } catch (e) {
+            badgeData.text[1] = 'invalid'
+            sendBadge(format, badgeData)
+          }
+        })
+      })
+    )
+
+    // For Visual Studio Team Services releases.
+    camp.route(
+      /^\/vso\/release\/([^/]+)\/([^/]+)\/([^/]+)\/([^/]+)\.(svg|png|gif|jpg|json)$/,
+      cache((data, match, sendBadge, request) => {
+        const name = match[1] // User name
+        const project = match[2] // Project ID, e.g. 953a34b9-5966-4923-a48a-c41874cfb5f5
+        const release = match[3] // Release definition ID, e.g. 1
+        const environment = match[4] // Environment ID, e.g. 1
+        const format = match[5]
+        const url = `https://${name}.vsrm.visualstudio.com/_apis/public/release/badge/${project}/${release}/${environment}`
+        const badgeData = getBadgeData('deployment', data)
+        fetchFromSvg(request, url, (err, res) => {
+          if (err != null) {
+            badgeData.text[1] = 'inaccessible'
+            sendBadge(format, badgeData)
+            return
+          }
+          try {
+            badgeData.text[1] = res.toLowerCase()
+            if (res === 'succeeded') {
+              badgeData.colorscheme = 'brightgreen'
+              badgeData.text[1] = 'passing'
+            } else if (res === 'partially succeeded') {
+              badgeData.colorscheme = 'orange'
               badgeData.text[1] = 'passing'
             } else if (res === 'failed') {
               badgeData.colorscheme = 'red'
