13bd32a8e691374d4e3301b3e31422feff1ca071
TobiasRoeddiger <usdkr@student.kit.edu>
2018-09-05 22:54:57 +0200

    added more test cases

--- services/hockeyapp/hockeyapp.tester.js
+++ services/hockeyapp/hockeyapp.tester.js
@@ -2,9 +2,45 @@
 
 const createServiceTester = require('../create-service-tester')
 
+const { isVPlusTripleDottedVersion } = require('../test-validators')
+
 const t = createServiceTester()
 module.exports = t
 
-t.create('Invalid AppToken')
+t.create('Invalid AppToken or AppId')
 .get('hockeyapp/v/1234/1234.json')
-.expectJSONTypes({ name: 'version', value: 'invalid'})
+.expectJSONTypes({ name: 'hockeyapp', value: 'invalid'})
+
+t.create('Android App')
+.get('hockeyapp/v/15011995/30071996.json')
+.intercept(nock =>
+    nock('https://rink.hockeyapp.net')
+      .get(
+        '/api/2/apps/30071996/app_versions'
+      )
+      .reply(200, {
+        app_versions: [
+            { 
+                version: '24',
+                shortversion: '1.0',
+            }
+        ]
+      }))
+.expectJSONTypes({ name: 'hockeyapp', value: '1.0.24'})
+
+t.create('iOS App')
+.get('hockeyapp/v/15011995/30071996.json')
+.intercept(nock =>
+    nock('https://rink.hockeyapp.net')
+      .get(
+        '/api/2/apps/30071996/app_versions'
+      )
+      .reply(200, {
+        app_versions: [
+            { 
+                version: '1.0.24',
+                shortversion: '1.0',
+            }
+        ]
+      }))
+.expectJSONTypes({ name: 'hockeyapp', value: '1.0.24'})
