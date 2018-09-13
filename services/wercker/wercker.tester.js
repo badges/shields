2ca35492b6bd24bf5735c6796e7318528932c7b3
chris48s <chris.shaw480@gmail.com>
2018-09-13 21:22:50 +0100

    update service tests tutorial, affects [wercker]

--- services/wercker/wercker.tester.js
+++ services/wercker/wercker.tester.js
@@ -21,9 +21,27 @@ t.create('CI build status (old v1 API)')
   .expectJSONTypes(Joi.object().keys({ name: 'build', value: isBuildStatus }))
 
 t.create('CI application not found')
-  .get('/somerandomproject/thatdoesntexits.json')
+  .get('/some-project/that-doesnt-exist.json')
   .expectJSON({ name: 'build', value: 'application not found' })
 
 t.create('CI private application')
   .get('/wercker/blueprint.json')
   .expectJSON({ name: 'build', value: 'private application not supported' })
+
+t.create('CI passed (mocked)')
+  .get('/wercker/go-wercker-api.json?style=_shields_test')
+  .intercept(nock =>
+    nock('https://app.wercker.com/api/v3/applications/')
+      .get('/wercker/go-wercker-api/builds?limit=1')
+      .reply(200, [{ status: 'finished', result: 'passed' }])
+  )
+  .expectJSON({ name: 'build', value: 'passing', colorB: '#4c1' })
+
+t.create('CI failed (mocked)')
+  .get('/wercker/go-wercker-api.json?style=_shields_test')
+  .intercept(nock =>
+    nock('https://app.wercker.com/api/v3/applications/')
+      .get('/wercker/go-wercker-api/builds?limit=1')
+      .reply(200, [{ status: 'finished', result: 'failed' }])
+  )
+  .expectJSON({ name: 'build', value: 'failed', colorB: '#e05d44' })
