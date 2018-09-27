8e10a9ead83ace12d87171a3beb0f32638a633de
chris48s <chris.shaw480@gmail.com>
2018-09-27 20:46:29 +0100

    re-order and re-name tests for clarity

--- services/wercker/wercker.tester.js
+++ services/wercker/wercker.tester.js
@@ -8,28 +8,11 @@ const { isBuildStatus } = require('../test-validators')
 const t = createServiceTester()
 module.exports = t
 
-t.create('CI status by ID')
-  .get('/ci/559e33c8e982fc615500b357.json')
-  .expectJSONTypes(Joi.object().keys({ name: 'build', value: isBuildStatus }))
-
-t.create('CI status by ID (with branch)')
-  .get('/ci/559e33c8e982fc615500b357/master.json')
-  .expectJSONTypes(Joi.object().keys({ name: 'build', value: isBuildStatus }))
-
-t.create('CI status (no builds yet)')
-  .get('/ci/559e33c8e982fc615500b357.json')
-  .intercept(nock =>
-    nock('https://app.wercker.com/api/v3')
-      .get('/runs?applicationId=559e33c8e982fc615500b357&limit=1')
-      .reply(200, [])
-  )
-  .expectJSON({ name: 'build', value: 'no builds' })
-
-t.create('Build status by name')
+t.create('Build status')
   .get('/build/wercker/go-wercker-api.json')
   .expectJSONTypes(Joi.object().keys({ name: 'build', value: isBuildStatus }))
 
-t.create('Build status by name (with branch)')
+t.create('Build status (with branch)')
   .get('/build/wercker/go-wercker-api/master.json')
   .expectJSONTypes(Joi.object().keys({ name: 'build', value: isBuildStatus }))
 
@@ -37,7 +20,7 @@ t.create('Build status (application not found)')
   .get('/build/some-project/that-doesnt-exist.json')
   .expectJSON({ name: 'build', value: 'application not found' })
 
-t.create('Build status by name (private application)')
+t.create('Build status (private application)')
   .get('/build/wercker/blueprint.json')
   .expectJSON({ name: 'build', value: 'private application not supported' })
 
@@ -58,3 +41,20 @@ t.create('Build failed (mocked)')
       .reply(200, [{ status: 'finished', result: 'failed' }])
   )
   .expectJSON({ name: 'build', value: 'failed', colorB: '#e05d44' })
+
+t.create('CI status by ID')
+  .get('/ci/559e33c8e982fc615500b357.json')
+  .expectJSONTypes(Joi.object().keys({ name: 'build', value: isBuildStatus }))
+
+t.create('CI status by ID (with branch)')
+  .get('/ci/559e33c8e982fc615500b357/master.json')
+  .expectJSONTypes(Joi.object().keys({ name: 'build', value: isBuildStatus }))
+
+t.create('CI status by ID (no runs yet)')
+  .get('/ci/559e33c8e982fc615500b357.json')
+  .intercept(nock =>
+    nock('https://app.wercker.com/api/v3')
+      .get('/runs?applicationId=559e33c8e982fc615500b357&limit=1')
+      .reply(200, [])
+  )
+  .expectJSON({ name: 'build', value: 'no builds' })
