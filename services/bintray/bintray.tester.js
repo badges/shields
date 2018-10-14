7ddaa8710c96d4888299c733c0def7ff0d065ee6
Jowita <jowita.mielnicka@gmail.com>
2018-10-14 21:31:50 +0200

    bintray test version which hits the live server

--- services/bintray/bintray.tester.js
+++ services/bintray/bintray.tester.js
@@ -1,13 +1,24 @@
 'use strict'
 
+const Joi = require('joi')
 const createServiceTester = require('../create-service-tester')
 const { colorScheme } = require('../test-helpers')
+const { isVPlusTripleDottedVersion } = require('../test-validators')
 
 const t = createServiceTester()
 
 module.exports = t
 
 t.create('version')
+  .get('/v/asciidoctor/maven/asciidoctorj.json')
+  .expectJSONTypes(
+    Joi.object().keys({
+      name: 'bintray',
+      value: isVPlusTripleDottedVersion,
+    })
+  )
+
+t.create('version (mocked)')
   .get('/v/asciidoctor/maven/asciidoctorj.json?style=_shields_test')
   .intercept(nock =>
     nock('https://bintray.com')
