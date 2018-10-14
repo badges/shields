2bd03c4fb97487b40c35fcd5b675bb715341215f
odero <billyx5@gmail.com>
2018-10-15 00:07:43 +0300

    Fix RR4 hash links

--- frontend/components/badge-examples.js
+++ frontend/components/badge-examples.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { HashLink as Link } from 'react-router-hash-link'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import resolveBadgeUrl from '../lib/badge-url'
--- frontend/components/header.js
+++ frontend/components/header.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { HashLink as Link } from 'react-router-hash-link'
 import React from 'react'
 
 export default () => (
