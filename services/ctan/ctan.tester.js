599e7914eced1e209aa115b47f167e78c2b21c86
Przemo Nowaczyk <pnowaczyk.mail@gmail.com>
2018-10-19 16:54:01 +0200

    refactor ctan service

--- services/ctan/ctan.tester.js
+++ services/ctan/ctan.tester.js
@@ -1,19 +1,34 @@
 'use strict'
 
 const Joi = require('joi')
-const createServiceTester = require('../create-service-tester')
+const ServiceTester = require('../service-tester')
 const { colorScheme } = require('../test-helpers')
 const { isVPlusDottedVersionAtLeastOne } = require('../test-validators')
 
-const t = createServiceTester()
-
+const t = new ServiceTester({ id: 'ctan', title: 'ctan' })
 module.exports = t
 
 t.create('license')
   .get('/l/novel.json')
   .expectJSON({
     name: 'license',
-    value: 'lppl1.3c,ofl',
+    value: 'lppl1.3c, ofl',
+  })
+
+t.create('license missing')
+  .get('/l/novel.json')
+  .intercept(nock =>
+    nock('http://www.ctan.org')
+      .get('/json/pkg/novel')
+      .reply(200, {
+        version: {
+          number: 'notRelevant',
+        },
+      })
+  )
+  .expectJSON({
+    name: 'license',
+    value: 'missing',
   })
 
 t.create('version')
