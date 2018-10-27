edd715757578692926ce34476e45137c6d8f4c8d
Paul Melnikow <email@paulmelnikow.com>
2018-10-27 16:15:14 -0400

    Test OAuth done route

--- services/github/auth/acceptor.spec.js
+++ services/github/auth/acceptor.spec.js
@@ -5,6 +5,7 @@ const Camp = require('camp')
 const got = require('got')
 const queryString = require('query-string')
 const sinon = require('sinon')
+const nock = require('nock')
 const config = require('../../../lib/test-config')
 const serverSecrets = require('../../../lib/server-secrets')
 const acceptor = require('./acceptor')
@@ -13,45 +14,94 @@ const baseUri = `http://127.0.0.1:${config.port}`
 const fakeClientId = 'githubdabomb'
 
 describe('Github token acceptor', function() {
-  describe('OAuth start endpoint', function() {
-    let sandbox
-    beforeEach(function() {
-      sandbox = sinon.createSandbox()
-      sandbox.stub(serverSecrets, 'gh_client_id').value(fakeClientId)
+  let sandbox
+  beforeEach(function() {
+    sandbox = sinon.createSandbox()
+    sandbox.stub(serverSecrets, 'gh_client_id').value(fakeClientId)
+    sandbox.stub(serverSecrets, 'shieldsIps').value([])
+  })
+
+  afterEach(function() {
+    sandbox.restore()
+  })
+
+  let camp
+  beforeEach(function(done) {
+    camp = Camp.start({ port: config.port, hostname: '::' })
+    camp.on('listening', () => done())
+  })
+
+  afterEach(function(done) {
+    if (camp) {
+      camp.close(() => done())
+      camp = null
+    }
+  })
+
+  beforeEach(function() {
+    acceptor.setRoutes(camp)
+  })
+
+  it('should start the OAuth process', async function() {
+    const res = await got(`${baseUri}/github-auth`, { followRedirect: false })
+
+    expect(res.statusCode).to.equal(302)
+
+    const qs = queryString.stringify({
+      client_id: fakeClientId,
+      redirect_uri: 'https://img.shields.io/github-auth/done',
     })
+    const expectedLocationHeader = `https://github.com/login/oauth/authorize?${qs}`
+    expect(res.headers.location).to.equal(expectedLocationHeader)
+  })
 
-    afterEach(function() {
-      sandbox.restore()
-    })
-
-    let camp
-    beforeEach(function(done) {
-      camp = Camp.start({ port: config.port, hostname: '::' })
-      camp.on('listening', () => done())
-    })
-
-    afterEach(function(done) {
-      if (camp) {
-        camp.close(() => done())
-        camp = null
-      }
-    })
-
-    beforeEach(function() {
-      acceptor.setRoutes(camp)
-    })
-
-    it('should start the OAuth process', async function() {
-      const res = await got(`${baseUri}/github-auth`, { followRedirect: false })
-
-      expect(res.statusCode).to.equal(302)
-
-      const qs = queryString.stringify({
-        client_id: fakeClientId,
-        redirect_uri: 'https://img.shields.io/github-auth/done',
+  describe('Finishing the OAuth process', function() {
+    context('no code is provided', function() {
+      it('should return an error', async function() {
+        const res = await got(`${baseUri}/github-auth/done`)
+        expect(res.body).to.equal(
+          'GitHub OAuth authentication failed to provide a code.'
+        )
       })
-      const expectedLocationHeader = `https://github.com/login/oauth/authorize?${qs}`
-      expect(res.headers.location).to.equal(expectedLocationHeader)
+    })
+
+    const fakeCode = '123456789'
+    const fakeAccessToken = 'abcdef'
+
+    context('a code is provided', function() {
+      let scope
+      beforeEach(function() {
+        nock.enableNetConnect(/127\.0\.0\.1/)
+
+        scope = nock('https://github.com')
+          .post('/login/oauth/access_token')
+          .reply((url, requestBody) => {
+            expect(queryString.parse(requestBody).code).to.equal(fakeCode)
+            return queryString.stringify({ access_token: fakeAccessToken })
+          })
+      })
+
+      afterEach(function() {
+        if (scope) {
+          scope.done()
+          scope = null
+        }
+      })
+
+      afterEach(function() {
+        nock.enableNetConnect()
+        nock.cleanAll()
+      })
+
+      it('should finish the OAuth process', async function() {
+        const res = await got(`${baseUri}/github-auth/done`, {
+          form: true,
+          body: { code: fakeCode },
+        })
+        expect(res.body).to.startWith(
+          '<p>Shields.io has received your app-specific GitHub user token.'
+        )
+      })
     })
   })
 })
