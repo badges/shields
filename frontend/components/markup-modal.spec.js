99b03813e8a728cd0bc37cb405537810eb0e67ea
Michal Piekarczyk <michal.piekarczyk@allegro.pl>
2018-10-28 15:25:37 +0100

    Add @babel/register

---
+++ frontend/components/markup-modal.spec.js
@@ -0,0 +1,19 @@
+// const chai = require('chai')
+// const { expect } = chai
+// import { shallow } from 'enzyme';
+// import MarkupModal from './markup-modal'
+// import React from 'react';
+//
+// describe('Badge URL functions', function () {
+//   it('pieakr 12', function () {
+//     const props = {
+//       example: null,
+//       baseUrl: 'http://localhost:8080',
+//       onRequestClose: () => {},
+//     }
+//
+//     const markupModalWrapper = shallow(<MarkupModal {...props} />)
+//
+//     expect(1).to.equal(2)
+//   })
+// })
