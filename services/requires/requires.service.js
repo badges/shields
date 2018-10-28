20773281cdf09cee708747092d871b0a9ddfedad
Tagan Hoyle <tagnum.elite@gmail.com>
2018-10-28 18:01:03 +0200

    Change Regex

--- services/requires/requires.service.js
+++ services/requires/requires.service.js
@@ -11,7 +11,7 @@ module.exports = class RequiresIo extends BaseJsonService {
   static get url() {
     return {
       base: 'requires',
-      format: '(github|bitbucket)/([^/]+/[^/]+)(?:/(.+))?',
+      format: '([^/]+)/([^/]+/[^/]+)(?:/(.+))?',
       capture: ['service', 'repo', 'branch'],
     }
   }
