14e9e133eeedf60171726d3f2c6d090f961f10bb
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2018-11-02 22:01:25 +0100

    Code refactoring

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const { checkErrorResponse } = require('./lib/error-helper')
 const analytics = require('./lib/analytics')
 const config = require('./lib/server-config')
 const GithubConstellation = require('./services/github/github-constellation')
-const Metrics = require('./lib/sys/metrics')
+const PrometheusMetrics = require('./lib/sys/prometheus-metrics')
 const sysMonitor = require('./lib/sys/monitor')
 const log = require('./lib/log')
 const { makeMakeBadgeFn } = require('./lib/make-badge')
@@ -51,7 +51,7 @@ const githubConstellation = new GithubConstellation({
   persistence: config.persistence,
   service: config.services.github,
 })
-const metrics = new Metrics({ ...config.metrics })
+const metrics = new PrometheusMetrics({ ...config.metrics.prometheus })
 const { apiProvider: githubApiProvider } = githubConstellation
 
 function reset() {
