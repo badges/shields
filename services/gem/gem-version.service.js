c43b4e2394af74638b995c910fc18096cf5615e3
Paul Melnikow <email@paulmelnikow.com>
2018-11-06 17:35:52 -0500

    Update GemVersion to match changes to tutorial

--- services/gem/gem-version.service.js
+++ services/gem/gem-version.service.js
@@ -5,19 +5,17 @@ const Joi = require('joi')
 const { renderVersionBadge } = require('../../lib/version')
 const BaseJsonService = require('../base-json')
 
-// Response should contain a string key 'version'
-// In most cases this will be a SemVer
-// but the registry doesn't actually enforce this
-const versionSchema = Joi.object({
+const schema = Joi.object({
+  // In most cases `version` will be a SemVer but the registry doesn't
+  // actually enforce this.
   version: Joi.string().required(),
 }).required()
 
 module.exports = class GemVersion extends BaseJsonService {
   async fetch({ gem }) {
-    const url = `https://rubygems.org/api/v1/gems/${gem}.json`
     return this._requestJson({
-      url,
-      schema: versionSchema,
+      url: `https://rubygems.org/api/v1/gems/${gem}.json`,
+      schema,
     })
   }
 
@@ -42,8 +40,7 @@ module.exports = class GemVersion extends BaseJsonService {
   static get url() {
     return {
       base: 'gem/v',
-      format: '(.+)',
-      capture: ['gem'],
+      pattern: ':gem',
     }
   }
 
