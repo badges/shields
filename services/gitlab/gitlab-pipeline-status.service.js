95f210aa9399c1b4c5112c6b07af5ef10307e52f
Paul Melnikow <email@paulmelnikow.com>
2018-11-15 11:11:24 -0500

    Add GItlab pipeline status badge

---
+++ services/gitlab/gitlab-pipeline-status.service.js
@@ -0,0 +1,107 @@
+'use strict'
+
+const Joi = require('joi')
+const validate = require('../../lib/validate')
+const BaseSvgScrapingService = require('../base-svg-scraping')
+const { InvalidParameter, NotFound } = require('../errors')
+const { isPipelineStatus } = require('./gitlab-helpers')
+
+const badgeSchema = Joi.object({
+  message: Joi.alternatives()
+    .try([isPipelineStatus, Joi.equal('unknown')])
+    .required(),
+}).required()
+
+const queryParamsSchema = Joi.object({
+  // TODO This accepts URLs with query strings and fragments, which should be
+  // rejected.
+  gitlab_url: Joi.string().uri({ scheme: ['https'] }),
+}).required()
+
+module.exports = class GitlabPipelineStatus extends BaseSvgScrapingService {
+  static get category() {
+    return 'build'
+  }
+
+  static get route() {
+    return {
+      base: 'gitlab/pipeline',
+      format: '([^/]+)/([^/]+)(?:/([^/]+))?',
+      capture: ['user', 'repo', 'branch'],
+      // Trailing optional parameters don't work. The issue relates to the `.`
+      // separator before the extension.
+      // pattern: ':user/:repo/:branch?',
+      queryParams: ['gitlab_url'],
+    }
+  }
+
+  static get examples() {
+    return [
+      {
+        title: 'Gitlab pipeline status',
+        urlPattern: ':user/:repo',
+        exampleUrl: 'gitlab-org/gitlab-ce',
+        staticExample: this.render({ status: 'passed' }),
+      },
+      {
+        title: 'Gitlab pipeline status (branch)',
+        urlPattern: ':user/:repo/:branch',
+        exampleUrl: 'gitlab-org/gitlab-ce/master',
+        staticExample: this.render({ status: 'passed' }),
+      },
+      // TODO Find an example of a self-hosted gitlab.
+      {
+        title: 'Gitlab pipeline status (self-hosted)',
+        urlPattern: ':user/:repo',
+        exampleUrl: 'gitlab-org/gitlab-ce/master',
+        staticExample: this.render({ status: 'passed' }),
+        query: { gitlab_url: 'https://gitlab.com' },
+      },
+    ]
+  }
+
+  static validateQueryParams(queryParams) {
+    return validate(
+      {
+        ErrorClass: InvalidParameter,
+        prettyErrorMessage: 'invalid query parameter',
+        traceErrorMessage: 'Query params did not match schema',
+        traceSuccessMessage: 'Query params after validation',
+      },
+      queryParams,
+      queryParamsSchema
+    )
+  }
+
+  static render({ status }) {
+    const color = {
+      passed: 'brightgreen',
+      running: 'blue',
+      pending: 'yellow',
+      canceled: 'orange',
+      failed: 'red',
+    }[status]
+
+    return {
+      message: status,
+      color,
+    }
+  }
+
+  async handle({ user, repo, branch = 'master' }, queryParams) {
+    const {
+      gitlab_url: baseUrl = 'https://gitlab.com',
+    } = this.constructor.validateQueryParams(queryParams)
+    const { message: status } = await this._requestSvg({
+      schema: badgeSchema,
+      url: `${baseUrl}/${user}/${repo}/badges/${branch}/pipeline.svg`,
+      errorMessages: {
+        401: 'repo not found',
+      },
+    })
+    if (status === 'unknown') {
+      throw new NotFound({ prettyMessage: 'branch not found' })
+    }
+    return this.constructor.render({ status })
+  }
+}
