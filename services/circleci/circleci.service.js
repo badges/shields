ce0b4920295eb0a79065f7b5bbd23b8d82c1e1e9
Haythem Tlili <haythem@octopush.io>
2018-11-20 22:35:44 +0100

    Fix CircleCI token example (#2373)
    The CircleCI token example contains a redundant fragment `/circleci/circleci/:token`.

--- services/circleci/circleci.service.js
+++ services/circleci/circleci.service.js
@@ -83,9 +83,9 @@ module.exports = class CircleCi extends BaseJsonService {
       },
       {
         title: 'CircleCI token',
-        pattern: 'circleci/token/:token/project/:vcsType/:owner/:repo/:branch',
+        pattern: 'token/:token/project/:vcsType/:owner/:repo/:branch',
         exampleUrl:
-          'circleci/token/b90b5c49e59a4c67ba3a92f7992587ac7a0408c2/project/github/RedSparr0w/node-csgo-parser/master',
+          'token/b90b5c49e59a4c67ba3a92f7992587ac7a0408c2/project/github/RedSparr0w/node-csgo-parser/master',
         staticExample: this.render({ status: 'success' }),
       },
     ]
