b600bde44e31ce9e8f744f99cb4411f1cc0bcfa5
Paul Melnikow <github@paulmelnikow.com>
2018-11-25 09:14:54 -0500

    The last of the badge examples (#2389)
    * Move PowerShell
    
    * Move Choco + Resharper
    
    * Move MyGet and Nuget

--- services/myget/myget.service.js
+++ services/myget/myget.service.js
@@ -2,8 +2,68 @@
 
 const { createServiceFamily } = require('../nuget/nuget-v3-service-family')
 
-module.exports = createServiceFamily({
+const {
+  NugetVersionService: Version,
+  NugetDownloadService: Downloads,
+} = createServiceFamily({
   defaultLabel: 'myget',
   serviceBaseUrl: 'myget',
   apiDomain: 'myget.org',
 })
+
+class MyGetVersionService extends Version {
+  static get examples() {
+    return [
+      {
+        title: 'MyGet',
+        pattern: 'myget/:feed/v/:packageName',
+        namedParams: { feed: 'mongodb', packageName: 'MongoDB.Driver.Core' },
+        staticExample: this.render({ version: '2.6.1' }),
+      },
+      {
+        title: 'MyGet (with prereleases)',
+        pattern: 'myget/:feed/vpre/:packageName',
+        namedParams: { feed: 'mongodb', packageName: 'MongoDB.Driver.Core' },
+        staticExample: this.render({ version: '2.7.0-beta0001' }),
+      },
+      {
+        title: 'MyGet tenant',
+        pattern: ':tenant.myget/:feed/v/:packageName',
+        namedParams: {
+          tenant: 'dotnet',
+          feed: 'dotnet-coreclr',
+          packageName: 'Microsoft.DotNet.CoreCLR',
+        },
+        staticExample: this.render({ version: '1.0.2-prerelease' }),
+      },
+    ]
+  }
+}
+
+class MyGetDownloadService extends Downloads {
+  static get examples() {
+    return [
+      {
+        title: 'MyGet',
+        pattern: 'myget/:feed/dt/:packageName',
+        namedParams: { feed: 'mongodb', packageName: 'MongoDB.Driver.Core' },
+        staticExample: this.render({ downloads: 419 }),
+      },
+      {
+        title: 'MyGet tenant',
+        pattern: ':tenant.myget/:feed/dt/:packageName',
+        namedParams: {
+          tenant: 'dotnet',
+          feed: 'dotnet-coreclr',
+          packageName: 'Microsoft.DotNet.CoreCLR',
+        },
+        staticExample: this.render({ downloads: 9748 }),
+      },
+    ]
+  }
+}
+
+module.exports = {
+  MyGetVersionService,
+  MyGetDownloadService,
+}
