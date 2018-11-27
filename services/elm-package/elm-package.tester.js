8651830a8c1e1786cf9e76acbb093203b625c0b0
Paul Melnikow <email@paulmelnikow.com>
2018-11-26 22:56:19 -0500

    Rewrite [ElmPackage]

--- services/elm-package/elm-package.tester.js
+++ services/elm-package/elm-package.tester.js
@@ -1,16 +1,15 @@
 'use strict'
 
 const Joi = require('joi')
-const ServiceTester = require('../service-tester')
 const { isSemver } = require('../test-validators')
 
-const t = new ServiceTester({ id: 'elm-package', title: 'ELM PACKAGE' })
+const t = require('../create-service-tester')()
 module.exports = t
 
 t.create('gets the package version of elm/core')
-  .get('/v/elm/core.json')
+  .get('/elm/core.json')
   .expectJSONTypes(Joi.object().keys({ name: 'elm package', value: isSemver }))
 
 t.create('invalid package name')
-  .get('/v/elm-community/frodo-is-not-a-package.json')
-  .expectJSON({ name: 'elm package', value: 'invalid' })
+  .get('/elm-community/frodo-is-not-a-package.json')
+  .expectJSON({ name: 'elm package', value: 'package not found' })
