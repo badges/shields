3707cc41e34d8bf02f90e4529438afa1ed96bea2
Paul Melnikow <github@paulmelnikow.com>
2018-12-02 11:23:33 -0500

    Add category (now required, I think)

--- services/dockbit/dockbit.service.js
+++ services/dockbit/dockbit.service.js
@@ -3,6 +3,7 @@
 const deprecatedService = require('../deprecated-service')
 // dockbit integration - deprecated as of December 2017.
 module.exports = deprecatedService({
+  category: 'build',
   url: {
     base: 'dockbit',
     format: '(?:.+)',
