9970aa99caf00c7bc1955f3edd38257d60ae35a6
Paul Melnikow <email@paulmelnikow.com>
2018-12-02 21:37:05 -0500

    Remove dead code

--- frontend/lib/service-definitions/service-definition-helper.js
+++
@@ -1,9 +0,0 @@
-export default class ServiceDefinitionHelper {
-  constructor(serviceDefinitionData) {
-    this.serviceDefinitionData = serviceDefinitionData
-  }
-
-  get isDeprecated() {
-    return this.serviceDefinitionData.isDeprecated
-  }
-}
