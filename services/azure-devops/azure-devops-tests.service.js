38714c4409798b27a1105d2d0c5a0e214f1bcb9d
Paul Melnikow <email@paulmelnikow.com>
2018-12-03 16:10:05 -0500

    Oops, missed one

--- services/azure-devops/azure-devops-tests.service.js
+++ services/azure-devops/azure-devops-tests.service.js
@@ -119,7 +119,7 @@ module.exports = class AzureDevOpsTests extends BaseAzureDevOpsService {
         title: 'Azure DevOps tests (compact)',
         pattern: ':organization/:project/:definitionId',
         exampleUrl: 'azuredevops-powershell/azuredevops-powershell/1',
-        query: {
+        queryParams: {
           compact_message: null,
         },
         keywords: ['vso', 'vsts', 'azure-devops'],
