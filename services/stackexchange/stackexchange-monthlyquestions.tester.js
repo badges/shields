ec07f23942373930a855ad13c8e1b5f6e1b02052
Monica Bui <bui1@ualberta.ca>
2018-12-03 18:12:44 -0700

    Add [StackExchange] monthly questions badge service (#2432)
    - Badge shows number of questions for a given library for the previous month (not current month because its in progress)
    - Service works for not just the StackOverflow site but for other StackExchange sites also
    
    Close #2378

---
+++ services/stackexchange/stackexchange-monthlyquestions.tester.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Joi = require('joi')
+const { isMetricOverTimePeriod } = require('../test-validators')
+const t = require('../create-service-tester')()
+module.exports = t
+
+t.create('Monthly Questions for StackOverflow Momentjs')
+  .get('/stackoverflow/qm/momentjs.json')
+  .expectJSONTypes(
+    Joi.object().keys({
+      name: 'stackoverflow momentjs questions',
+      value: isMetricOverTimePeriod,
+    })
+  )
+
+t.create('Monthly Questions for Tex Spacing')
+  .get('/tex/qm/spacing.json')
+  .expectJSONTypes(
+    Joi.object().keys({
+      name: 'tex spacing questions',
+      value: isMetricOverTimePeriod,
+    })
+  )
