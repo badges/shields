8070f0bef9f9774c7ab75c04e5f8a2d8420ac5d3
chris48s <chris48s@users.noreply.github.com>
2018-12-06 20:24:46 +0000

    add tests for rust [crates] and refactor (#2462)
    * add tests for rust [crates] and refactor

--- services/crates/crates-version.tester.js
+++ services/crates/crates-version.tester.js
@@ -1,22 +1,19 @@
 'use strict'
 
 const ServiceTester = require('../service-tester')
+const { isSemver } = require('../test-validators')
 
 const t = new ServiceTester({
   id: 'crates',
   title: 'crates.io',
-  pathPrefix: '/crates/l',
+  pathPrefix: '/crates/v',
 })
 module.exports = t
 
-t.create('license')
+t.create('version')
   .get('/libc.json')
-  .expectJSON({ name: 'license', value: 'MIT OR Apache-2.0' })
+  .expectJSONTypes({ name: 'crates.io', value: isSemver })
 
-t.create('license (with version)')
-  .get('/libc/0.2.44.json')
-  .expectJSON({ name: 'license', value: 'MIT OR Apache-2.0' })
-
-t.create('license (not found)')
+t.create('version (not found)')
   .get('/not-a-real-package.json')
   .expectJSON({ name: 'crates.io', value: 'not found' })
