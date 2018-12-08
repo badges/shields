efc3055ca5ffa17926fd1fe5575e800e17160df1
Paul Melnikow <email@paulmelnikow.com>
2018-12-08 18:36:41 -0500

    Rewrite [DynamicYaml]

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 'use strict'
 
-const jp = require('jsonpath')
 const path = require('path')
-const yaml = require('js-yaml')
 const Raven = require('raven')
 
 const serverSecrets = require('./lib/server-secrets')
@@ -10,7 +8,6 @@ Raven.config(process.env.SENTRY_DSN || serverSecrets.sentry_dsn).install()
 Raven.disableConsoleAlerts()
 
 const { loadServiceClasses } = require('./services')
-const { checkErrorResponse } = require('./lib/error-helper')
 const analytics = require('./lib/analytics')
 const config = require('./lib/server-config')
 const GithubConstellation = require('./services/github/github-constellation')
@@ -20,14 +17,8 @@ const log = require('./lib/log')
 const { staticBadgeUrl } = require('./lib/make-badge-url')
 const makeBadge = require('./gh-badges/lib/make-badge')
 const suggest = require('./lib/suggest')
-const {
-  makeBadgeData: getBadgeData,
-  setBadgeColor,
-} = require('./lib/badge-data')
-const {
-  handleRequest: cache,
-  clearRequestCache,
-} = require('./lib/request-handler')
+const { makeBadgeData } = require('./lib/badge-data')
+const { handleRequest, clearRequestCache } = require('./lib/request-handler')
 const { clearRegularUpdateCache } = require('./lib/regular-update')
 const { makeSend } = require('./lib/result-sender')
 
@@ -83,7 +74,7 @@ suggest.setRoutes(config.cors.allowedOrigin, githubApiProvider, camp)
 
 camp.notfound(/\.(svg|png|gif|jpg|json)/, (query, match, end, request) => {
   const format = match[1]
-  const badgeData = getBadgeData('404', query)
+  const badgeData = makeBadgeData('404', query)
   badgeData.text[1] = 'badge not found'
   badgeData.colorscheme = 'red'
   // Add format to badge data.
@@ -100,94 +91,13 @@ camp.notfound(/.*/, (query, match, end, request) => {
 
 loadServiceClasses().forEach(serviceClass =>
   serviceClass.register(
-    { camp, handleRequest: cache, githubApiProvider },
+    { camp, handleRequest, githubApiProvider },
     {
       handleInternalErrors: config.handleInternalErrors,
       cacheHeaders: config.cacheHeaders,
       profiling: config.profiling,
     }
   )
-)
-
-// User defined sources - JSON response
-camp.route(
-  /^\/badge\/dynamic\/(xml|yaml)\.(svg|png|gif|jpg|json)$/,
-  cache(config.cacheHeaders, {
-    queryParams: ['uri', 'url', 'query', 'prefix', 'suffix'],
-    handler: function(query, match, sendBadge, request) {
-      const type = match[1]
-      const format = match[2]
-      const prefix = query.prefix || ''
-      const suffix = query.suffix || ''
-      const pathExpression = query.query
-      let requestOptions = {}
-
-      const badgeData = getBadgeData('custom badge', query)
-
-      if ((!query.uri && !query.url) || !query.query) {
-        setBadgeColor(badgeData, 'red')
-        badgeData.text[1] = !query.query
-          ? 'no query specified'
-          : 'no url specified'
-        sendBadge(format, badgeData)
-        return
-      }
-
-      let url
-      try {
-        url = encodeURI(decodeURIComponent(query.url || query.uri))
-      } catch (e) {
-        setBadgeColor(badgeData, 'red')
-        badgeData.text[1] = 'malformed url'
-        sendBadge(format, badgeData)
-        return
-      }
-
-      switch (type) {
-        case 'yaml':
-          requestOptions = {
-            headers: {
-              Accept:
-                'text/x-yaml,  text/yaml, application/x-yaml, application/yaml, text/plain',
-            },
-          }
-          break
-      }
-
-      request(url, requestOptions, (err, res, data) => {
-        try {
-          if (
-            checkErrorResponse(badgeData, err, res, {
-              404: 'resource not found',
-            })
-          ) {
-            return
-          }
-
-          badgeData.colorscheme = 'brightgreen'
-
-          let innerText = []
-          switch (type) {
-            case 'yaml':
-              data = yaml.safeLoad(data)
-              data = jp.query(data, pathExpression)
-              if (!data.length) {
-                throw Error('no result')
-              }
-              innerText = data
-              break
-          }
-          badgeData.text[1] =
-            (prefix || '') + innerText.join(', ') + (suffix || '')
-        } catch (e) {
-          setBadgeColor(badgeData, 'lightgrey')
-          badgeData.text[1] = e.message
-        } finally {
-          sendBadge(format, badgeData)
-        }
-      })
-    },
-  })
 )
 
 // Any badge, old version. This route must be registered last.
---
+++ services/dynamic/dynamic-yaml.service.js
@@ -0,0 +1,69 @@
+'use strict'
+
+const yaml = require('js-yaml')
+const jp = require('jsonpath')
+const emojic = require('emojic')
+const BaseService = require('../base')
+const { InvalidResponse } = require('../errors')
+const trace = require('../trace')
+const {
+  createRoute,
+  queryParamSchema,
+  errorMessages,
+  renderDynamicBadge,
+} = require('./dynamic-helpers')
+
+module.exports = class DynamicYaml extends BaseService {
+  static get category() {
+    return 'dynamic'
+  }
+
+  static get route() {
+    return createRoute('yaml')
+  }
+
+  static get defaultBadgeData() {
+    return {
+      label: 'custom badge',
+    }
+  }
+
+  parseYml(buffer) {
+    const logTrace = (...args) => trace.logTrace('fetch', ...args)
+    let parsed
+    try {
+      parsed = yaml.safeLoad(buffer)
+    } catch (err) {
+      logTrace(emojic.dart, 'Response YAML (unparseable)', buffer)
+      throw new InvalidResponse({
+        prettyMessage: 'unparseable yaml response',
+        underlyingError: err,
+      })
+    }
+    logTrace(emojic.dart, 'Response YAML (before validation)', parsed, {
+      deep: true,
+    })
+    return parsed
+  }
+
+  async handle(namedParams, queryParams) {
+    const {
+      url,
+      query: pathExpression,
+      prefix,
+      suffix,
+    } = this.constructor._validateQueryParams(queryParams, queryParamSchema)
+
+    const { buffer } = await this._request({ url, errorMessages })
+
+    const data = this.parseYml(buffer)
+
+    const values = jp.query(data, pathExpression)
+
+    if (!values.length) {
+      throw new InvalidResponse({ prettyMessage: 'no result' })
+    }
+
+    return renderDynamicBadge({ values, prefix, suffix })
+  }
+}
