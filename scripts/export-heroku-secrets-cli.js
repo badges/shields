2b3e85e75fd53c549322b986965f67248237218a
PyvesB <PyvesDev@gmail.com>
2018-12-09 12:47:30 +0000

    Renamed file and switched to console.log

--- scripts/export-heroku-secrets-cli.js
+++ scripts/export-heroku-secrets-cli.js
@@ -6,9 +6,9 @@ const path = require('path')
 const secretsPath = path.join(__dirname, '..', 'private', 'secret.json')
 
 if (fs.existsSync(secretsPath)) {
-  console.info('Found existing private/secret.json, not overwriting.')
+  console.log('Found existing private/secret.json, not overwriting.')
 } else {
-  console.info('private/secret.json does not exist, creating.')
+  console.log('private/secret.json does not exist, creating.')
   const secrets = { wheelmap_token: process.env.WHEELMAP_TOKEN }
   const secretsString = JSON.stringify(secrets)
   fs.writeFileSync(secretsPath, secretsString)
