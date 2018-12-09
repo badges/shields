2cddbea035680e2d567f0db31efaa98083bd6b7e
chris48s <chris.shaw480@gmail.com>
2018-12-09 16:15:53 +0000

    refactor [bountysource] service

--- services/bountysource/bountysource.tester.js
+++ services/bountysource/bountysource.tester.js
@@ -2,7 +2,6 @@
 
 const Joi = require('joi')
 const ServiceTester = require('../service-tester')
-const { invalidJSON } = require('../response-fixtures')
 
 const t = (module.exports = new ServiceTester({
   id: 'bountysource',
@@ -25,30 +24,4 @@ t.create('bounties (invalid team)')
   .expectJSON({
     name: 'bounties',
     value: 'not found',
-  })
-
-t.create('bounties (connection error)')
-  .get('/team/mozilla-core/activity.json')
-  .networkOff()
-  .expectJSON({ name: 'bounties', value: 'inaccessible' })
-
-t.create('bounties (unexpected response)')
-  .get('/team/mozilla-core/activity.json')
-  .intercept(nock =>
-    nock('https://api.bountysource.com')
-      .get('/teams/mozilla-core')
-      .reply(invalidJSON)
-  )
-  .expectJSON({ name: 'bounties', value: 'invalid' })
-
-t.create('bounties (error response)')
-  .get('/team/mozilla-core/activity.json')
-  .intercept(nock =>
-    nock('https://api.bountysource.com')
-      .get('/teams/mozilla-core')
-      .reply(500, '{"error":"oh noes!!"}')
-  )
-  .expectJSON({
-    name: 'bounties',
-    value: 'invalid',
   })
