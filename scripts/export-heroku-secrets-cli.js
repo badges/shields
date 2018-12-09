4c8521f428368433166f7bbfc927d71ce84b386e
PyvesB <PyvesDev@gmail.com>
2018-12-09 14:47:04 +0000

    Handled case where directory doesn't exist

--- scripts/export-heroku-secrets-cli.js
+++ scripts/export-heroku-secrets-cli.js
@@ -9,6 +9,10 @@ if (fs.existsSync(secretsPath)) {
   console.log('Found existing private/secret.json, not overwriting.')
 } else {
   console.log('private/secret.json does not exist, creating.')
+  const privatePath = path.dirname(secretsPath)
+  if (!fs.existsSync(privatePath)) {
+    fs.mkdirSync(privatePath)
+  }
   const secrets = { wheelmap_token: process.env.WHEELMAP_TOKEN }
   const secretsString = JSON.stringify(secrets)
   fs.writeFileSync(secretsPath, secretsString)
