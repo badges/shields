79e88d62ec1990767be6fb0628b9d842b526fc46
Paul Melnikow <email@paulmelnikow.com>
2018-12-13 15:10:46 -0500

    Fields with labels and example text

--- frontend/components/markup-modal-content.js
+++ frontend/components/markup-modal-content.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Modal from 'react-modal'
 import styled from 'styled-components'
+import pathToRegexp from 'path-to-regexp'
+import humanizeString from 'humanize-string'
 import { badgeUrlFromPath, badgeUrlFromPattern } from '../../lib/make-badge-url'
 import generateAllMarkup from '../lib/generate-image-markup'
 import { advertisedStyles } from '../../supported-features.json'
 import { Snippet2 } from './snippet'
-import { BaseFont, H3, Badge, BlockInput } from './common'
+import { BaseFont, H3, Badge, BlockInput, InlineInput } from './common'
+
+const common = {
+  autoComplete: 'off',
+  autoCorrect: 'off',
+  autoCapitalize: 'off',
+  spellCheck: 'false',
+}
 
 const ContentContainer = styled(BaseFont)`
   text-align: center;
@@ -19,6 +27,142 @@ WeeSnippet.propTypes = {
   snippet: PropTypes.string.isRequired,
 }
 
+const AnnotatedInlineInputContainer = styled.span`
+  position: relative;
+`
+
+const PositionedLabel = styled.label`
+  position: absolute;
+  top: -20px;
+
+  width: 100%;
+  text-align: center;
+
+  font-family: system-ui;
+  font-size: 11px;
+  text-transform: lowercase;
+`
+
+const PositionedCaption = styled.span`
+  position: absolute;
+  top: 20px;
+
+  width: 100%;
+  text-align: center;
+
+  color: #999;
+
+  font-family: system-ui;
+  font-size: 11px;
+  text-transform: lowercase;
+`
+
+const AnnotatedInlineInput = ({ name, value, exampleValue, onChange }) => (
+  <AnnotatedInlineInputContainer>
+    <PositionedLabel htmlFor={name}>{humanizeString(name)}</PositionedLabel>
+    <PositionedCaption>{exampleValue}</PositionedCaption>
+    <InlineInput
+      type="text"
+      name={name}
+      value={value}
+      onChange={onChange}
+      {...common}
+    />
+  </AnnotatedInlineInputContainer>
+)
+
+const NamedParamFieldBlockContainer = styled.div`
+  margin-top: 50px;
+`
+
+const NamedParamFieldContainer = styled.span`
+  padding: 38px 15px 15px;
+
+  border-radius: 4px;
+  background: #eef;
+`
+
+class NamedParamFields extends React.Component {
+  constructor(props) {
+    super(props)
+
+    const namedParams = {}
+    props.tokens
+      .filter(t => typeof t !== 'string')
+      .map(t => t.name)
+      .forEach(name => {
+        namedParams[name] = ''
+      })
+
+    this.state = { namedParams }
+  }
+
+  handleTokenChange = event => {
+    const { name, value } = event.target
+    const { namedParams } = this.state
+    this.setState({
+      namedParams: {
+        ...namedParams,
+        [name]: value,
+      },
+    })
+  }
+
+  renderToken(token) {
+    if (typeof token === 'string') {
+      return <span>{token}</span>
+    } else {
+      const { delimiter, name } = token
+
+      const { exampleParams } = this.props
+      const exampleValue = exampleParams[name]
+
+      const { namedParams } = this.state
+      const value = namedParams[name]
+
+      return (
+        <span key={name}>
+          {delimiter}
+          <AnnotatedInlineInput
+            name={name}
+            value={value}
+            exampleValue={exampleValue}
+            onChange={this.handleTokenChange}
+          />
+        </span>
+      )
+    }
+  }
+
+  render() {
+    const { tokens } = this.props
+    return (
+      <NamedParamFieldBlockContainer>
+        <NamedParamFieldContainer>
+          {tokens.map(token => this.renderToken(token))}
+        </NamedParamFieldContainer>
+      </NamedParamFieldBlockContainer>
+    )
+  }
+}
+NamedParamFields.propTypes = {
+  tokens: PropTypes.arrayOf(
+    PropTypes.oneOfType([
+      PropTypes.string.isRequired,
+      PropTypes.shape({
+        delimiter: PropTypes.string.isRequired,
+        name: PropTypes.string.isRequired,
+        optional: PropTypes.bool.isRequired,
+        partial: PropTypes.bool.isRequired,
+        pattern: PropTypes.string.isRequired,
+        prefix: PropTypes.string.isRequired,
+        repeat: PropTypes.bool.isRequired,
+      }),
+    ])
+  ).isRequired,
+  exampleParams: PropTypes.object.isRequired,
+}
+
 export default class MarkupModalContent extends React.Component {
   static propTypes = {
     // This is an item from the `examples` array within the
@@ -26,7 +170,6 @@ export default class MarkupModalContent extends React.Component {
     // https://github.com/badges/shields/blob/master/services/service-definitions.js
     example: PropTypes.object,
     baseUrl: PropTypes.string.isRequired,
-    onRequestClose: PropTypes.func.isRequired,
   }
 
   state = {
@@ -166,62 +309,41 @@ export default class MarkupModalContent extends React.Component {
     ) : null
   }
 
-  render() {
+  renderFullPattern() {
     const {
       baseUrl,
-      onRequestClose,
       example: {
-        title,
         example: { pattern },
       },
     } = this.props
-    const { link, badgeUrl, exampleUrl, style } = this.state
+    return (
+      <Snippet2
+        snippet={pattern}
+        snippetToCopy={`${baseUrl}${pattern}.svg`}
+        fontSize="9pt"
+      />
+    )
+  }
 
-    const common = {
-      autoComplete: 'off',
-      autoCorrect: 'off',
-      autoCapitalize: 'off',
-      spellCheck: 'false',
-    }
+  render() {
+    const {
+      baseUrl,
+      example: {
+        title,
+        example: { pattern, namedParams },
+      },
+    } = this.props
+    const { link, badgeUrl, exampleUrl, style } = this.state
 
     return (
       <ContentContainer>
         <form action="">
           <H3>{title}</H3>
-          <Snippet2 snippet={pattern} snippetToCopy={`${baseUrl}${pattern}`} />
+          <NamedParamFields
+            tokens={pathToRegexp.parse(pattern)}
+            exampleParams={namedParams}
+          />
           {this.renderLivePreview()}
-          <p>
-            <label>
-              Link&nbsp;
-              <BlockInput
-                type="url"
-                value={link}
-                onChange={event => {
-                  this.setState({ link: event.target.value })
-                }}
-                {...common}
-              />
-            </label>
-          </p>
-          <p>
-            <label>
-              Path&nbsp;
-              <BlockInput
-                type="url"
-                value={badgeUrl}
-                onChange={event => {
-                  this.setState({ badgeUrl: event.target.value })
-                }}
-                {...common}
-              />
-            </label>
-          </p>
-          {exampleUrl && (
-            <p>
-              Example&nbsp;
-              <Snippet2 fontSize="10pt" snippet={exampleUrl} />
-            </p>
-          )}
           <p>
             <label>
               Style&nbsp;
@@ -241,6 +363,7 @@ export default class MarkupModalContent extends React.Component {
           </p>
           {this.renderMarkup()}
           {this.renderDocumentation()}
+          {this.renderFullPattern()}
         </form>
       </ContentContainer>
     )
