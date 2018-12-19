6d3798f26f1ce13617d94726cf8873309970e118
Caleb Cartwright <calebcartwright@users.noreply.github.com>
2018-12-19 14:45:22 -0600

    Migrates [Nexus] service to new service model (#2520)
    Ports the Nexus service to the new service model. Some related/relevant conversation in #2347 (and closes #2347). Also adds support for authentication which resolves #1699.

--- services/validators.js
+++ services/validators.js
@@ -20,6 +20,10 @@ module.exports = {
     .validRange()
     .required(),
 
+  optionalDottedVersionNClausesWithOptionalSuffix: Joi.string().regex(
+    /^\d+(\.\d+)*(-.*)?$/
+  ),
+
   // TODO This accepts URLs with query strings and fragments, which for some
   // purposes should be rejected.
   optionalUrl: Joi.string().uri({ scheme: ['http', 'https'] }),
