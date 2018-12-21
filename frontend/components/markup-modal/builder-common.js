8e83253fc01f3b4c163965624b8c9ba2593587ba
Paul Melnikow <email@paulmelnikow.com>
2018-12-21 14:03:14 -0500

    Add line break + space between

--- frontend/components/markup-modal/builder-common.js
+++ frontend/components/markup-modal/builder-common.js
@@ -1,6 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
 import styled, { css } from 'styled-components'
 
-const BuilderContainer = styled.div`
+const BuilderOuterContainer = styled.div`
+  margin-top: 5px;
+  margin-bottom: 5px;
+`
+
+// The inner container is inline-block so that its width matches its columns.
+const BuilderInnerContainer = styled.div`
   display: inline-block;
 
   padding: 11px 14px 10px;
@@ -8,5 +16,17 @@ const BuilderContainer = styled.div`
   border-radius: 4px;
   background: #eef;
 `
+
+const BuilderContainer = ({ children }) => (
+  <BuilderOuterContainer>
+    <BuilderInnerContainer>{children}</BuilderInnerContainer>
+  </BuilderOuterContainer>
+)
+BuilderContainer.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]),
+}
 
 export { BuilderContainer }
