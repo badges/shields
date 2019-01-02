19cb44c5fa48be2cff120678a88409a7e19f5de1
Paul Melnikow <email@paulmelnikow.com>
2019-01-01 22:48:37 -0500

    Add some test coverage in frontend/lib

--- frontend/lib/generate-image-markup.spec.js
+++ frontend/lib/generate-image-markup.spec.js
@@ -1,5 +1,54 @@
 import { test, given } from 'sazerac'
-import generateAllMarkup from './generate-image-markup'
+import {
+  markdown,
+  reStructuredText,
+  asciiDoc,
+  default as generateAllMarkup,
+} from './generate-image-markup'
+
+test(markdown, () => {
+  given('https://img.shields.io/badge.svg', undefined, 'Example').expect(
+    '![Example](https://img.shields.io/badge.svg)'
+  )
+  given(
+    'https://img.shields.io/badge.svg',
+    'https://example.com/example',
+    'Example'
+  ).expect(
+    '[![Example](https://img.shields.io/badge.svg)](https://example.com/example)'
+  )
+})
+
+test(reStructuredText, () => {
+  given('https://img.shields.io/badge.svg', undefined, 'Example').expect(
+    '.. image:: https://img.shields.io/badge.svg   :alt: Example'
+  )
+  given(
+    'https://img.shields.io/badge.svg',
+    'https://example.com/example',
+    'Example'
+  ).expect(
+    '.. image:: https://img.shields.io/badge.svg   :alt: Example   :target: https://example.com/example'
+  )
+})
+
+test(asciiDoc, () => {
+  given('https://img.shields.io/badge.svg', undefined, 'Example').expect(
+    'image:https://img.shields.io/badge.svg[Example]'
+  )
+  given(
+    'https://img.shields.io/badge.svg',
+    undefined,
+    'Example, with comma'
+  ).expect('image:https://img.shields.io/badge.svg["Example, with comma"]')
+  given(
+    'https://img.shields.io/badge.svg',
+    'https://example.com/example',
+    'Example'
+  ).expect(
+    'image:https://img.shields.io/badge.svg["Example",link="https://example.com/example"]'
+  )
+})
 
 test(generateAllMarkup, () => {
   given(
