566b58acbad0e4ba721bd77beeebe20cb6858cb9
chris48s <chris48s@users.noreply.github.com>
2019-01-06 13:25:12 +0000

    refactor [bower] service (#2646)

--- services/bower/bower-version.service.js
+++ services/bower/bower-version.service.js
@@ -1,17 +1,10 @@
 'use strict'
 
-const LegacyService = require('../legacy-service')
-const { makeBadgeData: getBadgeData } = require('../../lib/badge-data')
-const { addv: versionText } = require('../../lib/text-formatters')
-const { version: versionColor } = require('../../lib/color-formatters')
+const BaseBowerService = require('./bower-base')
+const { renderVersionBadge } = require('../../lib/version')
+const { InvalidResponse } = require('../errors')
 
-// This legacy service should be rewritten to use e.g. BaseJsonService.
-//
-// Tips for rewriting:
-// https://github.com/badges/shields/blob/master/doc/rewriting-services.md
-//
-// Do not base new services on this code.
-module.exports = class BowerVersion extends LegacyService {
+module.exports = class BowerVersion extends BaseBowerService {
   static get category() {
     return 'version'
   }
@@ -19,6 +12,7 @@ module.exports = class BowerVersion extends LegacyService {
   static get route() {
     return {
       base: 'bower',
+      pattern: ':vtype(v|vpre)/:packageName',
     }
   }
 
@@ -26,57 +20,31 @@ module.exports = class BowerVersion extends LegacyService {
     return [
       {
         title: 'Bower',
-        previewUrl: 'v/bootstrap',
+        namedParams: { vtype: 'v', packageName: 'bootstrap' },
+        pattern: 'v/:packageName',
+        staticExample: renderVersionBadge({ version: '4.2.1' }),
       },
       {
         title: 'Bower Pre Release',
-        previewUrl: 'vpre/bootstrap',
+        namedParams: { vtype: 'vpre', packageName: 'bootstrap' },
+        pattern: 'vpre/:packageName',
+        staticExample: renderVersionBadge({ version: '4.2.1' }),
       },
     ]
   }
 
-  static registerLegacyRouteHandler({ camp, cache }) {
-    camp.route(
-      /^\/bower\/(v|vpre)\/(.*)\.(svg|png|gif|jpg|json)$/,
-      cache((data, match, sendBadge, request) => {
-        const reqType = match[1]
-        const repo = match[2] // eg, `bootstrap`.
-        const format = match[3]
-        const badgeData = getBadgeData('bower', data)
+  async handle({ vtype, packageName }) {
+    const data = await this.fetch({ packageName })
 
-        // API doc: https://libraries.io/api#project
-        const options = {
-          method: 'GET',
-          json: true,
-          uri: `https://libraries.io/api/bower/${repo}`,
-        }
-
-        request(options, (err, res, data) => {
-          if (err != null) {
-            badgeData.text[1] = 'inaccessible'
-            sendBadge(format, badgeData)
-            return
-          }
-          if (res.statusCode !== 200) {
-            badgeData.text[1] = 'invalid'
-            sendBadge(format, badgeData)
-            return
-          }
-          try {
-            //if reqType is `v`, then stable release number, if `vpre` then latest release
-            const version =
-              reqType === 'v'
-                ? data.latest_stable_release.name
-                : data.latest_release_number
-            badgeData.text[1] = versionText(version)
-            badgeData.colorscheme = versionColor(version)
-            sendBadge(format, badgeData)
-          } catch (e) {
-            badgeData.text[1] = 'no releases'
-            sendBadge(format, badgeData)
-          }
-        })
-      })
-    )
+    if (vtype === 'v') {
+      if (data.latest_stable_release) {
+        return renderVersionBadge({ version: data.latest_stable_release.name })
+      }
+    } else {
+      if (data.latest_release_number) {
+        return renderVersionBadge({ version: data.latest_release_number })
+      }
+    }
+    throw new InvalidResponse({ prettyMessage: 'no releases' })
   }
 }
