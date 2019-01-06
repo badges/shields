bab1aea9bfa20328659d84d840f2315d1ce5f902
PyvesB <PyvesDev@gmail.com>
2019-01-06 21:01:26 +0000

    Rewrote Travis-Build service and separated tests

--- services/travis/travis-build.service.js
+++ services/travis/travis-build.service.js
@@ -1,19 +1,16 @@
 'use strict'
 
-const LegacyService = require('../legacy-service')
-const { makeBadgeData: getBadgeData } = require('../../lib/badge-data')
-const { checkErrorResponse } = require('../../lib/error-helper')
-const log = require('../../lib/log')
+const Joi = require('joi')
 
-// Handle .org and .com.
-//
-// This legacy service should be rewritten to use e.g. BaseJsonService.
-//
-// Tips for rewriting:
-// https://github.com/badges/shields/blob/master/doc/rewriting-services.md
-//
-// Do not base new services on this code.
-module.exports = class TravisBuild extends LegacyService {
+const BaseSvgScrapingService = require('../base-svg-scraping')
+
+const schema = Joi.object({
+  message: Joi.string()
+    .regex(/^[a-z]+$/)
+    .required(),
+}).required()
+
+module.exports = class TravisBuild extends BaseSvgScrapingService {
   static get category() {
     return 'build'
   }
@@ -21,6 +18,8 @@ module.exports = class TravisBuild extends LegacyService {
   static get route() {
     return {
       base: 'travis',
+      format: '(?:(com)/)?(?!php-v)([^/]+/[^/]+)(?:/(.+))?',
+      capture: ['comDomain', 'userRepo', 'branch'],
     }
   }
 
@@ -62,54 +61,34 @@ module.exports = class TravisBuild extends LegacyService {
     return { message: 'passing', color: 'brightgreen' }
   }
 
-  static registerLegacyRouteHandler({ camp, cache }) {
-    camp.route(
-      /^\/travis(-ci)?\/(?:(com)\/)?(?!php-v)([^/]+\/[^/]+)(?:\/(.+))?\.(svg|png|gif|jpg|json)$/,
-      cache((data, match, sendBadge, request) => {
-        const travisDomain = match[2] || 'org' // (com | org) org by default
-        const userRepo = match[3] // eg, espadrine/sc
-        const branch = match[4]
-        const format = match[5]
-        const options = {
-          method: 'HEAD',
-          uri: `https://api.travis-ci.${travisDomain}/${userRepo}.svg`,
-        }
-        if (branch != null) {
-          options.uri += `?branch=${branch}`
-        }
-        const badgeData = getBadgeData('build', data)
-        request(options, (err, res) => {
-          if (err != null) {
-            log.error(
-              `Travis error: data:${JSON.stringify(data)}\nStack: ${err.stack}`
-            )
-            if (res) {
-              log.error(`${res}`)
-            }
-          }
-          if (checkErrorResponse(badgeData, err, res)) {
-            sendBadge(format, badgeData)
-            return
-          }
-          try {
-            const state = res.headers['content-disposition'].match(
-              /filename="(.+)\.svg"/
-            )[1]
-            badgeData.text[1] = state
-            if (state === 'passing') {
-              badgeData.colorscheme = 'brightgreen'
-            } else if (state === 'failing') {
-              badgeData.colorscheme = 'red'
-            } else {
-              badgeData.text[1] = state
-            }
-            sendBadge(format, badgeData)
-          } catch (e) {
-            badgeData.text[1] = 'invalid'
-            sendBadge(format, badgeData)
-          }
-        })
-      })
-    )
+  static get defaultBadgeData() {
+    return {
+      label: 'build',
+    }
+  }
+
+  static render({ state }) {
+    let color
+    if (state === 'passing') {
+      color = 'brightgreen'
+    } else if (state === 'failing' || state === 'error') {
+      color = 'red'
+    }
+    return {
+      message: state,
+      color: color,
+    }
+  }
+
+  async handle({ comDomain, userRepo, branch }) {
+    const domain = comDomain || 'org'
+    const { message: state } = await this._requestSvg({
+      schema,
+      url: `https://api.travis-ci.${domain}/${userRepo}.svg`,
+      options: { qs: { branch } },
+      valueMatcher: />([^<>]+)<\/text><\/g>/,
+    })
+
+    return this.constructor.render({ state })
   }
 }
