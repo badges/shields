8314208d38a0132b8f05f6d95f34d76a0af4c6f1
Paul Melnikow <email@paulmelnikow.com>
2019-01-12 00:06:12 -0500

    Fix some service test failures

--- services/base.js
+++ services/base.js
@@ -34,10 +34,10 @@ const defaultBadgeDataSchema = Joi.object({
 
 const serviceDataSchema = Joi.object({
   isError: Joi.boolean(),
-  label: Joi.string(),
+  label: Joi.string().allow(''),
   // While a number of badges pass a number here, in the long run we may want
   // `render()` to always return a string.
-  message: Joi.alternatives(Joi.string().min(1), Joi.number()).required(),
+  message: Joi.alternatives(Joi.string().allow(''), Joi.number()).required(),
   color: Joi.string(),
   link: Joi.string().uri(),
   // Generally services should not use these options, which are provided to
