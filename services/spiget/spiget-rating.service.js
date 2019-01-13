a7bf77a790f4d7bb54ae697b61fef011e87798f9
Prouser123 <jcahill773@gmail.com>
2019-01-13 21:15:44 +0000

    merge service classes

--- services/spiget/spiget-rating.service.js
+++ services/spiget/spiget-rating.service.js
@@ -5,18 +5,30 @@ const { BaseSpigetService, documentation } = require('./spiget-base')
 const { starRating, metric } = require('../../lib/text-formatters')
 const { floorCount } = require('../../lib/color-formatters')
 
-class SpigetRatingsBase extends BaseSpigetService {
-  async handle({ resourceid }) {
+module.exports = class SpigetRatings extends BaseSpigetService {
+  static get route() {
+    return {
+      base: 'spiget',
+      pattern: ':format(rating|stars)/:resourceid',
+    }
+  }
+
+  async handle({ format, resourceid }) {
     const { rating } = await this.fetch({ resourceid })
     return this.constructor.render({
+      format,
       total: rating.count,
       average: rating.average,
     })
   }
 
-  static render({ total, average }) {
+  static render({ format, total, average }) {
+    const message =
+      format === 'stars'
+        ? starRating(average)
+        : `${average}/5 (${metric(total)})`
     return {
-      message: `${average}/5 (${metric(total)})`,
+      message,
       color: floorCount(average, 2, 3, 4),
     }
   }
@@ -28,50 +40,25 @@ class SpigetRatingsBase extends BaseSpigetService {
   static get defaultBadgeData() {
     return { label: 'rating' }
   }
-}
-
-class SpigetRating extends SpigetRatingsBase {
-  static get route() {
-    return {
-      base: 'spiget/rating',
-      pattern: ':resourceid',
-    }
-  }
-
-  static get examples() {
-    return [
-      {
-        title: 'Spiget Rating',
-        namedParams: {
-          resourceid: '9089',
-        },
-        staticPreview: this.render({ total: 325, average: 4.5 }),
-        documentation,
-      },
-    ]
-  }
-}
-
-class SpigetStars extends SpigetRatingsBase {
-  static render({ total, average }) {
-    const rating = (average / 100) * 5
-    return {
-      message: starRating(average),
-      color: floorCount(rating, 2, 3, 4),
-    }
-  }
-
-  static get route() {
-    return {
-      base: 'spiget/stars',
-      pattern: ':resourceid',
-    }
-  }
 
   static get examples() {
     return [
       {
         title: 'Spiget Stars',
+        pattern: 'stars/:resourceid',
+        namedParams: {
+          resourceid: '9089',
+        },
+        staticPreview: this.render({
+          format: 'stars',
+          total: 325,
+          average: 4.5,
+        }),
+        documentation,
+      },
+      {
+        title: 'Spiget Rating',
+        pattern: 'rating/:resourceid',
         namedParams: {
           resourceid: '9089',
         },
@@ -81,5 +68,3 @@ class SpigetStars extends SpigetRatingsBase {
     ]
   }
 }
-
-module.exports = { SpigetRating, SpigetStars }
