0aca8e68ff5b614e4e895907f4206a27361eb23c
chris48s <chris48s@users.noreply.github.com>
2019-01-13 12:10:28 +0000

    Refactor [osslifecycle] (#2750)
    * move files from osstracker to osslifecycle
    * refactor [osslifecycle]

---
+++ services/osslifecycle/osslifecycle.service.js
@@ -0,0 +1,67 @@
+'use strict'
+
+const BaseService = require('../base')
+const { InvalidResponse } = require('../errors')
+
+module.exports = class OssTracker extends BaseService {
+  static get category() {
+    return 'other'
+  }
+
+  static get route() {
+    return {
+      base: 'osslifecycle',
+      pattern: ':user/:repo/:branch*',
+    }
+  }
+
+  static get defaultBadgeData() {
+    return { label: 'oss lifecycle' }
+  }
+
+  static get examples() {
+    return [
+      {
+        title: 'NetflixOSS Lifecycle',
+        pattern: ':user/:repo',
+        namedParams: { user: 'Netflix', repo: 'osstracker' },
+        staticPreview: this.render({ status: 'active' }),
+      },
+      {
+        title: 'NetflixOSS Lifecycle (branch)',
+        pattern: ':user/:repo/:branch',
+        namedParams: { user: 'Netflix', repo: 'osstracker', branch: 'master' },
+        staticPreview: this.render({ status: 'active' }),
+      },
+    ]
+  }
+
+  async fetch({ user, repo, branch }) {
+    return this._request({
+      url: `https://raw.githubusercontent.com/${user}/${repo}/${branch}/OSSMETADATA`,
+    })
+  }
+
+  static render({ status }) {
+    return {
+      message: status,
+      color: 'lightgrey',
+    }
+  }
+
+  async handle({ user, repo, branch }) {
+    const { buffer } = await this.fetch({
+      user,
+      repo,
+      branch: branch || 'master',
+    })
+    try {
+      const status = buffer.match(/osslifecycle=([a-z]+)/im)[1]
+      return this.constructor.render({ status })
+    } catch (e) {
+      throw new InvalidResponse({
+        prettyMessage: 'metadata in unexpected format',
+      })
+    }
+  }
+}
