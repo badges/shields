7d4acba5c187d8264707fb5300ec5ddc1bb7392b
Paul Melnikow <github@paulmelnikow.com>
2019-01-16 19:38:36 -0500

    Validate namedParams in examples (#2786)
    Fix #2784

--- services/transform-example.js
+++ services/transform-example.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Joi = require('joi')
+const pathToRegexp = require('path-to-regexp')
 
 const optionalObjectOfKeyValues = Joi.object().pattern(
   /./,
@@ -63,6 +64,31 @@ function validateExample(example, index, ServiceClass) {
         `Static preview for ${
           ServiceClass.name
         } at index ${index} also declares a dynamic previewUrl, which is not allowed`
+      )
+    }
+
+    // Make sure we can build the full URL using these patterns.
+    try {
+      pathToRegexp.compile(pattern || ServiceClass.route.pattern)(namedParams)
+    } catch (e) {
+      throw Error(
+        `In example for ${
+          ServiceClass.name
+        } at index ${index}, ${e.message.toLowerCase()}`
+      )
+    }
+    // Make sure there are no extra keys.
+    let keys = []
+    pathToRegexp(pattern || ServiceClass.route.pattern, keys)
+    keys = keys.map(({ name }) => name)
+    const extraKeys = Object.keys(namedParams).filter(k => !keys.includes(k))
+    if (extraKeys.length) {
+      throw Error(
+        `In example for ${
+          ServiceClass.name
+        } at index ${index}, namedParams contains unknown keys: ${extraKeys.join(
+          ', '
+        )}`
       )
     }
   } else if (!previewUrl) {
