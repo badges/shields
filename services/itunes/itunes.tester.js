9d970dc80c8cfe426ca1bee4d3bfb9fb42443452
chris48s <chris.shaw480@gmail.com>
2019-01-17 21:35:35 +0000

    refactor [itunes] service

--- services/itunes/itunes.tester.js
+++ services/itunes/itunes.tester.js
@@ -4,7 +4,6 @@ const Joi = require('joi')
 const ServiceTester = require('../service-tester')
 
 const { isVPlusDottedVersionAtLeastOne } = require('../test-validators')
-const { invalidJSON } = require('../response-fixtures')
 
 const t = new ServiceTester({ id: 'itunes', title: 'iTunes' })
 module.exports = t
@@ -24,18 +23,4 @@ t.create('iTunes version (not found)')
 
 t.create('iTunes version (invalid)')
   .get('/v/x.json')
-  .expectJSON({ name: 'itunes app store', value: 'invalid' })
-
-t.create('iTunes version (connection error)')
-  .get('/v/324684580.json')
-  .networkOff()
-  .expectJSON({ name: 'itunes app store', value: 'inaccessible' })
-
-t.create('iTunes version (unexpected response)')
-  .get('/v/324684580.json')
-  .intercept(nock =>
-    nock('https://itunes.apple.com')
-      .get('/lookup?id=324684580')
-      .reply(invalidJSON)
-  )
   .expectJSON({ name: 'itunes app store', value: 'invalid' })
