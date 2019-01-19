e3c8508ce4df2f8153969ead0fee6b70497eb5ec
James Cahill <jcahill773@gmail.com>
2019-01-19 17:24:58 +0000

    Added various [Spiget] badges (#2745)
    * Spiget Downloads badge
    
    * Spiget Download Size badge
    
    * Spiget Latest version badge
    
    * Add a base class
    
    * Spiget Rating / Stars badges
    
    * Spiget versions badge
    
    * remove useless regex escape
    
    * use renderVersionBadge for rendering
    
    * misc fixes
    
    * use expectJSON when possible
    
    * use the download count color formatter
    
    * merge service classes
    
    * add keywords
    
    * Add tests for non-star ratings
    
    * Add nock tests
    
    * misc fixes
    
    * chore: minor formatting update on spiget dl

---
+++ services/spiget/spiget-tested-versions.tester.js
@@ -0,0 +1,71 @@
+'use strict'
+
+const Joi = require('joi')
+
+const t = (module.exports = require('../create-service-tester')())
+
+const { withRegex } = require('../test-validators')
+
+const multipleVersions = withRegex(/^([+]?\d*\.\d+)(-)([+]?\d*\.\d+)$/)
+
+t.create('EssentialsX - multiple versions supported - (id 9089)')
+  .get('/9089.json')
+  .expectJSONTypes(
+    Joi.object().keys({
+      name: 'tested versions',
+      value: multipleVersions,
+    })
+  )
+
+t.create('Invalid Resource (id 1)')
+  .get('/1.json')
+  .expectJSON({
+    name: 'tested versions',
+    value: 'not found',
+  })
+
+t.create('Nock - single version supported')
+  .get('/1.json')
+  .intercept(nock =>
+    nock('https://api.spiget.org/v2/resources/')
+      .get('/1')
+      .reply(200, {
+        downloads: 1,
+        file: {
+          size: 1,
+          sizeUnit: '1',
+        },
+        testedVersions: ['1.13'],
+        rating: {
+          count: 1,
+          average: 1,
+        },
+      })
+  )
+  .expectJSON({
+    name: 'tested versions',
+    value: '1.13',
+  })
+
+t.create('Nock - multiple versions supported')
+  .get('/1.json')
+  .intercept(nock =>
+    nock('https://api.spiget.org/v2/resources/')
+      .get('/1')
+      .reply(200, {
+        downloads: 1,
+        file: {
+          size: 1,
+          sizeUnit: '1',
+        },
+        testedVersions: ['1.10', '1.11', '1.12', '1.13'],
+        rating: {
+          count: 1,
+          average: 1,
+        },
+      })
+  )
+  .expectJSON({
+    name: 'tested versions',
+    value: '1.10-1.13',
+  })
