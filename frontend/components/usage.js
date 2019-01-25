64fc7bd0ae3db9bfc3cc2f7b66f179b4e0fdab3e
Paul Melnikow <email@paulmelnikow.com>
2019-01-25 14:27:16 -0500

    Clean lint

--- frontend/components/usage.js
+++ frontend/components/usage.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import styled from 'styled-components'
