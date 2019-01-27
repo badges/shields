813838f44c058af5fac3fe5dd55606f259d537ae
chris48s <chris.shaw480@gmail.com>
2019-01-27 16:30:22 +0000

    refactor [liberapay] service

---
+++ services/liberapay/liberapay-goal.tester.js
@@ -0,0 +1,31 @@
+'use strict'
+
+const Joi = require('joi')
+const t = (module.exports = require('..').createServiceTester())
+
+t.create('Goal Progress (valid)')
+  .get('/Liberapay.json')
+  .expectJSONTypes(
+    Joi.object().keys({
+      name: 'goal progress',
+      value: Joi.string().regex(/^[0-9]+%/),
+    })
+  )
+
+t.create('Goal Progress (not found)')
+  .get('/does-not-exist.json')
+  .expectJSON({ name: 'liberapay', value: 'not found' })
+
+t.create('Goal Progress (no goal set)')
+  .get('/Liberapay.json')
+  .intercept(nock =>
+    nock('https://liberapay.com')
+      .get('/Liberapay/public.json')
+      .reply(200, {
+        npatrons: 0,
+        giving: null,
+        receiving: null,
+        goal: null,
+      })
+  )
+  .expectJSON({ name: 'liberapay', value: 'no public goals' })
