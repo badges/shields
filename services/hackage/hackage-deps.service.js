d5d48910a386eb19ff4a4c867efb55d75340e315
chris48s <chris.shaw480@gmail.com>
2019-01-27 18:24:34 +0000

    resolve review comments

--- services/hackage/hackage-deps.service.js
+++ services/hackage/hackage-deps.service.js
@@ -23,21 +23,17 @@ module.exports = class HackageDeps extends BaseService {
       {
         title: 'Hackage-Deps',
         namedParams: { packageName: 'lens' },
-        staticPreview: this.render({ message: 'up to date' }),
+        staticPreview: this.render({ isOutdated: false }),
       },
     ]
   }
 
-  static render({ message }) {
-    if (message === 'up to date') {
-      return { message, color: 'brightgreen' }
+  static render({ isOutdated }) {
+    if (isOutdated) {
+      return { message: 'outdated', color: 'orange' }
     } else {
-      return { message, color: 'orange' }
+      return { message: 'up to date', color: 'brightgreen' }
     }
-  }
-
-  async fetch({ url }) {
-    return this._request({ url })
   }
 
   async handle({ packageName }) {
@@ -46,16 +42,13 @@ module.exports = class HackageDeps extends BaseService {
 
     // first call /reverse to check if the package exists
     // this will throw a 404 if it doesn't
-    await this.fetch({ url: reverseUrl })
+    await this._request({ url: reverseUrl })
 
     // if the package exists, then query /feed to check the dependencies
-    const { buffer } = await this.fetch({ url: feedUrl })
+    const { buffer } = await this._request({ url: feedUrl })
 
     const outdatedStr = `Outdated dependencies for ${packageName} `
-    if (buffer.indexOf(outdatedStr) >= 0) {
-      return this.constructor.render({ message: 'outdated' })
-    } else {
-      return this.constructor.render({ message: 'up to date' })
-    }
+    const isOutdated = buffer.includes(outdatedStr)
+    return this.constructor.render({ isOutdated })
   }
 }
