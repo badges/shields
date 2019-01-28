4b564ddfd310015fef938260a58a5ef914995322
Paul Melnikow <email@paulmelnikow.com>
2019-01-28 16:54:38 -0600

    Add a dev page for testing logos
    It can be helpful to have some diagnostic pages for development and quality control. I added one here for the logos, which renders all the named logos in ?style=flat and ?style=social.

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { HashRouter, StaticRouter, Route } from 'react-router-dom'
 import Main from '../frontend/components/main'
 import EndpointPage from '../frontend/components/endpoint-page'
+import LogoPage from '../frontend/components/development/logo-page'
 
 export default class Router extends React.Component {
   render() {
@@ -10,6 +11,7 @@ export default class Router extends React.Component {
         <Route path="/" exact component={Main} />
         <Route path="/examples/:category" component={Main} />
         <Route path="/endpoint" component={EndpointPage} />
+        <Route path="/dev/logos" component={LogoPage} />
       </div>
     )
 
