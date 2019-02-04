3d270d23d7e2765caf0843793bd94aebcdca1f53
pxgamer <owzie123@gmail.com>
2019-02-04 23:28:47 +0000

    Add HSTS service tests based on issue

---
+++ services/hsts/hsts.tester.js
@@ -0,0 +1,26 @@
+'use strict'
+
+const Joi = require('joi')
+const { ServiceTester } = require('..')
+const label = 'hsts'
+
+const t = (module.exports = new ServiceTester({
+  id: 'hsts',
+  title: 'HSTS Preload',
+}))
+
+t.create('gets the hsts status of github')
+  .get('/github.com.json')
+  .expectJSONTypes(Joi.object().keys({ name: label, value: 'preloaded' }))
+
+t.create('gets the hsts status of apviz')
+  .get('/apviz.io.json')
+  .expectJSON({ name: label, value: 'pending' })
+
+t.create('gets the hsts status of baidu')
+  .get('/baidu.com.json')
+  .expectJSON({ name: label, value: 'unknown' })
+
+t.create('invalid uri')
+  .get('/this.-is-not-a-site.json')
+  .expectJSON({ name: label, value: 'unknown' })
