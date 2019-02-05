18e6aca21b8d95031fb14a4c216bc6161da04873
pxgamer <owzie123@gmail.com>
2019-02-05 09:51:18 +0000

    Update tests to include mocks using nock

--- services/hsts/hsts.tester.js
+++ services/hsts/hsts.tester.js
@@ -1,6 +1,5 @@
 'use strict'
 
-const Joi = require('joi')
 const { ServiceTester } = require('..')
 const label = 'hsts'
 
@@ -11,16 +10,69 @@ const t = (module.exports = new ServiceTester({
 
 t.create('gets the hsts status of github')
   .get('/github.com.json')
-  .expectJSONTypes(Joi.object().keys({ name: label, value: 'preloaded' }))
+  .expectJSON({
+    name: label,
+    value: 'preloaded',
+  })
 
-t.create('gets the hsts status of apviz')
-  .get('/apviz.io.json')
-  .expectJSON({ name: label, value: 'pending' })
+t.create('gets the hsts status of httpforever')
+  .get('/httpforever.com.json')
+  .expectJSON({
+    name: label,
+    value: 'unknown',
+  })
 
-t.create('gets the hsts status of baidu')
-  .get('/baidu.com.json')
-  .expectJSON({ name: label, value: 'unknown' })
-
-t.create('invalid uri')
+t.create('gets the status of an invalid uri')
   .get('/does-not-exist.json')
-  .expectJSON({ name: label, value: 'unknown' })
+  .expectJSON({
+    name: label,
+    value: 'unknown',
+  })
+
+t.create('gets the hsts status of github (mock)')
+  .get('/github.com.json')
+  .intercept(nock =>
+    nock('https://hstspreload.org')
+      .get('/api/v2/status?domain=github.com')
+      .reply(200, { name: 'github.com', status: 'preloaded', bulk: false })
+  )
+  .expectJSON({
+    name: label,
+    value: 'preloaded',
+  })
+
+t.create('gets the hsts status of httpforever (mock)')
+  .get('/httpforever.com.json')
+  .intercept(nock =>
+    nock('https://hstspreload.org')
+      .get('/api/v2/status?domain=httpforever.com')
+      .reply(200, { name: 'httpforever.com', status: 'unknown', bulk: false })
+  )
+  .expectJSON({
+    name: label,
+    value: 'unknown',
+  })
+
+t.create('gets the hsts status of a pending site (mock)')
+  .get('/pending.mock.json')
+  .intercept(nock =>
+    nock('https://hstspreload.org')
+      .get('/api/v2/status?domain=pending.mock')
+      .reply(200, { name: 'pending.mock', status: 'pending', bulk: false })
+  )
+  .expectJSON({
+    name: label,
+    value: 'pending',
+  })
+
+t.create('gets the status of an invalid uri (mock)')
+  .get('/does-not-exist.json')
+  .intercept(nock =>
+    nock('https://hstspreload.org')
+      .get('/api/v2/status?domain=does-not-exist')
+      .reply(200, { name: 'does-not-exist', status: 'unknown', bulk: false })
+  )
+  .expectJSON({
+    name: label,
+    value: 'unknown',
+  })
