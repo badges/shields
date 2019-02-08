791f430c88594f70e09b9a7fe6b503a5df509c6f
pxgamer <owzie123@gmail.com>
2019-02-08 12:34:35 +0000

    Add documentation to the badge listing

--- services/hsts/hsts.service.js
+++ services/hsts/hsts.service.js
@@ -8,6 +8,20 @@ const schema = Joi.object({
   status: Joi.string().required(),
 }).required()
 
+const documentation = `
+<p>
+  <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security">
+  <code>Strict-Transport-Security</code> is an HTTP response header</a> that signals that browsers should
+  only access the site using HTTPS.
+</p>
+<p>
+  For a higher level of security, it's possible for a domain owner to
+  <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security#Preloading_Strict_Transport_Security">preload
+  this behavior into participating web browsers</a>. Chromium maintains the <a href="https://www.chromium.org/hsts">HSTS preload list</a>, which
+  is the de facto standard that has been adopted by several browsers. This service checks a domain's status in that list.
+</p>
+`
+
 module.exports = class HSTS extends BaseJsonService {
   static get category() {
     return 'monitoring'
@@ -27,6 +41,7 @@ module.exports = class HSTS extends BaseJsonService {
         namedParams: { domain: 'github.com' },
         staticPreview: this.render({ status: 'preloaded' }),
         keywords: ['security'],
+        documentation,
       },
     ]
   }
