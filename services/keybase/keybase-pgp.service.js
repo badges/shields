3e54227a317f5d93dd75e199c066a04e224b89a9
Paul-Emmanuel Raoul <skyper@skyplabs.net>
2019-02-09 11:17:53 +0000

    Add Keybase PGP badge

---
+++ services/keybase/keybase-pgp.service.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const { BaseJsonService } = require('..')
+
+const Joi = require('joi')
+const schema = Joi.object({
+  them: Joi.array().items(
+    Joi.object({
+      public_keys: {
+        primary: {
+          key_fingerprint: Joi.string().required(),
+        },
+      },
+    })
+  ),
+}).required()
+
+module.exports = class KeybasePGP extends BaseJsonService {
+  static get category() {
+    return 'social'
+  }
+
+  static get route() {
+    return {
+      base: 'keybase/pgp',
+      pattern: ':username',
+    }
+  }
+
+  static get defaultBadgeData() {
+    return { label: 'pgp' }
+  }
+
+  async handle({ username }) {
+    const data = await this.fetch({ username })
+    const fingerprint = data.them[0].public_keys.primary.key_fingerprint
+
+    return this.constructor.render(fingerprint)
+  }
+
+  async fetch({ username }) {
+    return this._requestJson({
+      schema,
+      url: `https://keybase.io/_/api/1.0/user/lookup.json?usernames=${username}`,
+    })
+  }
+
+  static render(fingerprint) {
+    return {
+      message: `${fingerprint}`,
+      color: 'brightgreen',
+    }
+  }
+}
