93a209f8a42eef91651a56cbc4db0e50983170ef
chris48s <chris48s@users.noreply.github.com>
2019-02-10 21:42:35 +0000

    refactor [amo] service (#2972)
    * refactor [amo] service
    
    * change colour

---
+++ services/amo/amo-version.service.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const { renderVersionBadge } = require('../../lib/version')
+const { BaseAmoService, keywords } = require('./amo-base')
+
+module.exports = class AmoVersion extends BaseAmoService {
+  static get category() {
+    return 'version'
+  }
+
+  static get route() {
+    return {
+      base: 'amo/v',
+      pattern: ':addonId',
+    }
+  }
+
+  static get examples() {
+    return [
+      {
+        title: 'Mozilla Add-on',
+        namedParams: { addonId: 'dustman' },
+        staticPreview: renderVersionBadge({ version: '2.1.0' }),
+        keywords,
+      },
+    ]
+  }
+
+  async handle({ addonId }) {
+    const data = await this.fetch({ addonId })
+    return renderVersionBadge({ version: data.addon.version })
+  }
+}
