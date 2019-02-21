2b0433d10019a1b41b576c03eced7e8ab0d80f02
Manuel Barkhau <mbarkhau@gmail.com>
2019-02-21 16:57:44 +0100

    change order of static url in usage.js

--- frontend/components/usage.js
+++ frontend/components/usage.js
@@ -175,17 +175,17 @@ export default class Usage extends React.PureComponent {
 
         <VerticalSpace />
 
-        <p>Using query string parameters (beta)</p>
-        <p>
-          <Snippet
-            snippet={`${baseUrl}/static/v1.svg?label=<LABEL>&message=<MESSAGE>&color=<COLOR>`}
-          />
-        </p>
         <p>Using dash "-" separator</p>
         <p>
           <Snippet snippet={`${baseUrl}/badge/<LABEL>-<MESSAGE>-<COLOR>.svg`} />
         </p>
         {this.constructor.renderStaticBadgeEscapingRules()}
+        <p>Using query string parameters</p>
+        <p>
+          <Snippet
+            snippet={`${baseUrl}/static/v1.svg?label=<LABEL>&message=<MESSAGE>&color=<COLOR>`}
+          />
+        </p>
 
         <H3 id="colors">Colors</H3>
         <p>
