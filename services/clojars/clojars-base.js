86a30f6916a9dbdab16135bcf0fe672557544977
Caleb Cartwright <calebcartwright@users.noreply.github.com>
2019-03-01 15:56:34 -0600

    refactor(sourceforge): ref. sourceforge and add folder test (#3127)
    doc: updated tutorial links and code snippets (#3124)
    
    Refactor [Coveralls] (#3130)
    
    * refactor(coveralls)
    
    * chore: added comment with link to api
    
    * doc: updated coveralls api doc comment
    
    * doc: updated coveralls api doc comment
    
    update clojars endpoint
    
    Updates clojars badge to use api endpoints
    
    Added release endpoint which returns latest stable release
    
    Updates version endpoint which will now also return snapshots if available or latest stable release
    
    check for invalid version and update variable send to render
    
    import InvalidResponse
    
    update require
    
    make pretty
    
    switch to prettyMessage
    
    remove metadata lines
    
    initial base class
    
    initial base class
    
    update schema
    
    update ClojarsDownloads base class
    
    simplify version classes
    
    remove transform from export
    
    update schema
    
    remove unused var
    
    replace == with ===
    
    remove extra line
    
    fix versionColor ?
    
    fix lint error
    
    make pretty
    
    refactor transform
    
    refactor transform and update tests
    
    add error messagest to download service
    
    fix error messages
    
    remove errorSchema, revert changes to tests
    
    refactor schema
    
    update ClojarsDownloads base class
    
    simplify version classes
    
    remove transform from export
    
    remove unused var
    
    fix lint error
    
    make pretty
    
    refactor transform and update tests
    
    add error messagest to download service
    
    fix error messages
    
    remove errorSchema, revert changes to tests
    
    refactor schema

---
+++ services/clojars/clojars-base.js
@@ -0,0 +1,64 @@
+'use strict'
+
+const Joi = require('joi')
+const { nonNegativeInteger } = require('../validators')
+const { BaseJsonService, InvalidResponse } = require('..')
+
+const clojarsSchema = Joi.object({
+  downloads: nonNegativeInteger,
+  latest_version: Joi.string().required(),
+  latest_release: Joi.string().required(),
+}).required()
+
+class BaseClojarsService extends BaseJsonService {
+  async fetch({ clojar }) {
+    const url = `https://clojars.org/api/artifacts/${clojar}`
+    return this._requestJson({
+      url,
+      schema: clojarsSchema,
+      errorMessages: {
+        404: 'not found',
+      },
+    })
+  }
+}
+
+class ClojarsVersionService extends BaseClojarsService {
+  static render({ clojar, version }) {
+    return {
+      message: `[${clojar} "${version}"]`,
+      color: versionColor(version),
+    }
+  }
+
+  static transform(json, version) {
+    if (version === 'release') {
+      return { version: json.latest_release }
+    } else if (version === 'version') {
+      return { version: json.latest_version }
+    } else {
+      throw new InvalidResponse({
+        prettyMessage: new Error('version is invalid or null'),
+      })
+    }
+  }
+
+  static get defaultBadgeData() {
+    return { label: 'clojars' }
+  }
+
+  static get category() {
+    return 'version'
+  }
+
+  static get examples() {
+    return [
+      {
+        namedParams: { clojar: 'prismic' },
+        staticPreview: this.render({ clojar: 'clojar', version: '1.2' }),
+      },
+    ]
+  }
+}
+
+module.exports = { BaseClojarsService, ClojarsVersionService }
--- services/clojars/clojars-release.service.js
+++ services/clojars/clojars-release.service.js
@@ -2,16 +2,16 @@
 
 const { ClojarsVersionService } = require('./clojars-base')
 
-module.exports = class ClojarsVersion extends ClojarsVersionService {
+module.exports = class ClojarsRelease extends ClojarsVersionService {
   async handle({ clojar }) {
     const json = await this.fetch({ clojar })
-    const { version } = this.constructor.transform(json, 'version')
+    const { version } = this.constructor.transform(json, 'release')
     return this.constructor.render({ clojar, version })
   }
 
   static get route() {
     return {
-      base: 'clojars/v',
+      base: 'clojars/r',
       pattern: ':clojar+',
     }
   }
