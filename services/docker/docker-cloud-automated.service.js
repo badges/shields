d5d499d3429fcdb9dd06885274449f7ce1ac5dfd
Fernandez Ludovic <lfernandez.dev@gmail.com>
2019-03-02 23:21:52 +0100

    feat: docker cloud build and automated.

---
+++ services/docker/docker-cloud-automated.service.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const Joi = require('joi')
+const { BaseJsonService } = require('..')
+const { dockerBlue, buildDockerUrl } = require('./docker-helpers')
+
+const cloudAutomatedBuildSchema = Joi.object({
+  objects: Joi.array()
+    .items(Joi.object({ build_settings: Joi.array() }).required())
+    .required(),
+}).required()
+
+module.exports = class DockerCloudAutomatedBuild extends BaseJsonService {
+  async fetch({ user, repo }) {
+    return this._requestJson({
+      schema: cloudAutomatedBuildSchema,
+      url: `https://cloud.docker.com/api/build/v1/source/?image=${user}/${repo}`,
+      errorMessages: { 404: 'repo not found' },
+    })
+  }
+
+  static render({ buildSettings }) {
+    if (buildSettings.length >= 1) {
+      return { message: 'automated', color: dockerBlue }
+    } else {
+      return { message: 'manual', color: 'yellow' }
+    }
+  }
+
+  async handle({ user, repo }) {
+    const data = await this.fetch({ user, repo })
+    return this.constructor.render({
+      buildSettings: data.objects[0].build_settings,
+    })
+  }
+
+  static get category() {
+    return 'build'
+  }
+
+  static get route() {
+    return buildDockerUrl('cloud-automated')
+  }
+
+  static get defaultBadgeData() {
+    return { label: 'docker build' }
+  }
+
+  static get examples() {
+    return [
+      {
+        title: 'Docker (Cloud) Automated build',
+        namedParams: {
+          user: 'jrottenberg',
+          repo: 'ffmpeg',
+        },
+        staticPreview: this.render({ buildSettings: ['test'] }),
+      },
+    ]
+  }
+}
---
+++ services/docker/docker-cloud-build.service.js
@@ -0,0 +1,61 @@
+'use strict'
+
+const Joi = require('joi')
+const { BaseJsonService } = require('..')
+const { dockerBlue, buildDockerUrl } = require('./docker-helpers')
+
+const cloudBuildSchema = Joi.object({
+  objects: Joi.array()
+    .items(Joi.object({ state: Joi.string() }).required())
+    .required(),
+}).required()
+
+module.exports = class DockerCloudBuild extends BaseJsonService {
+  async fetch({ user, repo }) {
+    return this._requestJson({
+      schema: cloudBuildSchema,
+      url: `https://cloud.docker.com/api/build/v1/source/?image=${user}/${repo}`,
+      errorMessages: { 404: 'repo not found' },
+    })
+  }
+
+  static render({ state }) {
+    if (state === 'Success') {
+      return { message: 'passing', color: 'brightgreen' }
+    }
+    if (state === 'Failed') {
+      return { message: 'failing', color: 'red' }
+    }
+    return { message: 'building', color: dockerBlue }
+  }
+
+  async handle({ user, repo }) {
+    const data = await this.fetch({ user, repo })
+    return this.constructor.render({ state: data.objects[0].state })
+  }
+
+  static get category() {
+    return 'build'
+  }
+
+  static get route() {
+    return buildDockerUrl('cloud-build')
+  }
+
+  static get defaultBadgeData() {
+    return { label: 'docker build' }
+  }
+
+  static get examples() {
+    return [
+      {
+        title: 'Docker (Cloud) Build Status',
+        namedParams: {
+          user: 'jrottenberg',
+          repo: 'ffmpeg',
+        },
+        staticPreview: this.render({ state: 'Success' }),
+      },
+    ]
+  }
+}
