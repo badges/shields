5021c813d63ba09428cd5a2ae3f8e4537ea845f3
Anand Chowdhary <mail@anandchowdhary.com>
2019-03-08 10:56:18 +0100

    Edited using GitWriter (Chrome 72 on Mac OS 10.14.3 in Enschede, NL)

--- services/netlify/netlify.tester.js
+++ services/netlify/netlify.tester.js
@@ -4,7 +4,7 @@ const { isColorMatcher } = require('../color-matcher')
 const t = (module.exports = require('../tester').createServiceTester())
 
 t.create('netlify (valid)')
-  .get('/0028c6a7-b7ae-49f6-b847-917b40b5b13f')
+  .get('/0028c6a7-b7ae-49f6-b847-917b40b5b13f.json')
   .expectBadge({
     label: 'netlify',
     message: isColorMatcher,
