76140306500f88630865699cfee0b5dcf944cfe9
Paul Melnikow <email@paulmelnikow.com>
2019-03-08 14:46:58 -0500

    Ehh maybe we should keep these; they show everything is wired up correctly

--- services/nodeping/nodeping-status.tester.js
+++ services/nodeping/nodeping-status.tester.js
@@ -37,3 +37,37 @@ t.create('NodePing status - down (mock)')
     label: 'Status',
     message: 'down',
   })
+
+t.create('NodePing status - custom up color/message')
+  .get(
+    '/jkiwn052-ntpp-4lbb-8d45-ihew6d9ucoei.json?style=_shields_test&up_color=blue&up_message=happy'
+  )
+  .intercept(nock =>
+    nock('https://nodeping.com')
+      .get(
+        '/reports/results/jkiwn052-ntpp-4lbb-8d45-ihew6d9ucoei/1?format=json'
+      )
+      .reply(200, [{ su: true }])
+  )
+  .expectBadge({
+    label: 'Status',
+    message: 'happy',
+    color: 'blue',
+  })
+
+t.create('NodePing status - custom down color/message')
+  .get(
+    '/jkiwn052-ntpp-4lbb-8d45-ihew6d9ucoei.json?style=_shields_test&down_color=yellow&down_message=sad'
+  )
+  .intercept(nock =>
+    nock('https://nodeping.com')
+      .get(
+        '/reports/results/jkiwn052-ntpp-4lbb-8d45-ihew6d9ucoei/1?format=json'
+      )
+      .reply(200, [{ su: false }])
+  )
+  .expectBadge({
+    label: 'Status',
+    message: 'sad',
+    color: 'yellow',
+  })
