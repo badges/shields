badges/shields

commit 3b5a14b207878bcd2377f613b392399ee58a5af6

Add custom error message
Co-Authored-By: AnandChowdhary <mail@anandchowdhary.com>

services/netlify/netlify.service.js
@@ -42,7 +42,9 @@ module.exports = class Netlify extends BaseSvgScrapingService {
     const url = `https://api.netlify.com/api/v1/badges/${appId}/deploy-status`
     return this._requestSvg({
       schema,
-      url,
+      errorMessages: {
+        404: 'app not found',
+      },
       valueMatcher: /(?<=fill-rule="nonzero" stroke=").([^"]+)/,
     })
   }

commit f1b4a49616709fe5c181ab442ef7d53383b65913

Add URL to fetch (broke in 3b5a14b)

services/netlify/netlify.service.js
@@ -45,6 +45,7 @@ module.exports = class Netlify extends BaseSvgScrapingService {
       errorMessages: {
         404: 'app not found',
       },
+      url,
       valueMatcher: /(?<=fill-rule="nonzero" stroke=").([^"]+)/,
     })
   }