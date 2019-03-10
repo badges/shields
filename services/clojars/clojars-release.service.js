a80e0bbdc813caefd56ec892d47ff40203585691
Matthew Ratzke <matt@degree9.io>
2019-03-10 14:08:07 -0600

    fix prettier formatting

--- services/clojars/clojars-release.service.js
+++ services/clojars/clojars-release.service.js
@@ -5,7 +5,10 @@ const { BaseClojarsVersionService } = require('./clojars-base')
 module.exports = class ClojarsVersionRelease extends BaseClojarsVersionService {
   async handle({ clojar }) {
     const json = await this.fetch({ clojar })
-    return this.constructor.render({ clojar, version: (json.latest_release ? json.latest_release : json.latest_version) })
+    return this.constructor.render({
+      clojar,
+      version: json.latest_release ? json.latest_release : json.latest_version,
+    })
   }
 
   static get route() {
