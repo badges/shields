30268e27f6370580f6dcada7426ec6114928a437
Prouser123 <jcahill773@gmail.com>
2019-03-19 08:52:33 +0000

    Split tests

---
+++ services/packagist/packagist-php-version.tester.js
@@ -0,0 +1,16 @@
+'use strict'
+
+const { isComposerVersion } = require('../test-validators')
+const t = (module.exports = require('../tester').createServiceTester())
+
+t.create('gets the package version of symfony')
+  .get('/symfony/symfony.json')
+  .expectBadge({ label: 'php', message: isComposerVersion })
+
+t.create('gets the package version of symfony 2.8')
+  .get('/symfony/symfony/v2.8.0.json')
+  .expectBadge({ label: 'php', message: isComposerVersion })
+
+t.create('invalid package name')
+  .get('/frodo/is-not-a-package.json')
+  .expectBadge({ label: 'php', message: 'not found' })
