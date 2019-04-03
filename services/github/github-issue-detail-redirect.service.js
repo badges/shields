7f24e9367111bd26a9219f3e18c3766185325734
Caleb Cartwright <calebcartwright@users.noreply.github.com>
2019-04-03 11:59:49 -0500

    refactor [GithubIssueDetail] (#3259)
    * refactor(GithubIssueDetail): ported to new service model
    
    * refactor(GithubIssueDetail): updates based on PR discussions
    
    * refactor(GithubIssueDetail): minor updates and additional tests
    
    * refactor(GithubIssueDetail): minor updates to render functions per PR disc.

---
+++ services/github/github-issue-detail-redirect.service.js
@@ -0,0 +1,33 @@
+'use strict'
+
+const { redirector } = require('..')
+
+const whichMap = {
+  s: 'state',
+  u: 'author',
+}
+
+module.exports = [
+  redirector({
+    category: 'issue-tracking',
+    route: {
+      base: 'github',
+      pattern:
+        ':issueKind(issues|pulls)/detail/:which(s|u)/:user/:repo/:number([0-9]+)',
+    },
+    transformPath: ({ which, user, repo, number }) =>
+      `/github/issues/detail/${whichMap[which]}/${user}/${repo}/${number}`,
+    dateAdded: new Date('2019-04-04'),
+  }),
+  redirector({
+    category: 'issue-tracking',
+    route: {
+      base: 'github/pulls/detail',
+      pattern:
+        ':which(state|title|author|label|comments|age|last-update)/:user/:repo/:number([0-9]+)',
+    },
+    transformPath: ({ which, user, repo, number }) =>
+      `/github/issues/detail/${which}/${user}/${repo}/${number}`,
+    dateAdded: new Date('2019-04-04'),
+  }),
+]
