18b2067829b612bd2062c84fe1b992f3b5638efd
chris48s <chris.shaw480@gmail.com>
2019-04-06 23:05:58 +0100

    refactor [puppetforge] service, add tests
    This migrates the puppetforge-modules from legacy
    services to the new service arch
    There are also some changes to the puppetforge-users
    badges, but its just moving code around

---
+++ services/puppetforge/puppetforge-module-downloads.tester.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const { isMetric } = require('../test-validators')
+const t = (module.exports = require('../tester').createServiceTester())
+
+t.create('module downloads')
+  .get('/camptocamp/openssl.json')
+  .expectBadge({
+    label: 'downloads',
+    message: isMetric,
+  })
+
+t.create('module downloads (not found)')
+  .get('/notarealuser/notarealpackage.json')
+  .expectBadge({
+    label: 'downloads',
+    message: 'not found',
+  })
---
+++ services/puppetforge/puppetforge-module-endorsement.service.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const { NotFound } = require('..')
+const { BasePuppetForgeModulesService } = require('./puppetforge-base')
+
+module.exports = class PuppetforgeModuleEndorsement extends BasePuppetForgeModulesService {
+  static get category() {
+    return 'rating'
+  }
+
+  static get route() {
+    return {
+      base: 'puppetforge/e',
+      pattern: ':user/:moduleName',
+    }
+  }
+
+  static get examples() {
+    return [
+      {
+        title: 'Puppet Forge endorsement',
+        namedParams: {
+          user: 'camptocamp',
+          moduleName: 'openssl',
+        },
+        staticPreview: this.render({ endorsement: 'approved' }),
+      },
+    ]
+  }
+
+  static get defaultBadgeData() {
+    return { label: 'endorsement' }
+  }
+
+  static render({ endorsement }) {
+    let color
+    if (endorsement === 'approved') {
+      color = 'green'
+    } else if (endorsement === 'supported') {
+      color = 'brightgreen'
+    } else {
+      color = 'red'
+    }
+    return { message: endorsement, color }
+  }
+
+  async handle({ user, moduleName }) {
+    const { endorsement } = await this.fetch({ user, moduleName })
+    if (endorsement == null) {
+      throw new NotFound({ prettyMessage: 'none' })
+    }
+    return this.constructor.render({ endorsement })
+  }
+}
