ff17086668140c100e01076a843081072acf221a
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2019-04-09 18:20:39 +0200

    E2e test for badge suggestion

--- cypress/integration/main-page.spec.js
+++ cypress/integration/main-page.spec.js
@@ -8,4 +8,15 @@ describe('Main page', function() {
 
     cy.contains('PyPI - License')
   })
+
+  it('Suggest badges', function() {
+    cy.visit('/')
+
+    cy.get('input[placeholder="search / project URL"]').type(
+      'https://github.com/badges/shields'
+    )
+    cy.contains('Suggest badges').click()
+
+    cy.contains('/github/issues/badges/shields.svg')
+  })
 })
