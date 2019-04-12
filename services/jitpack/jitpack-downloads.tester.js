1adf9958fa68bc52b0246ed7515f034b704ee357
Eray Erdin <eraygezer.94@gmail.com>
2019-04-12 14:06:36 +0300

    used isMetricOverTimePeriod in jitpack-downloads.tester

--- services/jitpack/jitpack-downloads.tester.js
+++ services/jitpack/jitpack-downloads.tester.js
@@ -1,16 +1,15 @@
 'use strict'
 
 const t = (module.exports = require('../tester').createServiceTester())
-
-const isMetric = /^.*\/(week|month)/
+const { isMetricOverTimePeriod } = require('../test-validators')
 
 t.create('weekly (github)')
   .get('/dw/github/erayerdin/kappdirs.json')
-  .expectBadge({ label: 'downloads', message: isMetric })
+  .expectBadge({ label: 'downloads', message: isMetricOverTimePeriod })
 
 t.create('monthly (github)')
   .get('/dm/github/erayerdin/kappdirs.json')
-  .expectBadge({ label: 'downloads', message: isMetric })
+  .expectBadge({ label: 'downloads', message: isMetricOverTimePeriod })
 
 t.create('unknown package (github)')
   .get('/dw/github/some-bogus-user/project.json')
