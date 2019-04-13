b7bf1de90bc7cbe0587c2ab96abb677de3662d85
Paul Melnikow <email@paulmelnikow.com>
2019-04-13 16:13:17 -0400

    Split up [codeclimate] tests

--- services/codeclimate/codeclimate-analysis.tester.js
+++ services/codeclimate/codeclimate-analysis.tester.js
@@ -5,54 +5,11 @@ const { ServiceTester } = require('../tester')
 const { isIntegerPercentage } = require('../test-validators')
 
 const t = (module.exports = new ServiceTester({
-  id: 'codeclimate',
+  id: 'CodeClimateCoverage',
   title: 'Code Climate',
+  pathPrefix: '/codeclimate',
 }))
 
-// Tests based on Code Climate's test reports endpoint.
-t.create('test coverage percentage')
-  .get('/c/jekyll/jekyll.json')
-  .expectBadge({
-    label: 'coverage',
-    message: isIntegerPercentage,
-  })
-
-t.create('test coverage percentage alternative coverage URL')
-  .get('/coverage/jekyll/jekyll.json')
-  .expectBadge({
-    label: 'coverage',
-    message: isIntegerPercentage,
-  })
-
-t.create('test coverage percentage alternative top-level URL')
-  .get('/jekyll/jekyll.json')
-  .expectBadge({
-    label: 'coverage',
-    message: isIntegerPercentage,
-  })
-
-t.create('test coverage letter')
-  .get('/c-letter/jekyll/jekyll.json')
-  .expectBadge({
-    label: 'coverage',
-    message: Joi.equal('A', 'B', 'C', 'D', 'E', 'F'),
-  })
-
-t.create('test coverage percentage for non-existent repo')
-  .get('/c/unknown/unknown.json')
-  .expectBadge({
-    label: 'coverage',
-    message: 'not found',
-  })
-
-t.create('test coverage percentage for repo without test reports')
-  .get('/c/angular/angular.js.json')
-  .expectBadge({
-    label: 'coverage',
-    message: 'unknown',
-  })
-
-// Tests based on Code Climate's snapshots endpoint.
 t.create('issues count')
   .get('/issues/angular/angular.js.json')
   .expectBadge({
