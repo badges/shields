2fdc475b26baeb53ebb96808bf6e7cd244febcc0
calebcartwright <caleb.cartwright@outlook.com>
2019-04-14 14:04:53 -0500

    refactor(JenkinsBuild): ported to new service model

---
+++ services/jenkins/jenkins-base.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const { BaseJsonService } = require('..')
+const serverSecrets = require('../../lib/server-secrets')
+
+module.exports = class JenkinsBase extends BaseJsonService {
+  async fetch({
+    url,
+    schema,
+    qs,
+    errorMessages = { 404: 'instance or job not found' },
+  }) {
+    const options = { qs, strictSSL: false }
+
+    if (serverSecrets.jenkins_user) {
+      options.auth = {
+        user: serverSecrets.jenkins_user,
+        pass: serverSecrets.jenkins_pass,
+      }
+    }
+
+    return this._requestJson({
+      url,
+      options,
+      schema,
+      errorMessages,
+    })
+  }
+}
--- services/jenkins/jenkins-build.service.js
+++ services/jenkins/jenkins-build.service.js
@@ -1,24 +1,66 @@
 'use strict'
 
-const LegacyService = require('../legacy-service')
-const { makeBadgeData: getBadgeData } = require('../../lib/badge-data')
-const serverSecrets = require('../../lib/server-secrets')
+const Joi = require('joi')
+const { renderBuildStatusBadge } = require('../build-status')
+const JenkinsBase = require('./jenkins-base')
 
-// This legacy service should be rewritten to use e.g. BaseJsonService.
-//
-// Tips for rewriting:
-// https://github.com/badges/shields/blob/master/doc/rewriting-services.md
-//
-// Do not base new services on this code.
-module.exports = class JenkinsBuild extends LegacyService {
+// https://github.com/jenkinsci/jenkins/blob/master/core/src/main/java/hudson/model/BallColor.java#L56
+const schema = Joi.object({
+  color: Joi.allow(
+    'red',
+    'red_anime',
+    'yellow',
+    'yellow_anime',
+    'blue',
+    'blue_anime',
+    // green included for backwards compatibility
+    'green',
+    'green_anime',
+    'grey',
+    'grey_anime',
+    'disabled',
+    'disabled_anime',
+    'aborted',
+    'aborted_anime',
+    'notbuilt',
+    'notbuilt_anime'
+  ).required(),
+}).required()
+
+const colorStatusMap = {
+  red: 'failing',
+  red_anime: 'building',
+  yellow: 'unstable',
+  yellow_anime: 'building',
+  blue: 'passing',
+  blue_anime: 'building',
+  green: 'passing',
+  green_anime: 'building',
+  grey: 'not built',
+  grey_anime: 'building',
+  disabled: 'not built',
+  disabled_anime: 'building',
+  aborted: 'not built',
+  aborted_anime: 'building',
+  notbuilt: 'not built',
+  notbuilt_anime: 'building',
+}
+
+module.exports = class JenkinsBuild extends JenkinsBase {
   static get category() {
     return 'build'
+  }
+
+  static get defaultBadgeData() {
+    return {
+      label: 'build',
+    }
   }
 
   static get route() {
     return {
       base: 'jenkins/s',
-      pattern: ':scheme(http|https)?/:host/:job*',
+      pattern: ':protocol(http|https)/:host+/:job+',
     }
   }
 
@@ -26,82 +68,40 @@ module.exports = class JenkinsBuild extends LegacyService {
     return [
       {
         title: 'Jenkins',
-        pattern: ':scheme/:host/:job',
         namedParams: {
-          scheme: 'https',
+          protocol: 'https',
           host: 'jenkins.qa.ubuntu.com',
           job:
             'view/Precise/view/All%20Precise/job/precise-desktop-amd64_default',
         },
-        staticPreview: {
-          label: 'build',
-          message: 'passing',
-          color: 'brightgreen',
-        },
+        staticPreview: renderBuildStatusBadge({ status: 'passing' }),
       },
     ]
   }
 
-  static registerLegacyRouteHandler({ camp, cache }) {
-    camp.route(
-      /^\/jenkins(?:-ci)?\/s\/(http(?:s)?)\/([^/]+)\/(.+)\.(svg|png|gif|jpg|json)$/,
-      cache((data, match, sendBadge, request) => {
-        const scheme = match[1] // http(s)
-        const host = match[2] // example.org:8080
-        const job = match[3] // folder/job
-        const format = match[4]
-        const options = {
-          json: true,
-          uri: `${scheme}://${host}/job/${job}/api/json?tree=color`,
-        }
-        if (job.indexOf('/') > -1) {
-          options.uri = `${scheme}://${host}/${job}/api/json?tree=color`
-        }
+  static render({ status }) {
+    if (status === 'unstable') {
+      return {
+        message: status,
+        color: 'yellow',
+      }
+    }
 
-        if (serverSecrets.jenkins_user) {
-          options.auth = {
-            user: serverSecrets.jenkins_user,
-            pass: serverSecrets.jenkins_pass,
-          }
-        }
+    return renderBuildStatusBadge({ status })
+  }
 
-        const badgeData = getBadgeData('build', data)
-        request(options, (err, res, json) => {
-          if (err !== null) {
-            badgeData.text[1] = 'inaccessible'
-            sendBadge(format, badgeData)
-            return
-          }
+  transform({ json }) {
+    return { status: colorStatusMap[json.color] }
+  }
 
-          try {
-            if (json.color === 'blue' || json.color === 'green') {
-              badgeData.colorscheme = 'brightgreen'
-              badgeData.text[1] = 'passing'
-            } else if (json.color === 'red') {
-              badgeData.colorscheme = 'red'
-              badgeData.text[1] = 'failing'
-            } else if (json.color === 'yellow') {
-              badgeData.colorscheme = 'yellow'
-              badgeData.text[1] = 'unstable'
-            } else if (
-              json.color === 'grey' ||
-              json.color === 'disabled' ||
-              json.color === 'aborted' ||
-              json.color === 'notbuilt'
-            ) {
-              badgeData.colorscheme = 'lightgrey'
-              badgeData.text[1] = 'not built'
-            } else {
-              badgeData.colorscheme = 'lightgrey'
-              badgeData.text[1] = 'building'
-            }
-            sendBadge(format, badgeData)
-          } catch (e) {
-            badgeData.text[1] = 'invalid'
-            sendBadge(format, badgeData)
-          }
-        })
-      })
-    )
+  async handle({ protocol, host, job }) {
+    const jobPrefix = job.indexOf('/') > -1 ? '' : 'job/'
+    const json = await this.fetch({
+      url: `${protocol}://${host}/${jobPrefix}${job}/api/json`,
+      schema,
+      qs: { tree: 'color' },
+    })
+    const { status } = this.transform({ json })
+    return this.constructor.render({ status })
   }
 }
