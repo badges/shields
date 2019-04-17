321d654ab780db230a6f0abd3dac3fbfc6ed0128
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2019-04-17 18:00:37 +0200

    Revert "getExampleWithServiceByPattern helper added"
    This reverts commit 80839fd705c036014adef97c76e40148ed6f695b.

--- frontend/lib/service-definitions/index.spec.js
+++ frontend/lib/service-definitions/index.spec.js
@@ -1,32 +1,11 @@
 import { expect } from 'chai'
 import { test, given } from 'sazerac'
-import {
-  findCategory,
-  getDefinitionsForCategory,
-  getExampleWithServiceByPattern,
-} from './index'
+import { findCategory, getDefinitionsForCategory } from './index'
 
 describe('Service definition helpers', function() {
   test(findCategory, () => {
     given('build').expect({ id: 'build', name: 'Build' })
     given('foo').expect(undefined)
-  })
-
-  describe('getExampleWithServiceByPattern', function() {
-    it('returns example with service', function() {
-      const result = getExampleWithServiceByPattern('/travis/:user/:repo')
-
-      expect(result.example.title).to.equal('Travis (.org)')
-      expect(result.service.name).to.equal('TravisBuild')
-    })
-
-    it('returns undefined when there is no example with given patten', function() {
-      const result = getExampleWithServiceByPattern(
-        '/unknown-pattern/:user/:repo'
-      )
-
-      expect(result).to.be.undefined
-    })
   })
 
   it('getDefinitionsForCategory', function() {
