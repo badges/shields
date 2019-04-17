3d2c0fed0115601d8f7b6f4a8526cd2a8b9b75c2
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2019-04-17 21:07:44 +0200

    More e2e tests for suggestion feature

--- cypress/integration/main-page.spec.js
+++ cypress/integration/main-page.spec.js
@@ -1,6 +1,8 @@
 'use strict'
 
 describe('Main page', function() {
+  const backendUrl = Cypress.env('backend_url')
+
   it('Search for badges', function() {
     cy.visit('/')
 
@@ -10,6 +12,7 @@ describe('Main page', function() {
   })
 
   it('Suggest badges', function() {
+    const badgeUrl = `${backendUrl}/github/issues/badges/shields.svg`
     cy.visit('/')
 
     cy.get('input[placeholder="search / project URL"]').type(
@@ -17,6 +20,38 @@ describe('Main page', function() {
     )
     cy.contains('Suggest badges').click()
 
-    cy.contains('/github/issues/badges/shields.svg')
+    cy.contains('GitHub issues')
+    cy.get(`img[src='${badgeUrl}']`)
+    cy.contains(badgeUrl)
+  })
+
+  it('Customization form is filled with suggested badge details', function() {
+    const badgeUrl = `${backendUrl}/github/issues/badges/shields.svg`
+    cy.visit('/')
+    cy.get('input[placeholder="search / project URL"]').type(
+      'https://github.com/badges/shields'
+    )
+    cy.contains('Suggest badges').click()
+
+    cy.contains(badgeUrl).click()
+
+    cy.get('input[name="user"]').should('have.value', 'badges')
+    cy.get('input[name="repo"]').should('have.value', 'shields')
+  })
+
+  it('Customizate suggested badge', function() {
+    const badgeUrl = `${backendUrl}/github/issues/badges/shields.svg`
+    cy.visit('/')
+    cy.get('input[placeholder="search / project URL"]').type(
+      'https://github.com/badges/shields'
+    )
+    cy.contains('Suggest badges').click()
+    cy.contains(badgeUrl).click()
+
+    cy.get('table input[name="color"]').type('orange')
+
+    cy.get(
+      `img[src='${backendUrl}/github/issues/badges/shields.svg?color=orange']`
+    )
   })
 })
