f6874f08bc45fe35787f23a2dd8c622e5f9339b1
Paul Melnikow <email@paulmelnikow.com>
2019-04-23 00:21:57 -0400

    Rewrite [travisphpversion]

--- services/travis/travis-php-version.tester.js
+++ services/travis/travis-php-version.tester.js
@@ -1,26 +1,20 @@
 'use strict'
 
 const { isPhpVersionReduction } = require('../test-validators')
-const { ServiceTester } = require('../tester')
-
-const t = (module.exports = new ServiceTester({
-  id: 'travis-php-version',
-  title: 'PHP version from .travis.yml',
-  pathPrefix: '/travis',
-}))
+const t = (module.exports = require('../tester').createServiceTester())
 
 t.create('gets the package version of symfony')
-  .get('/php-v/symfony/symfony.json')
+  .get('/symfony/symfony.json')
   .expectBadge({ label: 'php', message: isPhpVersionReduction })
 
 t.create('gets the package version of symfony 2.8')
-  .get('/php-v/symfony/symfony/2.8.json')
+  .get('/symfony/symfony/2.8.json')
   .expectBadge({ label: 'php', message: isPhpVersionReduction })
 
 t.create('gets the package version of yii')
-  .get('/php-v/yiisoft/yii.json')
+  .get('/yiisoft/yii.json')
   .expectBadge({ label: 'php', message: isPhpVersionReduction })
 
 t.create('invalid package name')
-  .get('/php-v/frodo/is-not-a-package.json')
-  .expectBadge({ label: 'php', message: 'invalid' })
+  .get('/frodo/is-not-a-package.json')
+  .expectBadge({ label: 'php', message: 'repo not found' })
