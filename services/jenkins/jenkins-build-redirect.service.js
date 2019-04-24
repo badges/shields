c4c14e8775c2b35a28541b8d7e963afc32ecfc62
calebcartwright <caleb.cartwright@outlook.com>
2019-04-23 19:42:48 -0500

    chore: cleanup JenkinsBuild redirector

--- services/jenkins/jenkins-build-redirect.service.js
+++ services/jenkins/jenkins-build-redirect.service.js
@@ -2,25 +2,28 @@
 
 const { redirector } = require('..')
 
+const transformPath = ({ protocol, host, job }) =>
+  `/jenkins/build/${protocol}/${host}/${job}`
+
+const commonProps = {
+  category: 'build',
+  transformPath,
+  dateAdded: new Date('2019-04-20'),
+}
+
 module.exports = [
   redirector({
-    category: 'build',
     route: {
       base: 'jenkins-ci/s',
-      pattern: ':protocol(http|https)/:host+/:job+',
+      pattern: ':protocol(http|https)/:host/:job+',
     },
-    transformPath: ({ protocol, host, job }) =>
-      `/jenkins/build/${protocol}/${host}/${job}`,
-    dateAdded: new Date('2019-04-20'),
+    ...commonProps,
   }),
   redirector({
-    category: 'build',
     route: {
       base: 'jenkins/s',
-      pattern: ':protocol(http|https)/:host+/:job+',
+      pattern: ':protocol(http|https)/:host/:job+',
     },
-    transformPath: ({ protocol, host, job }) =>
-      `/jenkins/build/${protocol}/${host}/${job}`,
-    dateAdded: new Date('2019-04-20'),
+    ...commonProps,
   }),
 ]
