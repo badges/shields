4fbfe3d0b92a4701e21e51897e5d73d7b3b1d15d
Prouser123 <jcahill773@gmail.com>
2019-04-25 13:21:18 +0100

    use existing test validators and shorthand createservicetester

--- services/maven-central/maven-central.tester.js
+++ services/maven-central/maven-central.tester.js
@@ -1,29 +1,28 @@
 'use strict'
 
-const Joi = require('joi')
-const { ServiceTester } = require('../tester')
-
-const t = new ServiceTester({ id: 'maven-central', title: 'Maven Central' })
-module.exports = t
+const {
+  isVPlusDottedVersionNClausesWithOptionalSuffix,
+} = require('../test-validators')
+const t = (module.exports = require('../tester').createServiceTester())
 
 t.create('latest version')
-  .get('/v/com.github.fabriziocucci/yacl4j.json') // http://repo1.maven.org/maven2/com/github/fabriziocucci/yacl4j/
+  .get('/com.github.fabriziocucci/yacl4j.json') // http://repo1.maven.org/maven2/com/github/fabriziocucci/yacl4j/
   .expectBadge({
     label: 'maven-central',
-    message: Joi.string().regex(/^v(.*)$/),
+    message: isVPlusDottedVersionNClausesWithOptionalSuffix,
   })
 
 t.create('latest 0.8 version')
-  .get('/v/com.github.fabriziocucci/yacl4j/0.8.json') // http://repo1.maven.org/maven2/com/github/fabriziocucci/yacl4j/
+  .get('/com.github.fabriziocucci/yacl4j/0.8.json') // http://repo1.maven.org/maven2/com/github/fabriziocucci/yacl4j/
   .expectBadge({
     label: 'maven-central',
-    message: Joi.string().regex(/^v0\.8(.*)$/),
+    message: isVPlusDottedVersionNClausesWithOptionalSuffix,
   })
 
 t.create('inexistent artifact')
-  .get('/v/inexistent-group-id/inexistent-artifact-id.json')
+  .get('/inexistent-group-id/inexistent-artifact-id.json')
   .expectBadge({ label: 'maven-central', message: 'not found' })
 
 t.create('inexistent version prefix')
-  .get('/v/com.github.fabriziocucci/yacl4j/99.json')
+  .get('/com.github.fabriziocucci/yacl4j/99.json')
   .expectBadge({ label: 'maven-central', message: 'version prefix not found' })
