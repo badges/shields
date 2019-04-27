446ea5f5ace55d17b3d4fd35b1030baaadbfbc2d
Ville Skyttä <ville.skytta@iki.fi>
2019-04-27 23:13:56 +0300

    Add [Fedora] package version support
    https://apps.fedoraproject.org/mdapi/

---
+++ services/fedora/fedora.service.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const Joi = require('joi')
+const { renderVersionBadge } = require('../version')
+const { BaseJsonService } = require('..')
+
+const schema = Joi.object({
+  version: Joi.string().required(),
+}).required()
+
+// No way to permalink to current "stable", https://pagure.io/mdapi/issue/69
+const defaultBranch = 'rawhide'
+
+module.exports = class Fedora extends BaseJsonService {
+  static get category() {
+    return 'version'
+  }
+
+  static get route() {
+    return {
+      base: 'fedora/v',
+      pattern: ':branch?/:packageName',
+    }
+  }
+
+  static get examples() {
+    return [
+      {
+        title: 'Fedora package',
+        namedParams: { branch: 'rawhide', packageName: 'rpm' },
+        staticPreview: renderVersionBadge({ version: '4.14.2.1' }),
+        documentation:
+          'See <a href="https://apps.fedoraproject.org/mdapi/">mdapi docs</a> for information on valid branches.',
+      },
+    ]
+  }
+
+  static get defaultBadgeData() {
+    return { label: 'fedora' }
+  }
+
+  async handle({ branch = defaultBranch, packageName }) {
+    const data = await this._requestJson({
+      schema,
+      url: `https://apps.fedoraproject.org/mdapi/${branch}/pkg/${packageName}`,
+      errorMessages: {
+        400: 'branch not found',
+      },
+    })
+    return renderVersionBadge({ version: data.version })
+  }
+}
---
+++ services/fedora/fedora.tester.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const { isVPlusDottedVersionNClauses } = require('../test-validators')
+const t = (module.exports = require('../tester').createServiceTester())
+
+t.create('Fedora package (default branch, valid)')
+  .get('/rpm.json')
+  .expectBadge({
+    label: 'fedora',
+    message: isVPlusDottedVersionNClauses,
+  })
+
+t.create('Fedora package (valid, mocked response)')
+  .get('/rawhide/rpm.json')
+  .intercept(nock =>
+    nock('https://apps.fedoraproject.org')
+      .get('/mdapi/rawhide/pkg/rpm')
+      .reply(200, { version: '4.14.2.1' })
+  )
+  .expectBadge({ label: 'fedora', message: 'v4.14.2.1' })
+
+t.create('Fedora package (not found)')
+  .get('/rawhide/not-a-package.json')
+  .expectBadge({ label: 'fedora', message: 'not found' })
+
+t.create('Fedora package (branch not found)')
+  .get('/not-a-branch/not-a-package.json')
+  .expectBadge({ label: 'fedora', message: 'branch not found' })
