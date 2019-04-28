811ea7e3dda7b053d3bc8ffcef81429b5e6b28d3
Ville Skyttä <ville.skytta@iki.fi>
2019-04-28 12:23:05 +0300

    Add [Debian] package version support
    https://wiki.debian.org/Services/api.ftp-master.debian.org

---
+++ services/debian/debian.tester.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const { isVPlusDottedVersionNClauses } = require('../test-validators')
+const t = (module.exports = require('../tester').createServiceTester())
+
+t.create('Debian package (default distribution, valid)')
+  .get('/apt.json')
+  .expectBadge({ label: 'debian', message: isVPlusDottedVersionNClauses })
+
+t.create('Debian package (valid, mocked response)')
+  .get('/unstable/apt.json')
+  .intercept(nock =>
+    nock('https://api.ftp-master.debian.org')
+      .get('/madison?f=json&s=unstable&package=apt')
+      .reply(200, [
+        {
+          apt: { unstable: { '1.8.0': { source: 'apt', component: 'main' } } },
+        },
+      ])
+  )
+  .expectBadge({ label: 'debian', message: 'v1.8.0' })
+
+t.create('Debian package (not found)')
+  .get('/stable/not-a-package.json')
+  .expectBadge({ label: 'debian', message: 'not found' })
+
+t.create('Debian package (distribution not found)')
+  .get('/not-a-distribution/apt.json')
+  .expectBadge({ label: 'debian', message: 'not found' })
