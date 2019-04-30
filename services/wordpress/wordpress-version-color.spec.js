7980a65d7f7512478f1c38d8dcbc37f6d9a62d59
Paul Melnikow <github@paulmelnikow.com>
2019-04-29 21:52:57 -0400

    [WordpressPlatformTests] Support tested version without trailing ".0" (#3396)
    See https://circleci.com/gh/badges/shields/52378 for an example failure,
    where a value of `5.2` is triggering "TypeError: Invalid Version: 5.2"

---
+++ services/wordpress/wordpress-version-color.spec.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const { expect } = require('chai')
+const { versionColorForWordpressVersion } = require('./wordpress-version-color')
+
+describe('Wordpress version color helpers', function() {
+  it('versionColorForWordpressVersion()', async function() {
+    expect(await versionColorForWordpressVersion('11.2.0')).to.equal(
+      'brightgreen'
+    )
+    expect(await versionColorForWordpressVersion('11.2')).to.equal(
+      'brightgreen'
+    )
+    expect(await versionColorForWordpressVersion('3.2.0')).to.equal('yellow')
+    expect(await versionColorForWordpressVersion('3.2')).to.equal('yellow')
+  })
+})
