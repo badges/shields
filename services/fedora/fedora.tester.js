0bdb279195a5ececac2966725d47c6990dbdd82b
Ville Skyttä <ville.skytta@iki.fi>
2019-05-01 20:01:47 +0300

    Drop superfluous test

--- services/fedora/fedora.tester.js
+++ services/fedora/fedora.tester.js
@@ -12,15 +12,6 @@ t.create('Fedora package (default branch, valid)')
     message: isVPlusDottedVersionNClausesWithOptionalSuffixAndEpoch,
   })
 
-t.create('Fedora package (valid, mocked response)')
-  .get('/rawhide/rpm.json')
-  .intercept(nock =>
-    nock('https://apps.fedoraproject.org')
-      .get('/mdapi/rawhide/pkg/rpm')
-      .reply(200, { version: '4.14.2.1' })
-  )
-  .expectBadge({ label: 'fedora', message: 'v4.14.2.1' })
-
 t.create('Fedora package (not found)')
   .get('/rawhide/not-a-package.json')
   .expectBadge({ label: 'fedora', message: 'not found' })
