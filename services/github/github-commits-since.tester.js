0a0b5b3f031178851c9d295b9967d81ede052723
Pierre-Yves B <PyvesDev@gmail.com>
2019-05-05 15:47:07 +0100

    Added support for branches in "commits since" GitHub badge (#3411)

--- services/github/github-commits-since.tester.js
+++ services/github/github-commits-since.tester.js
@@ -11,9 +11,39 @@ t.create('Commits since')
     color: 'blue',
   })
 
+t.create('Commits since (branch)')
+  .get('/badges/shields/60be4859585650e8c2b87669e3a39d98ca084e98/gh-pages.json')
+  .expectBadge({
+    label: Joi.string().regex(/^(commits since){1}[\s\S]+$/),
+    message: Joi.string().regex(/^\w+$/),
+  })
+
 t.create('Commits since by latest release')
   .get('/microsoft/typescript/latest.json')
   .expectBadge({
     label: Joi.string().regex(/^(commits since){1}[\s\S]+$/),
     message: Joi.string().regex(/^\d+\w?$/),
+  })
+
+t.create('Commits since by latest release (branch)')
+  .get('/microsoft/typescript/latest/master.json')
+  .expectBadge({
+    label: Joi.string().regex(/^(commits since){1}[\s\S]+$/),
+    message: Joi.string().regex(/^\d+\w?$/),
+  })
+
+t.create('Commits since (version not found)')
+  .get('/badges/shields/not-a-version.json')
+  .expectBadge({
+    label: 'github',
+    message: 'repo or version not found',
+  })
+
+t.create('Commits since (branch not found)')
+  .get(
+    '/badges/shields/a0663d8da53fb712472c02665e6ff7547ba945b7/not-a-branch.json'
+  )
+  .expectBadge({
+    label: 'github',
+    message: 'repo, branch or version not found',
   })
