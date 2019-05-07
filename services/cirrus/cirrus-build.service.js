e416332cc081a87a612359f6770363a9b47dcb45
Reece Dunham <me@rdil.rocks>
2019-05-07 12:22:02 -0400

    add base (needs tweaking)

---
+++ services/cirrus/cirrus-build.service.js
@@ -0,0 +1,81 @@
+'use strict'
+
+const Joi = require('joi')
+const { isBuildStatus, renderBuildStatusBadge } = require('../build-status')
+const { BaseSvgScrapingService } = require('..')
+
+const schema = Joi.object({
+  message: Joi.alternatives()
+    .try(isBuildStatus, Joi.equal('unknown'))
+    .required(),
+}).required()
+
+module.exports = class CirrusBuild extends BaseSvgScrapingService {
+  static get category() {
+    return 'build'
+  }
+
+  String route(String reponame) {
+    return 'https://example.com'
+  }
+
+  static get examples() {
+    const { staticPreview } = this
+    return [
+      {
+        title: 'Travis (.org)',
+        pattern: ':user/:repo',
+        namedParams: { user: 'rust-lang', repo: 'rust' },
+        staticPreview,
+      },
+      {
+        title: 'Travis (.org) branch',
+        pattern: ':user/:repo/:branch',
+        namedParams: { user: 'rust-lang', repo: 'rust', branch: 'master' },
+        staticPreview,
+      },
+      {
+        title: 'Travis (.com)',
+        pattern: 'com/:user/:repo',
+        namedParams: { user: 'ivandelabeldad', repo: 'rackian-gateway' },
+        staticPreview,
+      },
+      {
+        title: 'Travis (.com) branch',
+        pattern: 'com/:user/:repo/:branch',
+        namedParams: {
+          user: 'ivandelabeldad',
+          repo: 'rackian-gateway',
+          branch: 'master',
+        },
+        staticPreview,
+      },
+    ]
+  }
+
+  static get staticPreview() {
+    return { message: 'passing', color: 'brightgreen' }
+  }
+
+  static get defaultBadgeData() {
+    return {
+      label: 'build',
+    }
+  }
+
+  static render({ status }) {
+    return renderBuildStatusBadge({ status })
+  }
+
+  async handle({ comDomain, userRepo, branch }) {
+    const domain = comDomain || 'org'
+    const { message: status } = await this._requestSvg({
+      schema,
+      url: `https://api.travis-ci.${domain}/${userRepo}.svg`,
+      options: { qs: { branch } },
+      valueMatcher: />([^<>]+)<\/text><\/g>/,
+    })
+
+    return this.constructor.render({ status })
+  }
+}
