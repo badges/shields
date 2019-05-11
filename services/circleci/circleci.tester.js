7caf301730f5d9e93292219c02f9808ba9faa4d5
chris48s <chris48s@users.noreply.github.com>
2019-05-11 17:55:32 +0100

    switch [circleci] service to scrape SVG badge, reorganise routes (#3413)
    * switch [circleci] service to scrape SVG badge
    
    * reorganise [circleci] routes
    
    Default URL is now
    circleci/build/gh/badges/shields/master?token=abc123
    There are redirects on the legacy routes to provide
    backwards compatibility for existing users

--- services/circleci/circleci.tester.js
+++ services/circleci/circleci.tester.js
@@ -9,62 +9,77 @@ const t = (module.exports = new ServiceTester({
 }))
 
 t.create('circle ci (valid, without branch)')
-  .get('/project/github/RedSparr0w/node-csgo-parser.json')
+  .get('/build/gh/RedSparr0w/node-csgo-parser.json')
   .expectBadge({
     label: 'build',
     message: isBuildStatus,
   })
 
-t.create('circle ci (valid, with branch)')
-  .get('/project/github/RedSparr0w/node-csgo-parser/master.json')
+t.create('circle ci (valid repo, valid branch)')
+  .get('/build/gh/RedSparr0w/node-csgo-parser/master.json')
   .expectBadge({
     label: 'build',
     message: isBuildStatus,
   })
 
-t.create('build status with "github" as a default VCS')
-  .get('/project/RedSparr0w/node-csgo-parser/master.json')
+t.create('circle ci (valid repo, invalid branch)')
+  .get('/build/gh/RedSparr0w/node-csgo-parser/not-a-real-branch.json')
   .expectBadge({
     label: 'build',
-    message: isBuildStatus,
-  })
-
-t.create('circle ci (valid, with token)')
-  .get(
-    '/token/b90b5c49e59a4c67ba3a92f7992587ac7a0408c2/project/github/RedSparr0w/node-csgo-parser/master.json'
-  )
-  .expectBadge({
-    label: 'build',
-    message: isBuildStatus,
+    message: 'no builds',
   })
 
 t.create('circle ci (not found)')
-  .get('/project/github/PyvesB/EmptyRepo.json')
+  .get('/build/gh/PyvesB/EmptyRepo.json')
   .expectBadge({ label: 'build', message: 'project not found' })
 
-t.create('circle ci (no response data)')
-  .get('/project/github/RedSparr0w/node-csgo-parser.json')
-  .intercept(nock =>
-    nock('https://circleci.com')
-      .get(
-        '/api/v1.1/project/github/RedSparr0w/node-csgo-parser?filter=completed&limit=1'
-      )
-      .reply(200)
-  )
-  .expectBadge({ label: 'build', message: 'unparseable json response' })
-
-// we're passing &limit=1 so we expect exactly one array element
-t.create('circle ci (invalid json)')
-  .get('/project/github/RedSparr0w/node-csgo-parser.json')
-  .intercept(nock =>
-    nock('https://circleci.com')
-      .get(
-        '/api/v1.1/project/github/RedSparr0w/node-csgo-parser?filter=completed&limit=1'
-      )
-      .reply(200, [{ status: 'success' }, { status: 'fixed' }])
+t.create('circle ci (valid, with token)')
+  .get(
+    '/build/gh/RedSparr0w/node-csgo-parser/master.json?token=b90b5c49e59a4c67ba3a92f7992587ac7a0408c2'
   )
   .expectBadge({
     label: 'build',
-    message: 'invalid response data',
-    color: 'lightgrey',
+    message: isBuildStatus,
   })
+
+t.create('legacy route with VCS')
+  .get('/project/github/RedSparr0w/node-csgo-parser.json', {
+    followRedirect: false,
+  })
+  .expectStatus(301)
+  .expectHeader(
+    'Location',
+    '/circleci/build/github/redsparr0w/node-csgo-parser.json'
+  )
+
+t.create('legacy route (assume "github" as a default VCS)')
+  .get('/project/RedSparr0w/node-csgo-parser/master.json', {
+    followRedirect: false,
+  })
+  .expectStatus(301)
+  .expectHeader(
+    'Location',
+    '/circleci/build/gh/redsparr0w/node-csgo-parser/master.json'
+  )
+
+t.create('legacy route with token and VCS')
+  .get(
+    '/token/b90b5c49e59a4c67ba3a92f7992587ac7a0408c2/project/github/RedSparr0w/node-csgo-parser/master.json',
+    { followRedirect: false }
+  )
+  .expectStatus(301)
+  .expectHeader(
+    'Location',
+    '/circleci/build/github/redsparr0w/node-csgo-parser/master.json?token=b90b5c49e59a4c67ba3a92f7992587ac7a0408c2'
+  )
+
+t.create('legacy route with token (assume "github" as a default VCS)')
+  .get(
+    '/token/b90b5c49e59a4c67ba3a92f7992587ac7a0408c2/project/RedSparr0w/node-csgo-parser/master.json',
+    { followRedirect: false }
+  )
+  .expectStatus(301)
+  .expectHeader(
+    'Location',
+    '/circleci/build/gh/redsparr0w/node-csgo-parser/master.json?token=b90b5c49e59a4c67ba3a92f7992587ac7a0408c2'
+  )
