7e47fd331d3fa52ac86abd17d674de52394719ed
Josep Bernad Espinosa <josep.bernad@est.fib.upc.edu>
2019-05-11 18:46:57 +0200

    Apply suggested changes.

--- services/reddit/subreddit-subscribers.service.js
+++ services/reddit/subreddit-subscribers.service.js
@@ -9,12 +9,10 @@ const schema = Joi.object({ subscribers: nonNegativeInteger })
 
 module.exports = class SubredditSubscribers extends BaseJsonService {
   static get category() {
-    console.log('Category')
     return 'social'
   }
 
   static get route() {
-    console.log('Route')
     return {
       base: 'reddit/subreddit-subscribers',
       pattern: ':subreddit',
@@ -38,18 +36,14 @@ module.exports = class SubredditSubscribers extends BaseJsonService {
   }
 
   static render({ subreddit, subscribers }) {
-    console.log('Render')
     return {
       label: `follow r/${subreddit}`,
       message: metric(subscribers),
-      style: 'flat',
       color: 'red',
-      link: [`https://www.reddit.com/r/${subreddit}/`],
     }
   }
 
   async fetch({ subreddit }) {
-    console.log('Fetch')
     return this._requestJson({
       schema,
       url: `https://www.reddit.com/r/${subreddit}/about.json`,
@@ -57,7 +51,6 @@ module.exports = class SubredditSubscribers extends BaseJsonService {
   }
 
   async handle({ subreddit }) {
-    console.log('Handle')
     const json = await this.fetch({ subreddit })
     return this.constructor.render({
       subreddit,
