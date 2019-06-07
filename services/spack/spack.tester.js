f7f4f6520e6a0ec5a4f7385d845d522a707f1e6b
Axel Huebl <axel.huebl@plasma.ninja>
2019-06-07 18:52:50 +0200

    Spack Package Manager: Latest Version
    Add support for the Spack package manager (https://spack.io).
    Spack is a modern multi-language, multi-variant software package
    manager in high-performance computing (HPC).

---
+++ services/spack/spack.tester.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const { isVPlusDottedVersionAtLeastOne } = require('../test-validators')
+const t = (module.exports = require('../tester').createServiceTester())
+
+t.create('version (valid)')
+  .get('/adios2.json')
+  .expectBadge({
+    label: 'spack',
+    message: isVPlusDottedVersionAtLeastOne,
+  })
+
+t.create('version (not found)')
+  .get('/not-a-package.json')
+  .expectBadge({ label: 'spack', message: 'package not found' })
