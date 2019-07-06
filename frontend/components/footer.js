c0d0e68c3926eef31c3b2c0d21429d50d03344b6
PyvesB <PyvesDev@gmail.com>
2019-07-06 11:33:33 +0100

    Reworded footer

--- frontend/components/footer.js
+++ frontend/components/footer.js
@@ -70,10 +70,10 @@ export default function Footer({ baseUrl }) {
       </p>
 
       <p>
-        What is your favorite badge service to use?
+        Have an idea for an awesome new badge?
         <br />
-        <a href="https://github.com/badges/shields/blob/master/CONTRIBUTING.md">
-          Tell us
+        <a href="https://github.com/badges/shields/issues/new?labels=service-badge&template=3_Badge_request.md">
+          Tell us about it
         </a>{' '}
         and we might bring it to you!
       </p>
