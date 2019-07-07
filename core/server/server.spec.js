eec904c262d55569079bee394bf430eae0f9d487
Paul Melnikow <github@paulmelnikow.com>
2019-07-07 11:49:21 -0400

    Fix raster redirect and drop the legacy static .png route (#3659)

--- core/server/server.spec.js
+++ core/server/server.spec.js
@@ -41,7 +41,17 @@ describe('The server', function() {
     )
     expect(statusCode).to.equal(301)
     expect(headers.location).to.equal(
-      'http://raster.example.com/:fruit-apple-green.png'
+      'http://raster.example.test/:fruit-apple-green.png'
+    )
+  })
+
+  it('should redirect modern PNG badges as configured', async function() {
+    const { statusCode, headers } = await got(`${baseUrl}npm/v/express.png`, {
+      followRedirect: false,
+    })
+    expect(statusCode).to.equal(301)
+    expect(headers.location).to.equal(
+      'http://raster.example.test/npm/v/express.png'
     )
   })
 
@@ -115,7 +125,7 @@ describe('The server', function() {
 
     expect(statusCode).to.equal(302)
     // This value is set in `config/test.yml`
-    expect(headers.location).to.equal('http://badge-server.example.com')
+    expect(headers.location).to.equal('http://frontend.example.test')
   })
 
   it('should return the 410 badge for obsolete formats', async function() {
