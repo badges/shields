0a276f692f8d51573908d258e477207159844a18
Pierre-Yves B <PyvesDev@gmail.com>
2019-07-11 23:28:21 +0100

    Colorize [OSSLifecycle] badge (#3695)
    * Updated examples
    
    * Ran Prettier again

--- services/osslifecycle/osslifecycle.service.js
+++ services/osslifecycle/osslifecycle.service.js
@@ -30,7 +30,7 @@ module.exports = class OssTracker extends BaseService {
       {
         title: 'OSS Lifecycle',
         pattern: ':user/:repo',
-        namedParams: { user: 'Netflix', repo: 'osstracker' },
+        namedParams: { user: 'Teevity', repo: 'ice' },
         staticPreview: this.render({ status: 'active' }),
         keywords: ['Netflix'],
         documentation,
@@ -38,7 +38,11 @@ module.exports = class OssTracker extends BaseService {
       {
         title: 'OSS Lifecycle (branch)',
         pattern: ':user/:repo/:branch',
-        namedParams: { user: 'Netflix', repo: 'osstracker', branch: 'master' },
+        namedParams: {
+          user: 'Netflix',
+          repo: 'osstracker',
+          branch: 'documentation',
+        },
         staticPreview: this.render({ status: 'active' }),
         keywords: ['Netflix'],
         documentation,
@@ -50,10 +54,28 @@ module.exports = class OssTracker extends BaseService {
     return { label: 'oss lifecycle' }
   }
 
+  /**
+   * Return color for active, maintenance and archived statuses, which were the three
+   * example keywords used in Netflix's open-source meetup.
+   * See https://slideshare.net/aspyker/netflix-open-source-meetup-season-4-episode-1
+   * Other keywords are possible, but will appear in grey.
+   */
+  static getColor({ status }) {
+    if (status === 'active') {
+      return 'brightgreen'
+    } else if (status === 'maintenance') {
+      return 'yellow'
+    } else if (status === 'archived') {
+      return 'red'
+    }
+    return 'lightgrey'
+  }
+
   static render({ status }) {
+    const color = this.getColor({ status })
     return {
       message: status,
-      color: 'lightgrey',
+      color,
     }
   }
 
