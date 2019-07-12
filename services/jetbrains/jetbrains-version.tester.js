0720dd7ce9b8889ba91963684199a5594603732f
PyvesB <PyvesDev@gmail.com>
2019-07-12 22:33:43 +0100

    Removed inaccessible/invalid tests

--- services/jetbrains/jetbrains-version.tester.js
+++ services/jetbrains/jetbrains-version.tester.js
@@ -3,95 +3,6 @@
 const { isVPlusDottedVersionNClauses } = require('../test-validators')
 const t = (module.exports = require('../tester').createServiceTester())
 
-t.create('empty XML(v)')
-  .get('/9435.json')
-  .intercept(
-    nock =>
-      nock('https://plugins.jetbrains.com')
-        .get('/plugins/list?pluginId=9435')
-        .reply(200, '<?xml version="1.0" encoding="UTF-8"?>'),
-    {
-      'Content-Type': 'text/xml;charset=UTF-8',
-    }
-  )
-  .expectBadge({
-    label: 'jetbrains plugin',
-    message: 'unparseable xml response',
-  })
-
-t.create('404 status code(v)')
-  .get('/7495.json')
-  .intercept(nock =>
-    nock('https://plugins.jetbrains.com')
-      .get('/plugins/list?pluginId=7495')
-      .reply(404)
-  )
-  .expectBadge({ label: 'jetbrains plugin', message: 'not found' })
-
-t.create('missing required XML element(v)')
-  .get('/9435.json')
-  .intercept(
-    nock =>
-      nock('https://plugins.jetbrains.com')
-        .get('/plugins/list?pluginId=9435')
-        .reply(
-          200,
-          `<?xml version="1.0" encoding="UTF-8"?>
-                 <plugin-repository>
-                   <ff>"Code editing"</ff>
-                   <category name="Code editing">
-                     <!-- no required idea-plugin element here -->
-                   </category>
-                 </plugin-repository>`
-        ),
-    {
-      'Content-Type': 'text/xml;charset=UTF-8',
-    }
-  )
-  .expectBadge({ label: 'jetbrains plugin', message: 'invalid response data' })
-
-t.create('incorrect response format (JSON instead of XML)(v)')
-  .get('/7495.json')
-  .intercept(nock =>
-    nock('https://plugins.jetbrains.com')
-      .get('/plugins/list?pluginId=7495')
-      .reply(200, { version: 2.0 })
-  )
-  .expectBadge({
-    label: 'jetbrains plugin',
-    message: 'unparseable xml response',
-  })
-
-t.create('empty response(v)')
-  .get('/7495.json')
-  .intercept(nock =>
-    nock('https://plugins.jetbrains.com')
-      .get('/plugins/list?pluginId=7495')
-      .reply(200, '')
-  )
-  .expectBadge({
-    label: 'jetbrains plugin',
-    message: 'unparseable xml response',
-  })
-
-t.create('server error(v)')
-  .get('/7495.json')
-  .intercept(nock =>
-    nock('https://plugins.jetbrains.com')
-      .get('/plugins/list?pluginId=7495')
-      .reply(500)
-  )
-  .expectBadge({ label: 'jetbrains plugin', message: 'inaccessible' })
-
-t.create('connection error(v)')
-  .get('/7495.json')
-  .networkOff()
-  .expectBadge({ label: 'jetbrains plugin', message: 'inaccessible' })
-
-t.create('version for unknown plugin')
-  .get('/unknown-plugin.json')
-  .expectBadge({ label: 'jetbrains plugin', message: 'not found' })
-
 t.create('version (user friendly plugin id)')
   .get('/1347-scala.json')
   .expectBadge({
@@ -113,7 +24,7 @@ t.create('version (number as a plugin id)')
     message: isVPlusDottedVersionNClauses,
   })
 
-t.create('version)')
+t.create('version')
   .get('/9435.json')
   .intercept(
     nock =>
@@ -136,15 +47,6 @@ t.create('version)')
   )
   .expectBadge({ label: 'jetbrains plugin', message: 'v1.0' })
 
-t.create('XML with unknown root (v)')
-  .get('/9435.json')
-  .intercept(
-    nock =>
-      nock('https://plugins.jetbrains.com')
-        .get('/plugins/list?pluginId=9435')
-        .reply(200, '<?xml version="1.0" encoding="UTF-8"?><plugin />'),
-    {
-      'Content-Type': 'text/xml;charset=UTF-8',
-    }
-  )
-  .expectBadge({ label: 'jetbrains plugin', message: 'invalid response data' })
+t.create('version for unknown plugin')
+  .get('/unknown-plugin.json')
+  .expectBadge({ label: 'jetbrains plugin', message: 'not found' })
