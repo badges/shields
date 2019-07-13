102db63d46a051fc813ff8a5f67c1ce98fcc0ab7
Pierre-Yves B <PyvesDev@gmail.com>
2019-07-13 16:45:58 +0100

    Split [pypi] tests (#3704)
    * Split [pypi] tests
    
    * Changed filenames to match previous class names

---
+++ services/pypi/pypi-downloads.tester.js
@@ -0,0 +1,32 @@
+'use strict'
+
+const t = (module.exports = require('../tester').createServiceTester())
+const { isMetricOverTimePeriod } = require('../test-validators')
+
+t.create('daily downloads (valid)')
+  .get('/dd/djangorestframework.json')
+  .expectBadge({ label: 'downloads', message: isMetricOverTimePeriod })
+
+t.create('weekly downloads (valid)')
+  .get('/dw/djangorestframework.json')
+  .expectBadge({ label: 'downloads', message: isMetricOverTimePeriod })
+
+t.create('monthly downloads (valid)')
+  .get('/dm/djangorestframework.json')
+  .expectBadge({ label: 'downloads', message: isMetricOverTimePeriod })
+
+t.create('downloads (mixed-case package name)')
+  .get('/dd/DjangoRestFramework.json')
+  .expectBadge({ label: 'downloads', message: isMetricOverTimePeriod })
+
+t.create('daily downloads (not found)')
+  .get('/dd/not-a-package.json')
+  .expectBadge({ label: 'downloads', message: 'package not found' })
+
+t.create('weekly downloads (not found)')
+  .get('/dw/not-a-package.json')
+  .expectBadge({ label: 'downloads', message: 'package not found' })
+
+t.create('monthly downloads (not found)')
+  .get('/dm/not-a-package.json')
+  .expectBadge({ label: 'downloads', message: 'package not found' })
---
+++ services/pypi/pypi-implementation.tester.js
@@ -0,0 +1,22 @@
+'use strict'
+
+const t = (module.exports = require('../tester').createServiceTester())
+
+t.create('implementation (valid, package version in request)')
+  .get('/beehive/1.0.json')
+  .expectBadge({ label: 'implementation', message: 'cpython | jython | pypy' })
+
+t.create('implementation (valid, no package version specified)')
+  .get('/numpy.json')
+  .expectBadge({ label: 'implementation', message: 'cpython' })
+
+t.create('implementation (not specified)')
+  .get('/chai/1.1.2.json')
+  .expectBadge({ label: 'implementation', message: 'cpython' })
+
+t.create('implementation (invalid)')
+  .get('/not-a-package.json')
+  .expectBadge({
+    label: 'implementation',
+    message: 'package or version not found',
+  })
