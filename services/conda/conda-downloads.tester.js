83e4672e3d391abd7d9a85c1f73bef98f3dbfea9
PyvesB <PyvesDev@gmail.com>
2019-07-13 09:45:46 +0100

    Split [Conda] tests

---
+++ services/conda/conda-downloads.tester.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const { isMetric } = require('../test-validators')
+const t = (module.exports = require('../tester').createServiceTester())
+
+t.create('downloads')
+  .get('/d/conda-forge/zlib.json')
+  .expectBadge({
+    label: 'conda|downloads',
+    message: isMetric,
+  })
+
+t.create('downloads (skip prefix)')
+  .get('/dn/conda-forge/zlib.json')
+  .expectBadge({ label: 'downloads', message: isMetric })
+
+t.create('unknown package')
+  .get('/d/conda-forge/some-bogus-package-that-never-exists.json')
+  .expectBadge({ label: 'conda', message: 'not found' })
+
+t.create('unknown channel')
+  .get('/d/some-bogus-channel-that-never-exists/zlib.json')
+  .expectBadge({ label: 'conda', message: 'not found' })
---
+++ services/conda/conda-platform.tester.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const Joi = require('@hapi/joi')
+const isCondaPlatform = Joi.string().regex(/^\w+-[\w\d]+( \| \w+-[\w\d]+)*$/)
+const t = (module.exports = require('../tester').createServiceTester())
+
+t.create('platform')
+  .get('/p/conda-forge/zlib.json')
+  .expectBadge({
+    label: 'conda|platform',
+    message: isCondaPlatform,
+  })
+
+t.create('platform (skip prefix)')
+  .get('/pn/conda-forge/zlib.json')
+  .expectBadge({
+    label: 'platform',
+    message: isCondaPlatform,
+  })
