8ff783bc684e06b298b0b0acc9d0f059e60adf89
Pierre-Yves B <PyvesDev@gmail.com>
2019-08-01 07:49:51 +0100

    Handle missing UserDownloads in [ChromeWebStoreUsers] (#3812)

--- services/chrome-web-store/chrome-web-store-users.tester.js
+++ services/chrome-web-store/chrome-web-store-users.tester.js
@@ -2,6 +2,17 @@
 
 const { isMetric } = require('../test-validators')
 const { ServiceTester } = require('../tester')
+const pageWithoutUserDownloads = `<!DOCTYPE html>
+<html>
+   <body>
+      <span>
+         <meta itemprop="url" content="https://chrome.google.com/webstore/detail/night-video-tuner/ogffaloegjglncjfehdfplabnoondfjo"/>
+         <!--
+             <meta itemprop="interactionCount" content="UserDownloads:547"/>
+         -->
+      </span>
+   </body>
+</html>`
 
 const t = (module.exports = new ServiceTester({
   id: 'ChromeWebStoreUsers',
@@ -16,6 +27,19 @@ t.create('Downloads (redirect)')
 t.create('Users')
   .get('/users/alhjnofcnnpeaphgeakdhkebafjcpeae.json')
   .expectBadge({ label: 'users', message: isMetric })
+
+t.create('Users (count not found)')
+  .get('/users/ogffaloegjglncjfehdfplabnoondfjo.json')
+  .intercept(nock =>
+    nock('https://chrome.google.com')
+      .get('/webstore/detail/ogffaloegjglncjfehdfplabnoondfjo?hl=en&gl=US')
+      .reply(200, pageWithoutUserDownloads)
+  )
+  .expectBadge({
+    label: 'users',
+    message: 'count not found',
+    color: 'red',
+  })
 
 t.create('Users (not found)')
   .get('/users/invalid-name-of-addon.json')
