3d7f1a060c68252f6c9a439728f667d4fdf8bacb
chris48s <chris.shaw480@gmail.com>
2019-08-12 18:02:35 +0100

    improve docs for base service classes

--- core/base-service/base-json.js
+++ core/base-service/base-json.js
@@ -1,13 +1,42 @@
+/**
+ * @module
+ */
+
 'use strict'
 
 const BaseService = require('./base')
 const { parseJson } = require('./json')
 
+/**
+ * Services which query a JSON endpoint should extend BaseJsonService
+ *
+ * @abstract
+ */
 class BaseJsonService extends BaseService {
+  /**
+   * Parse data from JSON endpoint
+   *
+   * @param {string} buffer JSON repsonse from upstream API
+   * @returns {object} Parsed response
+   */
   _parseJson(buffer) {
     return parseJson(buffer)
   }
 
+  /**
+   * Request data from an upstream API serving JSON,
+   * parse it and validate against a schema
+   *
+   * @param {object} attrs Refer to individual attrs
+   * @param {Joi} attrs.schema Joi schema to validate the response against
+   * @param {string} attrs.url URL to request
+   * @param {object} [attrs.options={}] Options to pass to request. See
+   *    [documentation](https://github.com/request/request#requestoptions-callback)
+   * @param {object} [attrs.errorMessages={}] Key-value map of status codes
+   *    and custom error messages e.g: `{ 404: 'package not found' }`
+   * @returns {object} Parsed response
+   * @see https://github.com/request/request#requestoptions-callback
+   */
   async _requestJson({ schema, url, options = {}, errorMessages = {} }) {
     const mergedOptions = {
       ...{ headers: { Accept: 'application/json' } },
