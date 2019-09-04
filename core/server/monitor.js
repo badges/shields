3dbe6ed522ffa69b8ddbe39115faff9d429bf830
Paul Melnikow <github@paulmelnikow.com>
2019-09-04 11:12:51 -0400

    Update core/server/monitor.js

--- core/server/monitor.js
+++ core/server/monitor.js
@@ -18,7 +18,7 @@ function secretInvalid(req, res) {
 
 function setRoutes({ rateLimit }, { server, metricInstance }) {
   const ipRateLimit = new RateLimit({
-    // Exclude IPs for GitHub Camo, determinted experimentally by running e.g.
+    // Exclude IPs for GitHub Camo, determined experimentally by running e.g.
     // `curl --insecure -u ":shields-secret" https://s0.shields-server.com/sys/rate-limit`
     whitelist: /^(?:192\.30\.252\.\d+)|(?:140\.82\.115\.\d+)$/,
   })
