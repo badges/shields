645a00b283e55a9078e9fbc41ecb3a8ef21e0f95
Sandro Rodrigues <sandro.rodrigues@dipcode.com>
2019-09-09 13:04:52 +0100

    Fixed failing tests.

--- services/security-headers/security-headers.tester.js
+++ services/security-headers/security-headers.tester.js
@@ -4,17 +4,17 @@ const t = (module.exports = require('../tester').createServiceTester())
 
 t.create('grade of http://shields.io')
   .get('/http/shields.io.json')
-  .expectBadge({ label: 'securityheaders', message: 'F', color: 'red' })
+  .expectBadge({ label: 'security headers', message: 'F', color: 'red' })
 
 t.create('grade of https://shields.io')
   .get('/https/shields.io.json')
-  .expectBadge({ label: 'securityheaders', message: 'F', color: 'red' })
+  .expectBadge({ label: 'security headers', message: 'F', color: 'red' })
 
 t.create('grade when no network')
   .get('/https/shields.io.json')
   .networkOff()
   .expectBadge({
-    label: 'securityheaders',
+    label: 'security headers',
     message: 'error',
     color: 'lightgrey',
   })
