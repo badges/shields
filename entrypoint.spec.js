995b2bbfc36badf46f090804d7cbc3905e52351f
olramde <rladhkstn8@gmail.com>
2019-10-24 21:28:39 +0900

    using es6 arrow function

--- entrypoint.spec.js
+++ entrypoint.spec.js
@@ -5,7 +5,7 @@ const isSvg = require('is-svg')
 const got = require('./core/got-test-client')
 
 let server
-before(function() {
+before(() => {
   this.timeout('5s')
   // remove args comming from mocha
   // https://github.com/badges/shields/issues/3365
@@ -13,11 +13,11 @@ before(function() {
   server = require('./server')
 })
 
-after('shut down the server', async function() {
+after('shut down the server', async () => {
   await server.stop()
 })
 
-it('should render a badge', async function() {
+it('should render a badge', async () => {
   const { statusCode, body } = await got(
     'http://localhost:1111/badge/fruit-apple-green.svg'
   )
