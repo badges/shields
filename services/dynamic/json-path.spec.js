721ef780b4b880a19c9cad367e38afb651c9a031
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2019-10-29 19:30:45 +0100

    Test JSON path factory using chai-as-promised

--- services/dynamic/json-path.spec.js
+++ services/dynamic/json-path.spec.js
@@ -1,22 +1,22 @@
 'use strict'
 
-const { expect } = require('chai')
+const chai = require('chai')
+const { expect } = chai
 const jsonPath = require('./json-path')
+
+chai.use(require('chai-as-promised'))
 
 describe('JSON Path service factory', function() {
   describe('_getData()', function() {
-    it('should throw error if it is not overridden', async function() {
+    it('should throw error if it is not overridden', function() {
       class BaseService {}
       class JsonPathService extends jsonPath(BaseService) {}
       const jsonPathServiceInstance = new JsonPathService()
-      try {
-        await jsonPathServiceInstance._getData({})
-        expect.fail('Expected to throw')
-      } catch (e) {
-        expect(e.message).to.equal(
-          '_getData() function not implemented for JsonPathService'
-        )
-      }
+
+      return expect(jsonPathServiceInstance._getData({})).to.be.rejectedWith(
+        Error,
+        '_getData() function not implemented for JsonPathService'
+      )
     })
   })
 })
