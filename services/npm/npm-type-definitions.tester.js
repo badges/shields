5de4334ea709f9e8f0b464b18c03009bdc3f4b50
Caleb Cartwright <calebcartwright@users.noreply.github.com>
2019-11-16 14:40:55 -0600

    tests: update npm type definition tests (#4322)

--- services/npm/npm-type-definitions.tester.js
+++ services/npm/npm-type-definitions.tester.js
@@ -7,12 +7,20 @@ const isTypeDefinition = Joi.string().regex(
   /^((Flow|TypeScript)|(Flow \| TypeScript))$/
 )
 
-t.create('types (from dev dependencies + files)')
-  .get('/chalk.json')
+t.create('types (from dev dependencies)')
+  .get('/commander.json')
   .expectBadge({ label: 'types', message: isTypeDefinition })
 
 t.create('types (from files)')
   .get('/form-data-entries.json')
+  .intercept(nock =>
+    nock('https://registry.npmjs.org')
+      .get(`/form-data-entries/latest`)
+      .reply(200, {
+        maintainers: [],
+        files: ['index.js', 'index.d.ts'],
+      })
+  )
   .expectBadge({ label: 'types', message: isTypeDefinition })
 
 t.create('types (from types key)')
