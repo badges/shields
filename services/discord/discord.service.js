f929552dd55224450d838c469c752c6b9774f4bf
Zack <night@users.noreply.github.com>
2019-12-27 19:31:24 -0500

    Update discord.service.js

--- services/discord/discord.service.js
+++ services/discord/discord.service.js
@@ -4,8 +4,7 @@ const Joi = require('@hapi/joi')
 const { BaseJsonService } = require('..')
 
 const discordSchema = Joi.object({
-  presence_count: Joi.number()
-    .required(),
+  presence_count: Joi.number().required(),
 }).required()
 
 const documentation = `
