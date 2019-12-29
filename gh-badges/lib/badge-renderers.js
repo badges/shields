c2bf4fd8268133477fdd778c5ad15b5f838666dd
chris48s <chris.shaw480@gmail.com>
2019-12-29 20:31:46 +0000

    fix social badge link rendering, tidy up

--- gh-badges/lib/badge-renderers.js
+++ gh-badges/lib/badge-renderers.js
@@ -392,8 +392,9 @@ function social({
   // width can be measured using the correct characters.
   label = capitalize(label)
 
-  const height = 20
-  const { hasLogo, logoWidth, renderedLogo } = renderLogo({
+  const externalHeight = 20
+  const internalHeight = 19
+  const { logoWidth, renderedLogo } = renderLogo({
     logo,
     logoWidth: inLogoWidth,
     logoPadding,
@@ -405,11 +406,21 @@ function social({
   messageWidth += 10
   messageWidth -= 4
 
+  const labelTextX = ((labelWidth + logoWidth) / 2) * 10
+  const labelTextLength = (labelWidth - (10 + logoWidth)) * 10
+  const escapedLabel = escapeXml(label)
+
+  let [leftLink, rightLink] = links
+  leftLink = escapeXml(leftLink)
+  rightLink = escapeXml(rightLink)
+  const hasLeftLink = leftLink && leftLink.length
+  const hasRightLink = rightLink && rightLink.length
+
   function renderMessageBubble() {
     const messageBubbleMainX = labelWidth + 6.5
     const messageBubbleNotchX = labelWidth + 6
     return `
-      <rect x="${messageBubbleMainX}" y="0.5" width="${messageWidth}" height="19" rx="2" fill="#fafafa"/>
+      <rect x="${messageBubbleMainX}" y="0.5" width="${messageWidth}" height="${internalHeight}" rx="2" fill="#fafafa"/>
       <rect x="${messageBubbleNotchX}" y="7.5" width="0.5" height="5" stroke="#fafafa"/>
       <path d="M${messageBubbleMainX} 6.5 l-3 3v1 l3 3" stroke="d5d5d5" fill="#fafafa"/>
     `
@@ -419,24 +430,37 @@ function social({
     const messageTextX = (labelWidth + messageWidth / 2 + 6) * 10
     const messageTextLength = (messageWidth - 8) * 10
     const escapedMessage = escapeXml(message)
+    const shadow = `<text x="${messageTextX}" y="150" fill="#fff" transform="scale(0.1)" textLength="${messageTextLength}" lengthAdjust="spacing">${escapedMessage}</text>`
+    const text = `<text id="rlink" x="${messageTextX}" y="140" transform="scale(0.1)" textLength="${messageTextLength}" lengthAdjust="spacing">${escapedMessage}</text>`
+    if (hasRightLink) {
+      return `
+      ${shadow}
+      <a target="_blank" xlink:href="${rightLink}">${text}</a>
+      `
+    }
     return `
-      <text x="${messageTextX}" y="150" fill="#fff" transform="scale(0.1)" textLength="${messageTextLength}" lengthAdjust="spacing">${escapedMessage}</text>
-      <text x="${messageTextX}" y="140" transform="scale(0.1)" textLength="${messageTextLength}" lengthAdjust="spacing">${escapedMessage}</text>
+      ${shadow}
+      ${text}
     `
   }
 
-  const labelTextX = ((labelWidth + logoWidth) / 2) * 10
-  const labelTextLength = (labelWidth - (10 + logoWidth)) * 10
-  const escapedLabel = escapeXml(label)
+  function renderLeftLink() {
+    const rect = `<rect id="llink" stroke="#d5d5d5" fill="url(#a)" x=".5" y=".5" width="${labelWidth}" height="${internalHeight}" rx="2" />`
+    if (hasLeftLink) {
+      return `<a target="_blank" xlink:href="${leftLink}">${rect}</a>`
+    }
+    return rect
+  }
 
   return renderBadge(
     {
-      links,
+      links: [],
       labelWidth: labelWidth + 1,
       messageWidth: hasMessage ? messageWidth + 6 : 0,
-      height,
+      height: externalHeight,
     },
     `
+    <style>a #llink:hover{fill:url(#b);stroke:#ccc}a #rlink:hover{fill:#4183c4}</style>
     <linearGradient id="a" x2="0" y2="100%">
       <stop offset="0" stop-color="#fcfcfc" stop-opacity="0"/>
       <stop offset="1" stop-opacity=".1"/>
@@ -446,8 +470,7 @@ function social({
       <stop offset="1" stop-opacity=".1"/>
     </linearGradient>
     <g stroke="#d5d5d5">
-      <rect stroke="none" fill="#fcfcfc" x="0.5" y="0.5" width="${labelWidth}" height="19" rx="2"/>
-      <rect stroke="#d5d5d5" fill="url(#a)" x="0.5" y="0.5" width="${labelWidth}" height="19" rx="2"/>
+      <rect stroke="none" fill="#fcfcfc" x="0.5" y="0.5" width="${labelWidth}" height="${internalHeight}" rx="2"/>
       ${hasMessage ? renderMessageBubble() : ''}
     </g>
     ${renderedLogo}
@@ -455,7 +478,9 @@ function social({
       <text x="${labelTextX}" y="150" fill="#fff" transform="scale(0.1)" textLength="${labelTextLength}" lengthAdjust="spacing">${escapedLabel}</text>
       <text x="${labelTextX}" y="140" transform="scale(0.1)" textLength="${labelTextLength}" lengthAdjust="spacing">${escapedLabel}</text>
       ${hasMessage ? renderMessageText() : ''}
-    </g>`
+    </g>
+    ${renderLeftLink()}
+    `
   )
 }
 
