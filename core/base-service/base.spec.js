22a85bfbcd74117aff53e4f44193876aaf0fd71d
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2020-01-04 17:48:24 +0100

    A response size metric of an upstream service for dynamic badges like [dynamicyaml], (but not [jetbrains]) (#4434)
    * Response size metric for all services
    
    * Unused code removed
    
    * Test for service response size metric
    
    * All buckes of the service_response_bytes in a comment
    
    * Register parameter in PrometheusMetrics is optional
    
    * service response size metric enabled for dynamic badges
    
    * Better test name
    
    * JSDoc removed
    
    * One import from one file
    
    * Gather metrics in the background
    
    * Revert saving response time metrics in the background

--- core/base-service/base.spec.js
+++ core/base-service/base.spec.js
@@ -4,6 +4,8 @@ const Joi = require('@hapi/joi')
 const chai = require('chai')
 const { expect } = chai
 const sinon = require('sinon')
+const prometheus = require('prom-client')
+const PrometheusMetrics = require('../server/prometheus-metrics')
 const trace = require('./trace')
 const {
   NotFound,
@@ -13,7 +15,7 @@ const {
   Deprecated,
 } = require('./errors')
 const BaseService = require('./base')
-
+const { MetricHelper, MetricNames } = require('./metric-helper')
 require('../register-chai-plugins.spec')
 chai.use(require('chai-as-promised'))
 
@@ -62,6 +64,12 @@ class DummyService extends BaseService {
 
   async handle({ namedParamA }, { queryParamA }) {
     return this.constructor.render({ namedParamA, queryParamA })
+  }
+}
+
+class DummyServiceWithServiceResponseSizeMetricEnabled extends DummyService {
+  static get enabledMetrics() {
+    return [MetricNames.SERVICE_RESPONSE_SIZE]
   }
 }
 
@@ -498,6 +506,59 @@ describe('BaseService', function() {
     })
   })
 
+  describe('Metrics', function() {
+    let register
+    beforeEach(function() {
+      register = new prometheus.Registry()
+    })
+    const url = 'some-url'
+
+    it('service response size metric is optional', async function() {
+      const metricHelper = MetricHelper.create({
+        metricInstance: new PrometheusMetrics({ register }),
+        ServiceClass: DummyServiceWithServiceResponseSizeMetricEnabled,
+      })
+      const sendAndCacheRequest = async () => ({
+        buffer: 'x'.repeat(65536 + 1),
+        res: { statusCode: 200 },
+      })
+      const serviceInstance = new DummyServiceWithServiceResponseSizeMetricEnabled(
+        { sendAndCacheRequest, metricHelper },
+        defaultConfig
+      )
+
+      await serviceInstance._request({ url })
+
+      expect(register.getSingleMetricAsString('service_response_bytes'))
+        .to.contain(
+          'service_response_bytes_bucket{le="65536",category="other",family="undefined",service="dummy_service_with_service_response_size_metric_enabled"} 0\n'
+        )
+        .and.to.contain(
+          'service_response_bytes_bucket{le="131072",category="other",family="undefined",service="dummy_service_with_service_response_size_metric_enabled"} 1\n'
+        )
+    })
+
+    it('service response size metric is disabled by default', async function() {
+      const metricHelper = MetricHelper.create({
+        metricInstance: new PrometheusMetrics({ register }),
+        ServiceClass: DummyService,
+      })
+      const sendAndCacheRequest = async () => ({
+        buffer: 'x',
+        res: { statusCode: 200 },
+      })
+      const serviceInstance = new DummyService(
+        { sendAndCacheRequest, metricHelper },
+        defaultConfig
+      )
+
+      await serviceInstance._request({ url })
+
+      expect(
+        register.getSingleMetricAsString('service_response_bytes')
+      ).to.not.contain('service_response_bytes_bucket')
+    })
+  })
   describe('auth', function() {
     class AuthService extends DummyService {
       static get auth() {
--- services/dynamic/dynamic-json.service.js
+++ services/dynamic/dynamic-json.service.js
@@ -1,10 +1,15 @@
 'use strict'
 
+const { MetricNames } = require('../../core/base-service/metric-helper')
 const { createRoute } = require('./dynamic-helpers')
 const jsonPath = require('./json-path')
 const { BaseJsonService } = require('..')
 
 module.exports = class DynamicJson extends jsonPath(BaseJsonService) {
+  static get enabledMetrics() {
+    return [MetricNames.SERVICE_RESPONSE_SIZE]
+  }
+
   static get route() {
     return createRoute('json')
   }
