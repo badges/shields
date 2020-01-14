98932ca2aaed0e74c962725fac7b0d755544d926
Matan Kushner <hello@matchai.dev>
2020-01-14 17:23:05 -0500

    Fix failing tests

--- services/repology/repology-repositories.tester.js
+++ services/repology/repology-repositories.tester.js
@@ -2,17 +2,31 @@
 
 const Joi = require('@hapi/joi')
 const t = (module.exports = require('../tester').createServiceTester())
+const { projectWithTwentyRepositories } = require('./repology-test-helpers')
 
 t.create('Existing project')
-  .get('/starship')
+  .get('/starship.json')
   .expectBadge({
     label: 'in repositories',
     message: Joi.number().greater(0),
   })
 
 t.create('Non-existent project')
-  .get('/invalidprojectthatshouldnotexist')
+  .get('/invalidprojectthatshouldnotexist.json')
   .expectBadge({
     label: 'in repositories',
     message: 0,
+  })
+
+t.create('Project with repositories')
+  .get('/starship.json')
+  .intercept(nock =>
+    nock('https://repology.org/badge/tiny-repos')
+      .get('/starship.svg')
+      .reply(200, projectWithTwentyRepositories)
+  )
+  .expectBadge({
+    label: 'in repositories',
+    message: '20',
+    color: 'blue',
   })
---
+++ services/repology/repology-test-helpers.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const projectWithTwentyRepositories =
+  '<svg xmlns="http://www.w3.org/2000/svg" width="117" height="20"><clipPath id="clip"><rect rx="3" width="100%" height="100%" fill="#000"/></clipPath><linearGradient id="grad" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><g clip-path="url(#clip)"><rect width="100%" height="100%" fill="#555"/><rect x="93" y="0" width="24" height="20" fill="#007ec6"/><rect y="0" width="100%" height="20" fill="url(#grad)"/><g fill="#fff" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="46.5" y="15" fill="#010101" fill-opacity=".3" text-anchor="middle">in repositories</text><text x="46.5" y="14" text-anchor="middle">in repositories</text><text x="105.0" y="15" fill="#010101" fill-opacity=".3" text-anchor="middle">11</text><text x="105.0" y="14" text-anchor="middle">20</text></g></g></svg>'
+
+module.exports = {
+  projectWithTwentyRepositories,
+}
