623bc7dba8f356af6aad029e5d4d3ab111cf5b46
Matan Kushner <hello@matchai.dev>
2020-01-14 23:12:47 -0500

    Remove mocked svg test

--- services/repology/repology-repositories.tester.js
+++ services/repology/repology-repositories.tester.js
@@ -2,7 +2,6 @@
 
 const t = (module.exports = require('../tester').createServiceTester())
 const { nonNegativeInteger } = require('../validators')
-const { projectWithTwentyRepositories } = require('./repology-test-helpers')
 
 t.create('Existing project')
   .get('/starship.json')
@@ -16,17 +15,4 @@ t.create('Non-existent project')
   .expectBadge({
     label: 'in repositories',
     message: '0',
-  })
-
-t.create('Project with repositories')
-  .get('/starship.json')
-  .intercept(nock =>
-    nock('https://repology.org/badge/tiny-repos')
-      .get('/starship.svg')
-      .reply(200, projectWithTwentyRepositories)
-  )
-  .expectBadge({
-    label: 'in repositories',
-    message: '20',
-    color: 'blue',
   })
