77d04df7df6e59d6ba203c6cf1eb5e65fed35885
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2020-02-09 20:43:01 +0100

    prom-client JSON to InfluxDB line protocol converter

---
+++ core/server/metrics/format-converters.js
@@ -0,0 +1,18 @@
+'use strict'
+
+function promClientJsonToInflux(metrics) {
+  return metrics.flatMap(metric =>
+    metric.values.map(value => {
+      const optionalTags =
+        Object.entries(value.labels).length === 0
+          ? ''
+          : `,${Object.entries(value.labels)
+              .map(labelEntry => `${labelEntry[0]}=${labelEntry[1]}`)
+              .toString()}`
+      const optionalTimestamp = value.timestamp ? ` ${value.timestamp}` : ''
+      return `${metric.name}${optionalTags} value=${value.value}${optionalTimestamp}`
+    })
+  )
+}
+
+module.exports = { promClientJsonToInflux }
---
+++ core/server/metrics/format-converters.spec.js
@@ -0,0 +1,117 @@
+'use strict'
+
+const { expect } = require('chai')
+const { promClientJsonToInflux } = require('./format-converters')
+
+describe('Metric format converters', function() {
+  describe('prom-client JSON to InfluxDB line protocol', function() {
+    it('converts a metric without labels and with a timestamp', function() {
+      const json = [
+        {
+          help: 'Total number of active handles.',
+          name: 'nodejs_active_handles_total',
+          type: 'gauge',
+          values: [
+            {
+              value: 3,
+              labels: {},
+              timestamp: 1580576541941,
+            },
+          ],
+          aggregator: 'sum',
+        },
+      ]
+
+      const influx = promClientJsonToInflux(json)
+
+      expect(influx).to.be.deep.equal([
+        'nodejs_active_handles_total value=3 1580576541941',
+      ])
+    })
+
+    it('converts a metric with values', function() {
+      const json = [
+        {
+          help: 'Process heap space size available from node.js in bytes.',
+          name: 'nodejs_heap_space_size_available_bytes',
+          type: 'gauge',
+          values: [
+            {
+              value: 229576,
+              labels: {
+                space: 'read_only',
+              },
+              timestamp: 1580576541942,
+            },
+            {
+              value: 796296,
+              labels: {
+                space: 'new',
+              },
+              timestamp: 1580576541943,
+            },
+          ],
+          aggregator: 'sum',
+        },
+      ]
+
+      const influx = promClientJsonToInflux(json)
+
+      expect(influx).to.be.deep.equal([
+        'nodejs_heap_space_size_available_bytes,space=read_only value=229576 1580576541942',
+        'nodejs_heap_space_size_available_bytes,space=new value=796296 1580576541943',
+      ])
+    })
+
+    it('converts a metric with labels', function() {
+      const json = [
+        {
+          help: 'Total service requests',
+          name: 'service_requests_total',
+          type: 'counter',
+          values: [
+            {
+              value: 44,
+              labels: {
+                category: 'static',
+                family: 'static-badge',
+                service: 'static_badge',
+              },
+              timestamp: 1580576541943,
+            },
+          ],
+          aggregator: 'sum',
+        },
+      ]
+
+      const influx = promClientJsonToInflux(json)
+
+      expect(influx).to.be.deep.equal([
+        'service_requests_total,category=static,family=static-badge,service=static_badge value=44 1580576541943',
+      ])
+    })
+
+    it('converts a metric without a timestamp', function() {
+      const json = [
+        {
+          help: 'Start time of the process since unix epoch in seconds.',
+          name: 'process_start_time_seconds',
+          type: 'gauge',
+          values: [
+            {
+              value: 1580576451,
+              labels: {},
+            },
+          ],
+          aggregator: 'omit',
+        },
+      ]
+
+      const influx = promClientJsonToInflux(json)
+
+      expect(influx).to.be.deep.equal([
+        'process_start_time_seconds value=1580576451',
+      ])
+    })
+  })
+})
