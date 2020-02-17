f0a14c85df887caed0497d1029ff611f555151ec
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2020-02-17 21:14:23 +0100

    prom-client JSON to InfluxDB line protocol (version 2) converter

--- core/server/metrics/format-converters.js
+++ core/server/metrics/format-converters.js
@@ -1,4 +1,28 @@
 'use strict'
+const groupBy = require('lodash.groupby')
+
+function promClientJsonToInfluxV2(metrics, extraLabels = {}) {
+  return metrics
+    .flatMap(metric => {
+      const valuesByLabels = groupBy(metric.values, value =>
+        JSON.stringify(Object.entries(value.labels).sort())
+      )
+      return Object.values(valuesByLabels).map(metricsWithSameLabel => {
+        const labels = Object.entries(metricsWithSameLabel[0].labels)
+          .concat(Object.entries(extraLabels))
+          .sort((a, b) => a[0].localeCompare(b[0]))
+          .map(labelEntry => `${labelEntry[0]}=${labelEntry[1]}`)
+          .join(',')
+        const labelsFormatted = labels ? `,${labels}` : ''
+        const values = metricsWithSameLabel
+          .sort((a, b) => a.metricName.localeCompare(b.metricName))
+          .map(value => `${value.metricName || metric.name}=${value.value}`)
+          .join(',')
+        return `prometheus${labelsFormatted} ${values}`
+      })
+    })
+    .join('\n')
+}
 
 function promClientJsonToInflux(metrics) {
   return metrics.flatMap(metric =>
@@ -17,4 +41,4 @@ function promClientJsonToInflux(metrics) {
   )
 }
 
-module.exports = { promClientJsonToInflux }
+module.exports = { promClientJsonToInflux, promClientJsonToInfluxV2 }
