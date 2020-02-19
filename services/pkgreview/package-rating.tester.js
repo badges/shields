595babca24caa03ecd892172858017b0ee8fb49b
Kumar Abhirup <kumar@iqubex.com>
2020-02-19 20:57:04 +0530

    ✅ TEST: Complete Test Suitew

--- services/pkgreview/package-rating.tester.js
+++ services/pkgreview/package-rating.tester.js
@@ -1,6 +1,5 @@
 'use strict'
 
-// const { isBuildStatus } = require('../build-status')
 const t = (module.exports = require('../tester').createServiceTester())
 
 t.create('Stars Badge renders')
@@ -11,53 +10,54 @@ t.create('Rating Badge renders')
   .get('/rating/npm/react.json')
   .expectBadge({ label: 'rating' })
 
-// t.create('Build status (with branch)')
-//   .get('/build/wercker/go-wercker-api/master.json')
-//   .expectBadge({ label: 'build', message: isBuildStatus })
+t.create('Badge fails to render (package not found)')
+  .get('/rating/npm/icuwowgnbneiugvneiurfgrsfghw.json')
+  .expectBadge({
+    label: 'rating',
+    message: 'package not found',
+    color: 'lightgrey',
+  })
 
-// t.create('Build status (application not found)')
-//   .get('/build/some-project/that-doesnt-exist.json')
-//   .expectBadge({ label: 'build', message: 'application not found' })
+t.create('Badge fails to render (package not found)')
+  .get('/rating/lol/react.json')
+  .expectBadge({
+    label: 'rating',
+    message: 'package not found',
+    color: 'lightgrey',
+  })
 
-// t.create('Build status (private application)')
-//   .get('/build/wercker/blueprint.json')
-//   .expectBadge({ label: 'build', message: 'private application not supported' })
+t.create('Badge renders correctly')
+  .get('/rating/npm/react.json')
+  .intercept(nock =>
+    nock('https://pkgreview.dev/api/v1')
+      .get('/npm/react/')
+      .reply(200, {
+        reviewsCount: 3,
+        rating: 1,
+      })
+  )
+  .expectBadge({ label: 'rating', message: '5/5 (3)', color: 'brightgreen' })
 
-// t.create('Build passed')
-//   .get('/build/wercker/go-wercker-api.json')
-//   .intercept(nock =>
-//     nock('https://app.wercker.com/api/v3/applications/')
-//       .get('/wercker/go-wercker-api/builds?limit=1')
-//       .reply(200, [{ status: 'finished', result: 'passed' }])
-//   )
-//   .expectBadge({
-//     label: 'build',
-//     message: 'passing',
-//     color: 'brightgreen',
-//   })
+t.create('Badge renders correctly')
+  .get('/stars/npm/react.json')
+  .intercept(nock =>
+    nock('https://pkgreview.dev/api/v1')
+      .get('/npm/react/')
+      .reply(200, {
+        reviewsCount: 3,
+        rating: 1,
+      })
+  )
+  .expectBadge({ label: 'stars', message: '★★★★★', color: 'brightgreen' })
 
-// t.create('Build failed')
-//   .get('/build/wercker/go-wercker-api.json')
-//   .intercept(nock =>
-//     nock('https://app.wercker.com/api/v3/applications/')
-//       .get('/wercker/go-wercker-api/builds?limit=1')
-//       .reply(200, [{ status: 'finished', result: 'failed' }])
-//   )
-//   .expectBadge({ label: 'build', message: 'failing', color: 'red' })
-
-// t.create('CI status by ID')
-//   .get('/ci/559e33c8e982fc615500b357.json')
-//   .expectBadge({ label: 'build', message: isBuildStatus })
-
-// t.create('CI status by ID (with branch)')
-//   .get('/ci/559e33c8e982fc615500b357/master.json')
-//   .expectBadge({ label: 'build', message: isBuildStatus })
-
-// t.create('CI status by ID (no runs yet)')
-//   .get('/ci/559e33c8e982fc615500b357.json')
-//   .intercept(nock =>
-//     nock('https://app.wercker.com/api/v3')
-//       .get('/runs?applicationId=559e33c8e982fc615500b357&limit=1')
-//       .reply(200, [])
-//   )
-//   .expectBadge({ label: 'build', message: 'not built' })
+t.create('Badge renders correctly')
+  .get('/stars/npm/lol.json')
+  .intercept(nock =>
+    nock('https://pkgreview.dev/api/v1')
+      .get('/npm/lol/')
+      .reply(200, {
+        reviewsCount: 0,
+        rating: null,
+      })
+  )
+  .expectBadge({ label: 'stars', message: '☆☆☆☆☆', color: 'red' })
