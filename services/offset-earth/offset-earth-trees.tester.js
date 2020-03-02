8ca39d53f8778e34db4997f1f2fd1c9b083cca6b
Owen Voke <owzie123@gmail.com>
2020-03-02 14:32:39 +0000

    Add mocked tests for [OffsetEarth] tree service

--- services/offset-earth/offset-earth-trees.tester.js
+++ services/offset-earth/offset-earth-trees.tester.js
@@ -3,13 +3,45 @@
 const t = (module.exports = require('../tester').createServiceTester())
 const { isMetric } = require('../test-validators')
 
-t.create('profile (valid)')
+t.create('request for existing profile')
+  .timeout(10000)
   .get('/offsetearth.json')
   .expectBadge({
     label: 'trees',
     message: isMetric,
   })
 
-t.create('profile (invalid)')
-  .get('/no-profile-test.json')
+t.create('request for existing profile (mock)')
+  .get('/offsetearth.json')
+  .intercept(nock =>
+    nock('https://api.offset.earth')
+      .get('/users/offsetearth/profile')
+      .reply(200, {
+        treeMonths: [
+          {
+            projects: [
+              {
+                trees: [
+                  {
+                    value: 50,
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      })
+  )
+  .expectBadge({
+    label: 'trees',
+    message: isMetric,
+  })
+
+t.create('invalid profile (mock)')
+  .get('/non-existent-username.json')
+  .intercept(nock =>
+    nock('https://api.offset.earth')
+      .get('/users/non-existent-username/profile')
+      .reply(404, {})
+  )
   .expectBadge({ label: 'trees', message: 'profile not found' })
