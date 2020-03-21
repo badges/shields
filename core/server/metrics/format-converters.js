ee0082ca620739de4db9b8f7553ef380b9b86099
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2020-03-21 21:12:35 +0100

    Unused code removed

--- core/server/metrics/format-converters.js
+++ core/server/metrics/format-converters.js
@@ -24,21 +24,4 @@ function promClientJsonToInfluxV2(metrics, extraLabels = {}) {
     .join('\n')
 }
 
-function promClientJsonToInflux(metrics) {
-  return metrics.flatMap(metric =>
-    metric.values.map(value => {
-      const optionalTags =
-        Object.entries(value.labels).length === 0
-          ? ''
-          : `,${Object.entries(value.labels)
-              .map(labelEntry => `${labelEntry[0]}=${labelEntry[1]}`)
-              .toString()}`
-      const optionalTimestamp = value.timestamp ? ` ${value.timestamp}` : ''
-      return `${value.metricName || metric.name}${optionalTags} value=${
-        value.value
-      }${optionalTimestamp}`
-    })
-  )
-}
-
-module.exports = { promClientJsonToInflux, promClientJsonToInfluxV2 }
+module.exports = { promClientJsonToInfluxV2 }
