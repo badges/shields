044a54a51b3ef9f9da025103f8f94c00dad4fc33
Owen Voke <development@voke.dev>
2020-03-25 10:55:44 +0000

    Add [Treeware] trees service

---
+++ services/treeware/treeware-trees.service.js
@@ -0,0 +1,63 @@
+'use strict'
+
+const crypto = require('crypto')
+const Joi = require('@hapi/joi')
+const { metric } = require('../text-formatters')
+const { floorCount } = require('../color-formatters')
+const { BaseJsonService } = require('..')
+
+const apiSchema = Joi.object({
+  total: Joi.number().required(),
+}).required()
+
+module.exports = class TreewareTrees extends BaseJsonService {
+  static get category() {
+    return 'other'
+  }
+
+  static get route() {
+    return {
+      base: 'treeware/trees',
+      pattern: ':owner/:packageName',
+    }
+  }
+
+  static get examples() {
+    return [
+      {
+        title: 'Treeware',
+        namedParams: { owner: 'jamesmills', packageName: 'laravel-timezone' },
+        staticPreview: this.render({ count: 50 }),
+      },
+    ]
+  }
+
+  static get defaultBadgeData() {
+    return { label: 'trees' }
+  }
+
+  static render({ count }) {
+    return { message: metric(count), color: floorCount(count, 10, 50, 100) }
+  }
+
+  async fetch({ reference }) {
+    const url = `https://public.offset.earth/users/treeware/trees?ref=${reference}`
+    return this._requestJson({
+      url,
+      schema: apiSchema,
+      errorMessages: {
+        404: 'repository not found',
+      },
+    })
+  }
+
+  async handle({ owner, packageName }) {
+    const reference = crypto
+      .createHash('md5')
+      .update(`${owner}/${packageName}`)
+      .digest('hex')
+    const { total } = await this.fetch({ reference })
+
+    return this.constructor.render({ count: total })
+  }
+}
