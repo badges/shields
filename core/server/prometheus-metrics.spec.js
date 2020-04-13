92bb99163f234a5ab0497e2bf979d850c6c3f925
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2020-04-13 20:37:45 +0200

    Extra code removed in Prometheus tests

--- core/server/prometheus-metrics.spec.js
+++ core/server/prometheus-metrics.spec.js
@@ -7,7 +7,7 @@ const got = require('../got-test-client')
 const Metrics = require('./prometheus-metrics')
 
 describe('Prometheus metrics route', function() {
-  let port, baseUrl, camp
+  let port, baseUrl, camp, metrics
   beforeEach(async function() {
     port = await portfinder.getPortPromise()
     baseUrl = `http://127.0.0.1:${port}`
@@ -15,26 +15,22 @@ describe('Prometheus metrics route', function() {
     await new Promise(resolve => camp.on('listening', () => resolve()))
   })
   afterEach(async function() {
+    if (metrics) {
+      metrics.stop()
+    }
     if (camp) {
       await new Promise(resolve => camp.close(resolve))
       camp = undefined
     }
   })
 
-  it('returns metrics', async function() {
-    new Metrics().registerMetricsEndpoint(camp)
-
-    const { statusCode } = await got(`${baseUrl}/metrics`)
-
-    expect(statusCode).to.be.equal(200)
-  })
-
   it('returns default metrics', async function() {
-    const metrics = new Metrics()
+    metrics = new Metrics()
     metrics.registerMetricsEndpoint(camp)
 
-    const { body } = await got(`${baseUrl}/metrics`)
+    const { statusCode, body } = await got(`${baseUrl}/metrics`)
 
+    expect(statusCode).to.be.equal(200)
     expect(body).to.contain('nodejs_version_info')
   })
 })
