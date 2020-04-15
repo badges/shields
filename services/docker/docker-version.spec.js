23ceea1d724cadf73ac818ea5c30f1b41537474b
Caleb Cartwright <calebcartwright@users.noreply.github.com>
2020-04-15 13:35:05 -0500

    fix: TypeError in DockerVersion badge (#4907)

--- services/docker/docker-version.spec.js
+++ services/docker/docker-version.spec.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const { expect } = require('chai')
 const { test, given } = require('sazerac')
 const DockerVersion = require('./docker-version.service')
 const {
@@ -8,6 +9,7 @@ const {
   versionDataNoTagSemVerSort,
   versionDataWithTag,
 } = require('./docker-fixtures')
+const { InvalidResponse } = require('..')
 
 describe('DockerVersion', function() {
   test(DockerVersion.prototype.transform, () => {
@@ -46,5 +48,34 @@ describe('DockerVersion', function() {
     }).expect({
       version: '3.10.4',
     })
+  })
+
+  it('throws InvalidResponse error with latest tag and no amd64 architecture digests', function() {
+    expect(() => {
+      DockerVersion.prototype.transform({
+        sort: 'date',
+        data: {
+          results: [
+            {
+              name: 'latest',
+              images: [
+                {
+                  architecture: 'arm64',
+                  digest:
+                    'sha256:597bd5c319cc09d6bb295b4ef23cac50ec7c373fff5fe923cfd246ec09967b31',
+                },
+                {
+                  architecture: 'arm',
+                  digest:
+                    'sha256:c5ea49127cd44d0f50eafda229a056bb83b6e691883c56fd863d42675fae3909',
+                },
+              ],
+            },
+          ],
+        },
+      })
+    })
+      .to.throw(InvalidResponse)
+      .with.property('prettyMessage', 'digest not found for latest tag')
   })
 })
