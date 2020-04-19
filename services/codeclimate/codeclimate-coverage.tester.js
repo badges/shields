e352dfdb1ac47e7a9dcc7ea307244f457212b9bc
PyvesB <PyvesDev@gmail.com>
2020-04-19 12:10:23 +0200

    Fix [CodeclimateCoverage] tests

--- services/codeclimate/codeclimate-coverage.tester.js
+++ services/codeclimate/codeclimate-coverage.tester.js
@@ -4,15 +4,18 @@ const Joi = require('@hapi/joi')
 const { isIntegerPercentage } = require('../test-validators')
 const t = (module.exports = require('../tester').createServiceTester())
 
+// Examples for this service can be found through the explore page:
+// https://codeclimate.com/explore
+
 t.create('test coverage percentage')
-  .get('/coverage/jekyll/jekyll.json')
+  .get('/coverage/codeclimate/minidoc.json')
   .expectBadge({
     label: 'coverage',
     message: isIntegerPercentage,
   })
 
 t.create('test coverage letter')
-  .get('/coverage-letter/jekyll/jekyll.json')
+  .get('/coverage-letter/codeclimate/minidoc.json')
   .expectBadge({
     label: 'coverage',
     message: Joi.equal('A', 'B', 'C', 'D', 'E', 'F'),
