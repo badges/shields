4093db89034d0885d4b1a8e7e2820cb428fffef5
Xianming Zhong <chinesedfan@qq.com>
2020-05-30 23:30:52 +0800

    fix by prettier

--- badge-maker/index.test-d.ts
+++ badge-maker/index.test-d.ts
@@ -3,25 +3,33 @@ import { makeBadge, ValidationError } from '.'
 
 expectError(makeBadge('string is invalid'))
 expectError(makeBadge({}))
-expectError(makeBadge({
-  message: 'passed',
-  style: 'invalid style'
-}))
+expectError(
+  makeBadge({
+    message: 'passed',
+    style: 'invalid style',
+  })
+)
 
-expectType<string>(makeBadge({
-  message: 'passed',
-}))
-expectType<string>(makeBadge({
-  label: 'build',
-  message: 'passed',
-}))
-expectType<string>(makeBadge({
-  label: 'build',
-  message: 'passed',
-  labelColor: 'green',
-  color: 'red',
-  style: 'flat',
-}))
+expectType<string>(
+  makeBadge({
+    message: 'passed',
+  })
+)
+expectType<string>(
+  makeBadge({
+    label: 'build',
+    message: 'passed',
+  })
+)
+expectType<string>(
+  makeBadge({
+    label: 'build',
+    message: 'passed',
+    labelColor: 'green',
+    color: 'red',
+    style: 'flat',
+  })
+)
 
 const error = new ValidationError()
 expectAssignable<Error>(error)
