0fd557d7bb623e3852c92cebac586d5f6d6d89d8
Pierre-Yves B <PyvesDev@gmail.com>
2020-06-10 18:44:21 +0200

    Implement [YouTube] badge (#5132)
    * Implement [YouTube] badge
    
    * Update production-hosting.md with account owner
    
    * Add votes badge variant
    
    * Add links to tests
    
    * Switch to social badge style

---
+++ services/youtube/youtube-base.js
@@ -0,0 +1,72 @@
+'use strict'
+
+const Joi = require('@hapi/joi')
+const { BaseJsonService, NotFound } = require('..')
+const { metric } = require('../text-formatters')
+const { nonNegativeInteger } = require('../validators')
+
+const schema = Joi.object({
+  items: Joi.array()
+    .items(
+      Joi.object({
+        statistics: Joi.object({
+          viewCount: nonNegativeInteger,
+          likeCount: nonNegativeInteger,
+          dislikeCount: nonNegativeInteger,
+          commentCount: nonNegativeInteger,
+        }).required(),
+      })
+    )
+    .required(),
+}).required()
+
+module.exports = class YouTubeBase extends BaseJsonService {
+  static get category() {
+    return 'social'
+  }
+
+  static get auth() {
+    return {
+      passKey: 'youtube_api_key',
+      authorizedOrigins: ['https://www.googleapis.com'],
+      isRequired: true,
+    }
+  }
+
+  static get defaultBadgeData() {
+    return { label: 'youtube', color: 'red', namedLogo: 'youtube' }
+  }
+
+  static renderSingleStat({ statistics, statisticName, videoId }) {
+    return {
+      label: `${statisticName}s`,
+      message: metric(statistics[`${statisticName}Count`]),
+      style: 'social',
+      link: `https://www.youtube.com/watch?v=${encodeURIComponent(videoId)}`,
+    }
+  }
+
+  async fetch({ videoId }) {
+    return this._requestJson(
+      this.authHelper.withQueryStringAuth(
+        { passKey: 'key' },
+        {
+          schema,
+          url: 'https://www.googleapis.com/youtube/v3/videos',
+          options: {
+            qs: { id: videoId, part: 'statistics' },
+          },
+        }
+      )
+    )
+  }
+
+  async handle({ videoId }, queryParams) {
+    const json = await this.fetch({ videoId })
+    if (json.items.length === 0) {
+      throw new NotFound({ prettyMessage: 'video not found' })
+    }
+    const statistics = json.items[0].statistics
+    return this.constructor.render({ statistics, videoId }, queryParams)
+  }
+}
