9acc305d31bc639d8acae4012e5979dbf02f2ded
Pierre-Yves B <PyvesDev@gmail.com>
2020-06-25 08:26:23 +0200

    Handle [cpan] versions that aren't returned as strings (#5239)

--- services/cpan/cpan-version.tester.js
+++ services/cpan/cpan-version.tester.js
@@ -8,6 +8,21 @@ t.create('version (valid)').get('/Config-Augeas.json').expectBadge({
   message: isVPlusDottedVersionAtLeastOne,
 })
 
+t.create('version (version is a number rather than a string)')
+  .get('/Config-Augeas.json')
+  .intercept(nock =>
+    nock('https://fastapi.metacpan.org')
+      .get('/v1/release/Config-Augeas')
+      .reply(200, {
+        license: ['GPL'],
+        version: 0.88,
+      })
+  )
+  .expectBadge({
+    label: 'cpan',
+    message: 'v0.88',
+  })
+
 t.create('version (not found)').get('/not-a-package.json').expectBadge({
   label: 'cpan',
   message: 'not found',
