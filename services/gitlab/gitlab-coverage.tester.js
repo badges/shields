bab97efec55486b976397c74f0dd829ac29775a2
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2020-07-03 15:03:50 +0530

    Add tests for Gitlab Coverage badge

---
+++ services/gitlab/gitlab-coverage.tester.js
@@ -0,0 +1,44 @@
+'use strict'
+
+const { isIntegerPercentage } = require('../test-validators')
+const t = (module.exports = require('../tester').createServiceTester())
+
+t.create('Coverage').get('/gitlab-org/gitlab-runner.json').expectBadge({
+  label: 'coverage',
+  message: isIntegerPercentage,
+})
+
+t.create('Coverage (branch)')
+  .get('/gitlab-org/gitlab-runner/12-0-stable.json')
+  .expectBadge({
+    label: 'coverage',
+    message: isIntegerPercentage,
+  })
+
+t.create('Coverage (existent branch but coverage not set up)')
+  .get('/gitlab-org/gitlab-runner/master.json')
+  .expectBadge({
+    label: 'coverage',
+    message: 'not set up',
+  })
+
+t.create('Coverage (nonexistent branch)')
+  .get('/gitlab-org/gitlab-runner/nope-not-a-branch.json')
+  .expectBadge({
+    label: 'coverage',
+    message: 'not set up',
+  })
+
+t.create('Coverage (nonexistent repo)')
+  .get('/this-repo/does-not-exist.json')
+  .expectBadge({
+    label: 'coverage',
+    message: 'inaccessible',
+  })
+
+t.create('Coverage (custom gitlab URL)')
+  .get('/GNOME/pango.json?gitlab_url=https://gitlab.gnome.org')
+  .expectBadge({
+    label: 'coverage',
+    message: isIntegerPercentage,
+  })
