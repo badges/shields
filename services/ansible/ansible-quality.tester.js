cd3e41fb4c588e854d54b736769753169617ef40
Caleb Cartwright <caleb.cartwright@outlook.com>
2020-07-05 00:01:39 -0500

    tests: fix ansible quality service tests

--- services/ansible/ansible-quality.tester.js
+++ services/ansible/ansible-quality.tester.js
@@ -7,6 +7,10 @@ t.create('quality score (valid)')
   .get('/432.json')
   .expectBadge({ label: 'quality', message: nonNegativeInteger })
 
-t.create('quality score (not found)')
+t.create('quality score (project not found)')
   .get('/0101.json')
+  .expectBadge({ label: 'quality', message: 'not found' })
+
+t.create('quality score (no score available)')
+  .get('/2504.json')
   .expectBadge({ label: 'quality', message: 'no score available' })
