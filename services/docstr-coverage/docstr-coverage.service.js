53931c3989e09b0b8be9dc0482e3b8cf79b34e1f
Fabio <fabio.sangregorio@gmail.com>
2020-07-12 11:07:37 +0200

    Add working artifact download

--- services/docstr-coverage/docstr-coverage.service.js
+++ services/docstr-coverage/docstr-coverage.service.js
@@ -1,22 +1,52 @@
 'use strict'
 
 const Joi = require('@hapi/joi')
-const { BaseJsonService } = require('..')
+const { GithubAuthV3Service } = require('../github/github-auth-service')
+const url = require('url')
+var yauzl = require("yauzl"); // for unzipping the artifact
 
-const schema = Joi.object({
+const artifactsListSchema = Joi.object({
   workflow_runs: Joi.array().items(
-    Joi.object({
-      artifacts_url: Joi.string(),
-    })
-  ),
+    Joi.object({ artifacts_url: Joi.string() })),
 }).required()
 
-module.exports = class Example extends BaseJsonService {
+const singleArtifactSchema = Joi.object({
+  artifacts: Joi.array().items(
+    Joi.object({
+      name: Joi.string(), // TODO: filter artifact based on name
+      archive_download_url: Joi.string()
+    })),
+}).required()
+
+
+async function downloadZip(buffer) {
+  return new Promise((resolve, reject) => {
+    yauzl.fromBuffer(buffer, { lazyEntries: true }, function (err, zipfile) {
+      if (err) reject(err)
+      zipfile.readEntry()
+      zipfile.on("entry", function (entry) {
+        // directory entry
+        if (/\/$/.test(entry.fileName)) {
+          zipfile.readEntry()
+        } else {
+          // file entry
+          if (entry.fileName !== "docstr-coverage.txt") zipfile.readEntry();
+          zipfile.openReadStream(entry, function (err, readStream) {
+            if (err) reject(err);
+            let res;
+            readStream.pipe(res);
+            resolve(res)
+          })
+        }
+      })
+    })
+  })
+}
+
+module.exports = class DocstrCoverage extends GithubAuthV3Service {
   static get category() {
     return 'coverage'
   }
-
-  // https://api.github.com/repos/fabiosangregorio/telereddit/actions/workflows/docs.yml/runs?branch=master&status=success
 
   static get route() {
     return {
@@ -37,34 +67,37 @@ module.exports = class Example extends BaseJsonService {
     }
   }
 
+  // Github API test url: https://api.github.com/repos/fabiosangregorio/telereddit/actions/workflows/docs.yml/runs?branch=master&status=success
+  // Local badge test url: http://localhost:8080/docstr-coverage/fabiosangregorio/telereddit/docs.yml/master
   async fetch({ user, repo, workflow, branch }) {
+    // Get artifacts list from latest Github Workflow run
     const { workflow_runs } = await this._requestJson({
-      schema,
-      url: `https://api.github.com/repos/${user}/${repo}/actions/workflows/${workflow}/runs`,
-      options: {
-        qs: {
-          branch,
-          status: 'success',
-        },
-      },
+      schema: artifactsListSchema,
+      url: `repos/${user}/${repo}/actions/workflows/${workflow}/runs`,
+      options: { qs: { branch, status: 'success' } },
     })
 
     if (!workflow_runs || workflow_runs.length === 0) return '-'
-    const artifact_url = workflow_runs[0].artifacts_url
+    const artifacts_url = workflow_runs[0].artifacts_url
 
-    const https = require('https')
-    const fs = require('fs')
+    // Get single artifact download URL
+    const { artifacts } = await this._requestJson({
+      schema: singleArtifactSchema,
+      url: url.URL(artifacts_url).path
+    })
 
-    const file = fs.createWriteStream('./file.zip')
-    await https.get(
-      artifact_url,
-      {
-        headers: { 'User-Agent': 'docst-coverage' },
-      },
-      function (response) {
-        response.pipe(file)
-      }
-    )
+    if (!artifacts || artifacts.length === 0) return '-' // TODO: pretty print "no artifact in the run" error
+
+    const artifactUrl = url.URL(artifacts[0].archive_download_url).path
+
+    // Download the artifact
+    // TODO: check artifact size before downloading
+    const response = await this._request({ url: artifactUrl, options: { encoding: null } });
+    const buffer = Buffer.from(response.res.body)
+    // Download the artifact
+    const report = await downloadZip(buffer);
+    console.log(report);
+    return 1;
   }
 
   async handle({ user, repo, workflow, branch }) {
