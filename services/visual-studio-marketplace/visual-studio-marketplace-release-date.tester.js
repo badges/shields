543b4009976ba030d7f39cbdc99f195c0f33ba34
YashTotale <totaleyash@gmail.com>
2020-07-31 23:21:38 -0700

    VS Code Marketplace Release Date Badge added along with tests

---
+++ services/visual-studio-marketplace/visual-studio-marketplace-release-date.tester.js
@@ -0,0 +1,78 @@
+'use strict'
+
+const t = (module.exports = require('../tester').createServiceTester())
+const { isFormattedDate } = require('../test-validators')
+
+t.create('date')
+  .get('/visual-studio-marketplace/release-date/yasht.terminal-all-in-one.json')
+  .expectBadge({
+    label: 'release date',
+    message: isFormattedDate,
+  })
+
+t.create('date')
+  .get('/visual-studio-marketplace/release-date/yasht.terminal-all-in-one.json')
+  .intercept(nock =>
+    nock('https://marketplace.visualstudio.com/_apis/public/gallery/')
+      .post(`/extensionquery/`)
+      .reply(200, {
+        results: [
+          {
+            extensions: [
+              {
+                statistics: [
+                  {
+                    statisticName: 'averagerating',
+                    value: 2.5,
+                  },
+                  {
+                    statisticName: 'ratingcount',
+                    value: 10,
+                  },
+                ],
+                versions: [
+                  {
+                    version: '1.0.0',
+                  },
+                ],
+                releaseDate: '2013-04-13T07:50:27.000Z',
+              },
+            ],
+          },
+        ],
+      })
+  )
+  .expectBadge({
+    label: 'release date',
+    message: 'april 2013',
+    color: 'red',
+  })
+
+t.create('invalid extension id')
+  .get('/visual-studio-marketplace/release-date/yasht-terminal-all-in-one.json')
+  .expectBadge({
+    label: 'release date',
+    message: 'invalid extension id',
+  })
+
+t.create('non existent extension')
+  .get(
+    '/visual-studio-marketplace/release-date/yasht.terminal-all-in-one-fake.json'
+  )
+  .expectBadge({
+    label: 'release date',
+    message: 'extension not found',
+  })
+
+t.create('invalid')
+  .get('/visual-studio-marketplace/release-date/yasht.terminal-all-in-one.json')
+  .intercept(nock =>
+    nock('https://marketplace.visualstudio.com/_apis/public/gallery/')
+      .post(`/extensionquery/`)
+      .reply(401)
+  )
+  .expectBadge({
+    label: 'release date',
+    message: 'invalid',
+    color: 'lightgrey',
+  })
