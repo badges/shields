2ef6e95e22da20ae60f187825ea4e5ce991436b6
YashTotale <totaleyash@gmail.com>
2020-08-03 19:26:08 -0700

    made review changes

--- services/visual-studio-marketplace/visual-studio-marketplace-last-updated.tester.js
+++ services/visual-studio-marketplace/visual-studio-marketplace-last-updated.tester.js
@@ -10,45 +10,6 @@ t.create('date')
     message: isFormattedDate,
   })
 
-t.create('date')
-  .get('/visual-studio-marketplace/last-updated/yasht.terminal-all-in-one.json')
-  .intercept(nock =>
-    nock('https://marketplace.visualstudio.com/_apis/public/gallery/')
-      .post(`/extensionquery/`)
-      .reply(200, {
-        results: [
-          {
-            extensions: [
-              {
-                statistics: [
-                  {
-                    statisticName: 'averagerating',
-                    value: 2.5,
-                  },
-                  {
-                    statisticName: 'ratingcount',
-                    value: 10,
-                  },
-                ],
-                versions: [
-                  {
-                    version: '1.0.0',
-                  },
-                ],
-                releaseDate: '2013-04-13T07:50:27.000Z',
-                lastUpdated: '2013-04-13T07:50:27.000Z',
-              },
-            ],
-          },
-        ],
-      })
-  )
-  .expectBadge({
-    label: 'last updated',
-    message: 'april 2013',
-    color: 'red',
-  })
-
 t.create('invalid extension id')
   .get('/visual-studio-marketplace/last-updated/yasht-terminal-all-in-one.json')
   .expectBadge({
@@ -63,17 +24,4 @@ t.create('non existent extension')
   .expectBadge({
     label: 'last updated',
     message: 'extension not found',
-  })
-
-t.create('invalid')
-  .get('/visual-studio-marketplace/last-updated/yasht.terminal-all-in-one.json')
-  .intercept(nock =>
-    nock('https://marketplace.visualstudio.com/_apis/public/gallery/')
-      .post(`/extensionquery/`)
-      .reply(401)
-  )
-  .expectBadge({
-    label: 'last updated',
-    message: 'invalid',
-    color: 'lightgrey',
   })
