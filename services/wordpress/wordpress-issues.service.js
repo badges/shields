39ce3686e20c62d632c5d53030fa7daa6cd83e8c
Joe Izzard <Joe@Izzard.me>
2020-08-31 22:02:19 +0100

    feat: Rework existing code & add new badge
    Reworked existing code, and added a new badge type (opcl)

--- services/wordpress/wordpress-issues.service.js
+++ services/wordpress/wordpress-issues.service.js
@@ -2,37 +2,61 @@
 
 const BaseWordpress = require('./wordpress-base')
 
-module.exports = class WordpressPluginIssues extends BaseWordpress {
+class WordpressPluginIssues extends BaseWordpress {
   static get category() {
     return 'issue-tracking'
   }
 
   static get route() {
     return {
-      base: `wordpress/plugin/issues/`,
-      pattern: ':status(open|closed)/:variant(raw|percentage|outof)/:slug',
+      base: `wordpress/plugin/issues`,
+      pattern: ':variant(raw|percentage|outof)/:status(open|closed)/:slug',
     }
   }
 
   static get examples() {
     return [
       {
-        title: 'WordPress Plugin issues',
-        namedParams: { status: 'open', variant: 'outof', slug: 'akismet' },
-        staticPreview: {
-          label: 'closed issues',
-          message: '80/100',
-          color: 'brightgreen',
-        },
+        title: 'WordPress Plugin Issues',
+        pattern: 'raw/:status(open|closed)/:slug',
+        namedParams: { status: 'open', slug: 'akismet' },
+        staticPreview: this.render({
+          status: 'open',
+          variant: 'raw',
+          issues: '365',
+          closed_issues: '200',
+        }),
+      },
+      {
+        title: 'WordPress Plugin Issues (Percentage)',
+        pattern: 'percentage/:status(open|closed)/:slug',
+        namedParams: { status: 'open', slug: 'akismet' },
+        staticPreview: this.render({
+          status: 'open',
+          variant: 'percentage',
+          issues: '365',
+          closed_issues: '200',
+        }),
+      },
+      {
+        title: 'WordPress Plugin Issues (OutOf)',
+        pattern: 'outof/:status(open|closed)/:slug',
+        namedParams: { status: 'open', slug: 'akismet' },
+        staticPreview: this.render({
+          status: 'open',
+          variant: 'outof',
+          issues: 365,
+          closed_issues: 200,
+        }),
       },
     ]
   }
 
-  calcOpen(issues, closed_issues) {
+  static calcOpen(issues, closed_issues) {
     return issues - closed_issues
   }
 
-  colorCalc(closed_percentage) {
+  static colorCalc(closed_percentage) {
     if (closed_percentage < 50) {
       return 'red'
     } else if (closed_percentage < 90) {
@@ -42,7 +66,7 @@ module.exports = class WordpressPluginIssues extends BaseWordpress {
     }
   }
 
-  rawCalc(issues, closed_issues, status) {
+  static rawCalc(issues, closed_issues, status) {
     if (status === 'open') {
       return this.calcOpen(issues, closed_issues)
     } else {
@@ -50,7 +74,7 @@ module.exports = class WordpressPluginIssues extends BaseWordpress {
     }
   }
 
-  percentageCalc(issues, closed_issues, status) {
+  static percentageCalc(issues, closed_issues, status) {
     const percentClosed = Math.round((closed_issues / issues) * 100)
     if (status === 'open') {
       return 100 - percentClosed
@@ -59,7 +83,7 @@ module.exports = class WordpressPluginIssues extends BaseWordpress {
     }
   }
 
-  outOfCalc(issues, closed_issues, status) {
+  static outOfCalc(issues, closed_issues, status) {
     if (status === 'open') {
       const open_issues = this.calcOpen(issues, closed_issues)
       return `${open_issues}/${issues}`
@@ -68,7 +92,14 @@ module.exports = class WordpressPluginIssues extends BaseWordpress {
     }
   }
 
-  static render({ label, value, color }) {
+  static render({ status, variant, issues, closed_issues }) {
+    const { label, value, color } = this.transform({
+      status,
+      variant,
+      issues,
+      closed_issues,
+    })
+
     return {
       label,
       message: value,
@@ -76,13 +107,7 @@ module.exports = class WordpressPluginIssues extends BaseWordpress {
     }
   }
 
-  async handle({ status, variant, slug }) {
-    const json = await this.fetch({
-      extensionType: 'plugin',
-      slug,
-    })
-    const issues = json.support_threads
-    const closed_issues = json.support_threads_resolved
+  static transform({ status, variant, issues, closed_issues }) {
     const closed_percentage = this.percentageCalc(
       issues,
       closed_issues,
@@ -107,10 +132,120 @@ module.exports = class WordpressPluginIssues extends BaseWordpress {
 
     const color = this.colorCalc(closed_percentage)
 
-    return this.constructor.render({
+    return {
       label,
       value,
       color,
+    }
+  }
+
+  async handle({ variant, status, slug }) {
+    const json = await this.fetch({
+      extensionType: 'plugin',
+      slug,
+    })
+    const issues = json.support_threads
+    const closed_issues = json.support_threads_resolved
+
+    return this.constructor.render({
+      status,
+      variant,
+      issues,
+      closed_issues,
     })
   }
+}
+
+class WordpressPluginIssuesOpenClose extends BaseWordpress {
+  static get category() {
+    return 'issue-tracking'
+  }
+
+  static get route() {
+    return {
+      base: `wordpress/plugin/issues/opcl`,
+      pattern: ':slug',
+    }
+  }
+
+  static get examples() {
+    return [
+      {
+        title: 'WordPress Plugin issues (open/closed)',
+        namedParams: { slug: 'akismet' },
+        staticPreview: this.render({
+          issues: 300,
+          closed_issues: 259,
+        }),
+      },
+    ]
+  }
+
+  static calcOpen(issues, closed_issues) {
+    return issues - closed_issues
+  }
+
+  static colorCalc(closed_percentage) {
+    if (closed_percentage < 50) {
+      return 'red'
+    } else if (closed_percentage < 90) {
+      return 'yellow'
+    } else {
+      return 'brightgreen'
+    }
+  }
+
+  static percentageCalc(issues, closed_issues, status) {
+    const percentClosed = Math.round((closed_issues / issues) * 100)
+    if (status === 'open') {
+      return 100 - percentClosed
+    } else {
+      return percentClosed
+    }
+  }
+
+  static valueColor(issues, closed_issues) {
+    const closed_percentage = this.percentageCalc(
+      issues,
+      closed_issues,
+      'closed'
+    )
+
+    const color = this.colorCalc(closed_percentage)
+    const open_issues = this.calcOpen(issues, closed_issues)
+    const value = `${open_issues}/${closed_issues}`
+
+    return {
+      value,
+      color,
+    }
+  }
+
+  static render({ issues, closed_issues }) {
+    const { value, color } = this.valueColor(issues, closed_issues)
+    return {
+      label: 'open/closed',
+      message: value,
+      color,
+    }
+  }
+
+  async handle({ slug }) {
+    const { support_threads, support_threads_resolved } = await this.fetch({
+      extensionType: 'plugin',
+      slug,
+    })
+    const issues = support_threads
+    const closed_issues = support_threads_resolved
+
+    return this.constructor.render({
+      issues,
+      closed_issues,
+    })
+  }
+}
+
+module.exports = {
+  WordpressPluginIssues,
+  WordpressPluginIssuesOpenClose,
 }
