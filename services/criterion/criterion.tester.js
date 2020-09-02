60d48c4a686b7795a4480662ca8193bbef0cc602
tcross <tom.bz2@gmail.com>
2020-09-01 19:19:59 -0500

    use shared const and formatting

--- services/criterion/criterion.tester.js
+++ services/criterion/criterion.tester.js
@@ -1,17 +1,20 @@
 'use strict'
 
 const t = (module.exports = require('../tester').createServiceTester())
+const {
+  IMPROVED_STATUS,
+  REGRESSED_STATUS,
+  NOT_FOUND_STATUS,
+} = require('./constants')
 
-t.create('Criterion (improved)').get('/chmoder/credit_card.json').expectBadge({
-  label: 'criterion',
-  message: 'improved',
-})
+t.create('Criterion (improved)')
+  .get('/chmoder/credit_card.json')
+  .expectBadge({ label: 'criterion', message: IMPROVED_STATUS })
 
-t.create('Criterion (regressed)').get('/chmoder/data_vault.json').expectBadge({
-  label: 'criterion',
-  message: 'regressed',
-})
+t.create('Criterion (regressed)')
+  .get('/chmoder/data_vault.json')
+  .expectBadge({ label: 'criterion', message: REGRESSED_STATUS })
 
 t.create('Criterion (not found)')
   .get('/chmoder/not-a-repo.json')
-  .expectBadge({ label: 'criterion', message: 'no status found' })
+  .expectBadge({ label: 'criterion', message: NOT_FOUND_STATUS })
