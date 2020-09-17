9cfabf41afbded310a621ff8c33235ca602cc5bd
Jonathan Li <30177086+MonliH@users.noreply.github.com>
2020-09-17 11:49:13 -0400

    Document tokei badge behavior

--- services/tokei/tokei.service.js
+++ services/tokei/tokei.service.js
@@ -13,7 +13,16 @@ const documentation = `
 <p>
   The <code>provider</code> is the domain name of git host.
   If no TLD is provided, <code>.com</code> will be added.
-  For example, setting <code>gitlab</code> or <code>bitbucket.org</code> as the provider also works.
+  For example, setting <code>gitlab</code> or <code>bitbucket.org</code> as the
+  provider also works.
+  <br><br>
+  Tokei will automatically count all files with a recognized extension. It will
+  automatically ignore files and folders in <code>.ignore</code> files. If you
+  want to ignore files or folders specifically for tokei, add them to the
+  <code>.tokeignore</code> in the root of your repository.
+  See 
+    <a href="https://github.com/XAMPPRocky/tokei#excluding-folders">https://github.com/XAMPPRocky/tokei#excluding-folders</a>
+  for more info.
 </p>
 `
 
@@ -53,7 +62,7 @@ module.exports = class Tokei extends BaseJsonService {
     // By default, the API returns an svg, but when the Accept HTTP header is set to
     // `application/json`, it sends json data. The `_requestJson` method
     // automatically sets the Accept Header to what we need, so we don't need to
-    // specify it here
+    // specify it here.
     //
     // This behaviour of the API is "documented" here:
     // https://github.com/XAMPPRocky/tokei_rs/issues/8#issuecomment-475071147
