5931b86c8518366cac96653dd6d53d4416f3a819
Thakur Karthik <iamkarthik08@gmail.com>
2020-10-02 07:34:03 +0530

    Reactor powershellgallery pub puppetforge pypi services (#5635)
    Co-authored-by: repo-ranger[bot] <39074581+repo-ranger[bot]@users.noreply.github.com>

--- services/puppetforge/puppetforge-module-pdk-version.service.js
+++ services/puppetforge/puppetforge-module-pdk-version.service.js
@@ -5,33 +5,25 @@ const { NotFound } = require('..')
 const { BasePuppetForgeModulesService } = require('./puppetforge-base')
 
 module.exports = class PuppetforgeModulePdkVersion extends BasePuppetForgeModulesService {
-  static get category() {
-    return 'platform-support'
+  static category = 'platform-support'
+
+  static route = {
+    base: 'puppetforge/pdk-version',
+    pattern: ':user/:moduleName',
   }
 
-  static get route() {
-    return {
-      base: 'puppetforge/pdk-version',
-      pattern: ':user/:moduleName',
-    }
-  }
-
-  static get examples() {
-    return [
-      {
-        title: 'Puppet Forge – PDK version',
-        namedParams: {
-          user: 'tragiccode',
-          moduleName: 'azure_key_vault',
-        },
-        staticPreview: renderVersionBadge({ version: '1.7.1' }),
+  static examples = [
+    {
+      title: 'Puppet Forge – PDK version',
+      namedParams: {
+        user: 'tragiccode',
+        moduleName: 'azure_key_vault',
       },
-    ]
-  }
+      staticPreview: renderVersionBadge({ version: '1.7.1' }),
+    },
+  ]
 
-  static get defaultBadgeData() {
-    return { label: 'pdk version' }
-  }
+  static defaultBadgeData = { label: 'pdk version' }
 
   async handle({ user, moduleName }) {
     const data = await this.fetch({ user, moduleName })
