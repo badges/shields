005df9958fc248314ebbf2a31fb37dc7c95dc473
Paul Melnikow <email@paulmelnikow.com>
2020-10-16 15:55:41 -0400

    Fix?

--- scripts/run_package_tests.sh
+++ scripts/run_package_tests.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 # https://discuss.circleci.com/t/switch-nodejs-version-on-machine-executor-solved/26675/3
-set -euo pipefail
 
-export NVM_DIR="/opt/circleci/.nvm"
-# Be less strict to work around nvm error.
+# Start off less strict to work around an nvm error.
 # "/opt/circleci/.nvm/nvm.sh: line 1988: NVM_USE_OUTPUT: unbound variable"
 set -e
+export NVM_DIR="/opt/circleci/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+
 set -euo pipefail
 
 nvm install $NODE_VERSION
 nvm use $NODE_VERSION
 node --version
 
-# install the shields.io dependencies
+# Install the shields.io dependencies.
 if [[ "$NODE_VERSION" == "v10" ]]; then
     # Avoid a depcheck error.
     npm ci --ignore-scripts
@@ -22,15 +22,15 @@ else
     npm ci
 fi
 
-# run the package tests
+# Run the package tests.
 npm run test:package
 npm run check-types:package
 
-# delete the sheilds.io dependencies
+# Delete the shields.io dependencies.
 rm -rf node_modules/
 
-# run a smoke test (render a badge with the CLI)
-# with only the package dependencies installed
+# Run a smoke test (render a badge with the CLI) with only the package
+# dependencies installed.
 cd badge-maker
 npm link
 badge cactus grown :green @flat
