fe78cfd0ec187a3d4e03476fe343bd1272e564df
Paul Melnikow <email@paulmelnikow.com>
2020-10-16 01:26:16 -0400

    Match other services where this approach was taken

--- services/issuestats/issuestats.service.js
+++ services/issuestats/issuestats.service.js
@@ -4,7 +4,7 @@ const { deprecatedService } = require('..')
 
 module.exports = deprecatedService({
   category: 'issue-tracking',
-  route: { base: 'issuestats', pattern: ':params*' },
+  route: { base: 'issuestats', pattern: ':various+' },
   label: 'issue stats',
   dateAdded: new Date('2018-09-01'),
 })
