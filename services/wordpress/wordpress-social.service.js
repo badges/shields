a3131c1a355073f80903e18fe851e32108444af1
Joe Izzard <Joe@Izzard.me>
2020-10-16 18:18:55 +0100

    feat: added author and contributor badges
    Added author badges for plugin and theme, and contributor badge for plugins

---
+++ services/wordpress/wordpress-social.service.js
@@ -0,0 +1,140 @@
+'use strict'
+
+const { NotFound } = require('..')
+const BaseWordpress = require('./wordpress-base')
+
+const extensionData = {
+  plugin: {
+    capt: 'Plugin',
+    exampleSlug: 'bbpress',
+  },
+  theme: {
+    capt: 'Theme',
+    exampleSlug: 'twentytwenty',
+  },
+}
+
+function AuthorForType(extensionType) {
+  const { capt, exampleSlug } = extensionData[extensionType]
+
+  return class WordpressAuthor extends BaseWordpress {
+    static name = `Wordpress${capt}Author`
+
+    static category = 'social'
+
+    static route = {
+      base: `wordpress/${extensionType}/author`,
+      pattern: ':slug',
+    }
+
+    static examples = [
+      {
+        title: `WordPress ${capt} Author`,
+        namedParams: { slug: exampleSlug },
+        staticPreview: {
+          label: 'author',
+          message: 'wordpress',
+          style: 'social',
+        },
+      },
+    ]
+
+    static defaultBadgeData = {
+      label: 'author',
+      namedLogo: 'wordpress',
+    }
+
+    static render({ author, link }) {
+      return {
+        label: 'author',
+        message: author,
+        namedLogo: 'wordpress',
+        link,
+      }
+    }
+
+    transform(rawAuthor) {
+      return rawAuthor.substring(
+        rawAuthor.lastIndexOf('">') + 2,
+        rawAuthor.lastIndexOf('<')
+      )
+    }
+
+    async handle({ slug }) {
+      if (extensionType === 'plugin') {
+        const { author: rawAuthor, author_profile: profile } = await this.fetch(
+          {
+            extensionType,
+            slug,
+          }
+        )
+        const author = this.transform(rawAuthor)
+        return this.constructor.render({ author, link: profile })
+      } else {
+        const { author: rawAuthor } = await this.fetch({
+          extensionType,
+          slug,
+        })
+        const author = rawAuthor.display_name
+        const profile = rawAuthor.profile
+        return this.constructor.render({ author, link: profile })
+      }
+    }
+  }
+}
+
+class WordpressPluginContributor extends BaseWordpress {
+  static category = 'social'
+
+  static route = {
+    base: `wordpress/plugin/contributor`,
+    pattern: ':slug/:contributor',
+  }
+
+  static examples = [
+    {
+      title: `WordPress Plugin Contributor`,
+      namedParams: { slug: 'akismet', contributor: 'automattic' },
+      staticPreview: {
+        label: 'contributor',
+        message: 'wordpress',
+        style: 'social',
+      },
+    },
+  ]
+
+  static defaultBadgeData = {
+    label: 'contributor',
+    namedLogo: 'wordpress',
+  }
+
+  static render({ contributor, link }) {
+    return {
+      label: 'contributor',
+      message: contributor,
+      namedLogo: 'wordpress',
+      link,
+    }
+  }
+
+  async handle({ slug, contributor }) {
+    const { contributors } = await this.fetch({
+      extensionType: 'plugin',
+      slug,
+    })
+
+    if (contributor in contributors) {
+      const data = contributors[contributor]
+      return this.constructor.render({
+        contributor: data.display_name,
+        link: data.profile,
+      })
+    } else {
+      throw new NotFound({ prettyMessage: 'Contributor not found in plugin' })
+    }
+  }
+}
+
+const authorModules = ['plugin', 'theme'].map(AuthorForType)
+const modules = [...authorModules, WordpressPluginContributor]
+module.exports = modules
