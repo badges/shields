6e1ec983ac2a1695f707ea2e3ea39792ac7d89d2
Marcin Mielnicki <marcin.mielnicki@gmail.com>
2020-12-21 21:20:49 +0100

    Tests moved to core/server/server.spec.js

--- core/server/server.spec.js
+++ core/server/server.spec.js
@@ -4,6 +4,8 @@ const path = require('path')
 const { expect } = require('chai')
 const isSvg = require('is-svg')
 const config = require('config')
+const nock = require('nock')
+const sinon = require('sinon')
 const got = require('../got-test-client')
 const Server = require('./server')
 const { createTestServer } = require('./in-process-server-test-helpers')
@@ -364,6 +366,68 @@ describe('The server', function () {
         customConfig.private.gh_token = 'my-token'
         expect(() => new Server(customConfig)).to.not.throw()
       })
+    })
+  })
+
+  describe('running with metrics enabled', function () {
+    let server, baseUrl, scope, clock
+    before('Start the server', async function () {
+      // Fixes https://github.com/badges/shields/issues/2611
+      this.timeout(10000)
+      process.env.INSTANCE_ID = 'test-instance'
+      server = await createTestServer({
+        public: {
+          metrics: {
+            prometheus: { enabled: true },
+            influx: {
+              enabled: true,
+              url: 'http://localhost:1112/metrics',
+              instanceIdFrom: 'env-var',
+              instanceIdEnvVarName: 'INSTANCE_ID',
+              envLabel: 'localhost-env',
+              intervalSeconds: 1,
+            },
+          },
+        },
+        private: {
+          influx_username: 'influx-username',
+          influx_password: 'influx-password',
+        },
+      })
+      clock = sinon.useFakeTimers()
+      baseUrl = server.baseUrl
+      await server.start()
+    })
+    after('Shut down the server', async function () {
+      if (server) {
+        await server.stop()
+      }
+      server = undefined
+      nock.cleanAll()
+      delete process.env.INSTANCE_ID
+      clock.restore()
+    })
+
+    it('should push custom metrics', async function () {
+      scope = nock('http://localhost:1112', {
+        reqheaders: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+      })
+        .post(
+          '/metrics',
+          /prometheus,application=shields,category=static,env=localhost-env,family=static-badge,instance=test-instance,service=static_badge service_requests_total=1\n/
+        )
+        .basicAuth({ user: 'influx-username', pass: 'influx-password' })
+        .reply(200)
+      await got(`${baseUrl}badge/fruit-apple-green.svg`)
+
+      await clock.tickAsync(1100)
+
+      expect(scope.isDone()).to.be.equal(
+        true,
+        `pending mocks: ${scope.pendingMocks()}`
+      )
     })
   })
 })
