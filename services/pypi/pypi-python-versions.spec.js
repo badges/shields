6288c2f686b5d3dd27351667a634d0e8f698f831
Samuel FORESTIER <dev@samuel.domains>
2021-01-02 12:29:30 +0100

    [PyPI] Adds a proper specification file for Python versions service

---
+++ services/pypi/pypi-python-versions.spec.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const { test, given } = require('sazerac')
+const { render } = require('./pypi-python-versions.service')
+
+describe('PyPI Python Version', function () {
+  test(render, function () {
+    // Major versions are hidden if minor are present.
+    given({ versions: ['3', '3.4', '3.5', '3.6', '2', '2.7'] }).expect({
+      message: '2.7 | 3.4 | 3.5 | 3.6',
+      color: 'blue',
+    })
+
+    // Major versions are shown when minor are missing.
+    given({ versions: ['2', '3'] }).expect({
+      message: '2 | 3',
+      color: 'blue',
+    })
+
+    // Versions are properly sorted according to their Semver segments.
+    given({ versions: ['3.10', '3.9', '3.8', '3.7', '3.6'] }).expect({
+      message: '3.6 | 3.7 | 3.8 | 3.9 | 3.10',
+      color: 'blue',
+    })
+
+    // Only "one" version works too.
+    given({ versions: ['3', '3.9'] }).expect({
+      message: '3.9',
+      color: 'blue',
+    })
+
+    // Versions are missing...
+    given({ versions: [] }).expect({
+      message: 'missing',
+      color: 'red',
+    })
+  })
+})
