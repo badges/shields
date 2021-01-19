187ea821afec6687349a8f8c60d558da56435cab
Danielle.Derby@trinet.com <Danielle.Derby@trinet.com>
2021-01-19 15:29:53 -0600

    [Bitbucket Server] Adding Auth Tokens and Resolving Pull Request api issue.  Added the bitbucket_server_username & bitbucket_server_password variables to the /core/server/server.js file.  Also changed the user variable in the /services/bitbucket/bitbucket/bitbucket-pull-request.service.js the api documentation (https://docs.atlassian.com/bitbucket-server/rest/5.16.0/bitbucket-rest.html#idm8287391664) defines the parameter as requiring the project name that the repository is contained in not the user name.

--- services/bitbucket/bitbucket-pull-request.service.js
+++ services/bitbucket/bitbucket-pull-request.service.js
@@ -29,7 +29,7 @@ function pullRequestClassGenerator(raw) {
     static category = 'issue-tracking'
     static route = {
       base: `bitbucket/${routePrefix}`,
-      pattern: `:user/:repo`,
+      pattern: `:project/:repo`,
       queryParamSchema,
     }
 
@@ -37,7 +37,7 @@ function pullRequestClassGenerator(raw) {
       {
         title: 'Bitbucket open pull requests',
         namedParams: {
-          user: 'atlassian',
+          project: 'atlassian',
           repo: 'python-bitbucket',
         },
         staticPreview: this.render({ prs: 22 }),
@@ -45,7 +45,7 @@ function pullRequestClassGenerator(raw) {
       {
         title: 'Bitbucket Server open pull requests',
         namedParams: {
-          user: 'foo',
+          project: 'foo',
           repo: 'bar',
         },
         queryParams: { server: 'https://bitbucket.mydomain.net' },
@@ -83,10 +83,10 @@ function pullRequestClassGenerator(raw) {
       )
     }
 
-    async fetchCloud({ user, repo }) {
+    async fetchCloud({ project, repo }) {
       return this._requestJson(
         this.bitbucketAuthHelper.withBasicAuth({
-          url: `https://bitbucket.org/api/2.0/repositories/${user}/${repo}/pullrequests/`,
+          url: `https://bitbucket.org/api/2.0/repositories/${project}/${repo}/pullrequests/`,
           schema,
           options: { qs: { state: 'OPEN', limit: 0 } },
           errorMessages,
@@ -95,10 +95,10 @@ function pullRequestClassGenerator(raw) {
     }
 
     // https://docs.atlassian.com/bitbucket-server/rest/5.16.0/bitbucket-rest.html#idm46229602363312
-    async fetchServer({ server, user, repo }) {
+    async fetchServer({ server, project, repo }) {
       return this._requestJson(
         this.bitbucketServerAuthHelper.withBasicAuth({
-          url: `${server}/rest/api/1.0/projects/${user}/repos/${repo}/pull-requests`,
+          url: `${server}/rest/api/1.0/projects/${project}/repos/${repo}/pull-requests`,
           schema,
           options: {
             qs: {
@@ -113,16 +113,16 @@ function pullRequestClassGenerator(raw) {
       )
     }
 
-    async fetch({ server, user, repo }) {
+    async fetch({ server, project, repo }) {
       if (server !== undefined) {
-        return this.fetchServer({ server, user, repo })
+        return this.fetchServer({ server, project, repo })
       } else {
-        return this.fetchCloud({ user, repo })
+        return this.fetchCloud({ project, repo })
       }
     }
 
-    async handle({ user, repo }, { server }) {
-      const data = await this.fetch({ server, user, repo })
+    async handle({ project, repo }, { server }) {
+      const data = await this.fetch({ server, project, repo })
       return this.constructor.render({ prs: data.size })
     }
   }
