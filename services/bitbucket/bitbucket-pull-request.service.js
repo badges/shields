f4f75f2073264d94a9c44ba92d28d92d65ab0aca
Danielle.Derby@trinet.com <Danielle.Derby@trinet.com>
2021-01-19 18:37:39 -0600

    Update bitbucket-pull-request.service.js
    Updating the label as Bitbucket and Bitbucket Server have different parameters in the Api call.  Changing to compound name per @calebcartwright  recomendation.

--- services/bitbucket/bitbucket-pull-request.service.js
+++ services/bitbucket/bitbucket-pull-request.service.js
@@ -29,7 +29,7 @@ function pullRequestClassGenerator(raw) {
     static category = 'issue-tracking'
     static route = {
       base: `bitbucket/${routePrefix}`,
-      pattern: `:project/:repo`,
+      pattern: `:projectOrUser/:repo`,
       queryParamSchema,
     }
 
@@ -37,7 +37,7 @@ function pullRequestClassGenerator(raw) {
       {
         title: 'Bitbucket open pull requests',
         namedParams: {
-          project: 'atlassian',
+          projectOrUser: 'atlassian',
           repo: 'python-bitbucket',
         },
         staticPreview: this.render({ prs: 22 }),
@@ -45,7 +45,7 @@ function pullRequestClassGenerator(raw) {
       {
         title: 'Bitbucket Server open pull requests',
         namedParams: {
-          project: 'foo',
+          projectOrUser: 'foo',
           repo: 'bar',
         },
         queryParams: { server: 'https://bitbucket.mydomain.net' },
@@ -83,10 +83,10 @@ function pullRequestClassGenerator(raw) {
       )
     }
 
-    async fetchCloud({ project, repo }) {
+    async fetchCloud({ projectOrUser, repo }) {
       return this._requestJson(
         this.bitbucketAuthHelper.withBasicAuth({
-          url: `https://bitbucket.org/api/2.0/repositories/${project}/${repo}/pullrequests/`,
+          url: `https://bitbucket.org/api/2.0/repositories/${projectOrUser}/${repo}/pullrequests/`,
           schema,
           options: { qs: { state: 'OPEN', limit: 0 } },
           errorMessages,
@@ -95,10 +95,10 @@ function pullRequestClassGenerator(raw) {
     }
 
     // https://docs.atlassian.com/bitbucket-server/rest/5.16.0/bitbucket-rest.html#idm46229602363312
-    async fetchServer({ server, project, repo }) {
+    async fetchServer({ server, projectOrUser, repo }) {
       return this._requestJson(
         this.bitbucketServerAuthHelper.withBasicAuth({
-          url: `${server}/rest/api/1.0/projects/${project}/repos/${repo}/pull-requests`,
+          url: `${server}/rest/api/1.0/projects/${projectOrUser}/repos/${repo}/pull-requests`,
           schema,
           options: {
             qs: {
@@ -113,16 +113,16 @@ function pullRequestClassGenerator(raw) {
       )
     }
 
-    async fetch({ server, project, repo }) {
+    async fetch({ server, projectOrUser, repo }) {
       if (server !== undefined) {
-        return this.fetchServer({ server, project, repo })
+        return this.fetchServer({ server, projectOrUser, repo })
       } else {
-        return this.fetchCloud({ project, repo })
+        return this.fetchCloud({ projectOrUser, repo })
       }
     }
 
-    async handle({ project, repo }, { server }) {
-      const data = await this.fetch({ server, project, repo })
+    async handle({ projectOrUser, repo }, { server }) {
+      const data = await this.fetch({ server, projectOrUser, repo })
       return this.constructor.render({ prs: data.size })
     }
   }
