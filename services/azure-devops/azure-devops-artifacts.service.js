de308b45317d329ade4e9a90afb54ba9de5d91f5
Harry Pierson <harrypierson@hotmail.com>
2021-01-30 00:06:14 +0000

    initial azure artifacts badge

---
+++ services/azure-devops/azure-devops-artifacts.service.js
@@ -0,0 +1,88 @@
+'use strict'
+
+const Joi = require('joi')
+const { version: versionColor } = require('../color-formatters')
+const { addv } = require('../text-formatters')
+const {
+  renderVersionBadge: renderNugetVersionBadge,
+} = require('../nuget/nuget-helpers')
+const AzureDevOpsBase = require('./azure-devops-base')
+
+const getPackagesSchema = Joi.object({
+  count: Joi.number().required(),
+  value: Joi.array()
+    .items(
+      Joi.object({
+        id: Joi.string().required(),
+        normalizedName: Joi.string().required(),
+        protocolType: Joi.string().required(),
+        versions: Joi.array()
+          .items(
+            Joi.object({
+              version: Joi.string().required(),
+            })
+          )
+          .required()
+          .min(1),
+      })
+    )
+    .required(),
+}).required()
+
+module.exports = class Example extends AzureDevOpsBase {
+  static category = 'build'
+
+  static route = {
+    base: 'azure-devops/artifacts',
+    pattern: ':organization/:project/:feedId/:packageName',
+  }
+
+  async handle({ organization, project, feedId, packageName }) {
+    const url = `https://feeds.dev.azure.com/${organization}/${project}/_apis/packaging/Feeds/${feedId}/packages`
+    const options = {
+      qs: {
+        packageNameQuery: packageName,
+        $top: 1,
+        includeUrls: false,
+        'api-version': '6.0-preview.1',
+      },
+    }
+
+    const json = await this.fetch({
+      url,
+      options,
+      schema: getPackagesSchema,
+      errorMessages: {
+        404: 'organization, project or feed not found',
+      },
+    })
+
+    if (json.value.length === 0) {
+      return {
+        label: 'Azure Artifacts',
+        message: `unknown package ${packageName}`,
+        color: 'red',
+      }
+    }
+
+    const packageObj = json.value[0]
+    if (packageObj.normalizedName !== packageName.toLowerCase()) {
+      return {
+        label: 'Azure Artifacts',
+        message: `unknown package ${packageName}`,
+        color: 'red',
+      }
+    }
+
+    const protocolType = packageObj.protocolType
+    const version = packageObj.versions[0].version
+
+    return protocolType === 'NuGet'
+      ? renderNugetVersionBadge({ version, feed: `Azure Artifacts NuGet` })
+      : {
+          label: `Azure Artifacts ${protocolType}`,
+          message: addv(version),
+          color: versionColor(version),
+        }
+  }
+}
