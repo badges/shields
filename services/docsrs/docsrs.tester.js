43145d5e172382d1b72714fd66688288ea7080db
Shaurya Shubham <shauryashubham1234567890@gmail.com>
2021-01-30 03:50:37 +0000

    Fix incorrect tests

--- services/docsrs/docsrs.tester.js
+++ services/docsrs/docsrs.tester.js
@@ -3,9 +3,9 @@
 const t = (module.exports = require('../tester').createServiceTester())
 
 t.create('Passing docs')
-  .get('/tokio/0.3.0')
+  .get('/tokio/0.3.0.json')
   .expectBadge({ label: 'docs@0.3.0', message: 'passing' })
 
 t.create('Failing docs')
-  .get('/tensorlow/0.16.1')
-  .expectBadge({ label: 'docs@1.6.1', message: 'failing' })
+  .get('/tensorflow/0.16.1.json')
+  .expectBadge({ label: 'docs@0.16.1', message: 'failing' })
