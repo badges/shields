f825efec6b7b7cb5744b8b64cbf76de670152694
Tarun Singh <taruninsa1@gmail.com>
2021-03-29 19:08:34 +0530

    added youtube total channel views shield

---
+++ services/youtube/youtube-channel-views.service.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const { documentation, YouTubeChannelBase } = require('./youtube-channel-base')
+
+module.exports = class YouTubeSubscribes extends YouTubeChannelBase {
+  static route = {
+    base: 'youtube/channel/views',
+    pattern: ':channelId',
+  }
+
+  static get examples() {
+    const preview = this.render({
+      statistics: { viewCount: 30543 },
+      channelId: 'UCTNq28Ah5eyDgsYOA0oHzew',
+    })
+    // link[] is not allowed in examples
+    delete preview.link
+    return [
+      {
+        title: 'YouTube Channel Views',
+        namedParams: { channelId: 'UCTNq28Ah5eyDgsYOA0oHzew' },
+        staticPreview: preview,
+        documentation,
+      },
+    ]
+  }
+
+  static render({ statistics, channelId }) {
+    return super.renderSingleStat({
+      statistics,
+      statisticName: 'view',
+      channelId,
+    })
+  }
+}
