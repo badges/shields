7a5e319213a44537b66b006b372d6881bb22e6eb
Sean Glover <sean@seanglover.com>
2021-04-08 18:52:20 -0400

    lowercase hashes

--- services/nexus/nexus-version.js
+++ services/nexus/nexus-version.js
@@ -1,7 +1,7 @@
 'use strict'
 
 function isSnapshotVersion(version) {
-  const pattern = /(\d+\.)*[0-9a-fA-F]-SNAPSHOT/
+  const pattern = /(\d+\.)*[0-9a-f]-SNAPSHOT/
   return version && version.match(pattern)
 }
 
