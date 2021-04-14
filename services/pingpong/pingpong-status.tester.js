22cc1524c0c9318d3a7f354913844c95b846fbe9
Andrej Babic <hello@bluecap.se>
2021-04-14 16:09:35 +0200

    PingPong PR fixes

--- services/pingpong/pingpong-status.tester.js
+++ services/pingpong/pingpong-status.tester.js
@@ -14,38 +14,16 @@ t.create('PingPong: Status (valid)')
   .get('/sp_eb705b7c189f42e3b574dc790291c33f.json')
   .expectBadge({ label: 'status', message: isCorrectStatus })
 
-t.create('PingPong: Status (invalid, incorrect format)')
-  .get('/not-a-real-api-key.json')
-  .expectBadge({ label: 'status', message: 'invalid api key' })
-
 t.create('PingPong: Status (valid, incorrect format)')
   .get('/eb705b7c189f42e3b574dc790291c33f.json')
   .expectBadge({ label: 'status', message: 'invalid api key' })
 
-t.create('PingPong: Status (backend error)')
-  .get('/sp_key.json')
-  .intercept(nock =>
-    nock('https://api.pingpong.one')
-      .get('/widget/badge/status/sp_key')
-      .reply(503, '')
-  )
-  .expectBadge({ label: 'status', message: 'inaccessible' })
-
-t.create('PingPong: Status (unexpected response, valid json)')
-  .get('/sp_key.json')
-  .intercept(nock =>
-    nock('https://api.pingpong.one')
-      .get('/widget/badge/status/sp_key')
-      .reply(200, '[]')
-  )
-  .expectBadge({ label: 'status', message: 'invalid response data' })
-
-t.create('PingPong: Status (unexpected response, missing json values)')
+t.create('PingPong: Status (unexpected response)')
   .get('/sp_key.json')
   .intercept(
     nock =>
       nock('https://api.pingpong.one')
-        .get('/widget/badge/status/sp_key')
-        .reply(200, '{"message": "up"}') // no color key
+        .get('/widget/shields/status/sp_key')
+        .reply(200, '{"status": "up"}') // unexpected status message
   )
-  .expectBadge({ label: 'status', message: 'invalid response data' })
+  .expectBadge({ message: 'unknown', color: 'lightgrey' })
