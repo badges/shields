ee402469b5efa2cb4c86c7d0df5e51d89aec12a0
Ideotec <guille@ideotec.es>
2021-04-16 22:08:40 +1200

    Added GitLab Tag service

---
+++ services/gitlab/gitlab-tag.tester.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const Joi = require('joi')
+const t = (module.exports = require('../tester').createServiceTester())
+
+t.create('Tag (latest by date)')
+  .get('/fdroid/fdroidclient.json')
+  .expectBadge({ label: 'tag', message: Joi.string(), color: 'blue' })
