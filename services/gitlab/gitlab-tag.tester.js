14a20e6d70f52a8009af9a19ed6bfdb9d45466cf
Ideotec <guille@ideotec.es>
2021-04-17 09:33:28 +1200

    Added prettyMessage for when repo has no tags

--- services/gitlab/gitlab-tag.tester.js
+++ services/gitlab/gitlab-tag.tester.js
@@ -6,3 +6,7 @@ const t = (module.exports = require('../tester').createServiceTester())
 t.create('Tag (latest by date)')
   .get('/fdroid/fdroidclient.json')
   .expectBadge({ label: 'tag', message: Joi.string(), color: 'blue' })
+
+t.create('Tag (no tags)')
+  .get('/fdroid/fdroiddata.json')
+  .expectBadge({ label: 'tag', message: 'no tags found' })
