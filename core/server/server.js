b6e6a79b4f63a0b83ee7350579d2eb7a77f4a0ff
PyvesB <PyvesDev@gmail.com>
2021-04-24 11:26:47 +0100

    Deprecate [Bintray] service

--- core/server/server.js
+++ core/server/server.js
@@ -152,8 +152,6 @@ const publicConfigSchema = Joi.object({
 
 const privateConfigSchema = Joi.object({
   azure_devops_token: Joi.string(),
-  bintray_user: Joi.string(),
-  bintray_apikey: Joi.string(),
   discord_bot_token: Joi.string(),
   drone_token: Joi.string(),
   gh_client_id: Joi.string(),
