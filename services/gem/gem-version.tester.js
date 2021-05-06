4c9412539f205811ae7467b8481678f2f05e031b
profgrammer <javarep4199@gmail.com>
2021-05-06 18:09:38 +0530

    add regex for rc versions

--- services/gem/gem-version.tester.js
+++ services/gem/gem-version.tester.js
@@ -2,7 +2,7 @@
 
 const {
   isVPlusDottedVersionAtLeastOne,
-  isVPlusDottedVersionNClausesWithOptionalSuffix,
+  withRegex,
 } = require('../test-validators')
 const t = (module.exports = require('../tester').createServiceTester())
 
@@ -15,12 +15,15 @@ t.create('version (not found)')
   .get('/not-a-package.json')
   .expectBadge({ label: 'gem', message: 'not found' })
 
-t.create('latest version (valid)')
-  .get('/flame/latest.json')
-  .expectBadge({
-    label: 'gem',
-    message: isVPlusDottedVersionNClausesWithOptionalSuffix,
-  })
+// this is the same as isVPlusDottedVersionNClausesWithOptionalSuffix from test-validators.js
+// except that it also accepts regexes like 5.0.0.rc5 - the . before the rc5 is not accepted in the original
+const isVPlusDottedVersionNClausesWithOptionalSuffix = withRegex(
+  /^v\d+(\.\d+)*([-+~.].*)?$/
+)
+t.create('latest version (valid)').get('/flame/latest.json').expectBadge({
+  label: 'gem',
+  message: isVPlusDottedVersionNClausesWithOptionalSuffix,
+})
 
 t.create('latest version (not found)')
   .get('/not-a-package/latest.json')
