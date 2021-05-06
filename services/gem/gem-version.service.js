7edaea304aff8db8add0e07ad24a143647d67abf
profgrammer <javarep4199@gmail.com>
2021-05-06 12:30:44 +0530

    add optional distribution flag to GemVersion

--- services/gem/gem-version.service.js
+++ services/gem/gem-version.service.js
@@ -1,8 +1,9 @@
 'use strict'
 
 const Joi = require('joi')
-const { renderVersionBadge } = require('../version')
-const { BaseJsonService } = require('..')
+const { renderVersionBadge, latest } = require('../version')
+const { BaseJsonService, NotFound } = require('..')
+const { nonNegativeInteger } = require('../validators')
 
 const schema = Joi.object({
   // In most cases `version` will be a SemVer but the registry doesn't
@@ -10,9 +11,22 @@ const schema = Joi.object({
   version: Joi.string().required(),
 }).required()
 
+const versionSchema = Joi.array()
+  .items(
+    Joi.object({
+      prerelease: Joi.boolean().required(),
+      number: Joi.string().required(),
+      downloads_count: nonNegativeInteger,
+    })
+  )
+  .min(1)
+  .required()
+
+const defaultDistribution = 'stable'
+
 module.exports = class GemVersion extends BaseJsonService {
   static category = 'version'
-  static route = { base: 'gem/v', pattern: ':gem' }
+  static route = { base: 'gem/v', pattern: ':gem/:distribution?' }
   static examples = [
     {
       title: 'Gem',
@@ -35,8 +49,24 @@ module.exports = class GemVersion extends BaseJsonService {
     })
   }
 
-  async handle({ gem }) {
-    const { version } = await this.fetch({ gem })
-    return this.constructor.render({ version })
+  async fetchLatest({ gem }) {
+    return this._requestJson({
+      schema: versionSchema,
+      url: `https://rubygems.org/api/v1/versions/${gem}.json`,
+    })
+  }
+
+  async handle({ gem, distribution = defaultDistribution }) {
+    if (distribution === defaultDistribution) {
+      const { version } = await this.fetch({ gem })
+      return this.constructor.render({ version })
+    } else {
+      const data = await this.fetchLatest({ gem })
+      if (!Array.isArray(data) || data.length === 0) {
+        throw new NotFound()
+      }
+      const versions = data.map(version => version.number)
+      return this.constructor.render({ version: latest(versions) })
+    }
   }
 }
--- services/gem/gem-version.tester.js
+++ services/gem/gem-version.tester.js
@@ -1,6 +1,9 @@
 'use strict'
 
-const { isVPlusDottedVersionAtLeastOne } = require('../test-validators')
+const {
+  isVPlusDottedVersionAtLeastOne,
+  isVPlusDottedVersionNClausesWithOptionalSuffix,
+} = require('../test-validators')
 const t = (module.exports = require('../tester').createServiceTester())
 
 t.create('version (valid)').get('/formatador.json').expectBadge({
@@ -10,4 +13,15 @@ t.create('version (valid)').get('/formatador.json').expectBadge({
 
 t.create('version (not found)')
   .get('/not-a-package.json')
+  .expectBadge({ label: 'gem', message: 'not found' })
+
+t.create('latest version (valid)')
+  .get('/flame/latest.json')
+  .expectBadge({
+    label: 'gem',
+    message: isVPlusDottedVersionNClausesWithOptionalSuffix,
+  })
+
+t.create('latest version (not found)')
+  .get('/not-a-package/latest.json')
   .expectBadge({ label: 'gem', message: 'not found' })
