e63a555eb7e028785209bb1f3b64548bf9a37493
anatawa12 <anatawa12@icloud.com>
2021-05-30 10:00:50 +0900

    implement gradle-plugin-portal as a redirector

--- services/gradle-plugin-portal/gradle-plugin-portal.service.js
+++ services/gradle-plugin-portal/gradle-plugin-portal.service.js
@@ -1,31 +1,18 @@
 'use strict'
 
-const Joi = require('joi')
-const { renderVersionBadge } = require('../version')
-const { BaseXmlService, NotFound } = require('..')
+const { redirector } = require('..')
 
-const schema = Joi.object({
-  metadata: Joi.object({
-    versioning: Joi.object({
-      versions: Joi.object({
-        version: Joi.array().items(Joi.string().required()).single().required(),
-      }).required(),
-    }).required(),
-  }).required(),
-}).required()
-
-module.exports = class GradlePluginPortal extends BaseXmlService {
-  static category = 'version'
-
-  static route = {
+module.exports = redirector({
+  category: 'version',
+  isDeprecated: false,
+  route: {
     base: 'gradle-plugin-portal/v',
-    pattern: ':pluginId/:versionPrefix?',
-  }
-
-  static examples = [
+    // TODO: add /:versionPrefix? when maven-metadata have versionPrefix attribute.
+    pattern: ':pluginId'
+  },
+  examples: [
     {
       title: 'Gradle Plugin Portal',
-      pattern: ':pluginId',
       namedParams: {
         pluginId: 'com.gradle.plugin-publish',
       },
@@ -35,6 +22,7 @@ module.exports = class GradlePluginPortal extends BaseXmlService {
         color: 'blue',
       },
     },
+    /*
     {
       title: 'Gradle Plugin Portal with version prefix filter',
       pattern: ':pluginId/:versionPrefix',
@@ -48,34 +36,19 @@ module.exports = class GradlePluginPortal extends BaseXmlService {
         color: 'blue',
       },
     },
-  ]
-
-  static defaultBadgeData = {
-    label: 'plugin portal',
-  }
-
-  async fetch({ pluginId }) {
-    const plugin = encodeURIComponent(pluginId)
-    const group = plugin.replace(/\./g, '/')
-    const artifact = `${plugin}.gradle.plugin`
-    const url = `https://plugins.gradle.org/m2/${group}/${artifact}/maven-metadata.xml`
-    return this._requestXml({
-      schema,
-      url,
-      parserOptions: { parseNodeValue: false },
-    })
-  }
-
-  async handle({ pluginId, versionPrefix }) {
-    const data = await this.fetch({ pluginId })
-    const versions = data.metadata.versioning.versions.version.reverse()
-    let version = versions[0]
-    if (versionPrefix !== undefined) {
-      version = versions.filter(v => v.toString().startsWith(versionPrefix))[0]
-      // if the filter returned no results, throw a NotFound
-      if (version === undefined)
-        throw new NotFound({ prettyMessage: 'version prefix not found' })
+     */
+  ],
+  transformPath: () => `/maven-metadata/v`,
+  transformQueryParams: ({ pluginId }) => {
+    const groupPath = pluginId.replaceAll('.', '/')
+    const artifactId = `${pluginId}.gradle.plugin`
+    const metadataUrl = `https://plugins.gradle.org/m2/${groupPath}/${artifactId}/maven-metadata.xml`;
+    return {
+      metadataUrl,
+      label: 'plugin portal',
+      color: 'blue',
     }
-    return renderVersionBadge({ version })
-  }
-}
+  },
+  overrideTransformedQueryParams: true,
+  dateAdded: new Date('2021-05-30'),
+});
--- services/gradle-plugin-portal/gradle-plugin-portal.tester.js
+++ services/gradle-plugin-portal/gradle-plugin-portal.tester.js
@@ -1,55 +1,13 @@
 'use strict'
 
-const {
-  isVPlusDottedVersionNClausesWithOptionalSuffix,
-} = require('../test-validators')
 const t = (module.exports = require('../tester').createServiceTester())
 
-// https://plugins.gradle.org/m2/com/gradle/plugin-publish/com.gradle.plugin-publish.gradle.plugin/
-t.create('latest version').get('/com.gradle.plugin-publish.json').expectBadge({
-  label: 'plugin portal',
-  message: isVPlusDottedVersionNClausesWithOptionalSuffix,
-})
-
-// https://plugins.gradle.org/m2/com/gradle/plugin-publish/com.gradle.plugin-publish.gradle.plugin/
-t.create('latest 0.10 version')
-  .get('/com.gradle.plugin-publish/0.10.json')
-  .expectBadge({
-    label: 'plugin portal',
-    message: isVPlusDottedVersionNClausesWithOptionalSuffix,
-  })
-
-t.create('inexistent artifact')
-  .get('/inexistent-plugin-id.json')
-  .expectBadge({ label: 'plugin portal', message: 'not found' })
-
-t.create('inexistent version prefix')
-  .get('/com.gradle.plugin-publish/1000.json')
-  .expectBadge({ label: 'plugin portal', message: 'version prefix not found' })
-
-t.create('version ending with zero')
-  .get('/mocked-plugin-id.json')
-  .intercept(nock =>
-    nock('https://plugins.gradle.org/m2')
-      .get(
-        '/mocked-plugin-id/mocked-plugin-id.gradle.plugin/maven-metadata.xml'
-      )
-      .reply(
-        200,
-        `
-      <metadata>
-        <groupId>mocked-plugin-id</groupId>
-        <artifactId>mocked-plugin-id.gradle.plugin</artifactId>
-        <versioning>
-            <latest>1.30</latest>
-            <release>1.30</release>
-            <versions>
-                <version>1.30</version>
-            </versions>
-            <lastUpdated>20190902002617</lastUpdated>
-        </versioning>
-        </metadata>
-      `
-      )
+t.create('gradle plugin portal')
+  .get(
+    '/com.gradle.plugin-publish'
   )
-  .expectBadge({ label: 'plugin portal', message: 'v1.30' })
+  .expectRedirect(
+    `/maven-metadata/v.svg?color=blue&label=plugin%20portal&metadataUrl=${encodeURIComponent(
+      'https://plugins.gradle.org/m2/com/gradle/plugin-publish/com.gradle.plugin-publish.gradle.plugin/maven-metadata.xml'
+    )}`
+  )
