ac8ffe686988b413357380f5210a55834f39f172
Weihao <weihao.gao@outlook.com>
2021-06-05 17:47:15 -0400

    Update test file

--- services/bstats/bstats-managed-servers.tester.js
+++ services/bstats/bstats-managed-servers.tester.js
@@ -3,7 +3,7 @@
 const { isMetric } = require('../test-validators')
 const t = (module.exports = require('../tester').createServiceTester())
 
-t.create('Managed Servers').get('/1.json').expectBadge({
+t.create('Managed Servers').get('/11269.json').expectBadge({
   label: 'managed servers',
   message: isMetric,
 })
