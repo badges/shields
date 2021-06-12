d8c71c323818826d6406f85da888ed0a3f2bc9c6
anatawa12 <anatawa12@icloud.com>
2021-06-12 21:30:11 +0900

    add tests of maven-central for redirecting

--- services/maven-central/maven-central.tester.js
+++ services/maven-central/maven-central.tester.js
@@ -5,6 +5,22 @@ const {
 } = require('../test-validators')
 const t = (module.exports = require('../tester').createServiceTester())
 
+t.create('latest version redirection')
+  .get('/com.github.fabriziocucci/yacl4j.json') // http://repo1.maven.org/maven2/com/github/fabriziocucci/yacl4j/
+  .expectRedirect(
+    `/maven-metadata/v.svg?color=blue&label=maven-central&metadataUrl=${encodeURIComponent(
+      'https://repo1.maven.org/maven2/com/github/fabriziocucci/yacl4j/maven-metadata.xml'
+    )}`
+  )
+
+t.create('latest 0.8 version redirection')
+  .get('/com.github.fabriziocucci/yacl4j/0.8.json') // http://repo1.maven.org/maven2/com/github/fabriziocucci/yacl4j/
+  .expectRedirect(
+    `/maven-metadata/v.svg?color=blue&label=maven-central&metadataUrl=${encodeURIComponent(
+      'https://repo1.maven.org/maven2/com/github/fabriziocucci/yacl4j/maven-metadata.xml'
+    )}&versionPrefix=0.8`
+  )
+
 t.create('latest version')
   .get('/com.github.fabriziocucci/yacl4j.json') // http://repo1.maven.org/maven2/com/github/fabriziocucci/yacl4j/
   .expectBadge({
