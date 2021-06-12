b888cf9f064ea032a26b16324c0247abe39b8477
anatawa12 <anatawa12@icloud.com>
2021-06-12 22:09:13 +0900

    add example for gradle-plugin-portal

--- services/gradle-plugin-portal/gradle-plugin-portal.service.js
+++ services/gradle-plugin-portal/gradle-plugin-portal.service.js
@@ -12,12 +12,57 @@ module.exports = redirector({
   examples: [
     {
       title: 'Gradle Plugin Portal',
+      pattern: ':pluginId',
       namedParams: {
         pluginId: 'com.gradle.plugin-publish',
       },
       staticPreview: {
         label: 'plugin portal',
         message: 'v0.14.0',
+        color: 'blue',
+      },
+    },
+    {
+      title: 'Gradle Plugin Portal with version prefix filter',
+      pattern: ':pluginId/:versionPrefix',
+      namedParams: {
+        pluginId: 'com.gradle.plugin-publish',
+        versionPrefix: '0.13',
+      },
+      staticPreview: {
+        label: 'plugin portal',
+        message: 'v0.13.0',
+        color: 'blue',
+      },
+    },
+    {
+      title: 'Gradle Plugin Portal with version suffix filter',
+      pattern: ':pluginId',
+      queryParams: {
+        versionSuffix: '.1',
+      },
+      namedParams: {
+        pluginId: 'com.gradle.plugin-publish',
+      },
+      staticPreview: {
+        label: 'maven-central',
+        message: 'v30.1.1-android',
+        color: 'blue',
+      },
+    },
+    {
+      title: 'Gradle Plugin Portal with version prefix and suffix filter',
+      pattern: ':pluginId/:versionPrefix',
+      queryParams: {
+        versionSuffix: '.1',
+      },
+      namedParams: {
+        pluginId: 'com.gradle.plugin-publish',
+        versionPrefix: '0.10',
+      },
+      staticPreview: {
+        label: 'maven-central',
+        message: 'v29.0-android',
         color: 'blue',
       },
     },
