fabf736c7393fd5c1a916ec6a0994b9f330f92ae
PyvesB <PyvesDev@gmail.com>
2021-06-20 12:23:01 +0100

    Fix Gatsby files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,32 +1,30 @@
 import path from 'path';
 import {fileURLToPath} from 'url'
 
-export {
-  siteMetadata: {
-    title: 'Shields.io: Quality metadata badges for open source projects',
-    description:
-      'We serve fast and scalable informational images as badges for GitHub, Travis CI, Jenkins, WordPress and many more services. Use them to track the state of your projects, or for promotional purposes.',
-    author: '@shields_io',
-  },
-  plugins: [
-    {
-      resolve: 'gatsby-plugin-page-creator',
-      options: {
-        path: path.join(path.dirname(fileURLToPath(import.meta.url)), 'frontend', 'pages'),
-      },
+export const siteMetadata = {
+  title: 'Shields.io: Quality metadata badges for open source projects',
+  description:
+    'We serve fast and scalable informational images as badges for GitHub, Travis CI, Jenkins, WordPress and many more services. Use them to track the state of your projects, or for promotional purposes.',
+  author: '@shields_io',
+}
+export const plugins = [
+  {
+    resolve: 'gatsby-plugin-page-creator',
+    options: {
+      path: path.join(path.dirname(fileURLToPath(import.meta.url)), 'frontend', 'pages'),
     },
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-remove-trailing-slashes',
-    'gatsby-plugin-typescript',
-    // This currently is not being used.
-    // {
-    //   resolve: 'gatsby-source-filesystem',
-    //   options: {
-    //     name: 'static',
-    //     path: `${path.dirname(fileURLToPath(import.meta.url))}/frontend/static`,
-    //   },
-    // },
-  ],
-};
+  },
+  'gatsby-plugin-react-helmet',
+  'gatsby-plugin-catch-links',
+  'gatsby-plugin-styled-components',
+  'gatsby-plugin-remove-trailing-slashes',
+  'gatsby-plugin-typescript',
+  // This currently is not being used.
+  // {
+  //   resolve: 'gatsby-source-filesystem',
+  //   options: {
+  //     name: 'static',
+  //     path: `${path.dirname(fileURLToPath(import.meta.url))}/frontend/static`,
+  //   },
+  // },
+]
