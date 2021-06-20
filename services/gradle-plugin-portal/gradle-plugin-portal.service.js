626ffa52d5164750de0b30c6d33e33655ee7a42a
PyvesB <PyvesDev@gmail.com>
2021-06-20 12:02:58 +0100

    Fix new gradle-plugin-portal service

--- services/gradle-plugin-portal/gradle-plugin-portal.service.js
+++ services/gradle-plugin-portal/gradle-plugin-portal.service.js
@@ -1,8 +1,6 @@
-'use strict'
+import { redirector } from '../index.js'
 
-const { redirector } = require('..')
-
-module.exports = redirector({
+export default redirector({
   category: 'version',
   isDeprecated: false,
   route: {
