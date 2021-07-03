3e837bfd3300a29005ed88bb24acd635e3b66b01
PyvesB <PyvesDev@gmail.com>
2021-07-03 14:15:45 +0100

    Attempt fixing e2e tests

--- scripts/export-service-definitions-cli.js
+++ scripts/export-service-definitions-cli.js
@@ -1,10 +1,11 @@
 import yaml from 'js-yaml'
 import { collectDefinitions } from '../core/base-service/loader.js'
+;(async () => {
+  const definitions = await collectDefinitions()
 
-const definitions = await collectDefinitions()
+  // Omit undefined
+  // https://github.com/nodeca/js-yaml/issues/356#issuecomment-312430599
+  const cleaned = JSON.parse(JSON.stringify(definitions))
 
-// Omit undefined
-// https://github.com/nodeca/js-yaml/issues/356#issuecomment-312430599
-const cleaned = JSON.parse(JSON.stringify(definitions))
-
-process.stdout.write(yaml.dump(cleaned, { flowLevel: 5 }))
+  process.stdout.write(yaml.dump(cleaned, { flowLevel: 5 }))
+})()
