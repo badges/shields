9e74d1ec20599fa6f81c62e109e2952c26d5811f
chris48s <chris.shaw480@gmail.com>
2021-07-03 20:30:10 +0100

    deprecate [microbadger]

---
+++ services/microbadger/microbadger.tester.js
@@ -0,0 +1,35 @@
+'use strict'
+
+const { ServiceTester } = require('../tester')
+const t = (module.exports = new ServiceTester({
+  id: 'microbadger',
+  title: 'Microbadger',
+}))
+
+t.create('no longer available (previously image size)')
+  .get('/image-size/fedora/apache.json')
+  .expectBadge({
+    label: 'microbadger',
+    message: 'no longer available',
+  })
+
+t.create('no longer available (previously image size with tag)')
+  .get('/image-size/fedora/apache/latest.json')
+  .expectBadge({
+    label: 'microbadger',
+    message: 'no longer available',
+  })
+
+t.create('no longer available (previously layers)')
+  .get('/layers/fedora/apache.json')
+  .expectBadge({
+    label: 'microbadger',
+    message: 'no longer available',
+  })
+
+t.create('no longer available (previously layers with tag)')
+  .get('/layers/fedora/apache/latest.json')
+  .expectBadge({
+    label: 'microbadger',
+    message: 'no longer available',
+  })
