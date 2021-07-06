84f3888311bfa8ff258dbaba2afd637c8aaef59c
Seth Falco <seth@falco.fun>
2021-07-06 17:09:32 +0200

    fix: weird behavior with casing

--- services/wikiapiary/wikiapiary-extension.service.js
+++ services/wikiapiary/wikiapiary-extension.service.js
@@ -4,6 +4,27 @@ const Joi = require('joi')
 const { metric } = require('../text-formatters')
 const { BaseJsonService, NotFound } = require('..')
 
+const documentation = `
+  <p>
+    The name of an extension is case-sensitive excluding the first character.
+  </p>
+  <p>
+    For example, in the case of <code>ParserFunctions</code>, the following are
+    valid:
+    <ul>
+      <li><code>ParserFunctions</code></li>
+      <li><code>parserFunctions</code></li>
+    </ul>
+
+    However, the following are invalid:
+    <ul>
+      <li><code>parserfunctions</code></li>
+      <li><code>Parserfunctions</code></li>
+      <li><code>pARSERfUNCTIONS</code></li>
+    </ul>
+  </p>
+`
+
 const schema = Joi.object({
   query: Joi.object({
     results: Joi.alternatives([
@@ -31,14 +52,15 @@ module.exports = class WikiapiaryInstalls extends BaseJsonService {
   static category = 'downloads'
   static route = {
     base: 'wikiapiary',
-    pattern: ':variant(Extension|Skin|Farm|Generator|Host)/installs/:name',
+    pattern: ':variant(extension|skin|farm|generator|host)/installs/:name',
   }
 
   static examples = [
     {
       title: 'Wikiapiary installs',
-      namedParams: { variant: 'Extension', name: 'ParserFunctions' },
+      namedParams: { variant: 'extension', name: 'ParserFunctions' },
       staticPreview: this.render({ usage: 11170 }),
+      documentation,
       keywords: ['mediawiki'],
     },
   ]
@@ -67,7 +89,14 @@ module.exports = class WikiapiaryInstalls extends BaseJsonService {
 
     this.constructor.validate({ results })
 
-    const [usage] = results[`${variant}:${name}`].printouts['Has website count']
+    const keyLowerCase = `${variant}:${name.toLowerCase()}`
+    const resultKey = Object.keys(results).find(
+      key => keyLowerCase === key.toLowerCase()
+    )
+
+    if (!resultKey) throw new NotFound({ prettyMessage: 'not found' })
+
+    const [usage] = results[resultKey].printouts['Has website count']
     return this.constructor.render({ usage })
   }
 }
--- services/wikiapiary/wikiapiary-extension.tester.js
+++ services/wikiapiary/wikiapiary-extension.tester.js
@@ -4,17 +4,21 @@ const t = (module.exports = require('../tester').createServiceTester())
 const { isMetric } = require('../test-validators')
 
 t.create('Extension')
-  .get('/Extension/installs/ParserFunctions.json')
+  .get('/extension/installs/ParserFunctions.json')
   .expectBadge({ label: 'installs', message: isMetric })
 
 t.create('Skins')
-  .get('/Skin/installs/Vector.json')
+  .get('/skin/installs/Vector.json')
   .expectBadge({ label: 'installs', message: isMetric })
 
-t.create('Extension Not Exist')
-  .get('/Extension/installs/FakeExtensionThatDoesNotExist.json')
-  .expectBadge({ label: 'installs', message: 'does not exist' })
+t.create('Extension Not Found')
+  .get('/extension/installs/FakeExtensionThatDoesNotExist.json')
+  .expectBadge({ label: 'installs', message: 'not found' })
 
 t.create('Name Lowercase')
-  .get('/Extension/installs/parserfunctions.json')
-  .expectBadge({ label: 'installs', message: 'does not exist' })
+  .get('/extension/installs/parserfunctions.json')
+  .expectBadge({ label: 'installs', message: 'not found' })
+
+t.create('Name Title Case')
+  .get('/extension/installs/parserFunctions.json')
+  .expectBadge({ label: 'installs', message: isMetric })
