fc9dc78c5531a5491e2c74db5a7128c4e8330fda
PyvesB <PyvesDev@gmail.com>
2021-07-06 18:42:30 +0100

    Error for e2e test highly unhelpful, use advanced debugging technique

--- scripts/export-service-definitions-cli.js
+++ scripts/export-service-definitions-cli.js
@@ -1,11 +1,17 @@
+console.error("LOG 1")
 import yaml from 'js-yaml'
+console.error("LOG 2")
 import { collectDefinitions } from '../core/base-service/loader.js'
+console.error("LOG 3")
 ;(async () => {
+  console.error("LOG 4")
   const definitions = await collectDefinitions()
+  console.error("LOG 5")
 
   // Omit undefined
   // https://github.com/nodeca/js-yaml/issues/356#issuecomment-312430599
   const cleaned = JSON.parse(JSON.stringify(definitions))
+  console.error("LOG 6")
 
   process.stdout.write(yaml.dump(cleaned, { flowLevel: 5 }))
 })()
