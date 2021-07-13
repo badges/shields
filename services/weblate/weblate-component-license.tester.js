6f733c13597724a7e14e421d3e6dc5b062a66cc5
Seth Falco <seth@falco.fun>
2021-07-14 00:38:05 +0200

    chore: refactor weblate code

--- services/weblate/weblate-component-license.tester.js
+++ services/weblate/weblate-component-license.tester.js
@@ -2,7 +2,7 @@ import { createServiceTester } from '../tester.js'
 export const t = await createServiceTester()
 
 t.create('License')
-  .get('/godot-engine/godot.json?server=https://hosted.weblate.org')
+  .get('/godot-engine/godot.json')
   .expectBadge({ label: 'license', message: 'MIT' })
 
 t.create("Component Doesn't Exist")
