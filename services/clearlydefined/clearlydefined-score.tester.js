802a38d49212df8d702f7074fae039d36808bce6
Marc Bernard <59966492+mbtools@users.noreply.github.com>
2021-08-28 11:57:50 -0400

    [ClearlyDefined] New badge

--- services/clearlydefined/clearlydefined-score.tester.js
+++ services/clearlydefined/clearlydefined-score.tester.js
@@ -3,15 +3,15 @@ import { createServiceTester } from '../tester.js'
 export const t = await createServiceTester()
 
 t.create('ClearlyDefined Score')
-  .get('/score/npm/npmjs/-/jquery/3.4.1/score.json')
+  .get('/score/npm/npmjs/-/jquery/3.4.1.json')
   .expectBadge({
     label: 'score',
     message: Joi.string().regex(/^\d\/\d$/),
   })
 
 t.create('ClearlyDefined Score (not found)')
-  .get('/score/npm/npmjs/-/not-a-real-package/0.0.0/score.json')
+  .get('/score/npm/npmjs/-/not-a-real-package/0.0.0.json')
   .expectBadge({
     label: '404',
-    message: 'not found',
+    message: 'badge not found',
   })
