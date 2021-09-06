61cf7f318a1c041dfb725731a1c60d64581b18ef
Caleb Cartwright <caleb.cartwright@outlook.com>
2021-09-05 21:22:47 -0500

    tests: add auth test for gitlab

--- services/gitlab/gitlab-base.js
+++ services/gitlab/gitlab-base.js
@@ -4,7 +4,6 @@ export default class GitLabBase extends BaseJsonService {
   static auth = {
     passKey: 'gitlab_token',
     serviceKey: 'gitlab',
-    defaultToEmptyStringForUser: true,
   }
 
   async fetch({ url, options, schema, errorMessages }) {
