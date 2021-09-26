81b9a4a6d5984c593bdc2537d50d0802724f7e9c
Jan0660 <jan0660@tutanota.com>
2021-09-26 16:25:58 +0200

    Fix tester

--- services/revolt/revolt.tester.js
+++ services/revolt/revolt.tester.js
@@ -1,7 +1,12 @@
+import Joi from 'joi'
 import { createServiceTester } from '../tester.js'
 
 export const t = await createServiceTester()
 
 t.create('Server invite')
-  .get('/revolt/invite/Testers')
-  .expectBadge({ label: 'Revolt #Information & Rules' })
+  .get('/Testers.json')
+  .expectBadge({
+    label: 'chat',
+    message: Joi.string().regex(/^[0-9]+ members$/),
+    color: 'brightgreen',
+  })
