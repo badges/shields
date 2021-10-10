76d5e44cd7d258a4bbe07788539eada60cd7ea6e
Dominik Grzelak <grzelak.dominik@gmail.com>
2021-10-10 19:43:45 +0200

    Remove unnecessary test for negative downloads value

--- services/modrinth/modrinth.tester.js
+++ services/modrinth/modrinth.tester.js
@@ -10,16 +10,3 @@ t.create('Downloads')
 t.create('Downloads (not found)')
   .get('/not-existing.json')
   .expectBadge({ label: 'downloads', message: 'not found', color: 'red' })
-
-t.create('Downloads with negative downloads value')
-  .get('/mocked.json')
-  .intercept(nock =>
-    nock('https://api.modrinth.com/api/v1/mod')
-      .get('/mocked')
-      .reply(200, { downloads: -1 })
-  )
-  .expectBadge({
-    label: 'downloads',
-    message: 'invalid response data',
-    color: 'lightgray',
-  })
