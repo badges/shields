3bdf1cb36436665c9e8f93b5831926f1fe1cbad6
Piotrek Żygieło <11896137+pzygielo@users.noreply.github.com>
2021-12-23 05:14:26 +0100

    [YouTube] Drop support for removed dislikes (#7410)
    * [YouTube] Support no longer supporting dislikes
    
    * Drop dislikes and duplicated schema
    
    * Remove YT dislikes
    
    Co-authored-by: Piotrek Żygieło <pzygielo@users.noreply.github.com>
    Co-authored-by: repo-ranger[bot] <39074581+repo-ranger[bot]@users.noreply.github.com>

--- services/youtube/youtube-likes.service.js
+++ services/youtube/youtube-likes.service.js
@@ -1,24 +1,9 @@
-import Joi from 'joi'
-import { metric } from '../text-formatters.js'
 import { documentation, YouTubeVideoBase } from './youtube-base.js'
-
-const documentationWithDislikes = `
-  ${documentation}
-  <p>
-    When enabling the <code>withDislikes</code> option, 👍 corresponds to the number
-    of likes of a given video, 👎 corresponds to the number of dislikes.
-  </p>
-`
-
-const queryParamSchema = Joi.object({
-  withDislikes: Joi.equal(''),
-}).required()
 
 export default class YouTubeLikes extends YouTubeVideoBase {
   static route = {
     base: 'youtube/likes',
     pattern: ':videoId',
-    queryParamSchema,
   }
 
   static get examples() {
@@ -26,16 +11,8 @@ export default class YouTubeLikes extends YouTubeVideoBase {
       statistics: { likeCount: 7 },
       id: 'abBdk8bSPKU',
     })
-    const previewVotes = this.render(
-      {
-        statistics: { likeCount: 10236, dislikeCount: 396 },
-        id: 'pU9Q6oiQNd0',
-      },
-      { withDislikes: '' }
-    )
     // link[] is not allowed in examples
     delete previewLikes.link
-    delete previewVotes.link
     return [
       {
         title: 'YouTube Video Likes',
@@ -43,32 +20,15 @@ export default class YouTubeLikes extends YouTubeVideoBase {
         staticPreview: previewLikes,
         documentation,
       },
-      {
-        title: 'YouTube Video Likes and Dislikes',
-        namedParams: { videoId: 'pU9Q6oiQNd0' },
-        staticPreview: previewVotes,
-        queryParams: {
-          withDislikes: null,
-        },
-        documentation: documentationWithDislikes,
-      },
     ]
   }
 
   static render({ statistics, id }, queryParams) {
-    let renderedBadge = super.renderSingleStat({
+    const renderedBadge = super.renderSingleStat({
       statistics,
       statisticName: 'like',
       id,
     })
-    if (queryParams && typeof queryParams.withDislikes !== 'undefined') {
-      renderedBadge = {
-        ...renderedBadge,
-        message: `${metric(statistics.likeCount)} 👍 ${metric(
-          statistics.dislikeCount
-        )} 👎`,
-      }
-    }
     return renderedBadge
   }
 }
