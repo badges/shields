4ba5a00185c7a15511e6db8c42b1326466b8063f
tapanchudasama <tapan9740@gmail.com>
2021-12-23 16:52:00 +0530

    remove --live from tests

--- services/hackernews/hackernews-user-karma.tester.js
+++ services/hackernews/hackernews-user-karma.tester.js
@@ -3,14 +3,12 @@ import { isMetric } from '../test-validators.js'
 
 export const t = await createServiceTester()
 
-t.create('valid repo -- live').get('/pg.json').expectBadge({
+t.create('valid repo').get('/pg.json').expectBadge({
   label: 'pg',
   message: isMetric,
 })
 
-t.create('invalid user -- live')
-  .get('/hopefullythisdoesnotexist.json')
-  .expectBadge({
-    label: 'HackerNews User Karma',
-    message: 'user not found',
-  })
+t.create('invalid user').get('/hopefullythisdoesnotexist.json').expectBadge({
+  label: 'HackerNews User Karma',
+  message: 'user not found',
+})
