94a8d945c31e9365acbc7652deceef5103e638df
Pierre-Yves Bigourdan <10694593+PyvesB@users.noreply.github.com>
2022-01-09 18:26:03 +0000

    Fix [CodacyCoverage] tests and examples (#7470)

--- services/codacy/codacy-coverage.tester.js
+++ services/codacy/codacy-coverage.tester.js
@@ -2,15 +2,15 @@ import { isIntegerPercentage } from '../test-validators.js'
 import { createServiceTester } from '../tester.js'
 export const t = await createServiceTester()
 
-// https://app.codacy.com/gh/Seagate/cortx-s3server/dashboard?_ga=2.227678553.575705995.1626641804-993201653.1626641804
-// https://github.com/Seagate/cortx-s3server
-t.create('Coverage').get('/e02de8d738bb4701b6345624ea2de66c.json').expectBadge({
+// https://app.codacy.com/gh/kronenthaler/openstep-parser/dashboard
+// https://github.com/kronenthaler/openstep-parser
+t.create('Coverage').get('/d5402a91aa7b4234bd1c19b5e86a63be.json').expectBadge({
   label: 'coverage',
   message: isIntegerPercentage,
 })
 
 t.create('Coverage on branch')
-  .get('/e02de8d738bb4701b6345624ea2de66c/main.json')
+  .get('/d5402a91aa7b4234bd1c19b5e86a63be/master.json')
   .expectBadge({
     label: 'coverage',
     message: isIntegerPercentage,
