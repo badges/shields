ea3921dcd02653cf5ebb2fc4fb04733076c751bc
chris48s <chris.shaw480@gmail.com>
2022-01-10 21:02:23 +0000

    fix opm badge

--- services/opm/opm-version.service.js
+++ services/opm/opm-version.service.js
@@ -40,7 +40,7 @@ export default class OpmVersion extends BaseService {
     })
 
     // TODO: set followRedirect to false and intercept 302 redirects
-    const location = res.request.redirects[0]
+    const location = res.redirectUrls[0].toString()
     if (!location) {
       throw new NotFound({ prettyMessage: 'module not found' })
     }
