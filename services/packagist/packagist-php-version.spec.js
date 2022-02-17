1d934429ff0346f89e96f05a239ec81bc93911bc
Koala Yeung <koalay@gmail.com>
2022-02-17 20:09:07 +0800

    Improve PackagistPhpVersionService testing
    * resurrected some of the relevant older tests.
    * remove the api json array expand logics from test.

--- services/packagist/packagist-php-version.spec.js
+++ services/packagist/packagist-php-version.spec.js
@@ -1,15 +1,46 @@
 import { expect } from 'chai'
 import { NotFound } from '../index.js'
-import { BasePackagistService } from './packagist-base.js'
 import PackagistPhpVersion from './packagist-php-version.service.js'
 
 describe('PackagistPhpVersion', function () {
-  const json = {
-    packages: {
-      'frodo/the-one-package': [
+  const versions = [
+    {
+      version: 'dev-main',
+      require: { php: '^7.4 || 8' },
+    },
+    {
+      version: 'dev-2.x',
+      require: { php: '^7.2' },
+    },
+    {
+      version: 'dev-1.x',
+      require: { php: '^5.6 || ^7' },
+    },
+    {
+      version: '3.0.0',
+      require: { php: '^7.4 || 8' },
+    },
+    {
+      version: '2.0.0',
+      require: { php: '^7.2' },
+    },
+    {
+      version: '1.0.0',
+      require: { php: '^5.6 || ^7' },
+    },
+  ]
+
+  it('should throw NotFound when package version is missing', function () {
+    expect(() => PackagistPhpVersion.prototype.getPhpVersion(versions, '4.0.0'))
+      .to.throw(NotFound)
+      .with.property('prettyMessage', 'invalid version')
+  })
+
+  it('should throw NotFound when PHP version not found on package when using default release', async function () {
+    expect(() =>
+      PackagistPhpVersion.prototype.getPhpVersion([
         {
           version: '3.0.0',
-          require: { php: '^7.4 || 8' },
         },
         {
           version: '2.0.0',
@@ -19,57 +50,16 @@ describe('PackagistPhpVersion', function () {
           version: '1.0.0',
           require: { php: '^5.6 || ^7' },
         },
-      ],
-    },
-  }
-  it('should throw NotFound when package version is missing', function () {
-    expect(() =>
-      PackagistPhpVersion.prototype.getPhpVersion(
-        BasePackagistService.expandPackageVersions(
-          json,
-          'frodo/the-one-package'
-        ),
-        '4.0.0'
-      )
-    )
-      .to.throw(NotFound)
-      .with.property('prettyMessage', 'invalid version')
-  })
-
-  it('should throw NotFound when PHP version not found on package when using default release', async function () {
-    const specJson = {
-      packages: {
-        'frodo/the-one-package': [
-          {
-            version: '3.0.0',
-          },
-          {
-            version: '2.0.0',
-            require: { php: '^7.2' },
-          },
-          {
-            version: '1.0.0',
-            require: { php: '^5.6 || ^7' },
-          },
-        ],
-      },
-    }
-    expect(() =>
-      PackagistPhpVersion.prototype.getPhpVersion(
-        BasePackagistService.expandPackageVersions(
-          specJson,
-          'frodo/the-one-package'
-        )
-      )
+      ])
     )
       .to.throw(NotFound)
       .with.property('prettyMessage', 'version requirement not found')
   })
 
   it('should throw NotFound when PHP version not found on package when using specified release', async function () {
-    const specJson = {
-      packages: {
-        'frodo/the-one-package': [
+    expect(() =>
+      PackagistPhpVersion.prototype.getPhpVersion(
+        [
           {
             version: '3.0.0',
             require: { php: '^7.4 || 8' },
@@ -80,17 +70,8 @@ describe('PackagistPhpVersion', function () {
           },
           {
             version: '1.0.0',
-            require: '__unset',
           },
         ],
-      },
-    }
-    expect(() =>
-      PackagistPhpVersion.prototype.getPhpVersion(
-        BasePackagistService.expandPackageVersions(
-          specJson,
-          'frodo/the-one-package'
-        ),
         '1.0.0'
       )
     )
@@ -98,30 +79,47 @@ describe('PackagistPhpVersion', function () {
       .with.property('prettyMessage', 'version requirement not found')
   })
 
-  it('should return PHP version for the default release', async function () {
-    expect(
-      PackagistPhpVersion.prototype.getPhpVersion(
-        BasePackagistService.expandPackageVersions(
-          json,
-          'frodo/the-one-package'
-        )
-      )
-    )
+  it('should return PHP version for the default release', function () {
+    expect(PackagistPhpVersion.prototype.getPhpVersion(versions))
       .to.have.property('phpVersion')
       .that.equals('^7.4 || 8')
   })
 
-  it('should return PHP version for the specified release', async function () {
-    expect(
-      PackagistPhpVersion.prototype.getPhpVersion(
-        BasePackagistService.expandPackageVersions(
-          json,
-          'frodo/the-one-package'
-        ),
-        '2.0.0'
-      )
-    )
+  it('should return PHP version for the specified release', function () {
+    expect(PackagistPhpVersion.prototype.getPhpVersion(versions, '2.0.0'))
       .to.have.property('phpVersion')
       .that.equals('^7.2')
+  })
+
+  it('should throw NotFound when package version is missing', function () {
+    expect(() =>
+      PackagistPhpVersion.prototype.getPhpVersion([{ version: '1.0.x-dev' }])
+    )
+      .to.throw(NotFound)
+      .with.property('prettyMessage', 'version requirement not found')
+
+    expect(() =>
+      PackagistPhpVersion.prototype.getPhpVersion([
+        { version: '1.0.x-dev', require: {} },
+      ])
+    )
+      .to.throw(NotFound)
+      .with.property('prettyMessage', 'version requirement not found')
+  })
+
+  it('should return PHP version for the default branch', function () {
+    expect(
+      PackagistPhpVersion.prototype.getPhpVersion([
+        { version: 'dev-1.x', require: { php: '^7.3' } },
+        {
+          version: 'dev-2.x',
+          require: { php: '^7.4 || 8' },
+          'default-branch': true,
+        },
+        { version: 'dev-3.x', require: { php: '^8.1' } },
+      ])
+    )
+      .to.have.property('phpVersion')
+      .that.equals('^7.4 || 8')
   })
 })
