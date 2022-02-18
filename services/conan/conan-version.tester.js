8dc27b64f920d183d22d94f23db4fc90594eaa93
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2022-02-17 16:45:48 -0800

    Conditional mock based on presence of github token

--- services/conan/conan-version.tester.js
+++ services/conan/conan-version.tester.js
@@ -1,3 +1,4 @@
+import config from 'config'
 import { isSemver } from '../test-validators.js'
 import { createServiceTester } from '../tester.js'
 
@@ -7,30 +8,48 @@ t.create('gets the package version of zeromq')
   .get('/zeromq.json')
   .expectBadge({ label: 'conan', message: isSemver })
 
+const hasGithubToken = !!config.util.toObject().private.gh_token
+function mockConfig(nock, status, body) {
+  if (hasGithubToken) {
+    return nock('https://api.github.com/')
+      .get(
+        `/repos/conan-io/conan-center-index/contents/recipes/example/config.yml?ref=master`
+      )
+      .reply(
+        status,
+        body === undefined
+          ? undefined
+          : {
+              content: Buffer.from(body).toString('base64'),
+              encoding: 'base64',
+            }
+      )
+  } else {
+    return nock('https://raw.githubusercontent.com/')
+      .get(`/conan-io/conan-center-index/master/recipes/example/config.yml`)
+      .reply(status, body)
+  }
+}
+
 t.create('returns latest version')
   .intercept(nock =>
-    nock('https://raw.githubusercontent.com/')
-      .get(`/conan-io/conan-center-index/master/recipes/example/config.yml`)
-      .reply(
-        200,
-        `
+    mockConfig(
+      nock,
+      200,
+      `
 versions:
   1.69.0:
     folder: all
   1.70.0:
     folder: all
 `
-      )
+    )
   )
   .get('/example.json')
   .expectBadge({ label: 'conan', message: 'v1.70.0', color: 'blue' })
 
 t.create('treats invalid yaml as invalid')
-  .intercept(nock =>
-    nock('https://raw.githubusercontent.com/')
-      .get(`/conan-io/conan-center-index/master/recipes/example/config.yml`)
-      .reply(200, '[')
-  )
+  .intercept(nock => mockConfig(nock, 200, '['))
   .get('/example.json')
   .expectBadge({
     label: 'conan',
@@ -39,11 +58,7 @@ t.create('treats invalid yaml as invalid')
   })
 
 t.create('treats no results array as invalid')
-  .intercept(nock =>
-    nock('https://raw.githubusercontent.com/')
-      .get(`/conan-io/conan-center-index/master/recipes/example/config.yml`)
-      .reply(200, '')
-  )
+  .intercept(nock => mockConfig(nock, 200, ' '))
   .get('/example.json')
   .expectBadge({
     label: 'conan',
@@ -52,11 +67,7 @@ t.create('treats no results array as invalid')
   })
 
 t.create('treats 404 as not found')
-  .intercept(nock =>
-    nock('https://raw.githubusercontent.com/')
-      .get(`/conan-io/conan-center-index/master/recipes/example/config.yml`)
-      .reply(404)
-  )
+  .intercept(nock => mockConfig(nock, 404))
   .get('/example.json')
   .expectBadge({
     label: 'conan',
@@ -66,10 +77,6 @@ t.create('treats 404 as not found')
   })
 
 t.create('treats empty results array as not found')
-  .intercept(nock =>
-    nock('https://raw.githubusercontent.com/')
-      .get(`/conan-io/conan-center-index/master/recipes/example/config.yml`)
-      .reply(200, 'versions: []')
-  )
+  .intercept(nock => mockConfig(nock, 200, 'versions: []'))
   .get('/example.json')
   .expectBadge({ label: 'conan', message: 'no versions found', color: 'red' })
