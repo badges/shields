7aa2b1ebdbe4e3397dd9ad1e5f5085d3121af8bf
Caleb Cartwright <caleb.cartwright@outlook.com>
2022-02-19 12:31:41 -0600

    tests(securityheaders): increase timeout

--- services/security-headers/security-headers.tester.js
+++ services/security-headers/security-headers.tester.js
@@ -2,9 +2,11 @@ import { createServiceTester } from '../tester.js'
 export const t = await createServiceTester()
 
 t.create('grade of https://shields.io')
+  .timeout(15000)
   .get('/security-headers.json?url=https://shields.io')
   .expectBadge({ label: 'security headers', message: 'F', color: 'red' })
 
 t.create('grade of https://httpstat.us/301 as redirect')
+  .timeout(15000)
   .get('/security-headers.json?ignoreRedirects&url=https://httpstat.us/301')
   .expectBadge({ label: 'security headers', message: 'R', color: 'blue' })
