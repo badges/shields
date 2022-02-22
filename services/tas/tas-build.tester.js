93e9dfe76fee5cbd1deba084d0721484294ee842
Nevil Macwan <nevilm@lambdatest.com>
2022-02-22 19:39:15 +0530

    updated tests

--- services/tas/tas-build.tester.js
+++ services/tas/tas-build.tester.js
@@ -2,7 +2,7 @@ import Joi from 'joi'
 import { createServiceTester } from '../tester.js'
 export const t = await createServiceTester()
 
-t.create('tas build').get('/github/tasdemo/axios.json').expectBadge({
-  label: 'TAS',
+t.create('tas tests').get('/github/tasdemo/axios.json').expectBadge({
+  label: 'tests',
   message: Joi.string(),
 })
