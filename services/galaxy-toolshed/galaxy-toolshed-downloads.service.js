e5586750db7c138b97134dcdc6b7aa04e03202ea
Guillaume Gricourt <guipagui@gmail.com>
2022-03-25 15:41:46 +0100

    feat(galaxy-toolshed): add downloads feature

---
+++ services/galaxy-toolshed/galaxy-toolshed-downloads.service.js
@@ -0,0 +1,63 @@
+import Joi from 'joi'
+import { renderDownloadsBadge } from '../downloads.js'
+import BaseGalaxyToolshedService from './galaxy-toolshed-base.js'
+
+const repositoryRevisionInstallInfoSchema = Joi.array().items(
+  Joi.object({
+    times_downloaded: Joi.number().required(),
+  }).required(),
+  Joi.object({}),
+  Joi.object({})
+)
+
+export default class GalaxyToolshedDownloads extends BaseGalaxyToolshedService {
+  static category = 'downloads'
+  static route = {
+    base: 'galaxy-toolshed/downloads',
+    pattern: ':repositoryName/:owner',
+  }
+
+  static examples = [
+    {
+      title: 'Galaxy Toolshed - Downloads',
+      namedParams: {
+        repositoryName: 'sra_tools',
+        owner: 'iuc',
+      },
+      staticPreview: this.render({ downloads: 10000 }),
+    },
+  ]
+
+  static render({ downloads }) {
+    return renderDownloadsBadge({ downloads })
+  }
+
+  async fetch({ repositoryName, owner }) {
+    const changesetRevisions =
+      await this.fetchOrderedInstallableRevisionsSchema({
+        repositoryName,
+        owner,
+      })
+    return this.fetchRepositoryRevisionInstallInfoSchema({
+      schema: repositoryRevisionInstallInfoSchema,
+      repositoryName,
+      owner,
+      changesetRevision: changesetRevisions.shift(),
+    })
+  }
+
+  static transform(response) {
+    const metadata = response
+      .filter(function (x) {
+        return Object.keys(x).length > 0
+      })
+      .shift()
+    return metadata.times_downloaded
+  }
+
+  async handle({ repositoryName, owner }) {
+    const response = await this.fetch({ repositoryName, owner })
+    const downloads = this.constructor.transform(response)
+    return this.constructor.render({ downloads })
+  }
+}
