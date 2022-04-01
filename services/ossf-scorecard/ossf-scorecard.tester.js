cb67713a6c434a7f4faa8ff84ccdb55bfbe58daf
Rohan Khandelwal <rohankh@google.com>
2022-04-01 11:46:12 -0700

    renamed to ossf-scorecard + fixups

--- services/ossf-scorecard/ossf-scorecard.tester.js
+++ services/ossf-scorecard/ossf-scorecard.tester.js
@@ -1,10 +1,17 @@
+import Joi from 'joi'
 import { createServiceTester } from '../tester.js'
 export const t = await createServiceTester()
 
 t.create('score (valid)')
   .get('/github.com/rohankh532/org-workflow-add.json')
   .expectBadge({
-    label: 'scorecard score',
-    message: '4.2',
-    color: 'orange',
+    label: 'score',
+    message: Joi.number().min(0),
+    color: Joi.string().allow(
+      'red',
+      'yellow',
+      'yellowgreen',
+      'green',
+      'brightgreen'
+    ),
   })
