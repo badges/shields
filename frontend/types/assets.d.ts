d6395d0d7d1b28ed225f8e23c5fbcf01d091d9fe
Juan A <30204147+jaas666@users.noreply.github.com>
2022-05-14 17:54:13 -0500

    Declare assets

--- frontend/types/assets.d.ts
+++ frontend/types/assets.d.ts
@@ -1,5 +1,10 @@
 declare module '*.svg'
 declare module '*.json'
+declare module '*.png'
+declare module '*.ico'
+declare module '*.webmanifest'
+declare module '*.xml'
+
 
 // Handled by js-yaml-loader.
 declare module '*.yml'
