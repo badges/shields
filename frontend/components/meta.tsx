304b9a86d47c16ff7dc8b74df63521d87f78d047
Juan A <30204147+jaas666@users.noreply.github.com>
2022-05-14 17:02:33 -0500

    Add High res favicons

--- frontend/components/meta.tsx
+++ frontend/components/meta.tsx
@@ -2,7 +2,14 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 // eslint-disable-next-line
 // @ts-ignore
-import favicon from '../images/favicon.png'
+// import favicon from '../images/favicon.png'
+import appletouchicon from '../images/apple-touch-icon.png'
+import manifest from '../images/site.webmanifest'
+import icon32x32 from '../images/favicon-32x32.png'
+import icon16x16 from '../images/favicon-16x16.png'
+import maskicon from '../images/safari-pinned-tab.svg'
+import shortcuticon from '../images/favicon.ico'
+import msapplicationconfig from '../images/browserconfig.xml'
 import '@fontsource/lato'
 import '@fontsource/lekton'
 
@@ -20,6 +27,17 @@ export default function Meta(): JSX.Element {
       <meta content="width=device-width,initial-scale=1" name="viewport" />
       <meta content={description} name="description" />
       <link href={favicon} rel="icon" type="image/png" />
+      <link rel="icon" type="image/png" sizes="32x32" href={icon32x32} />
+      <link rel="icon" type="image/png" sizes="16x16" href={icon16x16} />
+      <link rel="apple-touch-icon" sizes="180x180" href={appletouchicon} />
+      <link rel="manifest" href={manifest} />
+      <link rel="mask-icon" href={maskicon} color="#5bbad5" />
+      <link rel="shortcut icon" href={shortcuticon} />
+      <meta name="msapplication-TileColor" content="#da532c" />
+      <meta name="msapplication-config" content={msapplicationconfig} />
+      <meta name="apple-mobile-web-app-title" content="Shields.io" />
+      <meta name="application-name" content="Shields.io" />
+      <meta name="theme-color" content="#ffffff" />
     </Helmet>
   )
 }
