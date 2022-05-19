115610521db5b0b72224a170356c94537bd98573
Juan A <30204147+jaas666@users.noreply.github.com>
2022-05-19 02:38:56 -0500

    Add Favicons (high res)

--- frontend/components/meta.tsx
+++ frontend/components/meta.tsx
@@ -2,7 +2,13 @@ import React from 'react'
 import { Helmet } from 'react-helmet'
 // eslint-disable-next-line
 // @ts-ignore
-import favicon from '../images/favicon.png'
+import appletouch from '../images/apple-touch-icon.png'
+import android192x192 from '../images/android-chrome-192x192.png'
+import android512x512 from '../images/android-chrome-512x512.png'
+import favicon16x16 from '../images/favicon-16x16.png'
+import favicon32x32 from '../images/favicon-32x32.png'
+import faviconico from '../images/favicon.ico'
+import mstile150x150 from '../images/mstile-150x150.png'
 import '@fontsource/lato'
 import '@fontsource/lekton'
 
@@ -16,10 +22,19 @@ export default function Meta(): JSX.Element {
       <title>
         Shields.io: Quality metadata badges for open source projects
       </title>
+      <link href={appletouch} rel="apple-touch-icon" sizes="180x180" />
+      <link href={android192x192} rel="icon" sizes="192x192" type="image/png" />
+      <link href={android512x512} rel="icon" sizes="512x512" type="image/png" />
+      <link href={favicon32x32} rel="icon" sizes="32x32" type="image/png" />
+      <link href={favicon16x16} rel="icon" sizes="16x16" type="image/png" />
+      <link href={faviconico} rel="shortcut icon" />
       <meta charSet="utf-8" />
       <meta content="width=device-width,initial-scale=1" name="viewport" />
       <meta content={description} name="description" />
-      <link href={favicon} rel="icon" type="image/png" />
+      <meta content="Shields.io" name="apple-mobile-web-app-title" />
+      <meta content="Shields.io" name="application-name" />
+      <meta content="#ffffff" name="theme-color" />
+      <meta content={mstile150x150} name="msapplication-TileImage" />
     </Helmet>
   )
 }
--- frontend/types/assets.d.ts
+++ frontend/types/assets.d.ts
@@ -1,5 +1,7 @@
 declare module '*.svg'
 declare module '*.json'
+declare module '*.png'
+declare module '*.ico'
 
 // Handled by js-yaml-loader.
 declare module '*.yml'
