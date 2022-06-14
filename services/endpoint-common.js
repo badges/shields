51748c6faf70b94a1f91514c731fd9ef37e17861
Prashant Rawat <pr7prashant@gmail.com>
2022-06-14 23:03:09 +0530

    add docstrings for endpoint-common service (#8079)
    Co-authored-by: repo-ranger[bot] <39074581+repo-ranger[bot]@users.noreply.github.com>

--- services/endpoint-common.js
+++ services/endpoint-common.js
@@ -1,3 +1,9 @@
+/**
+ * Common functions and utilities for tasks related to endpoint badges.
+ *
+ * @module
+ */
+
 import Joi from 'joi'
 import validate from '../core/base-service/validate.js'
 import { InvalidResponse } from './index.js'
@@ -14,6 +20,11 @@ const optionalNumberWhenAnyLogoPresent = Joi.alternatives()
   .conditional('namedLogo', { is: Joi.string().required(), then: Joi.number() })
   .conditional('logoSvg', { is: Joi.string().required(), then: Joi.number() })
 
+/**
+ * Joi schema for validating endpoint.
+ *
+ * @type {object}
+ */
 const endpointSchema = Joi.object({
   schemaVersion: 1,
   label: Joi.string().allow('').required(),
@@ -33,9 +44,18 @@ const endpointSchema = Joi.object({
   .oxor('namedLogo', 'logoSvg')
   .required()
 
-// Strictly validate according to the endpoint schema. This rejects unknown /
-// invalid keys. Optionally it prints those keys in the message in order to
-// provide detailed feedback.
+/**
+ * Strictly validate the data according to the endpoint schema.
+ * This rejects unknown/invalid keys.
+ * Optionally it prints those keys in the message to provide detailed feedback.
+ *
+ * @param {object} data Object containing the data for validation
+ * @param {object} attrs Refer to individual attributes
+ * @param {string} [attrs.prettyErrorMessage] If provided then error message is set to this value
+ * @param {boolean} [attrs.includeKeys] If true then includes error details in error message, defaults to false
+ * @throws {InvalidResponse|Error} Error if Joi validation fails due to invalid or no schema
+ * @returns {object} Value if Joi validation is success
+ */
 function validateEndpointData(
   data,
   { prettyErrorMessage = 'invalid response data', includeKeys = false } = {}
@@ -56,6 +76,17 @@ function validateEndpointData(
 
 const anySchema = Joi.any()
 
+/**
+ * Fetches data from the endpoint and validates the data.
+ *
+ * @param {object} serviceInstance Instance of Endpoint class
+ * @param {object} attrs Refer to individual attributes
+ * @param {string} attrs.url Endpoint URL
+ * @param {object} attrs.errorMessages Object containing error messages for different error codes
+ * @param {string} attrs.validationPrettyErrorMessage If provided then the error message is set to this value
+ * @param {boolean} attrs.includeKeys If true then includes error details in error message
+ * @returns {object} Data fetched from endpoint
+ */
 async function fetchEndpointData(
   serviceInstance,
   { url, errorMessages, validationPrettyErrorMessage, includeKeys }
