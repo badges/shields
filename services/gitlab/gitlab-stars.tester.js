d080e9f93c40f1a3918c2ab01bbc9b052aa3fe5d
guoxudong <guoxudong.dev@gmail.com>
2022-07-20 16:43:57 +0800

    feat: add gitlab stars service

---
+++ services/gitlab/gitlab-stars.tester.js
@@ -0,0 +1,20 @@
+import { isMetric } from '../test-validators.js'
+import { createServiceTester } from '../tester.js'
+
+export const t = await createServiceTester()
+
+t.create('Stars')
+  .get('/gitlab-org/gitlab.json')
+  .expectBadge({
+    label: 'stars',
+    message: isMetric,
+    color: 'blue',
+    link: ['https://gitlab.com/gitlab-org/gitlab'],
+  })
+
+t.create('Stars (project not found)')
+  .get('/user1/gitlab-does-not-have-this-repo.json')
+  .expectBadge({
+    label: 'stars',
+    message: 'project not found',
+  })
