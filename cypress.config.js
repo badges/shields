7ad6aafb95ac2943c635cdc426934a5db3290394
chris48s <chris.shaw480@gmail.com>
2022-07-24 10:55:30 +0100

    cypress 10

---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  fixturesFolder: false,
+  env: {
+    backend_url: 'http://localhost:8080',
+  },
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:3000',
+    supportFile: false,
+  },
+})
