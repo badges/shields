90049b51edb01b552f8ee550b364b0834f654964
Anatoli Babenia <anatoli@rainforce.org>
2022-08-08 11:33:29 +0300

    Write correct test

--- frontend/lib/generate-image-markup.spec.ts
+++ frontend/lib/generate-image-markup.spec.ts
@@ -51,14 +51,17 @@ test(reStructuredText, () => {
     '.. image:: https://img.shields.io/badge'
   )
   given('https://img.shields.io/badge', undefined, 'Example').expect(
-    '.. image:: https://img.shields.io/badge   :alt: Example'
+    '.. image:: https://img.shields.io/badge' +
+    '   :alt: Example'
   )
   given(
     'https://img.shields.io/badge',
     'https://example.com/example',
     'Example'
   ).expect(
-    '.. image:: https://img.shields.io/badge   :alt: Example   :target: https://example.com/example'
+    '.. image:: https://img.shields.io/badge' +
+    '   :alt: Example' +
+    '   :target: https://example.com/example'
   )
 })
 
