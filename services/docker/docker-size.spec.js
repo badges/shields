52af3d955df0a2ab3fb6755bc7566eb67ad07eae
PaulaBarszcz <paula.anna.barszcz@gmail.com>
2022-08-11 17:02:40 +0200

    added unit tests for noTagWithSemverSortTransform() and yesTagTransform()

--- services/docker/docker-size.spec.js
+++ services/docker/docker-size.spec.js
@@ -1,6 +1,6 @@
 import { test, given } from 'sazerac'
 import DockerSize from './docker-size.service.js'
-// import { sizeDataNoTagSemVerSort } from './docker-fixtures.js'
+import { sizeDataNoTagSemVerSort } from './docker-fixtures.js'
 
 describe('DockerSize', function () {
   test(DockerSize.prototype.noTagWithDateSortTransform, () => {
@@ -63,48 +63,45 @@ describe('DockerSize', function () {
       },
       'arm64777'
     ).expectError('Not Found: architecture not found')
+  })
 
-    // given({
-    //   tag: '',
-    //   sort: 'date',
-    //   data: {
-    //     results: [
-    //       {
-    //         images: [{ name: 'latest', size: 74661264 }],
-    //         full_size: 74661264,
-    //       },
-    //       {
-    //         images: [{ name: 'arm64v8-latest', size: 74661264 }],
-    //         full_size: 74661264,
-    //       },
-    //       {
-    //         images: [{ name: 'arm32v7-latest', size: 68001970 }],
-    //         full_size: 68001970,
-    //       },
-    //       {
-    //         images: [{ name: 'amd64-latest', size: 74661264 }],
-    //         full_size: 74661264,
-    //       },
-    //     ],
-    //   },
-    // }).expect({
-    //   size: 74661264,
-    // })
-    // given({
-    //   tag: '',
-    //   sort: 'semver',
-    //   data: sizeDataNoTagSemVerSort,
-    // }).expect({
-    //   size: 13448411,
-    // })
-    // given({
-    //   tag: 'latest',
-    //   data: {
-    //     images: [{ name: 'latest', size: 13448411 }],
-    //     full_size: 74661264,
-    //   },
-    // }).expect({
-    //   size: 13448411,
-    // })
+  test(DockerSize.prototype.yesTagTransform, () => {
+    given(
+      {
+        full_size: 300000000,
+        name: 'next',
+        images: [{ architecture: 'amd64', size: 219939484 }],
+      },
+      'amd64'
+    ).expect({
+      size: 219939484,
+    })
+    given({
+      full_size: 300000000,
+      name: 'next',
+      images: [{ architecture: 'amd64', size: 219939484 }],
+    }).expect({
+      size: 300000000,
+    })
+    given(
+      {
+        full_size: 300000000,
+        name: 'next',
+        images: [{ architecture: 'amd64', size: 219939484 }],
+      },
+      'arm64777'
+    ).expectError('Not Found: architecture not found')
+  })
+
+  test(DockerSize.prototype.noTagWithSemverSortTransform, () => {
+    given(sizeDataNoTagSemVerSort, 'amd64').expect({
+      size: 220000000,
+    })
+    given(sizeDataNoTagSemVerSort).expect({
+      size: 400000000,
+    })
+    given(sizeDataNoTagSemVerSort, 'nonexistentArch').expectError(
+      'Not Found: architecture not found'
+    )
   })
 })
