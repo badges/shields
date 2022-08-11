0190817b36f7dbb024dbe9ee4b55cef8cbd4dd95
PaulaBarszcz <paula.anna.barszcz@gmail.com>
2022-08-11 16:20:49 +0200

    add unit tests for noTagWithDateSortTransform()

--- services/docker/docker-size.spec.js
+++ services/docker/docker-size.spec.js
@@ -1,42 +1,110 @@
 import { test, given } from 'sazerac'
 import DockerSize from './docker-size.service.js'
-import { sizeDataNoTagSemVerSort } from './docker-fixtures.js'
+// import { sizeDataNoTagSemVerSort } from './docker-fixtures.js'
 
 describe('DockerSize', function () {
-  test(DockerSize.prototype.transform, () => {
-    given({
-      tag: '',
-      sort: 'date',
-      data: { results: [{ name: 'next', full_size: 219939484 }] },
-    }).expect({
+  test(DockerSize.prototype.noTagWithDateSortTransform, () => {
+    given(
+      {
+        count: 0,
+        results: [
+          {
+            full_size: 219939484,
+            name: 'next',
+            images: [{ architecture: 'amd64', size: 219939484 }],
+          },
+        ],
+      },
+      'amd64'
+    ).expectError('Not Found: repository not found')
+    given(
+      {
+        count: 152,
+        results: [
+          {
+            full_size: 300000000,
+            name: 'next',
+            images: [{ architecture: 'amd64', size: 219939484 }],
+          },
+        ],
+      },
+      'amd64'
+    ).expect({
       size: 219939484,
     })
     given({
-      tag: '',
-      sort: 'date',
-      data: {
+      count: 152,
+      results: [
+        {
+          full_size: 300000000,
+          name: 'next',
+          images: [
+            { architecture: 'amd64', size: 219939484 },
+            { architecture: 'arm64', size: 200000000 },
+          ],
+        },
+      ],
+    }).expect({
+      size: 300000000,
+    })
+    given(
+      {
+        count: 152,
         results: [
-          { name: 'latest', full_size: 74661264 },
-          { name: 'arm64v8-latest', full_size: 76310416 },
-          { name: 'arm32v7-latest', full_size: 68001970 },
-          { name: 'amd64-latest', full_size: 74661264 },
+          {
+            full_size: 300000000,
+            name: 'next',
+            images: [
+              { architecture: 'amd64', size: 219939484 },
+              { architecture: 'arm64', size: 200000000 },
+            ],
+          },
         ],
       },
-    }).expect({
-      size: 74661264,
-    })
-    given({
-      tag: '',
-      sort: 'semver',
-      data: sizeDataNoTagSemVerSort,
-    }).expect({
-      size: 13448411,
-    })
-    given({
-      tag: 'latest',
-      data: { name: 'latest', full_size: 13448411 },
-    }).expect({
-      size: 13448411,
-    })
+      'arm64777'
+    ).expectError('Not Found: architecture not found')
+
+    // given({
+    //   tag: '',
+    //   sort: 'date',
+    //   data: {
+    //     results: [
+    //       {
+    //         images: [{ name: 'latest', size: 74661264 }],
+    //         full_size: 74661264,
+    //       },
+    //       {
+    //         images: [{ name: 'arm64v8-latest', size: 74661264 }],
+    //         full_size: 74661264,
+    //       },
+    //       {
+    //         images: [{ name: 'arm32v7-latest', size: 68001970 }],
+    //         full_size: 68001970,
+    //       },
+    //       {
+    //         images: [{ name: 'amd64-latest', size: 74661264 }],
+    //         full_size: 74661264,
+    //       },
+    //     ],
+    //   },
+    // }).expect({
+    //   size: 74661264,
+    // })
+    // given({
+    //   tag: '',
+    //   sort: 'semver',
+    //   data: sizeDataNoTagSemVerSort,
+    // }).expect({
+    //   size: 13448411,
+    // })
+    // given({
+    //   tag: 'latest',
+    //   data: {
+    //     images: [{ name: 'latest', size: 13448411 }],
+    //     full_size: 74661264,
+    //   },
+    // }).expect({
+    //   size: 13448411,
+    // })
   })
 })
