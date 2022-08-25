bb326a0f930fbc5d0270916ca19558557e98b245
Paula Barszcz <paula.anna.barszcz@gmail.com>
2022-08-25 20:17:05 +0200

    [DockerSize] Docker image size multi arch (#8290)
    * Get the size of the docker image taking architecture into account
    
    Co-authored-by: chris48s <chris48s@users.noreply.github.com>

--- services/docker/docker-size.tester.js
+++ services/docker/docker-size.tester.js
@@ -9,6 +9,13 @@ t.create('docker image size (valid, library)')
     message: isFileSize,
   })
 
+t.create('docker image size (valid, library, arch parameter )')
+  .get('/_/mysql.json?arch=amd64')
+  .expectBadge({
+    label: 'image size',
+    message: isFileSize,
+  })
+
 t.create('docker image size (valid, library with tag)')
   .get('/_/alpine/latest.json')
   .expectBadge({
@@ -41,5 +48,19 @@ t.create('docker image size (invalid, unknown repository)')
   .get('/_/not-a-real-repo.json')
   .expectBadge({
     label: 'image size',
-    message: 'repository not found',
+    message: 'repository or tag not found',
+  })
+
+t.create('docker image size (invalid, wrong sorting method)')
+  .get('/jrottenberg/ffmpeg/3.2-alpine.json?sort=daterrr')
+  .expectBadge({
+    label: 'image size',
+    message: 'invalid query parameter: sort',
+  })
+
+t.create('docker image size (invalid, nonexisting arch)')
+  .get('/jrottenberg/ffmpeg/3.2-alpine.json?arch=nonexistingArch')
+  .expectBadge({
+    label: 'image size',
+    message: 'invalid query parameter: arch',
   })
