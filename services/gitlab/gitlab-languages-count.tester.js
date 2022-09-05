3176c09f122ad08f3dfc162b588be250983e3e33
guoxudong <guoxd@jihulab.com>
2022-09-05 10:27:21 +0800

    fix testr

--- services/gitlab/gitlab-languages-count.tester.js
+++ services/gitlab/gitlab-languages-count.tester.js
@@ -3,13 +3,13 @@ import { createServiceTester } from '../tester.js'
 
 export const t = await createServiceTester()
 
-t.create('Languages').get('/gitlab-org/gitlab.json').expectBadge({
+t.create('language count').get('/gitlab-org/gitlab.json').expectBadge({
   label: 'languages',
   message: isMetric,
   color: 'blue',
 })
 
-t.create('Languages (self-managed)')
+t.create('language count (self-managed)')
   .get('/gitlab-cn/gitlab.json?gitlab_url=https://jihulab.com')
   .expectBadge({
     label: 'languages',
@@ -17,7 +17,7 @@ t.create('Languages (self-managed)')
     color: 'blue',
   })
 
-t.create('Languages (project not found)')
+t.create('language count (project not found)')
   .get('/open/guoxudong.io/shields-test/do-not-exist.json')
   .expectBadge({
     label: 'languages',
