528159ac66fe05df4499bd480ef8f69326cc22fd
Tejas Dhamecha <tejasdhamecha231@gmail.com>
2022-10-03 18:21:54 +0530

    add tester

---
+++ services/github/github-gist-stars.tester.js
@@ -0,0 +1,22 @@
+import { createServiceTester } from '../tester.js'
+import { isMetric } from '../test-validators.js'
+
+export const t = await createServiceTester()
+
+t.create('Gist Total Stars')
+  .get('/47a4d00457a92aa426dbd48a18776322.json')
+  .expectBadge({
+    label: 'Stars',
+    message: isMetric,
+    color: 'blue',
+    link: ['https://gist.github.com/47a4d00457a92aa426dbd48a18776322'],
+  })
+
+t.create('Gist Total Stars (Not Found)')
+  .get('/invalid-gist-id.json')
+  .expectBadge({
+    label: 'Stars',
+    message: 'gist not found',
+    color: 'red',
+    link: [],
+  })
