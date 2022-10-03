e48a6571dd9c5ef0c373993be7cf69ccbe9994e1
Tejas Dhamecha <tejasdhamecha231@gmail.com>
2022-10-03 17:18:13 +0530

    add transform, render and handle method to service class

--- services/github/github-gist-stars.service.js
+++ services/github/github-gist-stars.service.js
@@ -1,5 +1,7 @@
 import gql from 'graphql-tag'
 import Joi from 'joi'
+import { metric } from '../text-formatters.js'
+import { NotFound } from '../index.js'
 import { GithubAuthV4Service } from './github-auth-service.js'
 
 const schema = Joi.object({
@@ -21,6 +23,16 @@ export default class GithubGistStars extends GithubAuthV4Service {
     pattern: ':gist',
   }
 
+  static defaultBadgeData = {
+    label: 'Stars',
+    color: 'blue',
+    namedLogo: 'github',
+  }
+
+  static render({ stargazerCount, url }) {
+    return { message: metric(stargazerCount), link: [url] }
+  }
+
   async fetch({ gist }) {
     const data = await this._requestGraphql({
       query: gql`
@@ -39,5 +51,29 @@ export default class GithubGistStars extends GithubAuthV4Service {
       schema,
     })
     return data
+  }
+
+  static transform({ data }) {
+    const {
+      data: {
+        viewer: { gist },
+      },
+    } = data
+
+    if (!gist) {
+      throw new NotFound({ prettyMessage: 'gist not found' })
+    }
+
+    const { stargazerCount, url } = gist
+
+    return { stargazerCount, url }
+  }
+
+  async handle({ gist }) {
+    const data = await this.fetch({ gist })
+    const { stargazerCount, url } = await this.constructor.transform({
+      data,
+    })
+    return this.constructor.render({ stargazerCount, url })
   }
 }
