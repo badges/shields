492b49e4177c26606d7664c14a6df8053b67bb5a
Paula Barszcz <paula.anna.barszcz@gmail.com>
2022-10-04 18:20:01 +0200

    update tests for WhatPulse badge:

--- services/whatpulse/whatpulse.tester.js
+++ services/whatpulse/whatpulse.tester.js
@@ -4,27 +4,19 @@ import { isOrdinalNumber } from '../test-validators.js'
 export const t = await createServiceTester()
 
 t.create('WhatPulse user as user id, category not from Ranks')
-  .get('/user/179734.json?category=DOWNLOAD')
-  .expectBadge({ label: 'WhatPulse DOWNLOAD', message: Joi.string() })
-
-t.create('WhatPulse user as user name, category from Ranks')
-  .get('/user/jerone.json?category=rankS/UplOad')
-  .expectBadge({ label: 'WhatPulse rankS/UplOad', message: isOrdinalNumber })
-
-t.create('WhatPulse team as team id, category not from Ranks')
-  .get('/team/1295.json?category=clicks')
-  .expectBadge({ label: 'WhatPulse clicks', message: Joi.string() })
+  .get('/uptime/user/179734.json')
+  .expectBadge({ label: 'uptime', message: Joi.string() })
 
 t.create('WhatPulse team as team name, category from Ranks')
-  .get('/team/dutch%20power%20cows.json?category=RANKS/clickS')
-  .expectBadge({ label: 'WhatPulse RANKS/clickS', message: isOrdinalNumber })
-
-t.create('WhatPulse invalid category name')
-  .get('/user/179734.json?category=nonExistentCategory')
-  .expectBadge({ label: 'WhatPulse', message: 'invalid category' })
+  .get('/keys/team/dutch power cows.json?rank')
+  .expectBadge({ label: 'keys', message: isOrdinalNumber })
 
 t.create(
-  'WhatPulse incorrect user name - invalid response from WhatPulse (does not contain all of the Joi-required fields)'
+  'WhatPulse invalid category name (not one of the options from the dropdown for the WhatPulse`s badge modal)'
 )
-  .get('/user/1797344444444444444.json?category=download')
+  .get('/UpTIMe/user/jerone.json')
+  .expectBadge({ label: '404', message: 'badge not found' })
+
+t.create('WhatPulse incorrect user name')
+  .get('/uptime/user/NonExistentUsername.json')
   .expectBadge({ label: 'WhatPulse', message: 'invalid response data' })
