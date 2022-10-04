27e9dbe93eb19e65cc58501520c494991c49221e
Paula Barszcz <paula.anna.barszcz@gmail.com>
2022-10-04 17:52:13 +0200

    WhatPulse badge changes are in progress

--- services/whatpulse/whatpulse.service.js
+++ services/whatpulse/whatpulse.service.js
@@ -3,8 +3,8 @@ import { BaseJsonService, NotFound } from '../index.js'
 import { ordinalNumber } from '../text-formatters.js'
 
 const schema = Joi.object({
-  Keys: Joi.alternatives(Joi.string(), Joi.number()),
-  Clicks: Joi.alternatives(Joi.string(), Joi.number()),
+  Keys: Joi.alternatives(Joi.string(), Joi.number()).required(),
+  Clicks: Joi.alternatives(Joi.string(), Joi.number()).required(),
   UptimeShort: Joi.string().required(),
   Download: Joi.string().required(),
   Upload: Joi.string().required(),
@@ -18,70 +18,56 @@ const schema = Joi.object({
 }).required()
 
 const queryParamSchema = Joi.object({
-  category: Joi.string().required(),
+  rank: Joi.equal(''),
 }).required()
 
 export default class WhatPulse extends BaseJsonService {
   static category = 'activity'
   static route = {
     base: 'whatpulse',
-    pattern: ':userOrTeam/:id',
+    pattern:
+      ':metric(keys|clicks|uptime|download|upload)/:userType(user|team)/:id',
     queryParamSchema,
   }
 
   static examples = [
     {
-      title: 'WhatPulse user stats - Keys',
-      namedParams: { userOrTeam: 'user', id: 'jerone' },
-      queryParams: { category: 'keys' },
+      title: 'WhatPulse user stats',
+      namedParams: { metric: 'keys', userType: 'user', id: '179734' },
       staticPreview: this.render({
-        category: 'Keys',
-        categoryValue: '26448513',
+        metric: 'keys',
+        metricValue: '26448513',
       }),
     },
     {
-      title: 'WhatPulse user stats - Ranks in Upload',
-      namedParams: { userOrTeam: 'user', id: 'jerone' },
-      queryParams: { category: 'Ranks/Upload' },
+      title: 'WhatPulse team stats - rank',
+      namedParams: {
+        metric: 'upload',
+        userType: 'team',
+        id: 'dutch power cows',
+      },
+      queryParams: { rank: null },
       staticPreview: this.render({
-        category: 'Ranks/Upload',
-        categoryValue: '5444ᵗʰ',
-      }),
-    },
-    {
-      title: 'WhatPulse team stats - UptimeShort',
-      namedParams: { userOrTeam: 'team', id: '1295' },
-      queryParams: { category: 'UptimeShort' },
-      staticPreview: this.render({
-        category: 'UptimeShort',
-        categoryValue: '21344764434',
-      }),
-    },
-    {
-      title: 'WhatPulse team stats - Ranks in Download',
-      namedParams: { userOrTeam: 'team', id: 'dutch power cows' },
-      queryParams: { category: 'Ranks/Download' },
-      staticPreview: this.render({
-        category: 'Ranks/Download',
-        categoryValue: '1ˢᵗ',
+        metric: 'upload',
+        metricValue: '1ˢᵗ',
       }),
     },
   ]
 
   static defaultBadgeData = { label: 'WhatPulse' }
 
-  static render({ category, categoryValue }) {
+  static render({ metric, metricValue }) {
     return {
-      label: `WhatPulse ${category}`,
-      message: categoryValue,
-      color: '#374856',
+      label: `${metric}`,
+      message: metricValue,
+      color: 'informational',
     }
   }
 
-  async fetch({ userOrTeam, id }) {
+  async fetch({ userType, id }) {
     return await this._requestJson({
       schema,
-      url: `https://api.whatpulse.org/${userOrTeam}.php?${userOrTeam}=${id}&format=json`,
+      url: `https://api.whatpulse.org/${userType}.php?${userType}=${id}&format=json`,
     })
   }
 
@@ -92,37 +78,39 @@ export default class WhatPulse extends BaseJsonService {
     }, {})
   }
 
-  transform({ json, category }) {
-    let categoryValue
+  transform({ json, metric }, { rank }) {
+    let metricValue
 
     // To enable comparisons with the categories from the url that were written in varied cases, we need to lowercase the keys in the object from the WhatPulse's API.
     const jsonLowercase = this.toLowerKeys(json)
     jsonLowercase.ranks = this.toLowerKeys(json.Ranks)
 
-    // When the user wants to show Ranks/Keys | Ranks/Clicks | Ranks/Download | Ranks/Upload | Ranks/Uptime,
-    // we need to extract the word after the slash. Additionally, we need to use ordinal numbers.
-    // When there is no slash, we can directly use the lowercase version of the param,
-    // to compare it against the lowercased response from the WhatPulse's API.
-    if (!category.includes('/')) {
-      categoryValue = jsonLowercase[category.toLowerCase()]
+    // Just metric, no rank.
+    if (rank === undefined) {
+      metricValue = jsonLowercase[metric]
+
+      // For uptime, we take the value from the `UptimeShort` field from the WhatPulse's API response.
+      if (metric === 'uptime') {
+        metricValue = jsonLowercase.uptimeshort
+      }
+
+      // Achieved rank with the given metric.
     } else {
-      const rankTypeStart = category.indexOf('/')
-      const categoryLowercase = category.toLowerCase().slice(rankTypeStart + 1)
-      const rank = jsonLowercase.ranks[categoryLowercase]
-      categoryValue = ordinalNumber(rank)
+      const rankFromResp = jsonLowercase.ranks[metric]
+      metricValue = ordinalNumber(rankFromResp)
     }
 
-    if (categoryValue) {
-      return categoryValue
+    if (metricValue) {
+      return metricValue
     } else {
-      throw new NotFound({ prettyMessage: 'invalid category' })
+      throw new NotFound({ prettyMessage: 'invalid metric' })
     }
   }
 
-  async handle({ userOrTeam, id }, { category }) {
-    const json = await this.fetch({ userOrTeam, id, category })
-    const categoryValue = this.transform({ json, category })
+  async handle({ metric, userType, id }, { rank }) {
+    const json = await this.fetch({ userType, id, metric })
+    const metricValue = this.transform({ json, metric }, { rank })
 
-    return this.constructor.render({ category, categoryValue })
+    return this.constructor.render({ metric, metricValue })
   }
 }
