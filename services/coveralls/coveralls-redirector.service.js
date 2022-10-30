ee9a25094458c73781ce8b8e949b7e4c307833b3
Paula Barszcz <paula.anna.barszcz@gmail.com>
2022-10-30 11:30:17 +0100

    Added GitLab to Coveralls badge + added redirect

--- services/coveralls/coveralls-redirector.service.js
+++ services/coveralls/coveralls-redirector.service.js
@@ -9,7 +9,18 @@ export default [
       pattern: ':user((?!github|bitbucket).*)/:repo/:branch*',
     },
     transformPath: ({ user, repo, branch }) =>
-      `/coveralls/github/${user}/${repo}${branch ? `/${branch}` : ''}`,
+      `/coverallsCoverage/github/${user}/${repo}${branch ? `/${branch}` : ''}`,
     dateAdded: new Date('2021-02-23'),
+  }),
+  redirector({
+    name: 'CoverallsPreGitlabRedirect',
+    category: 'coverage',
+    route: {
+      base: 'coveralls',
+      pattern: ':vcsType(github|bitbucket|gitlab)/:user/:repo/:branch*',
+    },
+    transformPath: ({ user, repo, branch }) =>
+      `/coverallsCoverage/github/${user}/${repo}${branch ? `/${branch}` : ''}`,
+    dateAdded: new Date('2022-10-30'),
   }),
 ]
