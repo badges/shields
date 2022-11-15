973a6f3c8ba298acfa4e8ec9d39a7157fa1267a5
Alexandre Sanlim <alexandreslima@outlook.com>
2022-11-15 12:21:17 +0000

    fix given

--- services/coincap/coincap-priceusd.spec.js
+++ services/coincap/coincap-priceusd.spec.js
@@ -3,14 +3,14 @@ import PriceUsdService from './coincap-priceusd.service.js'
 
 describe('PriceUsdFormat', function () {
   test(PriceUsdService.priceFormat, () => {
-    given({ price: '332432432' }).expect('$332,432,432.00')
-    given({ price: '332432432.3432432' }).expect('$332,432,432.34')
-    given({ price: '3' }).expect('$3.00')
-    given({ price: '33' }).expect('$33.00')
-    given({ price: '332' }).expect('$332.00')
-    given({ price: '3324' }).expect('$3,324.00')
-    given({ price: '332432' }).expect('$332,432.00')
-    given({ price: '332432.2' }).expect('$332,432.20')
-    given({ price: '332432.25' }).expect('$332,432.25')
+    given('3').expect('$3.00')
+    given('33').expect('$33.00')
+    given('332').expect('$332.00')
+    given('3324').expect('$3,324.00')
+    given('332432').expect('$332,432.00')
+    given('332432.2').expect('$332,432.20')
+    given('332432.25').expect('$332,432.25')
+    given('332432432').expect('$332,432,432.00')
+    given('332432432.3432432').expect('$332,432,432.34')
   })
 })
