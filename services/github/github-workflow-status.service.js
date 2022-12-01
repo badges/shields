e0b7dbd58cc4e0f7f7a47c51fff5e85c7162512a
chris48s <git@chris-shaw.dev>
2022-12-01 19:44:58 +0000

    custom deprecation message

--- services/github/github-workflow-status.service.js
+++ services/github/github-workflow-status.service.js
@@ -1,11 +1,28 @@
-import { deprecatedService } from '../index.js'
+import { BaseService } from '../index.js'
 
-export default deprecatedService({
-  category: 'build',
-  route: {
+export default class DeprecatedGithubWorkflowStatus extends BaseService {
+  static category = 'build'
+
+  static route = {
     base: 'github/workflow/status',
     pattern: ':various+',
-  },
-  label: 'build',
-  dateAdded: new Date('2022-10-03'),
-})
+  }
+
+  static examples = []
+
+  static defaultBadgeData = { label: 'build' }
+
+  async handle() {
+    return {
+      label: 'build',
+      message: 'https://github.com/badges/shields/issues/8671',
+      /*
+      This is a 'special' deprecation because we are making a breaking change
+      We've implemented it as a custom class instead of a normal
+      deprecatedService so that we can include link.
+      */
+      link: ['https://github.com/badges/shields/issues/8671'],
+      color: 'red',
+    }
+  }
+}
--- services/github/github-workflow-status.tester.js
+++ services/github/github-workflow-status.tester.js
@@ -10,33 +10,33 @@ t.create('no longer available (previously nonexistent repo)')
   .get('/badges/shields-fakeness/fake.json')
   .expectBadge({
     label: 'build',
-    message: 'no longer available',
+    message: 'https://github.com/badges/shields/issues/8671',
   })
 
 t.create('no longer available (previously nonexistent workflow)')
   .get('/actions/toolkit/not-a-real-workflow.json')
   .expectBadge({
     label: 'build',
-    message: 'no longer available',
+    message: 'https://github.com/badges/shields/issues/8671',
   })
 
 t.create('no longer available (previously valid workflow)')
   .get('/actions/toolkit/toolkit-unit-tests.json')
   .expectBadge({
     label: 'build',
-    message: 'no longer available',
+    message: 'https://github.com/badges/shields/issues/8671',
   })
 
 t.create('no longer available (previously valid workflow - branch)')
   .get('/actions/toolkit/toolkit-unit-tests/master.json')
   .expectBadge({
     label: 'build',
-    message: 'no longer available',
+    message: 'https://github.com/badges/shields/issues/8671',
   })
 
 t.create('no longer available (previously valid workflow - event)')
   .get('/actions/toolkit/toolkit-unit-tests.json?event=push')
   .expectBadge({
     label: 'build',
-    message: 'no longer available',
+    message: 'https://github.com/badges/shields/issues/8671',
   })
