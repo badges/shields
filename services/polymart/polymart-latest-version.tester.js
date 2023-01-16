a194a3dde5ed0eb1429c587e196b926190e56bda
MherZaqaryan <mher.zaqaryann@gmail.com>
2023-01-16 21:47:30 +0400

    change version regex

--- services/polymart/polymart-latest-version.tester.js
+++ services/polymart/polymart-latest-version.tester.js
@@ -1,13 +1,11 @@
-import { withRegex } from '../test-validators.js'
+import { isVPlusDottedVersionNClauses } from '../test-validators.js'
 import { createServiceTester } from '../tester.js'
 export const t = await createServiceTester()
 
-t.create('Polymart Plugin (id 323)')
-  .get('/323.json')
-  .expectBadge({
-    label: 'polymart',
-    message: withRegex(/^(?!not found$)/),
-  })
+t.create('Polymart Plugin (id 323)').get('/323.json').expectBadge({
+  label: 'polymart',
+  message: isVPlusDottedVersionNClauses,
+})
 
 t.create('Invalid Resource (id 0)').get('/0.json').expectBadge({
   label: 'polymart',
