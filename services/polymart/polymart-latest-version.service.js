2e9f697f99dd033dc82c8ef7d936a878ee9ed23f
MherZaqaryan <mher.zaqaryann@gmail.com>
2023-01-16 21:07:26 +0400

    remove version default badge color

--- services/polymart/polymart-latest-version.service.js
+++ services/polymart/polymart-latest-version.service.js
@@ -24,7 +24,6 @@ export default class PolymartLatestVersion extends BasePolymartService {
 
   static defaultBadgeData = {
     label: 'version',
-    color: 'blue',
   }
 
   static render({ version }) {
