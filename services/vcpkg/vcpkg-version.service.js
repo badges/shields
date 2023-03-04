4a5bf538ff98753067cb38976fe83875c1ea0cd5
Nicolas Jakob <njakob@users.noreply.github.com>
2023-03-04 21:40:22 +0100

    Fix [Vcpkg] version service for different version fields (#8945)
    * Fix [Vcpkg] version service for different version fields
    
    * Use match one to enforce a single alternative
    
    * Mock different version field test cases
    
    * Extract version field parsing as separate helper
    
    ---------
    
    Co-authored-by: repo-ranger[bot] <39074581+repo-ranger[bot]@users.noreply.github.com>

--- services/vcpkg/vcpkg-version.service.js
+++ services/vcpkg/vcpkg-version.service.js
@@ -3,10 +3,26 @@ import { ConditionalGithubAuthV3Service } from '../github/github-auth-service.js
 import { fetchJsonFromRepo } from '../github/github-common-fetch.js'
 import { renderVersionBadge } from '../version.js'
 import { NotFound } from '../index.js'
+import { parseVersionFromVcpkgManifest } from './vcpkg-version-helpers.js'
 
-const vcpkgManifestSchema = Joi.object({
-  version: Joi.string().required(),
-}).required()
+// Handle the different version fields available in Vcpkg manifests
+// https://learn.microsoft.com/en-us/vcpkg/reference/vcpkg-json?source=recommendations#version
+const vcpkgManifestSchema = Joi.alternatives()
+  .match('one')
+  .try(
+    Joi.object({
+      version: Joi.string().required(),
+    }).required(),
+    Joi.object({
+      'version-date': Joi.string().required(),
+    }).required(),
+    Joi.object({
+      'version-semver': Joi.string().required(),
+    }).required(),
+    Joi.object({
+      'version-string': Joi.string().required(),
+    }).required()
+  )
 
 export default class VcpkgVersion extends ConditionalGithubAuthV3Service {
   static category = 'version'
@@ -29,13 +45,14 @@ export default class VcpkgVersion extends ConditionalGithubAuthV3Service {
 
   async handle({ portName }) {
     try {
-      const { version } = await fetchJsonFromRepo(this, {
+      const manifest = await fetchJsonFromRepo(this, {
         schema: vcpkgManifestSchema,
         user: 'microsoft',
         repo: 'vcpkg',
         branch: 'master',
         filename: `ports/${portName}/vcpkg.json`,
       })
+      const version = parseVersionFromVcpkgManifest(manifest)
       return this.constructor.render({ version })
     } catch (error) {
       if (error instanceof NotFound) {
