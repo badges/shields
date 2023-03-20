77b0343bf37cbe52252498244bbde5f761dfae29
chris48s <git@chris-shaw.dev>
2023-03-20 15:06:15 +0000

    appease the lint gods

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -3,7 +3,7 @@ import clsx from 'clsx'
 import Layout from '@theme/Layout'
 import Link from '@docusaurus/Link'
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext'
-import HomepageFeatures from '../components/HomepageFeatures'
+import HomepageFeatures from '../components/homepage-features'
 import styles from './index.module.css'
 
 function HomepageHeader() {
