9d831bd88ba93cb21d789633b2c0356aa450eb95
chris48s <git@chris-shaw.dev>
2023-04-10 10:45:01 +0100

    error and exit if redis_url is set

--- core/server/server.js
+++ core/server/server.js
@@ -182,6 +182,7 @@ const privateConfigSchema = Joi.object({
   npm_token: Joi.string(),
   obs_user: Joi.string(),
   obs_pass: Joi.string(),
+  redis_url: Joi.string().uri({ scheme: ['redis', 'rediss'] }),
   postgres_url: Joi.string().uri({ scheme: 'postgresql' }),
   sentry_dsn: Joi.string(),
   sl_insight_userUuid: Joi.string(),
