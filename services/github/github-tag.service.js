3b269d2f146a937e26d0c0dd4b903b643dc2e86c
slaclau <77557628+slaclau@users.noreply.github.com>
2023-04-12 09:40:11 +0100

    Update github-tag.service.js

--- services/github/github-tag.service.js
+++ services/github/github-tag.service.js
@@ -99,7 +99,7 @@ class GithubTag extends GithubAuthV4Service {
     })
   }
 
-  static getLatestTag({ tags, sort, includePrereleases,
+  static getLatestTag({ tags, sort, includePrereleases, exclude }) {
     tags = tags.filter(tag => !tag.includes(exclude))
     if (sort === 'semver') {
       return latest(tags, { pre: includePrereleases })
