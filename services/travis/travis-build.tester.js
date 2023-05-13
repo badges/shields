ce815cc2e442a035c8df5614b70d32865a480c9f
chris48s <git@chris-shaw.dev>
2023-05-13 20:54:02 +0100

    deprecate [travis].org badges

--- services/travis/travis-build.tester.js
+++ services/travis/travis-build.tester.js
@@ -5,30 +5,19 @@ export const t = await createServiceTester()
 
 // Travis (.org) CI
 
-t.create('build status on default branch')
+t.create('build status without branch, deprecated')
   .get('/rust-lang/rust.json')
   .expectBadge({
     label: 'build',
-    message: Joi.alternatives().try(isBuildStatus, Joi.equal('unknown')),
+    message: 'no longer available',
   })
 
-t.create('build status on named branch')
+t.create('build status on named branch, deprecated')
   .get('/rust-lang/rust/stable.json')
   .expectBadge({
     label: 'build',
-    message: Joi.alternatives().try(isBuildStatus, Joi.equal('unknown')),
+    message: 'no longer available',
   })
-
-t.create('unknown repo')
-  .get('/this-repo/does-not-exist.json')
-  .expectBadge({ label: 'build', message: 'unknown' })
-
-t.create('invalid svg response')
-  .get('/foo/bar.json')
-  .intercept(nock =>
-    nock('https://api.travis-ci.org').get('/foo/bar.svg').reply(200)
-  )
-  .expectBadge({ label: 'build', message: 'unparseable svg response' })
 
 // Travis (.com) CI
 
