90929077175deee52baafe78af42bff179090d58
chris48s <git@chris-shaw.dev>
2023-05-22 20:50:45 +0100

    Allow user to filter github tags and releases

--- services/github/github-tag.spec.js
+++ services/github/github-tag.spec.js
@@ -53,4 +53,24 @@ describe('GithubTag', function () {
       color: 'blue',
     })
   })
+
+  test(GithubTag.getLimit, () => {
+    given({ sort: 'date', filter: undefined }).expect(1)
+    given({ sort: 'date', filter: '' }).expect(1)
+    given({ sort: 'date', filter: '!*-dev' }).expect(100)
+    given({ sort: 'semver', filter: undefined }).expect(100)
+    given({ sort: 'semver', filter: '' }).expect(100)
+    given({ sort: 'semver', filter: '!*-dev' }).expect(100)
+  })
+
+  test(GithubTag.applyFilter, () => {
+    const tags = ['v1.1.0', 'v1.2.0', 'server-2022-01-01']
+    given({ tags, filter: undefined }).expect(tags)
+    given({ tags, filter: '' }).expect(tags)
+    given({ tags, filter: '*' }).expect(tags)
+    given({ tags, filter: '!*' }).expect([])
+    given({ tags, filter: 'foo' }).expect([])
+    given({ tags, filter: 'server-*' }).expect(['server-2022-01-01'])
+    given({ tags, filter: '!server-*' }).expect(['v1.1.0', 'v1.2.0'])
+  })
 })
