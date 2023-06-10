b2ff593a75d3918cf7814087fccb08ad4980263e
chris48s <git@chris-shaw.dev>
2023-06-10 16:53:17 +0100

    rename HomepageFeatures to homepage-features

--- frontend/src/components/homepage-features.js
+++ frontend/src/components/homepage-features.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import clsx from 'clsx'
-import styles from './HomepageFeatures.module.css'
+import styles from './homepage-features.module.css'
 
 const FeatureList = [
   {
