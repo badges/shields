c586960ce8c57898f6f19f57d69be14672043e18
chris48s <chris48s@users.noreply.github.com>
2023-08-07 15:51:18 +0100

    allow calling [github] without auth (#9427)
    * allow calling [github] without auth
    
    * set a default for authType, fix broken tests

--- services/github/github-auth-service.spec.js
+++ services/github/github-auth-service.spec.js
@@ -41,6 +41,7 @@ describe('GithubAuthV3Service', function () {
     )
     const githubApiProvider = new GithubApiProvider({
       baseUrl: 'https://github-api.example.com',
+      authType: GithubApiProvider.AUTH_TYPES.TOKEN_POOL,
       restApiVersion: '2022-11-28',
     })
     const mockToken = { update: sinon.mock(), invalidate: sinon.mock() }
