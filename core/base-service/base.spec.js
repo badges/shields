62d56e1a7586d8d5d60b76c4715b715b494236f3
LitoMore <litomore@gmail.com>
2023-09-20 15:14:51 +0800

    Drop unused parameters

--- core/base-service/base.spec.js
+++ core/base-service/base.spec.js
@@ -373,7 +373,6 @@ describe('BaseService', function () {
         namedLogo: undefined,
         logo: undefined,
         logoWidth: undefined,
-        logoHeight: undefined,
         logoSize: undefined,
         logoPosition: undefined,
         links: [],
