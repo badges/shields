064d1886715d0168563ec57d1f5d566d58916061
Sergey Kupletsky <s.kupletsky@gmail.com>
2023-10-08 18:08:22 +0200

    fix issue with custom logo generation (#9644)
    * allow `logo`, `logoPosition`, `logoWidth`, and `links` as expected keys
    
    * add validation for `logo`, `logoPosition`, `logoWidth`, and `links` keys
    
    * add validation tests

--- badge-maker/index.d.ts
+++ badge-maker/index.d.ts
@@ -4,6 +4,10 @@ interface Format {
   labelColor?: string
   color?: string
   style?: 'plastic' | 'flat' | 'flat-square' | 'for-the-badge' | 'social'
+  logo?: string
+  logoPosition?: number
+  logoWidth?: number
+  links?: Array<string>
 }
 
 export declare class ValidationError extends Error {}
