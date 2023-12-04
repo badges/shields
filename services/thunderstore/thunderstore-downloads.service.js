8ed52589e02cecb6a87eb5895f16129a7c430b43
Lordfirespeed <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-04 16:41:48 +0000

    badge service and corresponding tester for thunderstore download count

---
+++ services/thunderstore/thunderstore-downloads.service.js
@@ -0,0 +1,42 @@
+import { renderDownloadsBadge } from '../downloads.js'
+import { BaseThunderstoreService, documentation } from './thunderstore-base.js'
+
+export default class ThunderstoreDownloads extends BaseThunderstoreService {
+  static category = 'downloads'
+
+  static route = {
+    base: 'thunderstore/downloads',
+    pattern: ':namespace/:packageName',
+  }
+
+  static examples = [
+    {
+      title: 'Thunderstore Downloads',
+      namedParams: {
+        namespace: 'notnotnotswipez',
+        packageName: 'MoreCompany',
+      },
+      staticPreview: renderDownloadsBadge({ downloads: 120000 }),
+      documentation,
+    },
+  ]
+
+  static defaultBadgeData = {
+    label: 'downloads',
+    namedLogo: 'thunderstore',
+  }
+
+  /**
+   * @param {object} pkg - Package specifier
+   * @param {string} pkg.namespace - the package namespace
+   * @param {string} pkg.packageName - the package name
+   * @returns {Promise<*>} - Promise containing the rendered badge payload
+   */
+  async handle({ namespace, packageName }) {
+    const { total_downloads: downloads } = await this.fetchPackage({
+      namespace,
+      packageName,
+    })
+    return renderDownloadsBadge({ downloads })
+  }
+}
