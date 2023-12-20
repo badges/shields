2c2683571a6453a4b558ab826257a6c3217da6fc
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-20 18:33:30 +0000

    use consistent test formatting

--- services/thunderstore/thunderstore-version.tester.js
+++ services/thunderstore/thunderstore-version.tester.js
@@ -3,10 +3,9 @@ import { isSemver } from '../test-validators.js'
 
 export const t = await createServiceTester()
 
-t.create('Version').get('/ebkr/r2modman.json').expectBadge({
-  label: 'thunderstore',
-  message: isSemver,
-})
+t.create('Version')
+  .get('/ebkr/r2modman.json')
+  .expectBadge({ label: 'thunderstore', message: isSemver })
 
 t.create('Version (not found)')
   .get('/not-a-namespace/not-a-package-name.json')
