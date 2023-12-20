9aff706ea06ce96dab70dbd4db0e23ddcd00d97c
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-20 18:11:08 +0000

    use `[x].json` for test assertions

--- services/thunderstore/thunderstore-version.tester.js
+++ services/thunderstore/thunderstore-version.tester.js
@@ -4,12 +4,12 @@ import { withRegex } from '../test-validators.js'
 export const t = await createServiceTester()
 
 t.create('Version')
-  .get('/ebkr/r2modman')
+  .get('/ebkr/r2modman.json')
   .expectBadge({
     label: 'thunderstore',
     message: withRegex(/^(?:0|[1-9]\d*)(\.(?:0|[1-9]\d*)){1,2}$/),
   })
 
 t.create('Version (not found)')
-  .get('/not-a-namespace/not-a-package-name')
+  .get('/not-a-namespace/not-a-package-name.json')
   .expectBadge({ label: 'thunderstore', message: 'not found', color: 'red' })
