c144067c1cc291e1dfe338727dda9f6a597f8014
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-20 18:23:18 +0000

    use existing version pattern

--- services/thunderstore/thunderstore-version.tester.js
+++ services/thunderstore/thunderstore-version.tester.js
@@ -1,14 +1,12 @@
 import { createServiceTester } from '../tester.js'
-import { withRegex } from '../test-validators.js'
+import { isSemver } from '../test-validators.js'
 
 export const t = await createServiceTester()
 
-t.create('Version')
-  .get('/ebkr/r2modman.json')
-  .expectBadge({
-    label: 'thunderstore',
-    message: withRegex(/^(?:0|[1-9]\d*)(\.(?:0|[1-9]\d*)){1,2}$/),
-  })
+t.create('Version').get('/ebkr/r2modman.json').expectBadge({
+  label: 'thunderstore',
+  message: isSemver,
+})
 
 t.create('Version (not found)')
   .get('/not-a-namespace/not-a-package-name.json')
