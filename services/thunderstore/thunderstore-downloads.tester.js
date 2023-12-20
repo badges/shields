83efdd53b288966d33a8b55918da2947fb3bdabb
Joseph Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-20 18:11:08 +0000

    use `[x].json` for test assertions

--- services/thunderstore/thunderstore-downloads.tester.js
+++ services/thunderstore/thunderstore-downloads.tester.js
@@ -4,9 +4,9 @@ import { isMetric } from '../test-validators.js'
 export const t = await createServiceTester()
 
 t.create('Downloads')
-  .get('/ebkr/r2modman')
+  .get('/ebkr/r2modman.json')
   .expectBadge({ label: 'downloads', message: isMetric })
 
 t.create('Downloads (not found)')
-  .get('/not-a-namespace/not-a-package-name')
+  .get('/not-a-namespace/not-a-package-name.json')
   .expectBadge({ label: 'downloads', message: 'not found', color: 'red' })
