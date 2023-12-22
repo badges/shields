d0bdb8244cf8820d6913effac1d41e5daf1cd2e0
Joe Clack <28568841+Lordfirespeed@users.noreply.github.com>
2023-12-22 14:29:03 +0000

    [Thunderstore] Add Thunderstore Badges  (#9782)
    * add base for thunderstore services
    
    * badge service and corresponding tester for thunderstore download count
    
    * badge service and corresponding tester for thunderstore latest package version
    
    * fix HTML
    
    * use stable package-metrics endpoint
    
    * remove erroneous statement from docs
    
    * remove `namedLogo` from default badge data on both services
    
    * follow route naming conventions
    
    * use `[x].json` for test assertions
    
    * use existing version pattern
    
    * document service `handle` return-type more narrowly
    
    * use consistent test formatting
    
    * add base for thunderstore services
    
    * badge service and corresponding tester for thunderstore download count
    
    * badge service and corresponding tester for thunderstore latest package version
    
    * fix HTML
    
    * use stable package-metrics endpoint
    
    * remove erroneous statement from docs
    
    * remove `namedLogo` from default badge data on both services
    
    * follow route naming conventions
    
    * use `[x].json` for test assertions
    
    * use existing version pattern
    
    * document service `handle` return-type more narrowly
    
    * use consistent test formatting
    
    * plural-ise base thunderstore docs
    
    * don't require unused attributes
    
    * declare BaseThunderstoreService abstract, add docstring
    
    * add thunderstoreGreen static variable
    
    * add thunderstore likes service
    
    ---------
    
    Co-authored-by: chris48s <chris48s@users.noreply.github.com>

---
+++ services/thunderstore/thunderstore-downloads.service.js
@@ -0,0 +1,41 @@
+import { renderDownloadsBadge } from '../downloads.js'
+import { BaseThunderstoreService, documentation } from './thunderstore-base.js'
+
+export default class ThunderstoreDownloads extends BaseThunderstoreService {
+  static category = 'downloads'
+
+  static route = {
+    base: 'thunderstore/dt',
+    pattern: ':namespace/:packageName',
+  }
+
+  static examples = [
+    {
+      title: 'Thunderstore Downloads',
+      namedParams: {
+        namespace: 'notnotnotswipez',
+        packageName: 'MoreCompany',
+      },
+      staticPreview: renderDownloadsBadge({ downloads: 120000 }),
+      documentation,
+    },
+  ]
+
+  static defaultBadgeData = {
+    label: 'downloads',
+  }
+
+  /**
+   * @param {object} pkg - Package specifier
+   * @param {string} pkg.namespace - the package namespace
+   * @param {string} pkg.packageName - the package name
+   * @returns {Promise<object>} - Promise containing the rendered badge payload
+   */
+  async handle({ namespace, packageName }) {
+    const { downloads } = await this.fetchPackageMetrics({
+      namespace,
+      packageName,
+    })
+    return renderDownloadsBadge({ downloads })
+  }
+}
