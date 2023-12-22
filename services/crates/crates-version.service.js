b71f812b68527b507262b2da4f74e4ffcc53f07a
chris48s <chris48s@users.noreply.github.com>
2023-12-22 13:25:38 +0000

    migrate examples to openApi part 17; affects [buildkite cii codeship crates jsdelivr npms] and bundlephobia (#9584)
    * update crates service test
    
    * migrate some services from examples to openApi
    
    * migrate crates from examples to openApi, improve titles
    
    * explain what hd,hw,hm,hy actually mean
    
    * improve descriptions

--- services/crates/crates-version.service.js
+++ services/crates/crates-version.service.js
@@ -1,19 +1,23 @@
 import { renderVersionBadge } from '../version.js'
-import { InvalidResponse } from '../index.js'
-import { BaseCratesService, keywords } from './crates-base.js'
+import { InvalidResponse, pathParams } from '../index.js'
+import { BaseCratesService, description } from './crates-base.js'
 
 export default class CratesVersion extends BaseCratesService {
   static category = 'version'
   static route = { base: 'crates/v', pattern: ':crate' }
 
-  static examples = [
-    {
-      title: 'Crates.io',
-      namedParams: { crate: 'rustc-serialize' },
-      staticPreview: renderVersionBadge({ version: '0.3.24' }),
-      keywords,
+  static openApi = {
+    '/crates/v/{crate}': {
+      get: {
+        summary: 'Crates.io Version',
+        description,
+        parameters: pathParams({
+          name: 'crate',
+          example: 'rustc-serialize',
+        }),
+      },
     },
-  ]
+  }
 
   transform(json) {
     if (json.errors) {
