63b4d698d3edae15399ad32beffed2ec475e2971
Sepehr Safari <safari.sepehr@gmail.com>
2024-01-11 11:27:45 +0330

    remove unnecessary test cases

--- services/nostr-band/nostr-band-followers.tester.js
+++ services/nostr-band/nostr-band-followers.tester.js
@@ -14,15 +14,3 @@ t.create('fetch: invalid npub').get('/invalidnpub.json').expectBadge({
   label: 'followers',
   message: 'invalid pubkey',
 })
-
-t.create('handle: valid npub')
-  .get('/npub18c556t7n8xa3df2q82rwxejfglw5przds7sqvefylzjh8tjne28qld0we7.json')
-  .expectBadge({
-    label: 'followers',
-    message: Joi.alternatives().try(isMetric, Joi.equal('invalid pubkey')),
-  })
-
-t.create('handle: invalid npub').get('/invalidnpub.json').expectBadge({
-  label: 'followers',
-  message: 'invalid pubkey',
-})
