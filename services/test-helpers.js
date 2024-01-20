18ec38723a38bdbeca1edd7b6e6d057a361375ac
jNullj <15849761+jNullj@users.noreply.github.com>
2024-01-21 00:59:43 +0200

    Add testAuth to test-helpers
    Add the testAuth function which tests auth of a service (badge) using a provided dummy response.

--- services/test-helpers.js
+++ services/test-helpers.js
@@ -1,3 +1,4 @@
+import { expect } from 'chai'
 import nock from 'nock'
 import config from 'config'
 import { fetch } from '../core/base-service/got.js'
@@ -78,6 +79,52 @@ function getBadgeExampleCall(serviceClass) {
   return exampleInvokeParams
 }
 
+/**
+ * Test authentication of a badge for it's first OpenAPI example using a provided dummyResponse
+ *
+ * @param {BaseService} serviceClass The service class tested.
+ * @param {object} dummyResponse An object containing the dummy response by the server.
+ * @throws {TypeError} - Throws a TypeError if the input `serviceClass` is not an instance of BaseService,
+ *   or if `serviceClass` is missing authorizedOrigins.
+ *
+ * @example
+ * // Example usage:
+ * testAuth(StackExchangeReputation, { items: [{ reputation: 8 }] })
+ */
+async function testAuth(serviceClass, dummyResponse) {
+  if (!(serviceClass.prototype instanceof BaseService)) {
+    throw new TypeError(
+      'Invalid serviceClass: Must be an instance of BaseService.',
+    )
+  }
+
+  cleanUpNockAfterEach()
+
+  const config = { private: { stackapps_api_key: 'fake-key' } }
+  const exampleInvokeParams = getBadgeExampleCall(serviceClass)
+  const authOrigin = serviceClass.auth.authorizedOrigins[0]
+
+  if (!authOrigin) {
+    throw new TypeError(`Missing authorizedOrigins for ${serviceClass.name}.`)
+  }
+
+  const scope = nock(authOrigin)
+    .get(/.*/)
+    .query(queryObject => queryObject.key === 'fake-key')
+    .reply(200, dummyResponse)
+  expect(
+    await serviceClass.invoke(defaultContext, config, exampleInvokeParams),
+  ).to.not.have.property('isError')
+
+  scope.done()
+}
+
 const defaultContext = { requestFetcher: fetch }
 
-export { cleanUpNockAfterEach, noToken, getBadgeExampleCall, defaultContext }
+export {
+  cleanUpNockAfterEach,
+  noToken,
+  getBadgeExampleCall,
+  testAuth,
+  defaultContext,
+}
