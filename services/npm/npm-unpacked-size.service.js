b5da8546be1776a290e2515f6eeb067ee7b5601a
Eray Erdin <eraygezer.94@gmail.com>
2024-02-07 14:53:09 +0300

    fetch unpacked size from latest version

--- services/npm/npm-unpacked-size.service.js
+++ services/npm/npm-unpacked-size.service.js
@@ -1,13 +1,24 @@
+import Joi from 'joi'
 import { BaseJsonService } from '../index.js'
 
 export default class NpmUnpackedSize extends BaseJsonService {
   static category = 'size'
   static route = { base: 'npm/unpacked-size', pattern: ':packageName' }
 
+  async fetch() {
+    return this._requestJson({
+      schema: Joi.any(),
+      url: 'https://registry.npmjs.org/express/latest',
+    })
+  }
+
   async handle() {
+    const { dist } = await this.fetch()
+    const { unpackedSize } = dist
+
     return {
-      label: 'example',
-      message: 'text',
+      label: 'unpacked size',
+      message: unpackedSize,
       color: 'blue',
     }
   }
