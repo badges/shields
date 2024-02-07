53b6dd4d971fe3ef889e92b10971b3d0cd792f04
Eray Erdin <eraygezer.94@gmail.com>
2024-02-07 15:17:22 +0300

    test against json endpoint instead of svg

--- services/npm/npm-unpacked-size.tester.js
+++ services/npm/npm-unpacked-size.tester.js
@@ -3,5 +3,5 @@ import { createServiceTester } from '../tester.js'
 export const t = await createServiceTester()
 
 t.create('Latest unpacked size')
-  .get('/npm/unpacked-size/express')
+  .get('/express.json')
   .expectBadge({ label: 'unpacked size', message: /.* kB/ })
