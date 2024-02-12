bb14a63bf37465bb33563d36243707b8dfd97467
Eray Erdin <eraygezer.94@gmail.com>
2024-02-12 23:09:24 +0300

    use isFileSize validator

--- services/npm/npm-unpacked-size.tester.js
+++ services/npm/npm-unpacked-size.tester.js
@@ -1,10 +1,11 @@
+import { isFileSize } from '../test-validators.js'
 import { createServiceTester } from '../tester.js'
 
 export const t = await createServiceTester()
 
 t.create('Latest unpacked size')
   .get('/express.json')
-  .expectBadge({ label: 'unpacked size', message: /.* kB/ })
+  .expectBadge({ label: 'unpacked size', message: isFileSize })
 
 t.create('Nonexistent unpacked size with version')
   .get('/express/4.16.0.json')
