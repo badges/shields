f8d3a42429aebbdfab34facd5a29bb5432d3965a
chris48s <git@chris-shaw.dev>
2024-02-18 16:06:13 +0000

    remove all the code for handling examples

--- core/base-service/service-definitions.js
+++ core/base-service/service-definitions.js
@@ -2,36 +2,34 @@ import Joi from 'joi'
 
 const arrayOfStrings = Joi.array().items(Joi.string()).min(0).required()
 
-const objectOfKeyValues = Joi.object()
-  .pattern(/./, Joi.string().allow(null))
-  .required()
-
-const openApiSchema = Joi.object().pattern(
-  /./,
-  Joi.object({
-    get: Joi.object({
-      summary: Joi.string().required(),
-      description: Joi.string(),
-      parameters: Joi.array()
-        .items(
-          Joi.object({
-            name: Joi.string().required(),
-            description: Joi.string(),
-            in: Joi.string().valid('query', 'path').required(),
-            required: Joi.boolean().required(),
-            schema: Joi.object({
-              type: Joi.string().required(),
-              enum: Joi.array(),
-            }).required(),
-            allowEmptyValue: Joi.boolean(),
-            example: Joi.string().allow(null),
-          }),
-        )
-        .min(1)
-        .required(),
+const openApiSchema = Joi.object()
+  .pattern(
+    /./,
+    Joi.object({
+      get: Joi.object({
+        summary: Joi.string().required(),
+        description: Joi.string(),
+        parameters: Joi.array()
+          .items(
+            Joi.object({
+              name: Joi.string().required(),
+              description: Joi.string(),
+              in: Joi.string().valid('query', 'path').required(),
+              required: Joi.boolean().required(),
+              schema: Joi.object({
+                type: Joi.string().required(),
+                enum: Joi.array(),
+              }).required(),
+              allowEmptyValue: Joi.boolean(),
+              example: Joi.string().allow(null),
+            }),
+          )
+          .min(1)
+          .required(),
+      }).required(),
     }).required(),
-  }).required(),
-)
+  )
+  .default({})
 
 const serviceDefinition = Joi.object({
   category: Joi.string().required(),
@@ -47,29 +45,6 @@ const serviceDefinition = Joi.object({
       queryParams: arrayOfStrings,
     }),
   ),
-  examples: Joi.array()
-    .items(
-      Joi.object({
-        title: Joi.string().required(),
-        example: Joi.object({
-          pattern: Joi.string(),
-          namedParams: objectOfKeyValues,
-          queryParams: objectOfKeyValues,
-        }).required(),
-        preview: Joi.object({
-          label: Joi.string(),
-          message: Joi.string().allow('').required(),
-          color: Joi.string().required(),
-          style: Joi.string(),
-          namedLogo: Joi.string(),
-        }).required(),
-        keywords: arrayOfStrings,
-        documentation: Joi.object({
-          __html: Joi.string().required(), // Valid HTML.
-        }),
-      }),
-    )
-    .default([]),
   openApi: openApiSchema,
 }).required()
 
@@ -84,15 +59,14 @@ const serviceDefinitionExport = Joi.object({
       Joi.object({
         id: Joi.string().required(),
         name: Joi.string().required(),
-        keywords: arrayOfStrings,
       }),
     )
     .required(),
   services: Joi.array().items(serviceDefinition).required(),
 }).required()
 
-function assertValidServiceDefinitionExport(examples, message = undefined) {
-  Joi.assert(examples, serviceDefinitionExport, message)
+function assertValidServiceDefinitionExport(openApiSpec, message = undefined) {
+  Joi.assert(openApiSpec, serviceDefinitionExport, message)
 }
 
 export {
