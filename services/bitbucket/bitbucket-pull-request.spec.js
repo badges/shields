988290d587e8c197061f94bd6f302dc3a5b893ae
jNullj <15849761+jNullj@users.noreply.github.com>
2024-02-22 23:08:37 +0200

    Refactor: use testAuth for BitbucketPullRequest

--- services/bitbucket/bitbucket-pull-request.spec.js
+++ services/bitbucket/bitbucket-pull-request.spec.js
@@ -1,73 +1,39 @@
-import { expect } from 'chai'
-import nock from 'nock'
-import { cleanUpNockAfterEach, defaultContext } from '../test-helpers.js'
-import { BitbucketRawPullRequests } from './bitbucket-pull-request.service.js'
+import { testAuth } from '../test-helpers.js'
+import {
+  BitbucketRawPullRequests,
+  BitbucketNonRawPullRequests,
+} from './bitbucket-pull-request.service.js'
 
-describe('BitbucketPullRequest', function () {
-  cleanUpNockAfterEach()
-
-  const user = 'admin'
-  const pass = 'password'
-
-  it('Sends auth headers to Bitbucket as configured', async function () {
-    const scope = nock('https://bitbucket.org/api/2.0/repositories/')
-      .get(/.*/)
-      .basicAuth({ user, pass })
-      .reply(200, { size: 42 })
-
-    expect(
-      await BitbucketRawPullRequests.invoke(
-        defaultContext,
-        {
-          public: {
-            services: {
-              bitbucketServer: {
-                authorizedOrigins: [],
-              },
-            },
-          },
-          private: { bitbucket_username: user, bitbucket_password: pass },
-        },
-        { user: 'shields-io', repo: 'test-repo' },
-      ),
-    ).to.deep.equal({
-      message: '42',
-      color: 'yellow',
+describe('BitbucketRawPullRequests', function () {
+  describe('auth', function () {
+    it('sends the auth information to Bitbucket cloud as configured', async function () {
+      return testAuth(
+        BitbucketRawPullRequests,
+        'BasicAuth',
+        { size: 42 },
+        { exampleOverride: { server: undefined } },
+      )
     })
 
-    scope.done()
+    it('sends the auth information to Bitbucket instence as configured', async function () {
+      return testAuth(BitbucketRawPullRequests, 'BasicAuth', { size: 42 })
+    })
   })
+})
 
-  it('Sends auth headers to Bitbucket Server as configured', async function () {
-    const scope = nock('https://bitbucket.example.test/rest/api/1.0/projects')
-      .get(/.*/)
-      .basicAuth({ user, pass })
-      .reply(200, { size: 42 })
-
-    expect(
-      await BitbucketRawPullRequests.invoke(
-        defaultContext,
-        {
-          public: {
-            services: {
-              bitbucketServer: {
-                authorizedOrigins: ['https://bitbucket.example.test'],
-              },
-            },
-          },
-          private: {
-            bitbucket_server_username: user,
-            bitbucket_server_password: pass,
-          },
-        },
-        { user: 'project', repo: 'repo' },
-        { server: 'https://bitbucket.example.test' },
-      ),
-    ).to.deep.equal({
-      message: '42',
-      color: 'yellow',
+describe('BitbucketNonRawPullRequests', function () {
+  describe('auth', function () {
+    it('sends the auth information to Bitbucket cloud as configured', async function () {
+      return testAuth(
+        BitbucketNonRawPullRequests,
+        'BasicAuth',
+        { size: 42 },
+        { exampleOverride: { server: undefined } },
+      )
     })
 
-    scope.done()
+    it('sends the auth information to Bitbucket instence as configured', async function () {
+      return testAuth(BitbucketNonRawPullRequests, 'BasicAuth', { size: 42 })
+    })
   })
 })
