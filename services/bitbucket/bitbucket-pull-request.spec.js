b9d51d1aba4c4e9db149a4e975ce51ddef4a6a05
jNullj <15849761+jNullj@users.noreply.github.com>
2024-02-23 22:35:43 +0200

    Fix BitbucketPullRequest tests using authOverride

--- services/bitbucket/bitbucket-pull-request.spec.js
+++ services/bitbucket/bitbucket-pull-request.spec.js
@@ -4,6 +4,36 @@ import {
   BitbucketNonRawPullRequests,
 } from './bitbucket-pull-request.service.js'
 
+const serverConfigOverride = {
+  public: {
+    services: {
+      bitbucketServer: {
+        authorizedOrigins: ['https://bitbucket.mydomain.net'],
+      },
+      bitbucket: {
+        authorizedOrigins: ['https://bitbucket.org'],
+      },
+    },
+  },
+  private: {
+    bitbucket_username: 'must-be-set-for-class-constructor',
+    bitbucket_password: 'must-be-set-for-class-constructor',
+  },
+}
+
+const cloudConfigOverride = {
+  public: {
+    services: {
+      bitbucket: {
+        authorizedOrigins: ['https://bitbucket.org'],
+      },
+      bitbucketServer: {
+        authorizedOrigins: [],
+      },
+    },
+  },
+}
+
 describe('BitbucketRawPullRequests', function () {
   describe('auth', function () {
     it('sends the auth information to Bitbucket cloud as configured', async function () {
@@ -11,12 +41,23 @@ describe('BitbucketRawPullRequests', function () {
         BitbucketRawPullRequests,
         'BasicAuth',
         { size: 42 },
-        { exampleOverride: { server: undefined } },
+        {
+          exampleOverride: { server: undefined },
+          configOverride: cloudConfigOverride,
+        },
       )
     })
 
     it('sends the auth information to Bitbucket instence as configured', async function () {
-      return testAuth(BitbucketRawPullRequests, 'BasicAuth', { size: 42 })
+      return testAuth(
+        BitbucketRawPullRequests,
+        'BasicAuth',
+        { size: 42 },
+        {
+          authOverride: BitbucketRawPullRequests.authServer,
+          configOverride: serverConfigOverride,
+        },
+      )
     })
   })
 })
@@ -28,12 +69,23 @@ describe('BitbucketNonRawPullRequests', function () {
         BitbucketNonRawPullRequests,
         'BasicAuth',
         { size: 42 },
-        { exampleOverride: { server: undefined } },
+        {
+          exampleOverride: { server: undefined },
+          configOverride: cloudConfigOverride,
+        },
       )
     })
 
     it('sends the auth information to Bitbucket instence as configured', async function () {
-      return testAuth(BitbucketNonRawPullRequests, 'BasicAuth', { size: 42 })
+      return testAuth(
+        BitbucketNonRawPullRequests,
+        'BasicAuth',
+        { size: 42 },
+        {
+          authOverride: BitbucketNonRawPullRequests.authServer,
+          configOverride: serverConfigOverride,
+        },
+      )
     })
   })
 })
