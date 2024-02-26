4778a3b71cf4f75456f473a861d282f6391924cf
Studio <studio@192.168.1.20>
2024-02-26 11:47:04 +0300

    add test for absent repo

--- services/github/github-created-at.tester.js
+++ services/github/github-created-at.tester.js
@@ -1,9 +1,14 @@
-import { createServiceTester } from '../tester.js'
 import { isFormattedDate } from '../test-validators.js'
+import { createServiceTester } from '../tester.js'
 
 export const t = await createServiceTester()
 
 t.create('created at').get('/erayerdin/firereact.json').expectBadge({
   label: 'created at',
   message: isFormattedDate,
+})
+
+t.create('created at').get('/erayerdin/not-a-valid-repo.json').expectBadge({
+  label: 'created at',
+  message: 'repo not found',
 })
