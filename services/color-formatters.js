ea9445b74e87abe2f693883255448b1400b0c30c
Eray Erdin (&mut self) <eraygezer.94@gmail.com>
2024-02-26 23:01:48 +0300

    [GithubCreatedAt] Add Created At Badge for Github (#9981)
    * init GithubCreatedAt service
    
    * render raw created_at
    
    * impl formatted date
    
    * add color
    
    * add test for absent repo
    
    * add `reversed` parameter to `age` color formatter
    
    * inverse the colors of the badge
    
    ---------
    
    Co-authored-by: Studio <studio@192.168.1.20>

--- services/color-formatters.js
+++ services/color-formatters.js
@@ -5,8 +5,8 @@
  * @module
  */
 
-import dayjs from 'dayjs'
 import pep440 from '@renovatebot/pep440'
+import dayjs from 'dayjs'
 
 /**
  * Determines the color used for a badge based on version.
@@ -175,24 +175,26 @@ function colorScale(steps, colors, reversed) {
 /**
  * Determines the color used for a badge according to the age.
  * Age is calculated as days elapsed till current date.
- * The color varies from bright green to red as the age increases.
+ * The color varies from bright green to red as the age increases
+ * or the other way around if `reverse` is given `true`.
  *
  * @param {string} date Date string
+ * @param {boolean} reversed Reverse the color scale a.k.a. the older, the better
  * @returns {string} Badge color
  */
-function age(date) {
-  const colorByAge = colorScale([7, 30, 180, 365, 730], undefined, true)
+function age(date, reversed = false) {
+  const colorByAge = colorScale([7, 30, 180, 365, 730], undefined, !reversed)
   const daysElapsed = dayjs().diff(dayjs(date), 'days')
   return colorByAge(daysElapsed)
 }
 
 export {
-  version,
-  pep440VersionColor,
-  downloadCount,
+  age,
+  colorScale,
   coveragePercentage,
+  downloadCount,
   floorCount,
   letterScore,
-  colorScale,
-  age,
+  pep440VersionColor,
+  version,
 }
