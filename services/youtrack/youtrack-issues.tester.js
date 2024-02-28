aedd31d71a14786b10195cb3f76a5189536e159c
Nigel George <nigel.george@shiroikuma.co.uk>
2024-02-28 22:05:43 +0900

    ci(url): update to use dedicated cloud hosted instance

--- services/youtrack/youtrack-issues.tester.js
+++ services/youtrack/youtrack-issues.tester.js
@@ -3,20 +3,22 @@ import { isMetricAllowNegative } from '../test-validators.js'
 
 export const t = await createServiceTester()
 
-t.create('Issues (IDEA)').get('/IDEA.json?query=%23Unresolved').expectBadge({
-  label: 'issues',
-  message: isMetricAllowNegative,
-})
+t.create('Issues (DEMO) (Cloud)')
+  .get('/DEMO.json?youtrack_url=https://shields.youtrack.cloud&query=%23Fixed')
+  .expectBadge({
+    label: 'issues',
+    message: isMetricAllowNegative,
+  })
 
-t.create('Issues (IDEA) (Invalid State)')
-  .get('/IDEA.json?query=%23ABCDEFG')
+t.create('Issues (DEMO) (Invalid State) (Cloud Hosted)')
+  .get('/DEMO.json?https://shields.youtrack.cloud&query=%23ABCDEFG')
   .expectBadge({
     label: 'issues',
     message: 'invalid',
   })
 
-t.create('Issues (DOESNOTEXIST) (Invalid Project)')
-  .get('/DOESNOTEXIST.json?query=%23Unresolved')
+t.create('Issues (DOESNOTEXIST) (Invalid Project) (Cloud Hosted)')
+  .get('/DOESNOTEXIST.json?https://shields.youtrack.cloud&query=%23Unresolved')
   .expectBadge({
     label: 'issues',
     message: 'invalid',
