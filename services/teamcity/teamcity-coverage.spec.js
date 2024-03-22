87c68e2bda5a733221261588be01090e124d5225
jNullj <15849761+jNullj@users.noreply.github.com>
2024-03-22 22:56:34 +0200

    Refactor TeamCity auth tests for use of testAuth

--- services/teamcity/teamcity-coverage.spec.js
+++ services/teamcity/teamcity-coverage.spec.js
@@ -1,44 +1,21 @@
-import { expect } from 'chai'
-import nock from 'nock'
-import { cleanUpNockAfterEach, defaultContext } from '../test-helpers.js'
+import { testAuth } from '../test-helpers.js'
 import TeamCityCoverage from './teamcity-coverage.service.js'
-import { user, pass, host, config } from './teamcity-test-helpers.js'
+import { config } from './teamcity-test-helpers.js'
 
 describe('TeamCityCoverage', function () {
-  cleanUpNockAfterEach()
-
-  it('sends the auth information as configured', async function () {
-    const scope = nock(`https://${host}`)
-      .get(
-        `/app/rest/builds/${encodeURIComponent(
-          'buildType:(id:bt678)',
-        )}/statistics`,
-      )
-      .query({})
-      // This ensures that the expected credentials are actually being sent with the HTTP request.
-      // Without this the request wouldn't match and the test would fail.
-      .basicAuth({ user, pass })
-      .reply(200, {
-        property: [
-          { name: 'CodeCoverageAbsSCovered', value: '82' },
-          { name: 'CodeCoverageAbsSTotal', value: '100' },
-        ],
-      })
-
-    expect(
-      await TeamCityCoverage.invoke(
-        defaultContext,
-        config,
+  describe('auth', function () {
+    it('sends the auth information as configured', async function () {
+      testAuth(
+        TeamCityCoverage,
+        'BasicAuth',
         {
-          buildId: 'bt678',
+          property: [
+            { name: 'CodeCoverageAbsSCovered', value: '93' },
+            { name: 'CodeCoverageAbsSTotal', value: '95' },
+          ],
         },
-        { server: 'https://mycompany.teamcity.com' },
-      ),
-    ).to.deep.equal({
-      message: '82%',
-      color: 'yellowgreen',
+        { configOverride: config },
+      )
     })
-
-    scope.done()
   })
 })
