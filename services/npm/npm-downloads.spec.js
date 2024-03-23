df8049a7659b8cba84070f09e8f5cd3d70f75a0c
chris48s <chris48s@users.noreply.github.com>
2024-03-23 19:38:05 +0000

    redirect [npm] /dt to /d18m (#10033)
    * redirect [npm] /dt to /d18m
    
    * fix unit test

--- services/npm/npm-downloads.spec.js
+++ services/npm/npm-downloads.spec.js
@@ -2,7 +2,7 @@ import { test, given } from 'sazerac'
 import NpmDownloads from './npm-downloads.service.js'
 
 describe('NpmDownloads', function () {
-  test(NpmDownloads._intervalMap.dt.transform, () => {
+  test(NpmDownloads._intervalMap.d18m.transform, () => {
     given({
       downloads: [
         { downloads: 2, day: '2018-01-01' },
@@ -13,7 +13,7 @@ describe('NpmDownloads', function () {
 
   test(NpmDownloads.render, () => {
     given({
-      interval: 'dt',
+      interval: 'd18m',
       downloadCount: 0,
     }).expect({
       color: 'red',
