55783b066ee4be1392eec5732e95d8d3ae20b2e7
Pierre Janineh <janinehpierre@gmail.com>
2024-04-09 11:59:58 +0300

    Created tests file

---
+++ services/npm/npm-latest-package-update.spec.js
@@ -0,0 +1,16 @@
+import { ServiceTester } from '../tester.js'
+
+export const t = new ServiceTester({
+  id: 'npm-latest-package-update',
+  title: 'NPM Latest Package update Tests',
+})
+
+/*
+>> TODO: Create tests
+
+t.create('NPM Latest Package update')
+
+t.create('NPM Latest Package update (negative)')
+
+t.create('NPM Latest Package update - Invalid')
+ */
