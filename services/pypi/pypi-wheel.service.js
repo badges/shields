e8671be7f22d5477fef006b354d7a2b963eea6cf
Leo Q <LeoQuote@users.noreply.github.com>
2024-04-22 00:33:42 +0800

    support setting pypiBaseUrl by environment variables and queryParameters; affects [pypi] (#10044)
    * support setting pypiBaseUrl by environment variables
    
    * Add support for pypiBaseUrl configuration
    
    * Update Pypi services to include pypiBaseUrl parameter
    
    * change package name example to a more well-known package
    
    * Update custom-environment-variables.yml
    
    * Update Pypi services to include pypiBaseUrl parameter
    
    * fix openapi mismatch
    
    * Update doc/server-secrets.md
    
    ---------
    
    Co-authored-by: chris48s <chris48s@users.noreply.github.com>

--- services/pypi/pypi-wheel.service.js
+++ services/pypi/pypi-wheel.service.js
@@ -1,5 +1,4 @@
-import { pathParams } from '../index.js'
-import PypiBase from './pypi-base.js'
+import PypiBase, { pypiGeneralParams } from './pypi-base.js'
 import { getPackageFormats } from './pypi-helpers.js'
 
 export default class PypiWheel extends PypiBase {
@@ -11,10 +10,7 @@ export default class PypiWheel extends PypiBase {
     '/pypi/wheel/{packageName}': {
       get: {
         summary: 'PyPI - Wheel',
-        parameters: pathParams({
-          name: 'packageName',
-          example: 'Django',
-        }),
+        parameters: pypiGeneralParams,
       },
     },
   }
@@ -35,8 +31,8 @@ export default class PypiWheel extends PypiBase {
     }
   }
 
-  async handle({ egg }) {
-    const packageData = await this.fetch({ egg })
+  async handle({ egg }, { pypiBaseUrl }) {
+    const packageData = await this.fetch({ egg, pypiBaseUrl })
     const { hasWheel } = getPackageFormats(packageData)
     return this.constructor.render({ hasWheel })
   }
