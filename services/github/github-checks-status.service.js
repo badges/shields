8cc39cd14cc87630f51336c0388b0767f6e2a51a
Marc Bernard <marc@marcbernardtools.com>
2024-05-25 19:35:42 +0200

    [GitHub] update description of commit status badge

--- services/github/github-checks-status.service.js
+++ services/github/github-checks-status.service.js
@@ -2,7 +2,20 @@ import Joi from 'joi'
 import { pathParams } from '../index.js'
 import { isBuildStatus, renderBuildStatusBadge } from '../build-status.js'
 import { GithubAuthV3Service } from './github-auth-service.js'
-import { documentation, httpErrorsFor } from './github-helpers.js'
+import {
+  documentation as commonDocumentation,
+  httpErrorsFor,
+} from './github-helpers.js'
+
+const description = `
+Displays the status of a (tag|commit|branch), as reported by the Commit Status API.
+Nowadays, GitHub Actions and many third party integrations report state via the
+Checks API. If this badge does not show expected values, please try out our
+corresponding Check Runs badge instead. You can read more about status checks in
+the [GitHub documentation](https://docs.github.com/en/pull-requests/collaborating-with-pull-requests/collaborating-on-repositories-with-code-quality-features/about-status-checks).
+
+${commonDocumentation}
+`
 
 const schema = Joi.object({
   state: isBuildStatus,
@@ -16,24 +29,40 @@ export default class GithubChecksStatus extends GithubAuthV3Service {
   }
 
   static openApi = {
-    '/github/checks-status/{user}/{repo}/{ref}': {
+    '/github/checks-status/{user}/{repo}/{branch}': {
       get: {
-        summary: 'GitHub tag checks state',
-        description: documentation,
-        parameters: pathParams(
-          {
-            name: 'user',
-            example: 'badges',
-          },
-          {
-            name: 'repo',
-            example: 'shields',
-          },
-          {
-            name: 'ref',
-            example: '3.3.0',
-          },
-        ),
+        summary: 'GitHub branch status',
+        description,
+        parameters: [
+          pathParam({ name: 'user', example: 'badges' }),
+          pathParam({ name: 'repo', example: 'shields' }),
+          pathParam({ name: 'branch', example: 'master' }),
+        ],
+      },
+    },
+    '/github/checks-status/{user}/{repo}/{commit}': {
+      get: {
+        summary: 'GitHub commit status',
+        description,
+        parameters: [
+          pathParam({ name: 'user', example: 'badges' }),
+          pathParam({ name: 'repo', example: 'shields' }),
+          pathParam({
+            name: 'commit',
+            example: '91b108d4b7359b2f8794a4614c11cb1157dc9fff',
+          }),
+        ],
+      },
+    },
+    '/github/checks-status/{user}/{repo}/{tag}': {
+      get: {
+        summary: 'GitHub tag status',
+        description,
+        parameters: [
+          pathParam({ name: 'user', example: 'badges' }),
+          pathParam({ name: 'repo', example: 'shields' }),
+          pathParam({ name: 'tag', example: '3.3.0' }),
+        ],
       },
     },
   }
