79f0c8d9ed6aacab1af6e35219aa0cbcab4e4596
chris48s <git@chris-shaw.dev>
2024-06-24 16:09:47 +0100

    fix npm badges when `maintainers` not in response

--- services/npm/npm-collaborators.tester.js
+++ services/npm/npm-collaborators.tester.js
@@ -16,3 +16,12 @@ t.create('contributor count for unknown package')
     label: 'npm collaborators',
     message: 'package not found',
   })
+
+t.create('contributor count for package package without a maintainers property')
+  .get('/package-without-maintainers.json')
+  .intercept(nock =>
+    nock('https://registry.npmjs.org')
+      .get('/package-without-maintainers/latest')
+      .reply(200, {}),
+  )
+  .expectBadge({ label: 'npm collaborators', message: '0' })
