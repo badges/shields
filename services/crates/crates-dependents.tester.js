ebd25f50cee6e9b64fc5e905f73f2f2e8db3030c
Eray Erdin <eraygezer.94@gmail.com>
2024-08-04 19:03:44 +0300

    remove redundant timeout

--- services/crates/crates-dependents.tester.js
+++ services/crates/crates-dependents.tester.js
@@ -2,13 +2,12 @@ import { isMetric } from '../test-validators.js'
 import { createServiceTester } from '../tester.js'
 export const t = await createServiceTester()
 
-t.create('dependent count').timeout(10000).get('/tokio.json').expectBadge({
+t.create('dependent count').get('/tokio.json').expectBadge({
   label: 'dependents',
   message: isMetric,
 })
 
 t.create('dependent count (nonexistent package)')
-  .timeout(10000)
   .get('/foobar-is-not-crate.json')
   .expectBadge({
     label: 'crates.io',
