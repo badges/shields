864897de8623457afc061f1dd37fe7151a4d52e3
Tapaj Kumar Das <tapaj@tapajkumardas.dev>
2024-10-10 22:50:23 +0530

    fix: updates test cases

--- services/homebrew/homebrew-cask-downloads.tester.js
+++ services/homebrew/homebrew-cask-downloads.tester.js
@@ -4,25 +4,25 @@ import { isMetricOverTimePeriod } from '../test-validators.js'
 export const t = await createServiceTester()
 
 t.create('daily downloads (valid)')
-  .get('dm/freetube.json')
+  .get('/dm/freetube.json')
   .expectBadge({ label: 'downloads', message: isMetricOverTimePeriod })
 
 t.create('yearly downloads (valid)')
-  .get('dq/freetube.json')
+  .get('/dq/freetube.json')
   .expectBadge({ label: 'downloads', message: isMetricOverTimePeriod })
 
 t.create('yearly downloads (valid)')
-  .get('dy/freetube.json')
+  .get('/dy/freetube.json')
   .expectBadge({ label: 'downloads', message: isMetricOverTimePeriod })
 
 t.create('daily downloads (not found)')
-  .get('dm/not-a-package.json')
+  .get('/dm/not-a-package.json')
   .expectBadge({ label: 'downloads', message: 'formula not found' })
 
 t.create('yearly downloads (not found)')
-  .get('dq/not-a-package.json')
+  .get('/dq/not-a-package.json')
   .expectBadge({ label: 'downloads', message: 'formula not found' })
 
 t.create('yearly downloads (not found)')
-  .get('dy/not-a-package.json')
+  .get('/dy/not-a-package.json')
   .expectBadge({ label: 'downloads', message: 'formula not found' })
