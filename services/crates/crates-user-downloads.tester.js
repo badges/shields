7e649118604d181c330ce0e13caa3401cabfc9a0
jNullj <15849761+jNullj@users.noreply.github.com>
2024-10-20 00:35:39 +0300

    fixed typo

--- services/crates/crates-user-downloads.tester.js
+++ services/crates/crates-user-downloads.tester.js
@@ -6,7 +6,7 @@ t.create('total user downloads')
   .get('/udt/3027.json')
   .expectBadge({ label: 'downloads', message: isMetric })
 
-// non-existent user returns 0 downloads with 200 OK rather then 404.
+// non-existent user returns 0 downloads with 200 OK status code rather than 404.
 t.create('total user downloads (user not found)')
   .get('/udt/2147483647.json') // 2147483647 is the maximum valid user id as API uses i32
   .expectBadge({ label: 'downloads', message: '0' })
