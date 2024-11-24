7a13ca806aa44894c299495ef75ed0da8f5392d1
chris48s <git@chris-shaw.dev>
2024-11-24 15:50:47 +0000

    chromewebstore: reformat size string, test against isIecFileSize

--- services/chrome-web-store/chrome-web-store-size.tester.js
+++ services/chrome-web-store/chrome-web-store-size.tester.js
@@ -1,11 +1,11 @@
 import { createServiceTester } from '../tester.js'
+import { isIecFileSize } from '../test-validators.js'
 
 export const t = await createServiceTester()
-const isFileSize = /^\d+(\.\d+)?(MiB|KiB)$/
 
 t.create('Size').get('/nccfelhkfpbnefflolffkclhenplhiab.json').expectBadge({
   label: 'extension size',
-  message: isFileSize,
+  message: isIecFileSize,
 })
 
 t.create('Size (not found)')
