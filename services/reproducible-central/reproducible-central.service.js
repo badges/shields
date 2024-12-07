4a167d7ce025529e60666d79e7948ce6c91490dd
Hervé Boutemy <hboutemy@apache.org>
2024-12-07 20:27:40 +0900

    improve code with provided snippets: thank you

--- services/reproducible-central/reproducible-central.service.js
+++ services/reproducible-central/reproducible-central.service.js
@@ -1,7 +1,9 @@
 import Joi from 'joi'
 import { BaseJsonService, pathParams } from '../index.js'
 
-const schema = Joi.object().pattern(Joi.string(), Joi.string()).required()
+const schema = Joi.object()
+  .pattern(Joi.string(), Joi.string().regex(/^\d+\/\d+$|^[X?]$/))
+  .required()
 
 export default class ReproducibleCentral extends BaseJsonService {
   static category = 'dependencies'
@@ -37,33 +39,25 @@ export default class ReproducibleCentral extends BaseJsonService {
     label: 'reproducible builds',
   }
 
-  static render({ message, color }) {
-    if (message === undefined) {
-      color = 'red'
-      message = 'version not available in Maven Central'
-    } else if (message === 'X') {
-      color = 'red'
-      message = 'unable to rebuild'
-    } else if (message === '?') {
-      color = 'grey'
-      message = 'version not evaluated'
-    } else if (message.indexOf('/') > 0) {
-      // {ok}/{count}
-      const ok = message.substring(0, message.indexOf('/'))
-      const count = message.substring(message.indexOf('/') + 1)
-      if (ok === count) {
-        color = 'brightgreen'
-      } else if (ok > count - ok) {
-        color = 'yellow'
-      } else {
-        color = 'red'
-      }
+  static render(rebuildResult) {
+    if (rebuildResult === undefined) {
+      return { color: 'red', message: 'version not available in Maven Central' }
+    } else if (rebuildResult === 'X') {
+      return { color: 'red', message: 'unable to rebuild' }
+    } else if (rebuildResult === '?') {
+      return { color: 'grey', message: 'version not evaluated' }
     }
 
-    return {
-      message,
-      color,
+    const [ok, count] = rebuildResult.split('/')
+    let color
+    if (ok === count) {
+      color = 'brightgreen'
+    } else if (ok > count - ok) {
+      color = 'yellow'
+    } else {
+      color = 'red'
     }
+    return { color, message: rebuildResult }
   }
 
   async fetch({ groupId, artifactId }) {
@@ -78,16 +72,16 @@ export default class ReproducibleCentral extends BaseJsonService {
 
   async handle({ groupId, artifactId, version }) {
     if (version.endsWith('-SNAPSHOT')) {
-      return this.constructor.render({
+      return {
         message: 'SNAPSHOT, not evaluated',
         color: 'grey',
-      })
+      }
     }
 
     const versions = await this.fetch({
       groupId,
       artifactId,
     })
-    return this.constructor.render({ message: versions[version] })
+    return this.constructor.render(versions[version])
   }
 }
