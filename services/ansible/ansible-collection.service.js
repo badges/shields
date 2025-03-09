336f0d2341573ae23ddb041c4a7475d523efb5af
Ari Kalfus <dev@artis3nal.com>
2025-03-09 19:05:28 +0000

    feat: add ansible collection v2 badge for latest version

--- services/ansible/ansible-collection.service.js
+++ services/ansible/ansible-collection.service.js
@@ -1,6 +1,19 @@
-import { deprecatedService } from '../index.js'
+import Joi from 'joi'
+// TODO: add download count class
+// import { renderDownloadsBadge } from '../downloads.js'
+import { renderVersionBadge } from '../version.js'
+import { BaseJsonService, deprecatedService, pathParams } from '../index.js'
 
-export const AnsibleGalaxyCollectionName = deprecatedService({
+const ansibleCollectionSchema = Joi.object({
+  deprecated: Joi.boolean().required(),
+  highest_version: Joi.object({
+    version: Joi.string().required(),
+  }).required(),
+  // Ansible docs don't mention this but it appears in the API responses
+  download_count: Joi.number().required(),
+}).required()
+
+const AnsibleGalaxyCollectionName = deprecatedService({
   category: 'other',
   route: {
     base: 'ansible/collection',
@@ -9,3 +22,40 @@ export const AnsibleGalaxyCollectionName = deprecatedService({
   label: 'collection',
   dateAdded: new Date('2023-10-10'),
 })
+
+class AnsibleGalaxyCollectionVersion extends BaseJsonService {
+  static category = 'downloads'
+  static route = { base: 'ansible/collection/v', pattern: ':namespace/:name' }
+
+  static openApi = {
+    '/ansible/collection/v/{namespace}/{name}': {
+      get: {
+        summary: 'Ansible Collection',
+        parameters: pathParams(
+          { name: 'namespace', example: 'community' },
+          { name: 'name', example: 'general' },
+        ),
+      },
+    },
+  }
+
+  // Use the collection namespace & name as default label, which aren't statically known
+  static defaultBadgeData = {}
+
+  async fetch({ namespace, name }) {
+    return this._requestJson({
+      schema: ansibleCollectionSchema,
+      url: `https://galaxy.ansible.com/api/v3/plugin/ansible/content/published/collections/index/${namespace}/${name}/`,
+    })
+  }
+
+  async handle({ namespace, name }) {
+    const { highest_version } = await this.fetch({ namespace, name })
+    return renderVersionBadge({
+      defaultLabel: `${namespace}.${name}`,
+      version: highest_version.version, // eslint-disable-line camelcase
+    })
+  }
+}
+
+export { AnsibleGalaxyCollectionName, AnsibleGalaxyCollectionVersion }
--- services/ansible/ansible-collection.tester.js
+++ services/ansible/ansible-collection.tester.js
@@ -1,10 +1,26 @@
 import { ServiceTester } from '../tester.js'
+import { isSemver } from '../test-validators.js'
+
 export const t = new ServiceTester({
   id: 'AnsibleGalaxyCollectionName',
   title: 'AnsibleGalaxyCollectionName',
   pathPrefix: '/ansible/collection',
 })
 
+export const t2 = new ServiceTester({
+  id: 'AnsibleGalaxyCollectionVersion',
+  title: 'AnsibleGalaxyCollectionVersion',
+  pathPrefix: '/ansible/collection/v',
+})
+
 t.create('collection name')
   .get('/278.json')
   .expectBadge({ label: 'collection', message: 'no longer available' })
+
+t2.create('collection version (valid)')
+  .get('/community/general')
+  .expectBadge({ label: 'community.general', message: isSemver })
+
+t2.create('collection version (not found)')
+  .get('/not/real')
+  .expectBadge({ label: 'not.real', message: 'not found' })
