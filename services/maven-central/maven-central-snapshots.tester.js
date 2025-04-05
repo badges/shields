732efeac726c2f0474a74f5c35cc069ff8f65258
Doc <nachito94@msn.com>
2025-04-05 13:05:56 -0300

    remove test version for snapshots
    maven central remove old snapshots then this test can cause issues if try to check a removed version

--- services/maven-central/maven-central-snapshots.tester.js
+++ services/maven-central/maven-central-snapshots.tester.js
@@ -8,11 +8,3 @@ t.create('latest version redirection')
       'https://central.sonatype.com/repository/maven-snapshots/me/mrdoc/minecraft/dlibcustomextensions/maven-metadata.xml',
     )}`,
   )
-
-t.create('latest 1.0 version redirection')
-  .get('/me.mrdoc.minecraft/dlibcustomextensions/1.0.json') // https://central.sonatype.com/repository/maven-snapshots/me/mrdoc/minecraft/dlibcustomextensions/
-  .expectRedirect(
-    `/maven-metadata/v.json?label=maven-central-snapshot&metadataUrl=${encodeURIComponent(
-      'https://central.sonatype.com/repository/maven-snapshots/me/mrdoc/minecraft/dlibcustomextensions/maven-metadata.xml',
-    )}&versionPrefix=1.0`,
-  )
