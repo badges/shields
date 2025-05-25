5ca42d0508f2c39ea44afc9f9f2e47a6601f58c7
DCjanus <DCjanus@dcjanus.com>
2025-05-25 09:20:32 +0800

    chore(deps-rs): split crate version route

--- services/deps-rs/deps-rs-crate.service.js
+++ services/deps-rs/deps-rs-crate.service.js
@@ -6,9 +6,20 @@ export default class DepsRsCrate extends BaseDepsRsService {
   static route = { base: 'deps-rs', pattern: ':crate/:version' }
 
   static openApi = {
+    '/deps-rs/{crate}/latest': {
+      get: {
+        summary: 'Deps.rs Crate Dependencies (latest)',
+        description,
+        parameters: pathParams({
+          name: 'crate',
+          example: 'syn',
+          description: 'The name of the Rust crate',
+        }),
+      },
+    },
     '/deps-rs/{crate}/{version}': {
       get: {
-        summary: 'Deps.rs Crate Dependencies',
+        summary: 'Deps.rs Crate Dependencies (specific version)',
         description,
         parameters: pathParams(
           {
@@ -19,7 +30,7 @@ export default class DepsRsCrate extends BaseDepsRsService {
           {
             name: 'version',
             example: '2.0.101',
-            description: 'Version number or "latest"',
+            description: 'Version number',
           },
         ),
       },
