9f57ae87455407565045d49a3267cedce20ae702
LitoMore <LitoMore@users.noreply.github.com>
2025-08-04 02:54:49 +0800

    chore: use the modern labels API for draft release action (#11259)

--- .github/actions/draft-release/entrypoint.sh
+++ .github/actions/draft-release/entrypoint.sh
@@ -59,7 +59,10 @@ PR_RESP=$(curl https://api.github.com/repos/"$REPO_NAME"/pulls \
 
 # Add the 'release' label to the PR
 PR_API_URL=$(echo "$PR_RESP" | jq -r ._links.issue.href)
-curl "$PR_API_URL" \
-    -X POST \
-    -H "Authorization: token $GITHUB_TOKEN" \
-    --data '{"labels":["release"]}'
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer $GITHUB_TOKEN" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "$PR_API_URL/labels" \
+  -d '{"labels":["release"]}'
