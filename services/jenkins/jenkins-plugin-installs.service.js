b161d249df21d73c3ded0f9185770a55bdf4fdb9
Pierre-Yves Bigourdan <10694593+PyvesB@users.noreply.github.com>
2025-09-20 14:54:34 +0200

    Deprecate [JenkinsPluginInstalls] per version (#10747)
    * Deprecate [JenkinsPluginInstalls] per version
    
    * Add missing comma

--- services/jenkins/jenkins-plugin-installs.service.js
+++ services/jenkins/jenkins-plugin-installs.service.js
@@ -1,22 +1,13 @@
 import Joi from 'joi'
 import { renderDownloadsBadge } from '../downloads.js'
 import { nonNegativeInteger } from '../validators.js'
-import { BaseJsonService, NotFound, pathParams } from '../index.js'
+import { BaseJsonService, pathParams } from '../index.js'
 
 const schemaInstallations = Joi.object()
   .keys({
     installations: Joi.object()
       .required()
       .pattern(nonNegativeInteger, nonNegativeInteger)
-      .min(1),
-  })
-  .required()
-
-const schemaInstallationsPerVersion = Joi.object()
-  .keys({
-    installationsPerVersion: Joi.object()
-      .required()
-      .pattern(Joi.string(), nonNegativeInteger)
       .min(1),
   })
   .required()
@@ -39,61 +30,38 @@ export default class JenkinsPluginInstalls extends BaseJsonService {
         }),
       },
     },
-    '/jenkins/plugin/i/{plugin}/{version}': {
-      get: {
-        summary: 'Jenkins Plugin installs (version)',
-        parameters: pathParams(
-          {
-            name: 'plugin',
-            example: 'view-job-filters',
-          },
-          {
-            name: 'version',
-            example: '1.26',
-          },
-        ),
-      },
-    },
   }
 
   static defaultBadgeData = { label: 'installs' }
 
-  static render({ installs: downloads, version }) {
-    return renderDownloadsBadge({
-      downloads,
-      versionedLabelPrefix: 'installs',
-      version,
-    })
+  static render({ installs: downloads }) {
+    return renderDownloadsBadge({ downloads })
   }
 
-  async fetch({ plugin, version }) {
+  async fetch({ plugin }) {
     return this._requestJson({
       url: `https://stats.jenkins.io/plugin-installation-trend/${plugin}.stats.json`,
-      schema: version ? schemaInstallationsPerVersion : schemaInstallations,
+      schema: schemaInstallations,
       httpErrors: {
         404: 'plugin not found',
       },
     })
   }
 
-  static transform({ json, version }) {
-    if (!version) {
-      const latestDate = Object.keys(json.installations).sort().slice(-1)[0]
-      return { installs: json.installations[latestDate] }
-    }
-
-    const installs = json.installationsPerVersion[version]
-    if (!installs) {
-      throw new NotFound({
-        prettyMessage: 'version not found',
-      })
-    }
-    return { installs }
+  static transform({ json }) {
+    const latestDate = Object.keys(json.installations).sort().slice(-1)[0]
+    return { installs: json.installations[latestDate] }
   }
 
   async handle({ plugin, version }) {
-    const json = await this.fetch({ plugin, version })
-    const { installs } = this.constructor.transform({ json, version })
-    return this.constructor.render({ installs, version })
+    if (version) {
+      return {
+        message: 'no longer available per version',
+        color: 'lightgrey',
+      }
+    }
+    const json = await this.fetch({ plugin })
+    const { installs } = this.constructor.transform({ json })
+    return this.constructor.render({ installs })
   }
 }
