3f01b0fae06f39d35daa97842a6af152daf7fc67
jNullj <15849761+jNullj@users.noreply.github.com>
2025-09-23 09:51:34 +0000

    update GithubWorkflowStatus deprecation label
    comply with new doc requirement for deprecatoin label showing service name.
    
    Fixes deprecation label compliance with updated docs
    Fixes #11368

--- services/github/github-workflow-status.tester.js
+++ services/github/github-workflow-status.tester.js
@@ -9,34 +9,34 @@ export const t = new ServiceTester({
 t.create('no longer available (previously nonexistent repo)')
   .get('/badges/shields-fakeness/fake.json')
   .expectBadge({
-    label: 'build',
+    label: 'githubworkflowstatus',
     message: 'https://github.com/badges/shields/issues/8671',
   })
 
 t.create('no longer available (previously nonexistent workflow)')
   .get('/actions/toolkit/not-a-real-workflow.json')
   .expectBadge({
-    label: 'build',
+    label: 'githubworkflowstatus',
     message: 'https://github.com/badges/shields/issues/8671',
   })
 
 t.create('no longer available (previously valid workflow)')
   .get('/actions/toolkit/toolkit-unit-tests.json')
   .expectBadge({
-    label: 'build',
+    label: 'githubworkflowstatus',
     message: 'https://github.com/badges/shields/issues/8671',
   })
 
 t.create('no longer available (previously valid workflow - branch)')
   .get('/actions/toolkit/toolkit-unit-tests/master.json')
   .expectBadge({
-    label: 'build',
+    label: 'githubworkflowstatus',
     message: 'https://github.com/badges/shields/issues/8671',
   })
 
 t.create('no longer available (previously valid workflow - event)')
   .get('/actions/toolkit/toolkit-unit-tests.json?event=push')
   .expectBadge({
-    label: 'build',
+    label: 'githubworkflowstatus',
     message: 'https://github.com/badges/shields/issues/8671',
   })
