4360f89d5d805cebfdeb415344424fe942b4241e
Pierre-Yves Bigourdan <10694593+PyvesB@users.noreply.github.com>
2025-11-11 22:43:48 +0100

    Fix [GreasyForkInstalls] tests (#11493)

--- services/greasyfork/greasyfork-downloads.service.js
+++ services/greasyfork/greasyfork-downloads.service.js
@@ -19,7 +19,7 @@ export default class GreasyForkInstalls extends BaseGreasyForkService {
           },
           {
             name: 'scriptId',
-            example: '407466',
+            example: '545651',
           },
         ),
       },
