7866b6de1e95a4ee35b6c23dc57b1dcbea968a57
Pierre-Yves Bigourdan <10694593+PyvesB@users.noreply.github.com>
2025-11-15 12:17:03 +0100

    Deprecate [DockerCloud] badges

--- services/docker/docker-cloud-automated.service.js
+++ services/docker/docker-cloud-automated.service.js
@@ -1,57 +1,11 @@
-import { BaseJsonService, pathParams } from '../index.js'
-import { dockerBlue, buildDockerUrl } from './docker-helpers.js'
-import { fetchBuild } from './docker-cloud-common-fetch.js'
+import { deprecatedService } from '../index.js'
 
-export default class DockerCloudAutomatedBuild extends BaseJsonService {
-  static category = 'build'
-  static route = buildDockerUrl('cloud/automated')
-
-  static auth = {
-    userKey: 'dockerhub_username',
-    passKey: 'dockerhub_pat',
-    authorizedOrigins: ['https://hub.docker.com', 'https://cloud.docker.com'],
-    isRequired: false,
-  }
-
-  static openApi = {
-    '/docker/cloud/automated/{user}/{repo}': {
-      get: {
-        summary: 'Docker Cloud Automated build',
-        parameters: pathParams(
-          {
-            name: 'user',
-            example: 'jrottenberg',
-          },
-          {
-            name: 'repo',
-            example: 'ffmpeg',
-          },
-        ),
-      },
-    },
-  }
-
-  static _cacheLength = 14400
-
-  static defaultBadgeData = { label: 'docker build' }
-
-  static render({ buildSettings }) {
-    if (buildSettings.length >= 1) {
-      return { message: 'automated', color: dockerBlue }
-    }
-    return { message: 'manual', color: 'yellow' }
-  }
-
-  async handle({ user, repo }) {
-    const data = await fetchBuild(this, { user, repo })
-
-    if (data.objects.length === 0) {
-      return this.constructor.render({
-        buildSettings: [],
-      })
-    }
-    return this.constructor.render({
-      buildSettings: data.objects[0].build_settings,
-    })
-  }
-}
+export default deprecatedService({
+  category: 'build',
+  route: {
+    base: 'docker/cloud/automated',
+    pattern: ':user/:repo',
+  },
+  label: 'dockercloud',
+  dateAdded: new Date('2025-11-15'),
+})
--- services/docker/docker-cloud-build.tester.js
+++ services/docker/docker-cloud-build.tester.js
@@ -1,75 +1,12 @@
-import { isBuildStatus } from '../build-status.js'
-import { createServiceTester } from '../tester.js'
-import { dockerBlue } from './docker-helpers.js'
-export const t = await createServiceTester()
+import { ServiceTester } from '../tester.js'
 
-t.create('docker cloud build status (valid user)')
-  .get('/pavics/magpie.json')
-  .expectBadge({
-    label: 'docker build',
-    message: isBuildStatus,
-  })
+export const t = new ServiceTester({
+  id: 'dockercloudbuild',
+  title: 'DockerCloudBuild',
+  pathPrefix: '/docker/cloud/build',
+})
 
-t.create('docker cloud build status (invalid, nonexisting user)')
-  .get('/pavicsssss/magpie.json')
-  .expectBadge({
-    label: 'docker build',
-    message: 'automated builds not set up',
-  })
-
-t.create(
-  "docker cloud build status (valid user, but the 'objects' array from the response is empty)",
-)
-  .get('/pavics/weaver.json')
-  .expectBadge({
-    label: 'docker build',
-    message: 'automated builds not set up',
-  })
-
-t.create('docker cloud build status (not found)')
-  .get('/badges/not-a-real-repo.json')
-  .intercept(nock =>
-    nock('https://cloud.docker.com/')
-      .get(
-        `/api/build/v1/source?image=${encodeURIComponent(
-          'badges/not-a-real-repo',
-        )}`,
-      )
-      .reply(404, { detail: 'Object not found' }),
-  )
-  .expectBadge({ label: 'docker build', message: 'repo not found' })
-
-t.create('docker cloud build status (passing)')
-  .get('/xenolf/lego.json')
-  .intercept(nock =>
-    nock('https://cloud.docker.com/')
-      .get(`/api/build/v1/source?image=${encodeURIComponent('xenolf/lego')}`)
-      .reply(200, { objects: [{ state: 'Success' }] }),
-  )
-  .expectBadge({
-    label: 'docker build',
-    message: 'passing',
-    color: 'brightgreen',
-  })
-
-t.create('docker cloud build status (failing)')
-  .get('/xenolf/lego.json')
-  .intercept(nock =>
-    nock('https://cloud.docker.com/')
-      .get(`/api/build/v1/source?image=${encodeURIComponent('xenolf/lego')}`)
-      .reply(200, { objects: [{ state: 'Failed' }] }),
-  )
-  .expectBadge({ label: 'docker build', message: 'failing', color: 'red' })
-
-t.create('docker cloud build status (building)')
-  .get('/xenolf/lego.json')
-  .intercept(nock =>
-    nock('https://cloud.docker.com/')
-      .get(`/api/build/v1/source?image=${encodeURIComponent('xenolf/lego')}`)
-      .reply(200, { objects: [{ state: 'Empty' }] }),
-  )
-  .expectBadge({
-    label: 'docker build',
-    message: 'building',
-    color: `#${dockerBlue}`,
-  })
+t.create('docker cloud build status').get('/pavics/magpie.json').expectBadge({
+  label: 'dockercloud',
+  message: 'no longer available',
+})
