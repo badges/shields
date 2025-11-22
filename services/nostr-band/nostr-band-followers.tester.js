a21964ddbae5594775ca3a5dfbe3ec8233eec808
Pierre-Yves Bigourdan <10694593+PyvesB@users.noreply.github.com>
2025-11-22 12:10:05 +0100

    Deprecate [NostrBand] bagdes (#11519)

--- services/nostr-band/nostr-band-followers.tester.js
+++ services/nostr-band/nostr-band-followers.tester.js
@@ -1,19 +1,15 @@
-import { isMetric } from '../test-validators.js'
-import { createServiceTester } from '../tester.js'
-export const t = await createServiceTester()
+import { ServiceTester } from '../tester.js'
 
-t.create('fetch: valid npub')
-  .timeout(15000)
-  .get('/npub18c556t7n8xa3df2q82rwxejfglw5przds7sqvefylzjh8tjne28qld0we7.json')
-  .expectBadge({
-    label: 'followers',
-    message: isMetric,
-  })
+export const t = new ServiceTester({
+  id: 'nostrband',
+  title: 'Nostr.band',
+})
 
-t.create('fetch: invalid npub')
-  .timeout(15000)
-  .get('/invalidnpub.json')
+t.create('followers')
+  .get(
+    '/followers/npub18c556t7n8xa3df2q82rwxejfglw5przds7sqvefylzjh8tjne28qld0we7.json',
+  )
   .expectBadge({
-    label: 'followers',
-    message: 'invalid pubkey',
+    label: 'nostrband',
+    message: 'no longer available',
   })
