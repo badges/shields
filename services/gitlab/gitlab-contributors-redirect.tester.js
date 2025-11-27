aafb54c4df1329b7e0fc3a20118bd738326edd34
Pierre-Yves Bigourdan <10694593+PyvesB@users.noreply.github.com>
2025-11-27 09:09:30 +0100

    Leverage expectRedirect in all redirector tests (#11525)

--- services/gitlab/gitlab-contributors-redirect.tester.js
+++ services/gitlab/gitlab-contributors-redirect.tester.js
@@ -2,8 +2,5 @@ import { createServiceTester } from '../tester.js'
 export const t = await createServiceTester()
 
 t.create('Contributors redirect')
-  .get('/gitlab-org/gitlab', {
-    followRedirect: false,
-  })
-  .expectStatus(301)
-  .expectHeader('Location', '/gitlab/contributors/gitlab-org/gitlab.svg')
+  .get('/gitlab-org/gitlab')
+  .expectRedirect('/gitlab/contributors/gitlab-org/gitlab.svg')
