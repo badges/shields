0e7af5263721f64c6e36c42c74c59ed96e39fb8c
jNullj <15849761+jNullj@users.noreply.github.com>
2025-12-02 00:07:28 +0200

    rewrite endpoint spec tests

--- services/endpoint/endpoint.spec.js
+++ services/endpoint/endpoint.spec.js
@@ -1,10 +1,15 @@
 import { expect } from 'chai'
+import sinon from 'sinon'
 import nock from 'nock'
+import configModule from 'config'
 import { defaultContext } from '../test-helpers.js'
 import { InvalidParameter } from '../index.js'
 import Endpoint from './endpoint.service.js'
 
 describe('Endpoint', function () {
+  afterEach(function () {
+    sinon.restore()
+  })
   it('allows unsecured endpoint when config enabled', async function () {
     nock('http://example.com').get('/badge').reply(200, {
       schemaVersion: 1,
@@ -12,10 +17,13 @@ describe('Endpoint', function () {
       message: 'allowed',
     })
 
+    sinon.stub(configModule.util, 'toObject').returns({
+      public: { allowUnsecuredEndpointRequests: true },
+    })
+
     const endpoint = new Endpoint(defaultContext, {
       handleInternalErrors: false,
     })
-    endpoint.allowUnsecuredEndpointRequests = true
     const result = await endpoint.handle(
       {},
       { url: 'http://example.com/badge' },
@@ -30,10 +38,13 @@ describe('Endpoint', function () {
       message: 'allowed',
     })
 
+    sinon.stub(configModule.util, 'toObject').returns({
+      public: { allowUnsecuredEndpointRequests: false },
+    })
+
     const endpoint = new Endpoint(defaultContext, {
       handleInternalErrors: false,
     })
-    endpoint.allowUnsecuredEndpointRequests = false
     let error
     try {
       await endpoint.handle({}, { url: 'http://example.com/badge' })
