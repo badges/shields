e2c4f2b7314d85bd4bb4bac7eb1f326ccea7689a
Pierre-Yves Bigourdan <10694593+PyvesB@users.noreply.github.com>
2025-12-13 09:45:24 +0100

    Switch ClearlyDefined to BaseService

--- services/clearlydefined/clearlydefined-score.service.js
+++ services/clearlydefined/clearlydefined-score.service.js
@@ -4,12 +4,8 @@ import {
   optionalNonNegativeInteger,
 } from '../validators.js'
 import { floorCount as floorCountColor } from '../color-formatters.js'
-import {
-  BaseJsonService,
-  NotFound,
-  InvalidResponse,
-  pathParams,
-} from '../index.js'
+import { BaseService, NotFound, pathParams } from '../index.js'
+import { parseJson } from '../../core/base-service/json.js'
 
 const schema = Joi.object({
   scores: Joi.object({
@@ -22,7 +18,7 @@ const schema = Joi.object({
 
 // This service based on the REST API for clearlydefined.io
 // https://api.clearlydefined.io/api-docs/
-export default class ClearlyDefinedService extends BaseJsonService {
+export default class ClearlyDefinedService extends BaseService {
   static category = 'analysis'
   static route = {
     base: 'clearlydefined',
@@ -71,23 +67,20 @@ export default class ClearlyDefinedService extends BaseJsonService {
   }
 
   async fetch({ type, provider, namespace, name, revision }) {
-    try {
-      return await this._requestJson({
-        schema,
-        url: `https://api.clearlydefined.io/definitions/${type}/${provider}/${namespace}/${name}/${revision}`,
+    const { buffer } = await this._request({
+      url: `https://api.clearlydefined.io/definitions/${type}/${provider}/${namespace}/${name}/${revision}`,
+      options: { headers: { Accept: 'application/json' } },
+    })
+    // If the type or provider is not found, the API returns a 200 response
+    // with an empty body. It cannot be parsed as JSON, we need to handle this
+    // case earlier than in the usual BaseJsonService._requestJson flow.
+    if (buffer.length === 0) {
+      throw new NotFound({
+        prettyMessage: 'unknown type, provider, or upstream issue',
       })
-    } catch (error) {
-      // ClearlyDefined API returns an empty response (200 status) for invalid type/provider
-      if (
-        error instanceof InvalidResponse &&
-        error.prettyMessage === 'unparseable json response'
-      ) {
-        throw new InvalidResponse({
-          prettyMessage: 'unknown type, provider, or upstream issue',
-        })
-      }
-      throw error
     }
+    const json = parseJson(buffer)
+    return this.constructor._validate(json, schema)
   }
 
   async handle({ type, provider, namespace, name, revision }) {
