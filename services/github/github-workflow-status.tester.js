9ee33df1904b0435b670435bb900bdfe7528b84c
jNullj <15849761+jNullj@users.noreply.github.com>
2025-12-17 09:57:28 +0200

    refactor test response for GithubWorkflowStatus into a const

--- services/github/github-workflow-status.tester.js
+++ services/github/github-workflow-status.tester.js
@@ -6,46 +6,29 @@ export const t = new ServiceTester({
   pathPrefix: '/github/workflow/status',
 })
 
+const depricationExpectedResponse = {
+  label: 'githubworkflowstatus',
+  message: 'https://github.com/badges/shields/issues/8671',
+  link: ['https://github.com/badges/shields/issues/8671'],
+  color: 'red',
+}
+
 t.create('no longer available (previously nonexistent repo)')
   .get('/badges/shields-fakeness/fake.json')
-  .expectBadge({
-    label: 'githubworkflowstatus',
-    message: 'https://github.com/badges/shields/issues/8671',
-    link: ['https://github.com/badges/shields/issues/8671'],
-    color: 'red',
-  })
+  .expectBadge(depricationExpectedResponse)
 
 t.create('no longer available (previously nonexistent workflow)')
   .get('/actions/toolkit/not-a-real-workflow.json')
-  .expectBadge({
-    label: 'githubworkflowstatus',
-    message: 'https://github.com/badges/shields/issues/8671',
-    link: ['https://github.com/badges/shields/issues/8671'],
-    color: 'red',
-  })
+  .expectBadge(depricationExpectedResponse)
 
 t.create('no longer available (previously valid workflow)')
   .get('/actions/toolkit/toolkit-unit-tests.json')
-  .expectBadge({
-    label: 'githubworkflowstatus',
-    message: 'https://github.com/badges/shields/issues/8671',
-    link: ['https://github.com/badges/shields/issues/8671'],
-    color: 'red',
-  })
+  .expectBadge(depricationExpectedResponse)
 
 t.create('no longer available (previously valid workflow - branch)')
   .get('/actions/toolkit/toolkit-unit-tests/master.json')
-  .expectBadge({
-    label: 'githubworkflowstatus',
-    message: 'https://github.com/badges/shields/issues/8671',
-    link: ['https://github.com/badges/shields/issues/8671'],
-    color: 'red',
-  })
+  .expectBadge(depricationExpectedResponse)
 
 t.create('no longer available (previously valid workflow - event)')
   .get('/actions/toolkit/toolkit-unit-tests.json?event=push')
-  .expectBadge({
-    label: 'githubworkflowstatus',
-    message: 'https://github.com/badges/shields/issues/8671',
-    link: ['https://github.com/badges/shields/issues/8671'],
-  })
+  .expectBadge(depricationExpectedResponse)
