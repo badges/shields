26594efb93f0098dc9ca681bd19a17be30959dd5
Pierre-Yves Bigourdan <10694593+PyvesB@users.noreply.github.com>
2025-12-17 09:31:51 +0100

    Update variable name

--- services/github/github-workflow-status.tester.js
+++ services/github/github-workflow-status.tester.js
@@ -6,7 +6,7 @@ export const t = new ServiceTester({
   pathPrefix: '/github/workflow/status',
 })
 
-const depricationExpectedResponse = {
+const expectedDeprecatedResponse = {
   label: 'githubworkflowstatus',
   message: 'https://github.com/badges/shields/issues/8671',
   link: ['https://github.com/badges/shields/issues/8671'],
@@ -15,20 +15,20 @@ const depricationExpectedResponse = {
 
 t.create('no longer available (previously nonexistent repo)')
   .get('/badges/shields-fakeness/fake.json')
-  .expectBadge(depricationExpectedResponse)
+  .expectBadge(expectedDeprecatedResponse)
 
 t.create('no longer available (previously nonexistent workflow)')
   .get('/actions/toolkit/not-a-real-workflow.json')
-  .expectBadge(depricationExpectedResponse)
+  .expectBadge(expectedDeprecatedResponse)
 
 t.create('no longer available (previously valid workflow)')
   .get('/actions/toolkit/toolkit-unit-tests.json')
-  .expectBadge(depricationExpectedResponse)
+  .expectBadge(expectedDeprecatedResponse)
 
 t.create('no longer available (previously valid workflow - branch)')
   .get('/actions/toolkit/toolkit-unit-tests/master.json')
-  .expectBadge(depricationExpectedResponse)
+  .expectBadge(expectedDeprecatedResponse)
 
 t.create('no longer available (previously valid workflow - event)')
   .get('/actions/toolkit/toolkit-unit-tests.json?event=push')
-  .expectBadge(depricationExpectedResponse)
+  .expectBadge(expectedDeprecatedResponse)
