727eb6cbb3da358cc044bcf5b7373c47d612008f
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2025-12-29 11:47:51 +0530

    feat: add zed service extension downloads

---
+++ services/zed/zed-downloads.service.js
@@ -0,0 +1,41 @@
+import Joi from 'joi'
+import { BaseJsonService, pathParams } from '../index.js'
+import { renderDownloadsBadge } from '../downloads.js'
+
+const schema = Joi.object({
+  data: Joi.array()
+    .items(
+      Joi.object({
+        download_count: Joi.number().integer().required(),
+      }),
+    )
+    .min(1)
+    .required(),
+}).required()
+
+export default class ZedDownloads extends BaseJsonService {
+  static category = 'downloads'
+  static route = { base: 'zed/downloads', pattern: ':extensionId' }
+  static openApi = {
+    '/zed/downloads/{extensionId}': {
+      get: {
+        summary: 'Zed Downloads',
+        parameters: pathParams({
+          name: 'extensionId',
+          example: 'react-snippets-es7',
+        }),
+      },
+    },
+  }
+
+  static defaultBadgeData = { label: 'installs' }
+
+  async handle({ extensionId }) {
+    const data = await this._requestJson({
+      schema,
+      url: `https://api.zed.dev/extensions/${encodeURIComponent(extensionId)}`,
+    })
+
+    return renderDownloadsBadge({ downloads: data.data.at(-1).download_count })
+  }
+}
