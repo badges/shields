c47230edd4ea9cb109d87d9fa9865b83b7023c8b
jNullj <15849761+jNullj@users.noreply.github.com>
2026-02-03 23:56:13 +0200

    add not-planned and duplicate to issue state to [GithubIssueDetail]
    Fixes GitHub issues/PRs - Support the "not planned" state
    Fixes #11662

--- services/github/github-issue-detail.service.js
+++ services/github/github-issue-detail.service.js
@@ -20,12 +20,19 @@ const stateMap = {
   schema: Joi.object({
     ...commonSchemaFields,
     state: Joi.equal('open', 'closed').required(),
+    state_reason: Joi.string().allow(null), // only for issues
     merged_at: Joi.string().allow(null),
   }).required(),
   transform: ({ json }) => {
     const mergedAt = json.pull_request?.merged_at ?? json.merged_at
+    const stateWithReason =
+      json.state_reason &&
+      (json.state_reason === 'not_planned' || json.state_reason === 'duplicate')
+        ? json.state_reason
+        : json.state
+
     return {
-      state: json.state,
+      state: stateWithReason,
       merged: mergedAt != null,
     }
   },
