a2a9ecc2f80cddf0c501ac276145fbe6e3fce157
Pierre-Yves Bigourdan <10694593+PyvesB@users.noreply.github.com>
2026-03-07 23:15:35 +0100

    Deprecate [Codeship] service

--- services/codeship/codeship.service.js
+++ services/codeship/codeship.service.js
@@ -1,79 +1,11 @@
-import Joi from 'joi'
-import { isBuildStatus, renderBuildStatusBadge } from '../build-status.js'
-import { BaseSvgScrapingService, pathParams } from '../index.js'
+import { deprecatedService } from '../index.js'
 
-const schema = Joi.object({
-  message: Joi.alternatives()
-    .try(
-      isBuildStatus,
-      Joi.equal('project not found', 'branch not found', 'ignored', 'blocked'),
-    )
-    .required(),
-}).required()
-
-const pendingStatus = 'pending'
-const notBuiltStatus = 'not built'
-
-const statusMap = {
-  testing: pendingStatus,
-  waiting: pendingStatus,
-  initiated: pendingStatus,
-  stopped: notBuiltStatus,
-  ignored: notBuiltStatus,
-  blocked: notBuiltStatus,
-  infrastructure_failure: 'failed',
-}
-
-export default class Codeship extends BaseSvgScrapingService {
-  static category = 'build'
-  static route = { base: 'codeship', pattern: ':projectId/:branch*' }
-
-  static openApi = {
-    '/codeship/{projectId}': {
-      get: {
-        summary: 'Codeship',
-        parameters: pathParams({
-          name: 'projectId',
-          example: 'd6c1ddd0-16a3-0132-5f85-2e35c05e22b1',
-        }),
-      },
-    },
-    '/codeship/{projectId}/{branch}': {
-      get: {
-        summary: 'Codeship (branch)',
-        parameters: pathParams(
-          {
-            name: 'projectId',
-            example: '0bdb0440-3af5-0133-00ea-0ebda3a33bf6',
-          },
-          {
-            name: 'branch',
-            example: 'master',
-          },
-        ),
-      },
-    },
-  }
-
-  static defaultBadgeData = { label: 'build' }
-
-  static render({ status }) {
-    status = statusMap[status] || status
-    return renderBuildStatusBadge({ status })
-  }
-
-  async fetch({ projectId, branch }) {
-    const url = `https://app.codeship.com/projects/${projectId}/status`
-    return this._requestSvg({
-      schema,
-      url,
-      options: { searchParams: { branch } },
-      valueMatcher: /<g id="status_2">(?:[.\s\S]*)\/><\/g><g id="([\w\s]*)"/,
-    })
-  }
-
-  async handle({ projectId, branch }) {
-    const { message: status } = await this.fetch({ projectId, branch })
-    return this.constructor.render({ status })
-  }
-}
+export default deprecatedService({
+  category: 'build',
+  route: {
+    base: 'codeship',
+    pattern: ':various+',
+  },
+  label: 'codeship',
+  dateAdded: new Date('2026-03-07'),
+})
--- services/codeship/codeship.tester.js
+++ services/codeship/codeship.tester.js
@@ -1,25 +1,10 @@
-import { isBuildStatus } from '../build-status.js'
-import { createServiceTester } from '../tester.js'
-export const t = await createServiceTester()
+import { ServiceTester } from '../tester.js'
 
-t.create('codeship (valid, no branch)')
+export const t = new ServiceTester({
+  id: 'codeship',
+  title: 'Codeship',
+})
+
+t.create('codeship (no longer available)')
   .get('/30419df0-80ff-0135-f7fb-06994b6b032d.json')
-  .expectBadge({
-    label: 'build',
-    message: isBuildStatus,
-  })
-
-t.create('codeship (valid, with branch)')
-  .get('/0bdb0440-3af5-0133-00ea-0ebda3a33bf6/master.json')
-  .expectBadge({
-    label: 'build',
-    message: isBuildStatus,
-  })
-
-t.create('codeship (repo not found)')
-  .get('/not-a-repo.json')
-  .expectBadge({ label: 'build', message: 'project not found' })
-
-t.create('codeship (branch not found)')
-  .get('/30419df0-80ff-0135-f7fb-06994b6b032d/not-a-branch.json')
-  .expectBadge({ label: 'build', message: 'branch not found' })
+  .expectBadge({ label: 'codeship', message: 'no longer available' })
