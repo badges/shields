b34ae524339f43d57013d370290181cee2057b4c
jNullj <15849761+jNullj@users.noreply.github.com>
2026-04-09 01:22:55 +0300

    fix: tests for visual-studio-marketplace
    fix paths and seperate legacy and newer paths

--- services/visual-studio-marketplace/visual-studio-marketplace.tester.js
+++ services/visual-studio-marketplace/visual-studio-marketplace.tester.js
@@ -3,115 +3,131 @@ import { ServiceTester } from '../tester.js'
 export const t = new ServiceTester({
   id: 'visualstudiomarketplace',
   title: 'Visual Studio Marketplace (deprecated)',
+  pathPrefix: '/visual-studio-marketplace',
 })
 
-// Downloads / Installs
+export const tLegacy = new ServiceTester({
+  id: 'visualstudiomarketplacelegacy',
+  title: 'VSCode Marketplace (deprecated)',
+  pathPrefix: '/vscode-marketplace',
+})
+
+// Downloads / Installs (visual-studio-marketplace)
 t.create('no longer available (visual-studio-marketplace downloads)')
-  .get('/visual-studio-marketplace/d/ritwickdey.LiveServer.json')
+  .get('/d/ritwickdey.LiveServer.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
 t.create('no longer available (visual-studio-marketplace installs)')
-  .get('/visual-studio-marketplace/i/ritwickdey.LiveServer.json')
+  .get('/i/ritwickdey.LiveServer.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
-t.create('no longer available (vscode-marketplace downloads)')
-  .get('/vscode-marketplace/d/ritwickdey.LiveServer.json')
+// Downloads / Installs (vscode-marketplace - legacy)
+tLegacy
+  .create('no longer available (vscode-marketplace downloads)')
+  .get('/d/ritwickdey.LiveServer.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
-t.create('no longer available (vscode-marketplace installs)')
-  .get('/vscode-marketplace/i/ritwickdey.LiveServer.json')
+tLegacy
+  .create('no longer available (vscode-marketplace installs)')
+  .get('/i/ritwickdey.LiveServer.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
-// Version
+// Version (visual-studio-marketplace)
 t.create('no longer available (version)')
-  .get('/visual-studio-marketplace/v/lextudio.restructuredtext.json')
+  .get('/v/lextudio.restructuredtext.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
-t.create('no longer available (version legacy)')
-  .get('/vscode-marketplace/v/lextudio.restructuredtext.json')
+// Version (vscode-marketplace - legacy)
+tLegacy
+  .create('no longer available (version legacy)')
+  .get('/v/lextudio.restructuredtext.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
-// Rating / Stars
+// Rating / Stars (visual-studio-marketplace)
 t.create('no longer available (rating)')
-  .get('/visual-studio-marketplace/r/ritwickdey.LiveServer.json')
+  .get('/r/ritwickdey.LiveServer.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
 t.create('no longer available (stars)')
-  .get('/visual-studio-marketplace/stars/ritwickdey.LiveServer.json')
+  .get('/stars/ritwickdey.LiveServer.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
-t.create('no longer available (rating legacy)')
-  .get('/vscode-marketplace/r/ritwickdey.LiveServer.json')
+// Rating / Stars (vscode-marketplace - legacy)
+tLegacy
+  .create('no longer available (rating legacy)')
+  .get('/r/ritwickdey.LiveServer.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
-t.create('no longer available (stars legacy)')
-  .get('/vscode-marketplace/stars/ritwickdey.LiveServer.json')
+tLegacy
+  .create('no longer available (stars legacy)')
+  .get('/stars/ritwickdey.LiveServer.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
-// Release date / Last updated
+// Release date / Last updated (visual-studio-marketplace)
 t.create('no longer available (release date)')
-  .get('/visual-studio-marketplace/release-date/yasht.terminal-all-in-one.json')
-  .expectBadge({
-    label: 'visual-studio-marketplace',
-    message: 'no longer available',
-  })
-
-t.create('no longer available (release date legacy)')
-  .get('/vscode-marketplace/release-date/yasht.terminal-all-in-one.json')
+  .get('/release-date/yasht.terminal-all-in-one.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
 t.create('no longer available (last updated)')
-  .get('/visual-studio-marketplace/last-updated/yasht.terminal-all-in-one.json')
+  .get('/last-updated/yasht.terminal-all-in-one.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
-t.create('no longer available (last updated legacy)')
-  .get('/vscode-marketplace/last-updated/yasht.terminal-all-in-one.json')
+// Release date / Last updated (vscode-marketplace - legacy)
+tLegacy
+  .create('no longer available (release date legacy)')
+  .get('/release-date/yasht.terminal-all-in-one.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
   })
 
-// Azure DevOps installs
+tLegacy
+  .create('no longer available (last updated legacy)')
+  .get('/last-updated/yasht.terminal-all-in-one.json')
+  .expectBadge({
+    label: 'visual-studio-marketplace',
+    message: 'no longer available',
+  })
+
+// Azure DevOps installs (visual-studio-marketplace)
 t.create('no longer available (azure devops installs total)')
-  .get(
-    '/visual-studio-marketplace/azure-devops/installs/total/swellaby.mirror-git-repository.json',
-  )
+  .get('/azure-devops/installs/total/swellaby.mirror-git-repository.json')
   .expectBadge({
     label: 'visual-studio-marketplace',
     message: 'no longer available',
