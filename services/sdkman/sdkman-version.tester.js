34aadbddca9abc3a02632e27b3caf98d00d2bd1c
Shaz666 <ahmedshahwaiz19@gmail.com>
2026-04-26 19:44:59 -0500

    refactor(sdkman): switch to non-mocked service tests

--- services/sdkman/sdkman-version.tester.js
+++ services/sdkman/sdkman-version.tester.js
@@ -3,25 +3,13 @@ import { createServiceTester } from '../tester.js'
 
 export const t = await createServiceTester()
 
-t.create('version (valid)')
-  .get('/java.json')
-  .intercept(nock =>
-    nock('https://api.sdkman.io')
-      .get('/2/candidates/default/java')
-      .reply(200, '21.0.2-tem'),
-  )
-  .expectBadge({
-    label: 'sdkman',
-    message: isVPlusDottedVersionNClausesWithOptionalSuffix,
-  })
+t.create('version (valid)').get('/java.json').expectBadge({
+  label: 'sdkman',
+  message: isVPlusDottedVersionNClausesWithOptionalSuffix,
+})
 
 t.create('version (not found)')
   .get('/not-a-real-sdk.json')
-  .intercept(nock =>
-    nock('https://api.sdkman.io')
-      .get('/2/candidates/default/not-a-real-sdk')
-      .reply(404),
-  )
   .expectBadge({ label: 'sdkman', message: 'not found' })
 
 t.create('version (empty response)')
