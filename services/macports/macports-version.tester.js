ae17fac491f5722f4e6b83a64be9cacfdcba72f7
Shaz666 <ahmedshahwaiz19@gmail.com>
2026-04-26 20:33:24 -0500

    revert(macports): restore mocked tests; ports.macports.org unreachable from CI

--- services/macports/macports-version.tester.js
+++ services/macports/macports-version.tester.js
@@ -3,10 +3,17 @@ import { createServiceTester } from '../tester.js'
 
 export const t = await createServiceTester()
 
-t.create('version (valid)').get('/git.json').expectBadge({
-  label: 'macports',
-  message: isVPlusDottedVersionAtLeastOne,
-})
+t.create('version (valid)')
+  .get('/git.json')
+  .intercept(nock =>
+    nock('https://ports.macports.org')
+      .get('/api/v1/ports/git/')
+      .reply(200, { version: '2.47.1' }),
+  )
+  .expectBadge({
+    label: 'macports',
+    message: isVPlusDottedVersionAtLeastOne,
+  })
 
 t.create('version (not found)')
   .get('/not-a-real-port.json')
