78a89781beb821fb7b29401fa65c5ba5304c8111
DunsinK <133840905+DunsinK@users.noreply.github.com>
2026-06-24 11:39:12 -0500

    Release Date Tester Update

--- services/winget/winget-release-date.tester.js
+++ services/winget/winget-release-date.tester.js
@@ -3,21 +3,34 @@ import { createServiceTester } from '../tester.js'
 
 export const t = await createServiceTester()
 
-// basic live test - hits real winget API
+// minimal winget-package tree/manifest fixtures
+function packageVersionTree(version, packageName) {
+  return {
+    type: 'tree',
+    name: version,
+    object: {
+      entries: [{ type: 'blob', name: `${packageName}.yaml` }],
+    },
+  }
+}
+
+function manifestText(packageName, nReleaseDate) {
+  return `PackageIdentifier: ${packageName}\nReleaseDate: ${nReleaseDate}\n`
+}
+
+// basic test
 t.create('release date for WSL')
   .get('/release-date/Microsoft.WSL.json')
   .expectBadge({ label: 'release date', message: isFormattedDate })
 
-// test more than one dots
-// test sort based on dotted version order instead of ASCII
-t.create('gets release date for latest version')
+// 0.200.170 would sort higher in ASCII, 0.1201.422.0 is latest in winget order
+t.create('gets release date for latest version (not ASCII order)')
   .intercept(nock =>
     nock('https://api.github.com/')
       .post('/graphql')
       .twice()
       .reply((uri, requestBody) => {
         const { query, variables } = JSON.parse(requestBody)
-
         if (query.includes('Tree')) {
           return [
             200,
@@ -26,206 +39,8 @@ t.create('gets release date for latest version')
                 repository: {
                   object: {
                     entries: [
-                      {
-                        type: 'tree',
-                        name: '0.1001.389.0',
-                        object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.installer.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.locale.en-US.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.yaml',
-                            },
-                          ],
-                        },
-                      },
-                      {
-                        type: 'tree',
-                        name: '0.1101.416.0',
-                        object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.installer.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.locale.en-US.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.yaml',
-                            },
-                          ],
-                        },
-                      },
-                      {
-                        type: 'tree',
-                        name: '0.1201.442.0',
-                        object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.installer.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.locale.en-US.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.yaml',
-                            },
-                          ],
-                        },
-                      },
-                      {
-                        type: 'tree',
-                        name: '0.137.141.0',
-                        object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.installer.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.locale.en-US.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.yaml',
-                            },
-                          ],
-                        },
-                      },
-                      {
-                        type: 'tree',
-                        name: '0.200.170.0',
-                        object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.installer.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.locale.en-US.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.yaml',
-                            },
-                          ],
-                        },
-                      },
-                      {
-                        type: 'tree',
-                        name: '0.503.261.0',
-                        object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.installer.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.locale.en-US.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.yaml',
-                            },
-                          ],
-                        },
-                      },
-                      {
-                        type: 'tree',
-                        name: '0.601.285.0',
-                        object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.installer.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.locale.en-US.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.yaml',
-                            },
-                          ],
-                        },
-                      },
-                      {
-                        type: 'tree',
-                        name: '0.601.297.0',
-                        object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.installer.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.locale.en-US.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.yaml',
-                            },
-                          ],
-                        },
-                      },
-                      {
-                        type: 'tree',
-                        name: '0.701.323.0',
-                        object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.installer.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.locale.en-US.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.yaml',
-                            },
-                          ],
-                        },
-                      },
-                      {
-                        type: 'tree',
-                        name: '0.801.344.0',
-                        object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.installer.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.locale.en-US.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Microsoft.DevHome.yaml',
-                            },
-                          ],
-                        },
-                      },
+                      packageVersionTree('0.200.170.0', 'Microsoft.DevHome'),
+                      packageVersionTree('0.1201.442.0', 'Microsoft.DevHome'),
                     ],
                   },
                 },
@@ -241,7 +56,7 @@ t.create('gets release date for latest version')
               data: {
                 repository: {
                   object: {
-                    text: 'PackageIdentifier: Microsoft.DevHome\nReleaseDate: 2024-03-15\n',
+                    text: manifestText('Microsoft.DevHome', '2024-03-15'),
                   },
                 },
               },
@@ -253,11 +68,9 @@ t.create('gets release date for latest version')
       }),
   )
   .get('/release-date/Microsoft.DevHome.json')
-  .expectBadge({ label: 'release date', message: isFormattedDate })
+  .expectBadge({ label: 'release date', message: 'march 2024' })
 
-// Both 'Some.Package' and 'Some.Package.Sub' are present in the response.
-// We should ignore 'Some.Package.Sub' in response to 'Some.Package' request.
-// In this test case, Canonical.Ubuntu.2404 is present, but it should not be treated as Canonical.Ubuntu version 2404.
+// 2404 is Ubuntu 24.04 sub-package folder, NOT canonical Ubuntu 24.04
 t.create('do not pick sub-package release date')
   .intercept(nock =>
     nock('https://api.github.com/')
@@ -275,94 +88,13 @@ t.create('do not pick sub-package release date')
                   object: {
                     entries: [
                       {
-                        type: 'blob',
-                        name: '.validation',
-                        object: {},
-                      },
-                      {
-                        type: 'tree',
-                        name: '1804',
-                        object: {
-                          entries: [
-                            {
-                              type: 'tree',
-                              name: '1804.6.4.0',
-                            },
-                          ],
-                        },
-                      },
-                      {
-                        type: 'tree',
-                        name: '2004',
-                        object: {
-                          entries: [
-                            {
-                              type: 'tree',
-                              name: '2004.6.16.0',
-                            },
-                          ],
-                        },
-                      },
-                      {
-                        type: 'tree',
-                        name: '2204.1.8.0',
-                        object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: 'Canonical.Ubuntu.installer.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Canonical.Ubuntu.locale.en-US.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Canonical.Ubuntu.locale.zh-CN.yaml',
-                            },
-                            {
-                              type: 'blob',
-                              name: 'Canonical.Ubuntu.yaml',
-                            },
-                          ],
-                        },
-                      },
-                      {
-                        type: 'tree',
-                        name: '2204',
-                        object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: '.validation',
-                            },
-                            {
-                              type: 'tree',
-                              name: '2204.0.10.0',
-                            },
-                            {
-                              type: 'tree',
-                              name: '2204.2.47.0',
-                            },
-                          ],
-                        },
-                      },
-                      {
                         type: 'tree',
                         name: '2404',
                         object: {
-                          entries: [
-                            {
-                              type: 'blob',
-                              name: '.validation',
-                            },
-                            {
-                              type: 'tree',
-                              name: '2404.0.5.0',
-                            },
-                          ],
+                          entries: [{ type: 'tree', name: '2404.0.5.0' }],
                         },
                       },
+                      packageVersionTree('2204.1.8.0', 'Canonical.Ubuntu'),
                     ],
                   },
                 },
@@ -378,7 +110,7 @@ t.create('do not pick sub-package release date')
               data: {
                 repository: {
                   object: {
-                    text: 'PackageIdentifier: Canonical.Ubuntu\nReleaseDate: 2024-03-15\n',
+                    text: manifestText('Canonical.Ubuntu', '2024-03-15'),
                   },
                 },
               },
@@ -389,4 +121,4 @@ t.create('do not pick sub-package release date')
       }),
   )
   .get('/release-date/Canonical.Ubuntu.json')
-  .expectBadge({ label: 'release date', message: isFormattedDate })
+  .expectBadge({ label: 'release date', message: 'march 2024' })
